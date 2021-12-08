d9a1325a25e532a5b2a9299c533962b31f60ccd0
Hongdonggeon <ghd700@naver.com>
2021-12-08 21:04:05 +0900

    feat(VendingMachineController) : 입력한 금액값 정수형으로 변환한 후, Money 객체로 만들기

--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -1,2 +1,18 @@
-package vendingmachine.controller;public class VendingMachineController {
+package vendingmachine.controller;
+
+import vendingmachine.domain.Money;
+import vendingmachine.view.InputView;
+
+public class VendingMachineController {
+	Money vendingMachineMoney;
+
+	public void play() {
+		vendingMachineMoney = new Money(castingStringMoneyToInt(InputView.inputVendingMachieInput()));
+	}
+
+	public int castingStringMoneyToInt(String stringMoney) {
+		return Integer.parseInt(stringMoney);
+	}
+
+
 }
