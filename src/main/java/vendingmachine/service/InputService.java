ed803f356e487b4a66eff3e8b9417463c7233da6
DK <ldk980130@gmail.com>
2021-12-09 01:37:52 +0900

    feat : 투입 금액 입력 기능 구현

--- src/main/java/vendingmachine/service/InputService.java
+++ src/main/java/vendingmachine/service/InputService.java
@@ -8,13 +8,14 @@ import vendingmachine.verification.Verification;
 
 public class InputService {
 
-	private static final String INPUT_MONEY_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+	private static final String INPUT_VENDING_MACHINE_MONEY_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
 	private static final String INPUT_ITEMS_MESSAGE = "상품명과 가격, 수량을 입력해 주세요.";
+	private static final String INPUT_USER_MONEY_MESSAGE = "투입 금액을 입력해 주세요.";
 
-	public int readMoney() {
+	public int readMoneyOfVendingMachine() {
 		while (true) {
 			try {
-				System.out.println(INPUT_MONEY_MESSAGE);
+				System.out.println(INPUT_VENDING_MACHINE_MONEY_MESSAGE);
 				return Verification.ofMoney(Console.readLine());
 			} catch (IllegalArgumentException e) {
 				System.out.println(e.getMessage());
@@ -27,6 +28,17 @@ public class InputService {
 			try {
 				System.out.println(INPUT_ITEMS_MESSAGE);
 				return Verification.ofItems(Console.readLine());
+			} catch (IllegalArgumentException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+
+	public int readMoneyOfUser() {
+		while (true) {
+			try {
+				System.out.println(INPUT_USER_MONEY_MESSAGE);
+				return Verification.ofMoney(Console.readLine());
 			} catch (IllegalArgumentException e) {
 				System.out.println(e.getMessage());
 			}
