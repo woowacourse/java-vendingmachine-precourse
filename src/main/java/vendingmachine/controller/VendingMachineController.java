8d8369bf3e2fc7433b8a77499b92c869401df14f
Hongdonggeon <ghd700@naver.com>
2021-12-09 02:43:39 +0900

    feat(outputview,vendingmachinecontroller) : 자판기가 보유한 동전 출력하기

--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -1,17 +1,33 @@
 package vendingmachine.controller;
 
+import java.util.LinkedHashMap;
+
+import vendingmachine.domain.Coin;
 import vendingmachine.domain.Money;
+import vendingmachine.domain.VendingMachine;
 import vendingmachine.view.InputView;
+import vendingmachine.view.OutputView;
 
 public class VendingMachineController {
 	Money vendingMachineMoney;
+	VendingMachine vendingMachine;
 
 	public void play() {
 		vendingMachineMoney = new Money(castingStringMoneyToInt(InputView.inputVendingMachieInput()));
+		vendingMachine = new VendingMachine(vendingMachineMoney);
+		OutputView.showVendingMahcineCoinStatus(castingCoinToInteger(vendingMachine.saveCoinStatus()));
 	}
 
 	public int castingStringMoneyToInt(String stringMoney) {
 		return Integer.parseInt(stringMoney);
+	}
+
+	public LinkedHashMap<Integer, Integer> castingCoinToInteger(LinkedHashMap<Coin, Integer> coinStatus) {
+		LinkedHashMap<Integer, Integer> intCoinStatus = new LinkedHashMap<>();
+		for (Coin coin : coinStatus.keySet() ) {
+			intCoinStatus.put(coin.getAmount(), coinStatus.get(coin));
+		}
+		return intCoinStatus;
 	}
 
 
--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -1,2 +1,16 @@
-package vendingmachine.view;public class OutputView {
+package vendingmachine.view;
+
+import java.util.LinkedHashMap;
+
+public class OutputView {
+	public static String VENDING_MACHINE_HAVING_COIN_STATUS = "자판기가 보유한 동전";
+	public static String COIN_VALUE_UNIT = "원";
+	public static String COIN_COUNT_UNIT = "개";
+	public static String HYPHEN = " - ";
+
+	public static void showVendingMahcineCoinStatus(LinkedHashMap<Integer, Integer> vendingMachineCoinStatus) {
+		for (Integer coinValueUnit : vendingMachineCoinStatus.keySet()) {
+			System.out.println(coinValueUnit + COIN_VALUE_UNIT + HYPHEN + vendingMachineCoinStatus.get(coinValueUnit) + COIN_COUNT_UNIT);
+		}
+	}
 }
