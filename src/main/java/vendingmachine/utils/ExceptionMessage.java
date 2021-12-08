523f7dd3be08e2cdba0f888d838f58fb305209cb
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-12-08 20:26:06 +0900

    test(BeverageTest) : 음료 생성 에러, 성공 상황 테스트 완료

--- src/main/java/vendingmachine/utils/ExceptionMessage.java
+++ src/main/java/vendingmachine/utils/ExceptionMessage.java
@@ -6,4 +6,6 @@ public class ExceptionMessage {
     public static final String ERROR_PRICE_NOT_NUMBER = "금액은 숫자여야 합니다.";
     public static final String ERROR_PRICE_NOT_DIVIDE_BY_TEN = "금액은 10원으로 나눠져야 합니다.";
     public static final String ERROR_COIN_NOT_FOUND = "존재하지 않는 Coin 입니다.";
+
+    public static final String ERROR_BEVERAGE_FORMAT = "올바르지 않는 상품 입력 포맷입니다.";
 }
---
+++ src/test/java/vendingmachine/domain/BeverageTest.java
@@ -0,0 +1,65 @@
+package vendingmachine.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BeverageTest {
+
+    @DisplayName("실패_[로 시작하지 않음")
+    @Test
+    void NotStartWithSquareBracket_false(){
+        assertThrows(IllegalArgumentException.class, () ->
+                new Beverage("{콜라,1500,20]"));
+    }
+
+    @DisplayName("실패_]로 끝나지 않음")
+    @Test
+    void NotEndWithSquareBracket_false(){
+        assertThrows(IllegalArgumentException.class, () ->
+                new Beverage("[콜라,1500,20}"));
+    }
+
+    @DisplayName("실패_가격이 숫자가 아님")
+    @Test
+    void PriceNotNumber_false(){
+        assertThrows(IllegalArgumentException.class, () ->
+                new Beverage("[콜라,a,20]"));
+    }
+
+    @DisplayName("실패_가격이 100원 이하")
+    @Test
+    void PriceUnder100_false(){
+        assertThrows(IllegalArgumentException.class, () ->
+                new Beverage("[콜라,99,20]"));
+    }
+
+    @DisplayName("실패_가격이 10으로 안나눠짐")
+    @Test
+    void PriceNotDivideBy10_false(){
+        assertThrows(IllegalArgumentException.class, () ->
+                new Beverage("[콜라,101,20]"));
+    }
+
+    @DisplayName("실패_수량이 숫자가 아님")
+    @Test
+    void CountNotNumber_false(){
+        assertThrows(IllegalArgumentException.class, () ->
+                new Beverage("[콜라,1000,a}"));
+    }
+
+    @DisplayName("실패_수량이 0 초과가 아님")
+    @Test
+    void CountUnder0_false(){
+        assertThrows(IllegalArgumentException.class, () ->
+                new Beverage("[콜라,1000,0]"));
+    }
+    
+    @DisplayName("음료 생성 성공")
+    @Test
+    void CreateBeverage_true(){
+        Beverage beverage = new Beverage("[콜라,1000,1]");
+    }
+
+}
