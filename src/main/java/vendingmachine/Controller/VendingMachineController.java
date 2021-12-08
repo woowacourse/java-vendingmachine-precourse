47777b4d0b2324654dbc20a6513df7d49ec0fb26
BETTERFUTURE4 <bumkyu11@naver.com>
2021-12-08 20:13:44 +0900

    feat: 보유금액을 보유 동전 개수로 변환하는 기능 구현

--- src/main/java/vendingmachine/Controller/VendingMachineController.java
+++ src/main/java/vendingmachine/Controller/VendingMachineController.java
@@ -1,12 +1,24 @@
 package vendingmachine.Controller;
 
+import java.util.HashMap;
+
+import vendingmachine.Model.Coin;
+
 public class VendingMachineController {
 	private int machineMoney;
+	private HashMap<Coin, Integer> machineCoins = new HashMap<>();
+
 	public VendingMachineController() {
 		set();
 	}
 
-	public void set() {
+	private void set() {
 		machineMoney = InputController.setMoney();
+
+		for (Coin coin : Coin.values()) {
+			int divisor = coin.getAmount();
+			machineCoins.put(coin, machineMoney / divisor);
+			machineMoney %= divisor;
+		}
 	}
 }
