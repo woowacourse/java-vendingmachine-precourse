150405899bb33a89b915dcb3be736963ca6be8ee
nailseong <nnis9857@gmail.com>
2021-12-09 08:28:14 +0900

    feat: 자판기 초기화

---
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -0,0 +1,26 @@
+package vendingmachine.controller;
+
+import vendingmachine.model.CoinList;
+import vendingmachine.model.ProductList;
+import vendingmachine.model.VendingMachine;
+
+public class VendingMachineController {
+
+	private final ViewController viewController = new ViewController();
+	private VendingMachine vendingMachine;
+
+	public void init() {
+		int moneyOfVendingMachine = viewController.returnMoneyOfVendingMachine();
+		CoinList coinList = new CoinList(moneyOfVendingMachine);
+		coinList.init();
+
+		ProductList productList = new ProductList();
+		String productListRawInput = viewController.returnProductList();
+		productList.init(productListRawInput);
+
+		int deposit = viewController.returnDepositAmount();
+
+		this.vendingMachine = new VendingMachine(deposit, productList, coinList);
+	}
+
+}
