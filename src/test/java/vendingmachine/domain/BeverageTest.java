552ac736606391c75f1e7a4575920c5a88b7516d
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-12-09 00:12:20 +0900

    test : 일급 컬렉션 분리로 인한 테스트 리팩토링

--- src/test/java/vendingmachine/domain/BeverageTest.java
+++ src/test/java/vendingmachine/domain/BeverageTest.java
@@ -1,6 +1,7 @@
 package vendingmachine.domain;
 
 import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,13 +9,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BeverageTest extends DomainTest {
-
-
+    
     @DisplayName("실패_;로 구분하지 않음")
     @Test
     void delimiterNotSemiColon_false() {
         assertThrows(IllegalArgumentException.class, () ->
-                vendingMachine.getBeverages().addBeverage("[콜라,1500,20],[사이다,1000,20]"));
+                vendingMachine.createBeverages("[콜라,1500,20],[사이다,1000,20]"));
     }
 
 
@@ -22,81 +22,81 @@ class BeverageTest extends DomainTest {
     @Test
     void notStartWithSquareBracket_false() {
         assertThrows(IllegalArgumentException.class, () ->
-                vendingMachine.getBeverages().addBeverage("{콜라,1500,20]"));
+                vendingMachine.createBeverages("{콜라,1500,20]"));
     }
 
     @DisplayName("실패_]로 끝나지 않음")
     @Test
     void notEndWithSquareBracket_false() {
         assertThrows(IllegalArgumentException.class, () ->
-                vendingMachine.getBeverages().addBeverage("[콜라,1500,20}"));
+                vendingMachine.createBeverages("[콜라,1500,20}"));
     }
 
     @DisplayName("실패_가격이 숫자가 아님")
     @Test
     void priceNotNumber_false() {
         assertThrows(IllegalArgumentException.class, () ->
-                vendingMachine.getBeverages().addBeverage("[콜라,a,20]"));
+                vendingMachine.createBeverages("[콜라,a,20]"));
     }
 
     @DisplayName("실패_가격이 100원 이하")
     @Test
     void priceUnder100_false() {
         assertThrows(IllegalArgumentException.class, () ->
-                vendingMachine.getBeverages().addBeverage("[콜라,99,20]"));
+                vendingMachine.createBeverages("[콜라,99,20]"));
     }
 
     @DisplayName("실패_가격이 10으로 안나눠짐")
     @Test
     void priceNotDivideBy10_false() {
         assertThrows(IllegalArgumentException.class, () ->
-                vendingMachine.getBeverages().addBeverage("[콜라,101,20]"));
+                vendingMachine.createBeverages("[콜라,101,20]"));
     }
 
     @DisplayName("실패_수량이 숫자가 아님")
     @Test
     void countNotNumber_false() {
         assertThrows(IllegalArgumentException.class, () ->
-                vendingMachine.getBeverages().addBeverage("[콜라,1000,a}"));
+                vendingMachine.createBeverages("[콜라,1000,a}"));
     }
 
     @DisplayName("실패_수량이 0 초과가 아님")
     @Test
     void countUnder0_false() {
         assertThrows(IllegalArgumentException.class, () ->
-                vendingMachine.getBeverages().addBeverage("[콜라,1000,0]"));
+                vendingMachine.createBeverages("[콜라,1000,0]"));
     }
 
     @DisplayName("성공_음료 생성")
     @Test
     void createBeverage_true() {
+        vendingMachine.createBeverages("[콜라,2000,10]");
         Beverages beverages = vendingMachine.getBeverages();
-        beverages.addBeverage("[콜라,1000,1]");
         Beverage beverage = beverages.getBeverageList().get(0);
 
         SoftAssertions.assertSoftly(soft -> {
             soft.assertThat(beverage.getProductName()).isEqualTo("콜라");
-            soft.assertThat(beverage.getCount()).isEqualTo(1);
-            soft.assertThat(beverage.getPrice()).isEqualTo(1000);
+            soft.assertThat(beverage.getCount()).isEqualTo(10);
+            soft.assertThat(beverage.getPrice()).isEqualTo(2000);
         });
     }
 
     @DisplayName("성공_음료 개수 감소")
     @Test
     void sellBeverage_true() {
+        vendingMachine.createBeverages("[콜라,2000,10]");
         Beverages beverages = vendingMachine.getBeverages();
-        beverages.addBeverage("[콜라,1000,1]");
         Beverage beverage = beverages.getBeverageList().get(0);
 
         beverage.reduceCount();
-        assertThat(beverage.getCount()).isEqualTo(0);
+        assertThat(beverage.getCount()).isEqualTo(9);
     }
 
     @DisplayName("성공_음료수 찾기")
     @Test
     void findBeverage_true() {
+        vendingMachine.createBeverages("[콜라,2000,10]");
         Beverages beverages = vendingMachine.getBeverages();
-        beverages.addBeverage("[콜라,1000,1]");
         Beverage beverage = beverages.getBeverageByName("콜라");
 
         assertThat(beverage.getProductName()).isEqualTo("콜라");
@@ -105,16 +105,16 @@ class BeverageTest extends DomainTest {
     @DisplayName("실패_음료수 찾기")
     @Test
     void findBeverage_false() {
+        vendingMachine.createBeverages("[콜라,2000,10]");
         Beverages beverages = vendingMachine.getBeverages();
-        beverages.addBeverage("[콜라,1000,1]");
         assertThrows(IllegalArgumentException.class, () -> beverages.getBeverageByName("사이다"));
     }
 
     @DisplayName("성공_음료수 목록에서 제거")
     @Test
     void removeBeverage_true() {
+        vendingMachine.createBeverages("[콜라,2000,10]");
         Beverages beverages = vendingMachine.getBeverages();
-        beverages.addBeverage("[콜라,1000,1]");
         beverages.removeBeverage(beverages.getBeverageByName("콜라"));
 
         assertThat(beverages.getBeverageList().size()).isEqualTo(0);
--- src/test/java/vendingmachine/domain/MoneyTest.java
+++ src/test/java/vendingmachine/domain/MoneyTest.java
@@ -24,14 +24,14 @@ public class MoneyTest extends DomainTest {
     @DisplayName("실패_투입금액이 최소 상품 금액보다 낮음")
     @Test
     void moneyNotEnough_false() {
-        vendingMachine.getBeverages().addBeverage("[콜라,2000,10]");
+        vendingMachine.createBeverages("[콜라,2000,10]");
         assertThrows(IllegalArgumentException.class, () -> vendingMachine.insertMoney("1000"));
     }
 
     @DisplayName("성공_투입금액 투입 성공")
     @Test
     void createMoney_true() {
-        vendingMachine.getBeverages().addBeverage("[콜라,2000,10]");
+        vendingMachine.createBeverages("[콜라,2000,10]");
         vendingMachine.insertMoney("3000");
         Money money = vendingMachine.getMoney();
 
@@ -41,15 +41,17 @@ public class MoneyTest extends DomainTest {
     @DisplayName("실패_투입 금액이 지불해야할 금액보다 적음")
     @Test
     void checkInsertMoneyEnough_false() {
-        vendingMachine.insertMoney("1000");
-        assertThrows(IllegalArgumentException.class, () -> vendingMachine.getMoney().isInsertPriceUnderBeveragePrice(2000));
+        vendingMachine.createBeverages("[콜라,2000,10]");
+        vendingMachine.insertMoney("3000");
+        assertThrows(IllegalArgumentException.class, () -> vendingMachine.getMoney().isInsertPriceUnderBeveragePrice(4000));
     }
 
     @DisplayName("성공_투입 금액이 지불해야할 금액보다 많음")
     @Test
     void checkInsertMoneyEnough_true() {
-        vendingMachine.insertMoney("1000");
-        assertDoesNotThrow(() -> vendingMachine.getMoney().isInsertPriceUnderBeveragePrice(500));
+        vendingMachine.createBeverages("[콜라,2000,10]");
+        vendingMachine.insertMoney("3000");
+        assertDoesNotThrow(() -> vendingMachine.getMoney().isInsertPriceUnderBeveragePrice(2000));
     }
 
 }
