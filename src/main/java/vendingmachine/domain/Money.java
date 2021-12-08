692b2d3d6c5e712ebcd7aab676a2bb78433e2101
Hongdonggeon <ghd700@naver.com>
2021-12-08 20:25:18 +0900

    feat(Money) : 입력할 돈 원시값 포장

--- src/main/java/vendingmachine/domain/Money.java
+++ src/main/java/vendingmachine/domain/Money.java
@@ -1,2 +1,12 @@
-package vendingmachine.domain;public class Money {
+package vendingmachine.domain;
+
+import vendingmachine.utils.Validator;
+
+public class Money {
+	private int money;
+
+	public Money(int money) {
+		Validator.validateMoney(money);
+		this.money = money;
+	}
 }
