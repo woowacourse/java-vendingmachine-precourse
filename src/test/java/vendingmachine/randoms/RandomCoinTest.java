97c051cda94c818433f2ed23b4c736edb42aec5a
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2021-12-08 21:38:42 +0900

    test(RandomCoinTest): 동전 생성 메서드에 대한 테스트 추가

---
+++ src/test/java/vendingmachine/randoms/RandomCoinTest.java
@@ -0,0 +1,29 @@
+package vendingmachine.randoms;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import vendingmachine.Coin;
+import vendingmachine.controller.CoinController;
+
+public class RandomCoinTest {
+
+	@Test
+	void coinAmountTest() {
+		//given
+		CoinController coinController = new CoinController();
+
+		//when
+		Map<Coin, Integer> coinIntegerMap = coinController.setCoinsByBalance(1000);
+
+		//then
+		assertThat(coinIntegerMap.get(Coin.COIN_10) * 10 +
+			coinIntegerMap.get(Coin.COIN_50) * 50 +
+			coinIntegerMap.get(Coin.COIN_100) * 100 +
+			coinIntegerMap.get(Coin.COIN_500) * 500
+		).isEqualTo(1000);
+	}
+}
