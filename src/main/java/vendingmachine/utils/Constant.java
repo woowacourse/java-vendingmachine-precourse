01a930d5a72c489344865bf62b3c85c3c292bce2
jochungbeom <cndqjacndqja@gmail.com>
2021-12-08 22:09:18 +0900

    feat: 상품명과 가격, 수량 입력 구현

--- src/main/java/vendingmachine/utils/Constant.java
+++ src/main/java/vendingmachine/utils/Constant.java
@@ -5,5 +5,6 @@ public class Constant {
 	public static int MINIMUM_COIN = 10;
 	public static int PLUS_COIN_ELEMENT = 1;
 	public static String DELIMITER_OF_COIN_STORE_STATUS_REPRESENT = " - ";
-
+	public static String DELIMITER_OF_PRODUCT_LIST = ";";
+	public static int FIRST_INDEX_FOR_SLICE_PRODUCT_STRING = 1;
 }
--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,5 +1,11 @@
 package vendingmachine.view;
 
+import static vendingmachine.utils.Constant.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
@@ -8,6 +14,19 @@ public class InputView {
 		String input = Console.readLine();
 		printEmptyLine();
 		return Integer.parseInt(input);
+	}
+
+	public static List<String> inputProductList() {
+		System.out.println("상품명과 가격, 수량을 입력해 주세요.");
+		String input = Console.readLine();
+		return splitProduct(Arrays.asList(input.split(DELIMITER_OF_PRODUCT_LIST)));
+		//[콜라,1500,20];[사이다,1000,10]
+	}
+
+	private static List<String> splitProduct(List<String> productList) {
+		return productList.stream()
+			.map(i -> i.substring(FIRST_INDEX_FOR_SLICE_PRODUCT_STRING, i.length()-1))
+			.collect(Collectors.toList());
 	}
 
 	private static void printEmptyLine() {
