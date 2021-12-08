1821c4449e2b42e9c7ebed5326d212c66c6259c1
yxxnghwan <younghwan960@gmail.com>
2021-12-09 01:57:35 +0900

    feat: 자판기 보유금액 입력

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,7 +1,12 @@
 package vendingmachine;
 
+import vendingmachine.controller.VendingMachineController;
+import vendingmachine.controller.ViewMappingKey;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        VendingMachineController controller = new VendingMachineController();
+        controller.view(ViewMappingKey.INPUT_FIRST_MONEY);
     }
 }
--- src/main/java/vendingmachine/view/InputFirstMoneyView.java
+++ src/main/java/vendingmachine/view/InputFirstMoneyView.java
@@ -1,8 +1,17 @@
 package vendingmachine.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import vendingmachine.util.SystemMessage;
+
 public class InputFirstMoneyView implements View {
 	@Override
 	public void show() {
+		System.out.println(SystemMessage.INPUT_FIRST_MONEY);
+		int firstMoney = readFirstMoney();
+	}
 
+	private int readFirstMoney() {
+		String firstMoneyStr = Console.readLine();
+		return Integer.parseInt(firstMoneyStr);
 	}
 }
