a7997736873a044db0e99c72a96c79bb698a1b6a
nailseong <nnis9857@gmail.com>
2021-12-09 00:09:15 +0900

    feat: 상품 정보를 입력 받기전에 메시지 출력

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -4,6 +4,7 @@ public class OutputView {
 
 	private static final String ERROR_FORM = "[ERROR] : ";
 	private static final String INPUT_MONEY_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+	private static final String INPUT_PRODUCT_INFO_MESSAGE = "상품명과 가격, 수량을 입력해 주세요.";
 
 	public static void printError(Exception exception) {
 		System.out.println(ERROR_FORM + exception.getMessage());
@@ -11,6 +12,10 @@ public class OutputView {
 
 	public void askInputMoneyOfVendingMachine() {
 		System.out.println(INPUT_MONEY_MESSAGE);
+	}
+
+	public void askProductInfo() {
+		System.out.println(INPUT_PRODUCT_INFO_MESSAGE);
 	}
 
 }
