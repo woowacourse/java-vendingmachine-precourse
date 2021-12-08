f8c2420afe3ae372b0549ea1cf2a842915913b66
nailseong <nnis9857@gmail.com>
2021-12-08 20:26:38 +0900

    feat: 돈이 자연수이고, 10으로 나누어 떨어지는지 검증

---
+++ src/main/java/vendingmachine/validator/MoneyValidator.java
@@ -0,0 +1,27 @@
+package vendingmachine.validator;
+
+public class MoneyValidator {
+
+	public static boolean isValidMoney(String input) {
+		checkNumber(input);
+
+		checkMultipleOfTen(input);
+
+		return true;
+	}
+
+	private static void checkNumber(String input) {
+		for (int i = 0; i < input.length(); i++) {
+			if (!Character.isDigit(input.charAt(i))) {
+				throw new IllegalArgumentException("금액은 자연수여야 합니다.");
+			}
+		}
+	}
+
+	private static void checkMultipleOfTen(String input) {
+		if (input.charAt(input.length() - 1) != '0') {
+			throw new IllegalArgumentException("금액은 10으로 나누어 떨어져야 합니다.");
+		}
+	}
+
+}
