6c0fac0d93d32ceba5f7b8907955ce25470b1877
SeungHan <jk05018@naver.com>
2021-12-09 00:07:48 +0900

    fix(InputCondition) : 상품명 한글 입력시 오류 문제 해결

--- src/main/java/vendingmachine/util/InputCondition.java
+++ src/main/java/vendingmachine/util/InputCondition.java
@@ -5,6 +5,7 @@ public class InputCondition {
 	public static final int MIN_MONEY_UNIT = 10;
 	public static final int ZERO = 0;
 	public static final String NUMBER_REGEX = "^[-]{0,1}[0-9]+";
-	public static final String ITEM_REGEX = "(\\[[a-zA-Z0-9]+,[0-9]+0,[0-9]+\\])";
+	public static final String ITEM_REGEX = "(\\[[a-zA-Z0-9가-힣]+,[0-9]+0,[0-9]+\\])";
 	public static final String MULTIPLE_REGEX = ITEM_REGEX + "(;"+ ITEM_REGEX + ")*";
+	public static final String ITEM_NAME_REGEX = "[a-zA-Z0-9가-힣]+";
 }
