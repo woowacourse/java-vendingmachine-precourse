9fd0255d23664311220e4729441703dd1e0f34e8
nailseong <nnis9857@gmail.com>
2021-12-09 07:16:09 +0900

    feat: 투입 금액을 묻는 메시지 출력

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -5,6 +5,7 @@ public class OutputView {
 	private static final String ERROR_FORM = "[ERROR] : ";
 	private static final String INPUT_MONEY_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
 	private static final String INPUT_PRODUCT_INFO_MESSAGE = "상품명과 가격, 수량을 입력해 주세요.";
+	private static final String ASK_DEPOSIT_AMOUNT_MESSAGE = "투입 금액을 입력해 주세요.";
 
 	public static void printError(Exception exception) {
 		System.out.println(ERROR_FORM + exception.getMessage());
@@ -16,6 +17,10 @@ public class OutputView {
 
 	public void askProductInfo() {
 		System.out.println(INPUT_PRODUCT_INFO_MESSAGE);
+	}
+
+	public void askDepositAmount() {
+		System.out.println(ASK_DEPOSIT_AMOUNT_MESSAGE);
 	}
 
 }
