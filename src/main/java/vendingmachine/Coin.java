7ad9f0d2255f322bb09b7192878e56775deb6b5e
Garrrruii <hishally012@gmail.com>
2021-12-08 21:00:17 +0900

    [feat] class Coin

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,5 +1,9 @@
 package vendingmachine;
 
+import java.util.ArrayList;
+import java.util.List;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInList;
+
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
@@ -7,10 +11,35 @@ public enum Coin {
     COIN_10(10);
 
     private final int amount;
+    private int number = 0;
 
     Coin(final int amount) {
         this.amount = amount;
     }
 
-    // 추가 기능 구현
+    public void setCoinCountRandom(int amount){
+        List<Integer> numberList=new ArrayList<Integer>();
+        int maxNumber=amount/this.amount;
+
+        for(int i=0;i<maxNumber;++i){
+            numberList.add(i);
+        }
+
+        int number=pickNumberInList(numberList);
+        setCoinCountAs(number);
+    }
+
+    public void setCoinCountAs(int number) {
+        this.number = number;
+    }
+
+    public void useCoin(int number){
+        this.number-=number;
+    }
+
+    public int maxCoinCount(int amount) {
+        int maxCount = amount / this.amount;
+        if (maxCount > this.number) maxCount = this.number;
+        return maxCount;
+    }
 }
