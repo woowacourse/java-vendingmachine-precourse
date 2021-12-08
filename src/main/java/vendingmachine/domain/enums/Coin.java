f9110845931951dc6d3e907bd1c9094449e323ed
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-12-08 17:59:50 +0900

    feat(Coin) : 코인 리스트, 리스트에서 코인 탐색하는 메서드 추가

--- src/main/java/vendingmachine/domain/enums/Coin.java
+++ src/main/java/vendingmachine/domain/enums/Coin.java
@@ -1,5 +1,11 @@
 package vendingmachine.domain.enums;
 
+import vendingmachine.utils.ExceptionMessage;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
@@ -16,5 +22,18 @@ public enum Coin {
         this.amount = amount;
     }
 
-    // 추가 기능 구현
+    public static List<Integer> getCoinAmountList(){
+        return Arrays.asList(Coin.values())
+                .stream()
+                .map(coin -> coin.getAmount())
+                .collect(Collectors.toList());
+    }
+
+    public static Coin getCoinByNumber(int number){
+        return Arrays.stream(Coin.values())
+                .filter(coin -> coin.getAmount() == number)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(ExceptionMessage.ERROR_PREFIX + ExceptionMessage.ERROR_COIN_NOT_FOUND));
+    }
+
 }
--- src/main/java/vendingmachine/utils/ExceptionMessage.java
+++ src/main/java/vendingmachine/utils/ExceptionMessage.java
@@ -5,4 +5,5 @@ public class ExceptionMessage {
     public static final String ERROR_PREFIX = "[ERROR] ";
     public static final String ERROR_PRICE_NOT_NUMBER = "금액은 숫자여야 합니다.";
     public static final String ERROR_PRICE_NOT_DIVIDE_BY_TEN = "금액은 10원으로 나눠져야 합니다.";
+    public static final String ERROR_COIN_NOT_FOUND = "존재하지 않는 Coin 입니다.";
 }
