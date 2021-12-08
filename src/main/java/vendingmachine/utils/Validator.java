3e67a6e5319441bed039cb91aa40e72c5b8630f8
Hongdonggeon <ghd700@naver.com>
2021-12-08 20:45:37 +0900

    feat(Validator) : 자판기가 보유하는 돈 음수인지 검증하기

--- src/main/java/vendingmachine/utils/Validator.java
+++ src/main/java/vendingmachine/utils/Validator.java
@@ -1,2 +1,11 @@
-package vendingmachine.utils;public class Validator {
+package vendingmachine.utils;
+
+public class Validator {
+	public static String INVALIDATE_ZERO_UNDER_NUMBER_ERROR_MESSAGE = "금액은 0원 이상 입력해야한다.";
+
+	public static void validateMoney(int money) {
+		if (money <= 0) {
+			throw new IllegalArgumentException(INVALIDATE_ZERO_UNDER_NUMBER_ERROR_MESSAGE);
+		}
+	}
 }
