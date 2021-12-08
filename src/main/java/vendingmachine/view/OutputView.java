74e160e4712121b7af7f5570bc804c684bbe1c65
nailseong <nnis9857@gmail.com>
2021-12-09 07:22:21 +0900

    feat: 남은 예치금을 출력

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -6,6 +6,8 @@ public class OutputView {
 	private static final String INPUT_MONEY_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
 	private static final String INPUT_PRODUCT_INFO_MESSAGE = "상품명과 가격, 수량을 입력해 주세요.";
 	private static final String ASK_DEPOSIT_AMOUNT_MESSAGE = "투입 금액을 입력해 주세요.";
+	private static final String REMAINING_DEPOSIT_MESSAGE = "투입 금액: ";
+	private static final String MONETARY_UNIT = "원";
 
 	public static void printError(Exception exception) {
 		System.out.println(ERROR_FORM + exception.getMessage());
@@ -21,6 +23,10 @@ public class OutputView {
 
 	public void askDepositAmount() {
 		System.out.println(ASK_DEPOSIT_AMOUNT_MESSAGE);
+	}
+
+	public void printRemainingDeposit(int deposit) {
+		System.out.println("\n" + REMAINING_DEPOSIT_MESSAGE + deposit + MONETARY_UNIT);
 	}
 
 }
