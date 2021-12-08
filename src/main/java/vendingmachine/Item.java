f2e67373a8407e9d89fdc25c9604d48fe398c076
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2021-12-08 17:03:06 +0900

    feat: 상품명, 가격, 수량 입력 검증 기능

---
+++ src/main/java/vendingmachine/Item.java
@@ -0,0 +1,33 @@
+package vendingmachine;
+
+import static vendingmachine.ValidationUtils.*;
+
+public class Item {
+	private String name;
+	private int price;
+	private int count;
+
+	public Item(String name, int price, int count) {
+		this.name = name;
+		this.price = price;
+		this.count = count;
+	}
+
+
+
+	public static boolean validName(String name) {
+		return !isBlank(name);
+	}
+
+	public static boolean validPrice(int price) {
+		return validUnitMoney(price) && isPositive(price);
+	}
+
+	public static boolean validCount(int count) {
+		return isPositive(count);
+	}
+
+	public static boolean validItemStatus(String name, int price, int count) {
+		return validName(name) && validPrice(price) && validCount(count);
+	}
+}
