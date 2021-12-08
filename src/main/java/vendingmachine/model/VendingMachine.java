0a913c8e273bc21c03eca558efc4cd69ba6672e6
nailseong <nnis9857@gmail.com>
2021-12-09 07:46:12 +0900

    feat: VendingMachine 모델 추가

---
+++ src/main/java/vendingmachine/model/VendingMachine.java
@@ -0,0 +1,15 @@
+package vendingmachine.model;
+
+public class VendingMachine {
+
+	private int deposit;
+	private ProductList productList;
+	private CoinList coinList;
+
+	public VendingMachine(int deposit, ProductList productList, CoinList coinList) {
+		this.deposit = deposit;
+		this.productList = productList;
+		this.coinList = coinList;
+	}
+	
+}
