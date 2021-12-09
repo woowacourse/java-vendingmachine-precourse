cb8b68529ae222dba2210cb6c1139d490647863d
jaeseo yoo <wotj102@gmail.com>
2021-12-09 19:47:24 +0900

    refactor: domain 패키지 생성 및 클래스 이동

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,5 +1,7 @@
 package vendingmachine;
 
+import vendingmachine.domain.VendingMachine;
+
 public class Application {
     public static void main(String[] args) {
         VendingMachine vendingMachine = new VendingMachine();
