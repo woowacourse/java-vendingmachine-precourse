ce5badaa9a7e0d720d3503f8c27b070c595fd0ec
sha11 <sha1119@naver.com>
2021-12-09 22:39:41 +0900

    feat: ProductFunction Class 구현
    ProductList 기능 구현
    
    ProductList 기능 구현

---
+++ src/main/java/vendingmachine/ProductFunction.java
@@ -0,0 +1,16 @@
+package vendingmachine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductFunction {
+    private List<Product> productList = new ArrayList<>();
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    
+
+
+}
