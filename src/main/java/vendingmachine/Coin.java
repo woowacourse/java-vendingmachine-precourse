c5f096c8cf04e125e176abba4b04b8066d763aaa
Jihyun <bjih1999@naver.com>
2021-12-09 19:25:33 +0900

    feat(Coin): 금액 정보 확인 기능
    금액을 입력받아 금액에 해당하는 Coin 상수를 리턴하는 of 메소드 구현
    Coin의 금액들을 확인할 수 있는 coinAmountList 멤버 변수 추가

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,5 +1,9 @@
 package vendingmachine;
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
@@ -7,6 +11,22 @@ public enum Coin {
     COIN_10(10);
 
     private final int amount;
+
+    public static List<Integer> coinAmountList = Arrays.stream(Coin.values())
+                                                        .map(Coin::getAmount)
+                                                        .collect(Collectors.toList());
+
+    public static final Map<Integer, String> AmountMap = Collections.unmodifiableMap(
+            Stream.of(values()).collect(Collectors.toMap(Coin::getAmount, Coin::name))
+    );
+
+    public static Coin of(final int amount) {
+        if (!coinAmountList.contains(amount)) {
+            throw new NoSuchElementException(ErrorMessage.NO_COIN_MATCH.getCompleteMessage());
+        }
+
+        return Coin.valueOf(AmountMap.get(amount));
+    }
 
     Coin(final int amount) {
         this.amount = amount;
