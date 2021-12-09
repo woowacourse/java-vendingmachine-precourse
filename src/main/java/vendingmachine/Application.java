fbfb88813039b520e7be3cc5c5e766dbc2e0f802
yxxnghwan <younghwan960@gmail.com>
2021-12-10 00:01:10 +0900

    feat: controller 객체 static으로 변경

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -4,9 +4,9 @@ import vendingmachine.controller.VendingMachineController;
 import vendingmachine.controller.ViewMappingKey;
 
 public class Application {
+    public static VendingMachineController controller = new VendingMachineController();
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        VendingMachineController controller = new VendingMachineController();
         controller.view(ViewMappingKey.INPUT_FIRST_MONEY);
     }
 }
