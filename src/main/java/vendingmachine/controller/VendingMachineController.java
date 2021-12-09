3a708082ffad7d9e4efcceeae08463d87f93656b
Yunkeun <yunx2@naver.com>
2021-12-09 18:35:32 +0900

    feat: 자판기가 보유한 금액을 입력

--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -1,5 +1,13 @@
 package vendingmachine.controller;
 
+import vendingmachine.view.InputView;
+
 public class VendingMachineController {
 
+	private VendingMachineController() {
+	}
+
+	public static void useVendingMachine() {
+		InputView.writeVendingMachineAmount();
+	}
 }
--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,5 +1,20 @@
 package vendingmachine.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import vendingmachine.utils.ExceptionUtils;
+
 public class InputView {
 
+	private InputView() {
+	}
+
+	public static String writeVendingMachineAmount() {
+		String inputMoney = Console.readLine();
+		try {
+			ExceptionUtils.validateMoney(inputMoney);
+			return inputMoney;
+		} catch (IllegalArgumentException IAE) {
+			return writeVendingMachineAmount();
+		}
+	}
 }
