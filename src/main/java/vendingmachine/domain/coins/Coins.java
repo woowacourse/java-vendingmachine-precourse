10bfd6dd64be1db7c6c6cc84f41bc4d2c709885e
devHudi <devhudi@gmail.com>
2021-12-10 00:55:07 +0900

    feat: 자판기 보유 금액에 맞는 무작위 동전 생성 기능 구현

--- src/main/java/vendingmachine/domain/coins/Coins.java
+++ src/main/java/vendingmachine/domain/coins/Coins.java
@@ -3,20 +3,42 @@ package vendingmachine.domain.coins;
 import java.util.HashMap;
 import java.util.Map;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import vendingmachine.domain.coin.Coin;
 import vendingmachine.domain.coinamount.CoinAmount;
 
 public class Coins {
-	private final Map<Coin, CoinAmount> coins = new HashMap<>();
+	private final Map<Coin, CoinAmount> coins;
 
-	private Coins() {
-		// TODO: 생성자 구현 필요
+	private Coins(int vendingMachineBalance) {
+		this.coins = generateRandomCoins(vendingMachineBalance);
 	}
 
 	public static Coins from(String vendingMachineBalance) {
 		CoinsValidator.validateVendingMachineBalance(vendingMachineBalance);
-		// TODO: 입력값에 따른 무작위 동전을 생성해야함
-		return new Coins();
+
+		int parsedBalance = Integer.parseInt(vendingMachineBalance);
+		return new Coins(parsedBalance);
+	}
+
+	// TODO: 리팩토링 필요
+	private Map<Coin, CoinAmount> generateRandomCoins(int vendingMachineBalance) {
+		Map<Coin, CoinAmount> coins = new HashMap<>();
+
+		for (int i = 0; i < 3; i++) {
+			Coin coin = Coin.values()[i];
+			int maxAmount = getMaxCoinAmount(vendingMachineBalance, coin);
+			int amount = Randoms.pickNumberInRange(0, maxAmount);
+			coins.put(coin, CoinAmount.from(amount));
+			vendingMachineBalance -= coin.getAmount() * amount;
+		}
+
+		coins.put(Coin.COIN_10, CoinAmount.from(getMaxCoinAmount(vendingMachineBalance, Coin.COIN_10)));
+		return coins;
+	}
+
+	private int getMaxCoinAmount(int remainingBalance, Coin coin) {
+		return remainingBalance / coin.getAmount();
 	}
 
 	public CoinAmount getCoinAmount(Coin coin) {
