1d4342c775e5d30bb017e62f721cbc757b492696
Jeong Satchel <bugod96@gmail.com>
2021-12-09 23:31:57 +0900

    feat(VendingMachine): create random coin amount info from money input

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -1,12 +1,33 @@
 package vendingmachine.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import org.assertj.core.util.Arrays;
+import vendingmachine.Coin;
 import vendingmachine.view.InputView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class VendingMachine {
 
-    private int totalMoney;
+    private final HashMap<Integer, Integer> coins = new HashMap<>();
 
     public VendingMachine() {
-        totalMoney = InputView.getVendingMachineTotalMoneyInput();
+        int totalMoney = InputView.getVendingMachineTotalMoneyInput();
+
+        for (Coin value : Coin.values()) {
+            this.coins.put(value.getAmount(), 0);
+        }
+        List<Integer> coinTypes = Stream.of(10, 50, 100, 500).collect(Collectors.toList());
+
+        while (totalMoney > 0) {
+            int randomCoin = Randoms.pickNumberInList(coinTypes);
+            if (totalMoney >= randomCoin) {
+                coins.put(randomCoin, coins.get(randomCoin)+1);
+                totalMoney -= randomCoin;
+            }
+        }
     }
 }
