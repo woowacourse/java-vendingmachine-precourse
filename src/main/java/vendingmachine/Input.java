a4ca3beb25829e1ceea4febcc8b6b1d2242627d5
wannagohome <iwwuf7@gmail.com>
2021-12-09 19:23:44 +0900

    feat: validate vending machine input is number

--- src/main/java/vendingmachine/Input.java
+++ src/main/java/vendingmachine/Input.java
@@ -5,6 +5,16 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
     public int requestVendingMachineHoldingAmount() {
         System.out.println(Constant.VENDING_MACHINE_HOLING_AMOUNT_REQUEST_STRING);
-        return Integer.parseInt(Console.readLine());
+        String input = Console.readLine();
+        validateVendingMachineAmountIsNumber(input);
+        return Integer.parseInt(input);
+    }
+
+    private void validateVendingMachineAmountIsNumber(String input) throws IllegalArgumentException {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
