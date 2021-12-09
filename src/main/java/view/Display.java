466211abe4339bab6ffe229a8520e1ddb576cd4a
koo <gudonghee2000@gmail.com>
2021-12-09 21:15:20 +0900

    refactor : Display클래스의 매직넘버를 상수로 변경

--- src/main/java/view/Display.java
+++ src/main/java/view/Display.java
@@ -1,11 +1,12 @@
 package view;
 
 public class Display {
+	private static final String ASK_INPUT_VENDING_MACHINE_CHANGE_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
 
 	private Display() {
 	}
 
 	public static void askInputVendingMachineChange() {
-		System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
+		System.out.println(ASK_INPUT_VENDING_MACHINE_CHANGE_MESSAGE);
 	}
 }
