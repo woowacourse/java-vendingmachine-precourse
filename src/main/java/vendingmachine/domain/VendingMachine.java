3bbd13943e360b150de459994227ebb6da7171bc
Seungmin Lim <seungmin9066@gmail.com>
2021-12-09 12:47:57 +0900

    [추가] 상품판매하는 메서드 추가

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -2,22 +2,27 @@ package vendingmachine.domain;
 
 import static vendingmachine.constant.Constant.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 
 public class VendingMachine {
-
-	private final HashMap<String, Object> productsOnSale = new HashMap<>();
+	private final HashMap<String, Integer> productPrice = new HashMap<>();
+	private final HashMap<String, Integer> productStocks = new HashMap<>();
 
 	public void addProduct(String products) {
 		for (String productPriceAndStock : products.split(DISTINGUISH_BETWEEN_PRODUCTS)) {
 			String[] product = productPriceAndStock.split(DISTINGUISH_BETWEEN_PRODUCT_INFORMATION);
 			// validateProduct(product);
-			productsOnSale.put(product[NAME],
-				new ArrayList<>(Arrays.asList(Integer.parseInt(product[PRICE]), Integer.parseInt(product[STOCKS]))));
+			productPrice.put(product[NAME], Integer.parseInt(product[PRICE]));
+			productStocks.put(product[NAME], Integer.parseInt(product[STOCKS]));
 		}
 	}
 
+	private void sellProduct(String productName, int amountPaid) {
+		if (!canSell()) {
+			return;
+		}
+		productStocks.put(productName, productStocks.get(productName) - 1);
+		giveChange(amountPaid);
+	}
 
 }
