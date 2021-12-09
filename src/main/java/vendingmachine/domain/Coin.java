7997df1238ffc56d8b271f8a38a5febdd449ca66
grace_goose <choieungi@gm.gist.ac.kr>
2021-12-09 22:59:56 +0900

    feat(Coin): 동전 domain에서 무작위 동전 한 개를 출력하는 기능

---
+++ src/main/java/vendingmachine/domain/Coin.java
@@ -0,0 +1,31 @@
+package vendingmachine.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum Coin {
+    COIN_500(500),
+    COIN_100(100),
+    COIN_50(50),
+    COIN_10(10);
+
+    private final int amount;
+
+    Coin(final int amount) {
+        this.amount = amount;
+    }
+
+    public int getRandomAmount() {
+        return Randoms.pickNumberInList(coinValueList());
+    }
+
+    private List<Integer> coinValueList() {
+        List<Integer> coinList = new ArrayList<>();
+        for (Coin coin : Coin.values()) {
+            coinList.add(coin.amount);
+        }
+        return coinList;
+    }
+}
