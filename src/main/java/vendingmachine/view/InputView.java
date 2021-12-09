81f8b59455d3cca8ff0c1d5cf3d37545d8f994e7
sun <syoun602@gmail.com>
2021-12-09 15:28:54 +0900

    fix: add new line to match requirement

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,6 +1,7 @@
 package vendingmachine.view;
 
 public class InputView {
+    private static final String NEW_LINE = "\n";
     private static final String INPUT_INITIAL_AMOUNT_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
     private static final String INPUT_PRODUCT_MESSAGE = "상품명과 가격, 수량을 입력해 주세요.";
     private static final String INPUT_USER_INSERT_AMOUNT_MESSAGE = "투입 금액을 입력해 주세요.";
@@ -14,6 +15,6 @@ public class InputView {
     }
 
     public static void printUserInsertAmountMessage() {
-        System.out.println(INPUT_USER_INSERT_AMOUNT_MESSAGE);
+        System.out.println(NEW_LINE + INPUT_USER_INSERT_AMOUNT_MESSAGE);
     }
 }
