49ed63e1a0508e21be47b326fccec641255c7cfc
iamtaehoon <kimth9981@naver.com>
2021-12-09 20:06:51 +0900

    feat(Quantity): 수량 클래스 생성

--- src/main/java/vendingmachine/domain/Quantity.java
+++ src/main/java/vendingmachine/domain/Quantity.java
@@ -1,6 +1,27 @@
 package vendingmachine.domain;
 
+import static vendingmachine.Constant.*;
+
 public class Quantity {
-    public Quantity(String s) {
+    public Quantity(String quantityInput) {
+        validate(quantityInput);
+    }
+
+    public void validate(String quantityInput) {
+        int quantity = validateItIsNumber(quantityInput);
+        validateItIsPositive(quantity);
+    }
+
+    private void validateItIsPositive(int quantity) {
+        if (quantity < ZERO) {
+            throw new IllegalArgumentException("상품의 수량은 0 이상이어야 합니다.");
+        }
+    }
+
+    private int validateItIsNumber(String quantityInput) {
+        if (!quantityInput.matches(NUMBER_REGEX)) {
+            throw new IllegalArgumentException("상품의 수량은 숫자여야 합니다.");
+        }
+        return Integer.parseInt(quantityInput);
     }
 }
