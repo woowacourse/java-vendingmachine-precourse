45dea83b5188fade906b0f98e23dd46dd8ca8a9e
sha11 <sha1119@naver.com>
2021-12-09 20:46:06 +0900

    feat: InputView 구현
    입력 기능 구현
    
    자판기 보유금액 입력 기능 구현

---
+++ src/main/java/vendingmachine/InputView.java
@@ -0,0 +1,12 @@
+package vendingmachine;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static String inputHowMuch() {
+        System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+
+}
