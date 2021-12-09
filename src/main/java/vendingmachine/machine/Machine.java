7f245face3ee69ec8a5ec6d0ad547a5a44d168e2
TaeHyeon Kim <kth990303@naver.com>
2021-12-09 18:47:14 +0900

    feat: 자판기 투입금액 toString 메소드 구현

--- src/main/java/vendingmachine/machine/Machine.java
+++ src/main/java/vendingmachine/machine/Machine.java
@@ -1,6 +1,9 @@
 package vendingmachine.machine;
 
 public class Machine {
+    private static final String INPUT_AMOUNT_NOW_MESSAGE = "투입 금액: ";
+    private static final String AMOUNT_UNIT_MESSAGE = "원";
+
     private int amountByClient;
 
     public Machine(int amountByClient) {
@@ -13,5 +16,11 @@ public class Machine {
 
     public void buyProduct(int price) {
         amountByClient -= price;
+    }
+
+    @Override
+    public String toString() {
+        return INPUT_AMOUNT_NOW_MESSAGE + amountByClient
+                + AMOUNT_UNIT_MESSAGE;
     }
 }
