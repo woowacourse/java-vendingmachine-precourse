b919e89946a227919796889578575fc88314f0ca
nailseong <nnis9857@gmail.com>
2021-12-10 00:58:43 +0900

    fix(CoinList): hashMap 자료형을 LinkedHashMap으로 변경

--- src/main/java/vendingmachine/model/CoinList.java
+++ src/main/java/vendingmachine/model/CoinList.java
@@ -2,17 +2,17 @@ package vendingmachine.model;
 
 import static vendingmachine.util.RandomNumberGenerator.*;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 // TODO: 2021/12/08 더 좋은 이름
 public class CoinList {
 
-	private final HashMap<Coin, Integer> hashMap;
+	private final LinkedHashMap<Coin, Integer> hashMap;
 	private final int totalMoney;
 
 	public CoinList(int totalMoney) {
 		this.totalMoney = totalMoney;
-		this.hashMap = new HashMap<>();
+		this.hashMap = new LinkedHashMap<>();
 		hashMap.put(Coin.COIN_500, 0);
 		hashMap.put(Coin.COIN_100, 0);
 		hashMap.put(Coin.COIN_50, 0);
