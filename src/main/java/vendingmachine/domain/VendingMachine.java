be1e2c6dedc3bee31d39114227dc12d9c70a4ba7
iamtaehoon <kimth9981@naver.com>
2021-12-09 18:27:26 +0900

    feat(CoinRepository): 자판기에 있는 동전의 개수 보여주는 기능 구현

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -27,5 +27,6 @@ public class VendingMachine {
             throw new IllegalArgumentException("해당 금액은 동전으로 만들 수 없는 단위의 숫자입니다.");
         }
         coinRepository = new CoinRepository(CoinGenerator.makeCoins(inputMoney));
+        System.out.println(coinRepository);
     }
 }
--- src/main/java/vendingmachine/repository/CoinRepository.java
+++ src/main/java/vendingmachine/repository/CoinRepository.java
@@ -10,4 +10,13 @@ public class CoinRepository { // 1급 콜렉션
     public CoinRepository(HashMap<Coin, Integer> coinRepository) {
         this.coinRepository = coinRepository;
     }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        coinRepository.keySet()
+            .stream()
+            .forEach(coin -> sb.append(coin.getAmount() + "원 - " + coinRepository.get(coin) + "개\n"));
+        return sb.toString();
+    }
 }
