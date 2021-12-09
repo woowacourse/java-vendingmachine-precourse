711723c370b579b6fc521e16222ab3a2f00f0203
Inhyeok Jo <alzpqm@naver.com>
2021-12-10 01:19:49 +0900

    feat: Application에서 Controller를 생성해서 게임을 시작할 수 있게함

--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -1,4 +1,24 @@
 package vendingmachine.controller;
 
+import vendingmachine.view.VendingMachineInputView;
+import vendingmachine.view.VendingMachineOutputView;
+
 public class VendingMachineController {
+	private final static VendingMachineController vendingMachineController = new VendingMachineController();
+	private final VendingMachineInputView vendingMachineInputView;
+	private final VendingMachineOutputView vendingMachineOutputView;
+
+	private VendingMachineController() {
+		vendingMachineInputView = VendingMachineInputView.getVendingMachineInputView();
+		vendingMachineOutputView = VendingMachineOutputView.getVendingMachineOutputView();
+	}
+
+	public static VendingMachineController getVendingMachineController(){
+		return vendingMachineController;
+	}
+
+	public void run(){
+		vendingMachineOutputView.printAmountInputMessage();
+		int amount = vendingMachineInputView.getInitialAmount();
+	}
 }
