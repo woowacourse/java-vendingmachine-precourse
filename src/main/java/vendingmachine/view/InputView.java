938e5f8f2206dc5a9c4de68819ee1a8ecbcb31bb
EastHShin <xldk78@gmail.com>
2021-12-10 03:36:02 +0900

    feat(view): 자판기의 보유금액을 입력받는 기능 구현

---
+++ src/main/java/vendingmachine/view/InputView.java
@@ -0,0 +1,11 @@
+package vendingmachine.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+	public int getSavedMoney() {
+		return Integer.parseInt(Console.readLine());
+	}
+
+}
