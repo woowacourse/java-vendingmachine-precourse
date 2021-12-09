4dab2924d396988990ae32880f12573ba39670a9
TaeHyeon Kim <kth990303@naver.com>
2021-12-09 17:24:19 +0900

    feat: 상품 리스트 클래스 초기구현

---
+++ src/main/java/vendingmachine/products/Products.java
@@ -0,0 +1,12 @@
+package vendingmachine.products;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Products {
+    private final List<Product> products;
+
+    public Products() {
+        products = new ArrayList<>();
+    }
+}
