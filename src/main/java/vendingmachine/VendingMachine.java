d267b9bff4d2eab903982c7c1c01b6d5bb65e814
TaeHyeon Kim <kth990303@naver.com>
2021-12-09 19:28:06 +0900

    feat: 상품 구매 로직 구현

--- src/main/java/vendingmachine/VendingMachine.java
+++ src/main/java/vendingmachine/VendingMachine.java
@@ -2,6 +2,7 @@ package vendingmachine;
 
 import vendingmachine.coin.CoinController;
 import vendingmachine.machine.MachineController;
+import vendingmachine.products.Product;
 import vendingmachine.products.ProductsController;
 
 public class VendingMachine {
@@ -18,6 +19,22 @@ public class VendingMachine {
     }
 
     public void run() {
+        while(productsController.canBuyAnyProduct(machineController.nowInputAmount())){
+            machineController.nowInputAmountInfo();
+            if (!buyProduct()) {
+                // 잔액 반환
+                break;
+            }
+        }
+    }
 
+    public boolean buyProduct() {
+        String buyProductName = machineController.buyWhichProduct();
+        Product product = productsController.findProduct(buyProductName);
+        if (!machineController.buyProduct(product.getPrice())) {
+            return false;
+        }
+        productsController.buyProduct(product);
+        return true;
     }
 }
