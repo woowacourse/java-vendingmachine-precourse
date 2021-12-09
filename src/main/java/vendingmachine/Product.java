c84a5e0269c6b2e88475499e8c9c1f9ea96be6a9
Jihyun <bjih1999@naver.com>
2021-12-09 16:23:24 +0900

    feat(Product): 입력받은 상품 정보로 상품을 생성
    Product 클래스 생성
    입력받은 상품 정보로 Product를 생성할 수 있도록 생성자 추가

---
+++ src/main/java/vendingmachine/Product.java
@@ -0,0 +1,18 @@
+package vendingmachine;
+
+public class Product {
+
+    private String name;
+
+    private int price;
+
+    private int stockQuantity;
+
+    public Product(String name, int price, int stockQuantity) {
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+
+}
