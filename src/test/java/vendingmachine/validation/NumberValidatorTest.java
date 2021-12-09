1151f2e317918320f62517d486c9fb8d719cc067
Byeongju-Kong <bjuuuu98@gmail.com>
2021-12-09 16:15:25 +0900

    test: 테스트 케이스 추가

--- src/test/java/vendingmachine/validation/NumberValidatorTest.java
+++ src/test/java/vendingmachine/validation/NumberValidatorTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 class NumberValidatorTest {
     @ParameterizedTest
     @DisplayName("양의 정수가 아니면 true, 양의 정수면 false를 반환한다.")
-    @CsvSource({"-1, true", "?, true", "1.2, true", "0, true", "1, false"})
+    @CsvSource({"-1, true", "?, true", "1.2, true", "0, true", "1, false", "1000, false"})
     void isNotPositiveInteger(final String given, final boolean expected) {
         boolean actual = NumberValidator.isNotPositiveInteger(given);
         assertThat(actual).isEqualTo(expected);
