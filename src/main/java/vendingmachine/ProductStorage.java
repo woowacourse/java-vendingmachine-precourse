be2ab96ac5e7635a783816cf3e08257ddc8148a0
2young <yh20studio@gmail.com>
2021-12-09 20:28:37 +0900

    fix(): 상품 입력 시 이름이 공백인 경우 예외 처리를 하도록 수정

--- src/main/java/vendingmachine/ProductStorage.java
+++ src/main/java/vendingmachine/ProductStorage.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 public class ProductStorage {
 	private static final String DELIMITER = ",";
+	private static final String EMPTY = "";
 	private static final int NAME = 0;
 	private static final int PRICE = 1;
 	private static final int QUANTITY = 2;
@@ -11,6 +12,7 @@ public class ProductStorage {
 	private static final int ENDING_NUMBER_OF_PRODUCT = 1;
 	private static final int NUMBER_OF_INPUT_COLUMN = 3;
 	private static final String ERROR_INPUT_FORMAT = "상품은 상품명, 가격, 수량을 쉼표로 구분하여 입력해야 합니다.";
+	private static final String ERROR_NAME_EMPTY = "상품명은 공백일 수 없습니다.";
 	private static final String ERROR_NAME_DUPLICATE = "상품명은 중복될 수 없습니다.";
 
 	HashMap<String, Product> storage;
@@ -40,8 +42,15 @@ public class ProductStorage {
 	}
 
 	private String validateName(String name) {
+		validateEmptyName(name);
 		validateDuplicateName(name);
 		return name;
+	}
+
+	private void validateEmptyName(String name) {
+		if (name.equals(EMPTY)) {
+			throw new IllegalArgumentException(ERROR_NAME_EMPTY);
+		}
 	}
 
 	private void validateDuplicateName(String name) {
@@ -56,5 +65,4 @@ public class ProductStorage {
 		String quantity = productInformation[QUANTITY];
 		storage.put(name, new Product(price, quantity));
 	}
-
 }
