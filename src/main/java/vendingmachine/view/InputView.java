20a025a7e681d51ab70209a2bb57df69c74f86b6
jaeseo yoo <wotj102@gmail.com>
2021-12-09 20:00:27 +0900

    refactor: enum 객체 상수 가져오는 방식 수정

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -8,7 +8,7 @@ public class InputView {
 
     public int inputMoneyForMakeCoin() {
         while (true) {
-            print(Input.valueOf("MONEY_GUIDE_MESSAGE").getMessage());
+            print(Input.MONEY_GUIDE_MESSAGE.getMessage());
             String Money = Console.readLine();
             try {
                 tryToInputMoneyForMakeCoin(Money);
@@ -26,15 +26,15 @@ public class InputView {
     private void validateDigit(String Money) {
         for (int m = 0; m < Money.length(); m++) {
             if (!Character.isDigit(Money.charAt(m))) {
-                print(Input.valueOf("MONEY_DIGIT_ERROR_MESSAGE").getMessage());
+                print(Input.MONEY_DIGIT_ERROR_MESSAGE.getMessage());
                 throw  new IllegalArgumentException();
             }
         }
     }
 
     private void validateNull(String Money) {
-        if (Money.length() == Condition.valueOf("LENGTH_0").getNumber()) {
-            print(Input.valueOf("MONEY_LENGTH_0_ERROR_MESSAGE").getMessage());
+        if (Money.length() == Condition.LENGTH_0.getNumber()) {
+            print(Input.MONEY_LENGTH_0_ERROR_MESSAGE.getMessage());
             throw new IllegalArgumentException();
         }
     }
