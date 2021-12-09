fcfec6900334b04d996e0f99c36115edca0c3377
awesomeo184 <awesomeo184@gmail.com>
2021-12-10 03:49:06 +0900

    test(domain): 상품 금액을 투입하고 상품을 구매하는 기능 테스트 작성

--- src/test/java/vendingmachine/VendingMachineTest.java
+++ src/test/java/vendingmachine/VendingMachineTest.java
@@ -1,19 +1,65 @@
 package vendingmachine;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import vendingmachine.domain.Changes;
+import vendingmachine.domain.Product;
 import vendingmachine.domain.VendingMachine;
+
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.*;
 
 class VendingMachineTest {
 
+    private VendingMachine vendingMachine;
+    private Product coke = new Product("콜라", 1500);
+    private Product cider = new Product("사이다", 1500);
+    private int seedMoney;
+
+    @BeforeEach
+    void setUp() {
+        seedMoney = 450;
+        vendingMachine = new VendingMachine(seedMoney);
+    }
+
     @Test
     void 보유금액을_입력받아_무작위로_잔돈을_생성한다() {
-        int seedMoney = 3000;
-        VendingMachine vendingMachine = new VendingMachine(seedMoney);
         Changes changes = vendingMachine.getChanges();
         assertThat(changes.sum()).isEqualTo(seedMoney);
     }
 
+    @Test
+    void 상품_금액을_투입하고_상품을_구매한다() {
+        int money = 3000;
+        vendingMachine.addProduct(coke, 2);
+        vendingMachine.inputMoney(money);
+
+        vendingMachine.get(coke);
+
+        Map<Product, Integer> products = vendingMachine.getProducts();
+
+        assertThat(products.get(coke)).isEqualTo(1);
+        assertThat(vendingMachine.getInputMoney()).isEqualTo(1500);
+    }
+
+    @Test
+    void 구입하려는_상품가격이_투입금액보다_크면_예외를_일으킨다() {
+        int money = 1400;
+        vendingMachine.addProduct(coke, 2);
+        vendingMachine.inputMoney(money);
+
+        assertThatThrownBy(() -> vendingMachine.get(coke)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 구입하려는_상품의_재고가_모자라면_예외를_일으킨다() {
+        int money = 3000;
+        vendingMachine.addProduct(coke, 1);
+        vendingMachine.inputMoney(money);
+
+        vendingMachine.get(coke);
+
+        assertThatThrownBy(() -> vendingMachine.get(coke)).isInstanceOf(IllegalArgumentException.class);
+    }
 }
