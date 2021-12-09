678fd681308f2f4ee2e1625aae6d17d3635c75ac
Seungmin Lim <seungmin9066@gmail.com>
2021-12-09 12:16:58 +0900

    [리팩터] 변수명 수정

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -8,15 +8,16 @@ import java.util.HashMap;
 
 public class VendingMachine {
 
-	private final HashMap<String, Object> productsPriceAndStocks = new HashMap<>();
+	private final HashMap<String, Object> productsOnSale = new HashMap<>();
 
 	public void addProduct(String products) {
 		for (String productPriceAndStock : products.split(DISTINGUISH_BETWEEN_PRODUCTS)) {
 			String[] product = productPriceAndStock.split(DISTINGUISH_BETWEEN_PRODUCT_INFORMATION);
 			// validateProduct(product);
-			productsPriceAndStocks.put(product[NAME],
+			productsOnSale.put(product[NAME],
 				new ArrayList<>(Arrays.asList(Integer.parseInt(product[PRICE]), Integer.parseInt(product[STOCKS]))));
 		}
-
 	}
+
+
 }
