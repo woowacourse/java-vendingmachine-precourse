9f28358f744df310fb05859e19e9050df27b6437
jochungbeom <cndqjacndqja@gmail.com>
2021-12-09 15:49:52 +0900

    feat: 입력 받은 상품 리스트를 ","로 나누어 상태를 저장한다.

---
+++ src/test/java/vendingmachine/view/InputViewTest.java
@@ -0,0 +1,15 @@
+package vendingmachine.view;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class InputViewTest {
+
+	@Test
+	void test() {
+	}
+
+}
