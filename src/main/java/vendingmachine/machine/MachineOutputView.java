f3cc4a36ac32ad6454a662fb6ffd17e1b4bbe165
TaeHyeon Kim <kth990303@naver.com>
2021-12-09 19:56:31 +0900

    style: 투입금액 출력 시 개행문자 추가

--- src/main/java/vendingmachine/machine/MachineOutputView.java
+++ src/main/java/vendingmachine/machine/MachineOutputView.java
@@ -2,6 +2,7 @@ package vendingmachine.machine;
 
 public class MachineOutputView {
     public static void printInputAmount(String machineAmount) {
+        System.out.println();
         System.out.println(machineAmount);
     }
 }
