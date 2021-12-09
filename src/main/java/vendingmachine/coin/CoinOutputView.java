0e947af85498175512422bc8a45fd9f0d1af3def
TaeHyeon Kim <kth990303@naver.com>
2021-12-09 20:35:01 +0900

    feat: 잔돈 반환 출력 메소드 구현

--- src/main/java/vendingmachine/coin/CoinOutputView.java
+++ src/main/java/vendingmachine/coin/CoinOutputView.java
@@ -1,9 +1,15 @@
 package vendingmachine.coin;
 
+import java.util.List;
+
 import static vendingmachine.coin.Coin.*;
 
 public class CoinOutputView {
     private static final String COINS_COUNTS_MESSAGE = "자판기가 보유한 동전";
+    private static final String REPAY_COINS_MESSAGE = "잔돈";
+    private static final String[] COIN_MESSAGE = {"500원 - ", "100원 - ", "50원 - ", "10원 - "};
+    private static final String COIN_COUNTS_UNIT = "개";
+    private static final int NO_COIN = 0;
 
     public static void initCoinsPrintInfo() {
         System.out.println();
@@ -13,5 +19,16 @@ public class CoinOutputView {
         System.out.println(COIN_50.toString());
         System.out.println(COIN_10.toString());
         System.out.println();
+    }
+
+    public static void repayCoinsPrintInfo(List<Integer> repayList) {
+        System.out.println(REPAY_COINS_MESSAGE);
+        for (int i = 0; i < repayList.size(); i++) {
+            int counts = repayList.get(i);
+            if (counts == NO_COIN) {
+                continue;
+            }
+            System.out.println(COIN_MESSAGE[i] + counts + COIN_COUNTS_UNIT);
+        }
     }
 }
