11458dd2d805291e8025a0f9769f69265dd17653
jochungbeom <cndqjacndqja@gmail.com>
2021-12-09 16:44:19 +0900

    feat: 투입 금액이 구매 상품보다 많은지 확인 구현

--- src/main/java/vendingmachine/domain/Product.java
+++ src/main/java/vendingmachine/domain/Product.java
@@ -4,18 +4,20 @@ import java.util.List;
 
 public class Product {
 	private String name;
-	private String price;
-	private String quantity;
-
-	public Product(String name, String price, String quantity) {
-		this.name = name;
-		this.price = price;
-		this.quantity = quantity;
-	}
+	private int price;
+	private int quantity;
 
 	public Product(List<String> productToString) {
 		this.name = productToString.get(0);
-		this.price = productToString.get(1);
-		this.quantity = productToString.get(2);
+		this.price = Integer.parseInt(productToString.get(1));
+		this.quantity = Integer.parseInt(productToString.get(2));
+	}
+
+	public boolean isSameName(String name) {
+		return this.name.equals(name);
+	}
+
+	private boolean isPurchaseProduct(int inputAmount) {
+		return price <= inputAmount;
 	}
 }
