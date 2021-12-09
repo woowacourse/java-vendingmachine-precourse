0ed84b6f41ccd9bb90d4ed0efb8a2361078c3960
nailseong <nnis9857@gmail.com>
2021-12-10 01:08:32 +0900

    feat: main 메서드에서 실행되도록 코드 추가

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,7 +1,15 @@
 package vendingmachine;
 
+import vendingmachine.controller.VendingMachineController;
+import vendingmachine.controller.ViewController;
+
 public class Application {
+
 	public static void main(String[] args) {
-		// TODO: 프로그램 구현
+		ViewController viewController = new ViewController();
+		VendingMachineController vendingMachineController = new VendingMachineController(viewController);
+
+		vendingMachineController.init();
+		vendingMachineController.run();
 	}
 }
