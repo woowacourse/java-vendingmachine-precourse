c9a2e2ad5744a2b9abb27a9deb0d328811dfae52
YJGwon <yj970125@gmail.com>
2021-12-09 18:33:44 +0900

    feat: 해당 금액이 보유금액보다 작거나 같으면 해당 동전을 한 개 생성한다
    Coin.java
        member 추가
        - count: 동전의 개수
        - amounts: 금액의 List
        method 추가
        - getAmount(): 동전의 amount에 대한 getter
        - getAmounts(): amounts에 대한 getter
        - isAmount(amount): 동전의 금액이 해당 금액인지 boolean return
        - findByAmount(amount): 해당 금액의 Coin return
        - add(): count 1 증가
    
    Cashier.java
        - getCoinAmountList() 삭제
    
    Cashier.makeCoins()는 랜덤으로 생성한 금액이 보유 금액보다 작거나 같으면 해당 금액의 coin을 생성한다

--- src/main/java/vendingmachine/Cashier.java
+++ src/main/java/vendingmachine/Cashier.java
@@ -13,16 +13,11 @@ import camp.nextstep.edu.missionutils.Randoms;
  */
 public class Cashier {
 	public void makeCoins(int holdingAmount) {
-		int coinAmount = Randoms.pickNumberInList(getCoinAmountList());
-		// TODO: 해당 금액이 보유금액보다 작거나 같으면 해당 동전을 한 개 생성한다
-	}
-
-	private ArrayList<Integer> getCoinAmountList() {
-		ArrayList<Integer> coinAmountList = new ArrayList<>();
-		coinAmountList.add(10);
-		coinAmountList.add(50);
-		coinAmountList.add(100);
-		coinAmountList.add(500);
-		return coinAmountList;
+		int coinAmount = Randoms.pickNumberInList(Coin.getAmounts());
+		if (coinAmount <= holdingAmount) {
+			Coin.findByAmount(coinAmount).add();
+			holdingAmount = holdingAmount - coinAmount;
+		}
+		// TODO: 보유 금액이 0이 될때까지 반복한다
 	}
 }
--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,16 +1,47 @@
 package vendingmachine;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum Coin {
-    COIN_500(500),
-    COIN_100(100),
-    COIN_50(50),
-    COIN_10(10);
+	COIN_500(500),
+	COIN_100(100),
+	COIN_50(50),
+	COIN_10(10);
 
-    private final int amount;
+	private final int amount;
+	private int count = 0;
+	private static List<Integer> amounts = Stream.of(Coin.values())
+		.map(Coin::getAmount)
+		.collect(Collectors.toList());
 
-    Coin(final int amount) {
-        this.amount = amount;
-    }
+	Coin(final int amount) {
+		this.amount = amount;
+	}
 
-    // 추가 기능 구현
+	// 추가 기능 구현
+	public int getAmount() {
+		return this.amount;
+	}
+
+	public static List<Integer> getAmounts() {
+		return amounts;
+	}
+
+	public static Coin findByAmount(int amount) {
+		return Arrays.stream(Coin.values())
+			.filter(coin -> coin.isAmount(amount))
+			.findFirst()
+			.orElse(null);
+	}
+
+	public boolean isAmount(int amount) {
+		return this.amount == amount;
+	}
+
+	public void add() {
+		this.count++;
+	}
 }
