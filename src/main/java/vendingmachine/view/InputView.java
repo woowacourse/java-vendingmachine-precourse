8c377884318776eaccf2ca45d10ca4154f0e5a7c
Jeong Satchel <bugod96@gmail.com>
2021-12-09 23:33:56 +0900

    feat(InputView): ask for total money input

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public static int getVendingMachineTotalMoneyInput() {
+        System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
+
         String input = Console.readLine();
         return Integer.parseInt(input);
     }
