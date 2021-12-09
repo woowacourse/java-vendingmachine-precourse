92d79ea9d8d75dbd10398e3aae711c1dbade3dfa
TaeHyeon Kim <kth990303@naver.com>
2021-12-09 20:28:51 +0900

    refactor: 검증 메소트 클래스 통합

--- src/main/java/vendingmachine/ValidatorMessage.java
+++ src/main/java/vendingmachine/ValidatorMessage.java
@@ -2,4 +2,6 @@ package vendingmachine;
 
 public class ValidatorMessage {
     public static final String ERROR_MESSAGE = "[ERROR] ";
+
+    public static final String NULL_PRODUCT_MESSAGE = "존재하지 않는 상품입니다.";
 }
--- src/main/java/vendingmachine/products/ProductsValidator.java
+++
@@ -1,5 +0,0 @@
-package vendingmachine.products;
-
-public class ProductsValidator {
-    public static final String NULL_PRODUCT_MESSAGE = "존재하지 않는 상품입니다.";
-}
