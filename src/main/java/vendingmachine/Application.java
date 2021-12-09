9c3898ad7ca91020e59225a12db1e2c60fc6926c
wilgur513 <wilgur513@naver.com>
2021-12-09 21:18:31 +0900

    feat: validate exchange is must number

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -3,8 +3,12 @@ package vendingmachine;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
-        String value = Console.readLine();
-    }
+	public static void main(String[] args) {
+		System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
+		String value = Console.readLine();
+
+		if (!value.matches("[0-9]*")) {
+            throw new IllegalArgumentException("[ERROR] 금액은 숫자여야 합니다.");
+		}
+	}
 }
