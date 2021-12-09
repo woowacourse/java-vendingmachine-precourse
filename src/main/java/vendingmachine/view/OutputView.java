35836e381af205b942aabbf8db2a3c1e5b21e9b2
Seungmin Lim <seungmin9066@gmail.com>
2021-12-09 19:08:46 +0900

    [추가] 자판기 세팅 메시지 출력 메서드 추가

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -1,4 +1,9 @@
 package vendingmachine.view;
 
+import static vendingmachine.constant.Constant.*;
+
 public class OutputView {
+	public static void setVendingMachineMoney() {
+		System.out.println(ENTER_VENDING_MACHINE_MONEY_MESSAGE);
+	}
 }
