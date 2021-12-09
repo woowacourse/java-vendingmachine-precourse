33559265e5468aaad76e442605cb70ffad99864e
grace_goose <choieungi@gm.gist.ac.kr>
2021-12-09 21:23:05 +0900

    feat: 상품 객체 구현

---
+++ src/main/java/vendingmachine/Product.java
@@ -0,0 +1,15 @@
+package vendingmachine;
+
+public class Product {
+    private final String name;
+    private final int price;
+    private int count;
+
+    public Product(String name, int price, int count) {
+        this.name = name;
+        this.price = price;
+        this.count = count;
+    }
+
+
+}
