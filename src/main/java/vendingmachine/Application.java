6ad9b6303785a2f9803390df7d5a785ab87c36aa
wilgur513 <wilgur513@naver.com>
2021-12-09 21:45:51 +0900

    feat: split item list by semicolon

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -3,6 +3,7 @@ package vendingmachine;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Arrays;
+import java.util.List;
 import vendingmachine.reader.ExchangeAmountReader;
 
 public class Application {
@@ -10,11 +11,7 @@ public class Application {
 		int amount = new ExchangeAmountReader().read();
 		Coins coins = generateCoins(amount);
 		printExchangeCoins(coins);
-	}
-
-	private static void printExchangeCoins(Coins coins) {
-		System.out.println("자판기가 보유한 동전");
-		coins.stream().forEach(coin -> System.out.println(coin.getAmount() + "원 - " + coins.getCount(coin) + "개"));
+		List<Item> items = inputItemList();
 	}
 
 	private static Coins generateCoins(int amount) {
@@ -35,5 +32,20 @@ public class Application {
 	private static Coin pickRandomCoin() {
 		int amount = Randoms.pickNumberInList(Arrays.asList(10, 50, 100, 500));
 		return Coin.valueOf(amount);
+	}
+
+	private static void printExchangeCoins(Coins coins) {
+		System.out.println("자판기가 보유한 동전");
+		coins.stream().forEach(coin -> System.out.println(coin.getAmount() + "원 - " + coins.getCount(coin) + "개"));
+	}
+
+	private static List<Item> inputItemList() {
+		System.out.println("상품명과 가격, 수량을 입력해 주세요.");
+		String value = Console.readLine();
+		String[] items = value.split(";");
+		for (String item : items) {
+			System.out.println(item);
+		}
+		return null;
 	}
 }
---
+++ src/main/java/vendingmachine/Item.java
@@ -0,0 +1,17 @@
+package vendingmachine;
+
+public class Item {
+	private final String name;
+	private final int price;
+	private int quantity;
+
+	private Item(String name, int price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	public static Item of(String name, int price, int quantity) {
+		return new Item(name, price, quantity);
+	}
+}
