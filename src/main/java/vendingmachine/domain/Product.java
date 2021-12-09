0a3deb718f4208d6123b2896fa18107526b187dd
iamtaehoon <kimth9981@naver.com>
2021-12-09 20:12:31 +0900

    feat(Product): Product 클래스 생성

---
+++ src/main/java/vendingmachine/domain/Product.java
@@ -0,0 +1,17 @@
+package vendingmachine.domain;
+
+public class Product {
+    private String name;
+    private Price price;
+    private Quantity quantity;
+
+    private Product(String name, Price price, Quantity quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public static Product registerProduct(String name, Price price, Quantity quantity) {
+        return new Product(name, price, quantity);
+    }
+}
--- src/main/java/vendingmachine/utils/ProductValidator.java
+++ src/main/java/vendingmachine/utils/ProductValidator.java
@@ -5,10 +5,11 @@ import static vendingmachine.Constant.*;
 import java.util.ArrayList;
 
 import vendingmachine.domain.Price;
+import vendingmachine.domain.Product;
 import vendingmachine.domain.Quantity;
 
 public class ProductValidator {
-    public static String validateForm(String productInfoInput) {
+    public static Product validateForm(String productInfoInput) {
         StringBuffer sb = new StringBuffer(productInfoInput);
         if (!((sb.charAt(0) == '[') && (sb.charAt(productInfoInput.length() - 1) == ']'))) {
             throw new IllegalArgumentException("상품의 상태는 []로 감싸져야 합니다.");
@@ -19,17 +20,21 @@ public class ProductValidator {
         // split 후 안에 세 개의 문자가 있는지 확인한다.
         productInfoInput = sb.toString();
         String[] split = productInfoInput.split(",", -1);
+        String name = validateProductName(split);
+        Price price = new Price(split[1]);
+        Quantity quantity = new Quantity(split[2]);
+        // productName, [price, quantity]를 가진 걸 리턴
+        return Product.registerProduct(name, price, quantity);
+    }
+
+    private static String validateProductName(String[] split) {
         if (split.length != 3) {
             throw new IllegalArgumentException("상품명, 가격, 수량이 제대로 입력되지 않았습니다.");
         }
         if (isNameEmpty(split[0])) {
             throw new IllegalArgumentException("이름이 제대로 입력되지 않은 상품이 있습니다.");
         }
-        //split[1]은 Price, split[2]는 Quantity
-        Price price = new Price(split[1]);
-        Quantity quantity = new Quantity(split[2]);
-        // productName, [price, quantity]를 가진 걸 리턴
-        return new Product(split[0], price, quantity);
+        return split[0];
     }
 
     private static boolean isNameEmpty(String name) {
