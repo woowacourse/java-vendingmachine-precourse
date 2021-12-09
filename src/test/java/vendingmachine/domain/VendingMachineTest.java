b34923b77e651906d4f51a9a83d4efab1f5c8976
iamtaehoon <kimth9981@naver.com>
2021-12-09 16:52:06 +0900

    test(VendingMachine): 입력 금액이 10원보다 작을 때 오류가 나는지 확인하는 테스트 작성

--- src/test/java/vendingmachine/domain/VendingMachineTest.java
+++ src/test/java/vendingmachine/domain/VendingMachineTest.java
@@ -14,12 +14,15 @@ class VendingMachineTest {
     @DisplayName("10원 이상 10으로 나눠떨어지는 정상적인 금액을 입력받는다.")
     @ValueSource(ints = {10, 100, 180, 20, 2000, 10000, 40000})
     void 자판기_초기_금액_입력_정상(int inputMoney) {
-        //given
         VendingMachine vm = new VendingMachine();
-
-        //when
-        //then
         vm.putInitialAmount(inputMoney);
     }
 
+    @ParameterizedTest
+    @DisplayName("초기 금액으로 10원 밑으로 입력받으면 에러를 반환한다.")
+    @ValueSource(ints = {9, 6, 0, -1, -10, -100, -87, -240, -900, -10000})
+    void 자판기_초기_금액_입력_동전_최소보다_적은_경우_오류() {
+        VendingMachine vm = new VendingMachine();
+        Assertions.assertThatThrownBy(() -> vm.putInitialAmount(inputMoney)).isInstanceOf(IllegalArgumentException.class); //TODO: 문구는 나중에 생성
+    }
 }
