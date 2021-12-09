4b2f6649df4212f973eee7a3216b298594defd8f
eonju97 <eonju97.dev@gmail.com>
2021-12-10 08:43:33 +0900

    feat: 에러 메세지 생성

---
+++ src/main/java/vendingmachine/ErrorMessage.java
@@ -0,0 +1,22 @@
+package vendingmachine;
+
+public enum ErrorMessage {
+    CONTAINS_BLANK_ERROR_MESSAGE("[ERROR] 빈 칸은 입력하실 수 없습니다."),
+    AMOUNT_FORMAT_ERROR_MESSAGE("[ERROR] 금액은 숫자여야 합니다."),
+    AMOUNT_UNIT_ERROR_MESSAGE("[ERROR] 금액은 10원 단위로 나누어 떨어져야 합니다."),
+    AMOUNT_NEGATIVE_ERROR_MESSAGE("[ERROR] 금액은 음수가 될 수 없습니다."),
+    OBJECT_DUPLICATE_ERROR_MESSAGE("[ERROR] 중복된 상품명을 사용하실 수 없습니다."),
+    OBJECT_AMOUNT_ERROR_MESSAGE("[ERROR] 수량은 1개 이상 입력하셔야합니다."),
+    OBJECT_PRICE_ERROR_MESSAGE("[ERROR] 가격은 100원 이상 입력하셔야합니다."),
+    OBJECT_SOLD_OUT_ERROR_MESSAGE("[ERROR] 해당 상품은 현재 없는 상품입니다.");
+
+    private String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
