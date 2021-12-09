79e7245fa18aacf6a74cfd44a51055654b4e4830
2young <yh20studio@gmail.com>
2021-12-09 20:03:50 +0900

    feat(moneySlot): 투입 금액이 올바른 입력인지 검증하는 기능 추가

--- src/main/java/vendingmachine/MoneySlot.java
+++ src/main/java/vendingmachine/MoneySlot.java
@@ -1,9 +1,41 @@
 package vendingmachine;
 
 public class MoneySlot {
+	private static final int MINIMUM_DIVISIBLE_NUMBER = 10;
+	private static final int ZERO = 0;
+	private static final String NOT_INTEGER = "자판기에 투입하는 금액은 숫자만 입력이 가능합니다.";
+	private static final String NOT_POSITIVE = "자판기에 투입하는 금액은 0보다 커야합니다.";
+	private static final String NOT_DIVISIBLE = "자판기에 투입하는 금액은 10원으로 나누어떨어져야 합니다.";
+
 	private int money;
 
 	public void insert(String money) {
-		this.money = Integer.parseInt(money);
+		validate(money);
+	}
+
+	private void validate(String money) {
+		validateInteger(money);
+		validatePositive();
+		validateDivisible();
+	}
+
+	private void validateInteger(String money) {
+		try {
+			this.money = Integer.parseInt(money);
+		} catch (IllegalArgumentException illegalArgumentException) {
+			throw new IllegalArgumentException(NOT_INTEGER);
+		}
+	}
+
+	private void validatePositive() {
+		if (money <= ZERO) {
+			throw new IllegalArgumentException(NOT_POSITIVE);
+		}
+	}
+
+	private void validateDivisible() {
+		if (Math.floorMod(money, MINIMUM_DIVISIBLE_NUMBER) != ZERO) {
+			throw new IllegalArgumentException(NOT_DIVISIBLE);
+		}
 	}
 }
