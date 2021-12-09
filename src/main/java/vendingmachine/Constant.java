9468aac8d53c78b0dee57a9bee11514c85f37472
wannagohome <iwwuf7@gmail.com>
2021-12-09 23:25:21 +0900

    style: change names
     - vending machine hold -> machine money

--- src/main/java/vendingmachine/Constant.java
+++ src/main/java/vendingmachine/Constant.java
@@ -1,8 +1,8 @@
 package vendingmachine;
 
 public class Constant {
-    public static final String VENDING_MACHINE_HOLD_REQUEST_STRING = "자판기가 보유하고 있는 금액을 입력해 주세요.";
-    public static final String VENDING_MACHINE_INPUT_IS_NOT_NUMBER_ERROR_STRING = "[ERROR] 금액은 숫자여야 합니다.";
-    public static final String VENDING_MACHINE_INPUT_IS_NOT_POSITIVE_NUMBER_ERROR_STRING = "[ERROR] 금액은 양수이어야 합니다.";
-    public static final String VENDING_MACHINE_INPUT_IS_NOT_MULTIPLE_OF_10 = "[ERROR] 금액은 10의 배수이어야 합니다.";
+    public static final String MACHINE_MONEY_INPUT_REQUEST_STRING = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    public static final String MACHINE_MONEY_INPUT_IS_NOT_NUMBER_ERROR_STRING = "[ERROR] 금액은 숫자여야 합니다.";
+    public static final String MACHINE_MONEY_INPUT_IS_NOT_POSITIVE_NUMBER_ERROR_STRING = "[ERROR] 금액은 양수이어야 합니다.";
+    public static final String MACHINE_MONEY_INPUT_IS_NOT_MULTIPLE_OF_10 = "[ERROR] 금액은 10의 배수이어야 합니다.";
 }
--- src/main/java/vendingmachine/Input.java
+++ src/main/java/vendingmachine/Input.java
@@ -3,47 +3,47 @@ package vendingmachine;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
-    public int requestVendingMachineHold() {
-        System.out.println(Constant.VENDING_MACHINE_HOLD_REQUEST_STRING);
-        return readVendingMachineHold();
+    public int requestMachineMoney() {
+        System.out.println(Constant.MACHINE_MONEY_INPUT_REQUEST_STRING);
+        return readMachineMoney();
     }
 
-    private int readVendingMachineHold() {
+    private int readMachineMoney() {
         String input = Console.readLine();
 
         try {
-            validateVendingMachineInput(input);
+            validateMachineMoneyInput(input);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            return readVendingMachineHold();
+            return readMachineMoney();
         }
         return Integer.parseInt(input);
     }
 
-    private void validateVendingMachineInput(String input) throws IllegalArgumentException {
-        validateVendingMachineInputIsPositiveNumber(input);
-        validateVendingMachineInputIsMultipleOf10(input);
+    private void validateMachineMoneyInput(String input) throws IllegalArgumentException {
+        validateMachineMoneyInputIsPositiveNumber(input);
+        validateMachineMoneyInputIsMultipleOf10(input);
     }
 
     private void validateVendingMachineAmountIsNumber(String input) throws IllegalArgumentException {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(Constant.VENDING_MACHINE_INPUT_IS_NOT_NUMBER_ERROR_STRING);
+            throw new IllegalArgumentException(Constant.MACHINE_MONEY_INPUT_IS_NOT_NUMBER_ERROR_STRING);
         }
     }
 
-    private void validateVendingMachineInputIsPositiveNumber(String input) throws IllegalArgumentException {
+    private void validateMachineMoneyInputIsPositiveNumber(String input) throws IllegalArgumentException {
         validateVendingMachineAmountIsNumber(input);
         if (Integer.parseInt(input) < 0) {
-            throw new IllegalArgumentException(Constant.VENDING_MACHINE_INPUT_IS_NOT_POSITIVE_NUMBER_ERROR_STRING);
+            throw new IllegalArgumentException(Constant.MACHINE_MONEY_INPUT_IS_NOT_POSITIVE_NUMBER_ERROR_STRING);
         }
     }
 
-    private void validateVendingMachineInputIsMultipleOf10(String input) throws IllegalArgumentException {
+    private void validateMachineMoneyInputIsMultipleOf10(String input) throws IllegalArgumentException {
         validateVendingMachineAmountIsNumber(input);
         if (Integer.parseInt(input) % 10 != 0) {
-            throw new IllegalArgumentException(Constant.VENDING_MACHINE_INPUT_IS_NOT_MULTIPLE_OF_10);
+            throw new IllegalArgumentException(Constant.MACHINE_MONEY_INPUT_IS_NOT_MULTIPLE_OF_10);
         }
     }
 }
