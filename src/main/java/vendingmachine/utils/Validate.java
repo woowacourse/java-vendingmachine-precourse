82fe3e09ed1a7c2cec88bf0a37d38d2892fdd272
jochungbeom <cndqjacndqja@gmail.com>
2021-12-09 21:27:35 +0900

    feat: [ERROR] 음수일 때, 에러 출력 후 다시 입력

--- src/main/java/vendingmachine/utils/Validate.java
+++ src/main/java/vendingmachine/utils/Validate.java
@@ -4,10 +4,21 @@ import static vendingmachine.utils.Constant.*;
 
 public class Validate {
 	public static void validateInputHoldingAmountMoney(String input) {
+		validateInputStringToInteger(input);
+		validateIsNotNegative(input);
+	}
+
+	private static void validateInputStringToInteger(String input) {
 		try {
 			Integer.parseInt(input);
 		} catch (Exception exception) {
 			throw new IllegalArgumentException(ERROR_MESSAGE + "금액은 숫자여야 합니다.");
+		}
+	}
+
+	private static void validateIsNotNegative(String input) {
+		if (Integer.parseInt(input) < 0) {
+			throw new IllegalArgumentException(ERROR_MESSAGE + "금액은 음수일 수 없습니다.");
 		}
 	}
 }
