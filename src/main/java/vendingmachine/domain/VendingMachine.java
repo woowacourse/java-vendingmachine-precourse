2ad5564072baa153283a61ffc1006032eb9bd488
Seungmin Lim <seungmin9066@gmail.com>
2021-12-09 12:13:11 +0900

    [추가] 자판기에 상품넣는 메서드 추가

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -1,2 +1,22 @@
-package vendingmachine.domain;public class VendingMachine {
+package vendingmachine.domain;
+
+import static vendingmachine.constant.Constant.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class VendingMachine {
+
+	private final HashMap<String, Object> productsPriceAndStocks = new HashMap<>();
+
+	public void addProduct(String products) {
+		for (String productPriceAndStock : products.split(DISTINGUISH_BETWEEN_PRODUCTS)) {
+			String[] product = productPriceAndStock.split(DISTINGUISH_BETWEEN_PRODUCT_INFORMATION);
+			// validateProduct(product);
+			productsPriceAndStocks.put(product[NAME],
+				new ArrayList<>(Arrays.asList(Integer.parseInt(product[PRICE]), Integer.parseInt(product[STOCKS]))));
+		}
+
+	}
 }
