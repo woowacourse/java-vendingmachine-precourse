3e7210f298c1165c729cbb9aae42e1d2d0a1d327
steven <seokju73@gmail.com>
2021-12-09 16:57:08 +0900

    Feat: Add input methods in view section

---
+++ src/main/java/vendingmachine/view/InputView.java
@@ -0,0 +1,27 @@
+package vendingmachine.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    
+    public InputView() {}
+    
+    public String depositAmout() {
+        return Console.readLine();
+    }
+    
+    public String commodityInfo() {
+        return Console.readLine();
+    }
+    
+    public String insertMoney() {
+        return Console.readLine();
+    }
+    
+    public String selectCommodity() {
+        return Console.readLine();
+    }
+    
+    
+    
+}
