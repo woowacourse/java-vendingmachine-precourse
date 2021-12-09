97196d31593b8fc6e77433e9a2daafd2fe43c518
TaeHyeon Kim <kth990303@naver.com>
2021-12-09 20:34:49 +0900

    feat: 잔돈 반환 메소드 구현

--- src/main/java/vendingmachine/coin/CoinController.java
+++ src/main/java/vendingmachine/coin/CoinController.java
@@ -1,6 +1,8 @@
 package vendingmachine.coin;
 
 
+import java.util.List;
+
 public class CoinController {
     private final CoinService coinService;
 
@@ -21,7 +23,15 @@ public class CoinController {
         return amount;
     }
 
+    public List<Integer> repayCoins(int balance) {
+        return coinService.repayCoinsByBalance(balance);
+    }
+
     public void initCoinsPrint() {
         CoinOutputView.initCoinsPrintInfo();
+    }
+
+    public void repayCoinsPrint(int balance) {
+        CoinOutputView.repayCoinsPrintInfo(repayCoins(balance));
     }
 }
