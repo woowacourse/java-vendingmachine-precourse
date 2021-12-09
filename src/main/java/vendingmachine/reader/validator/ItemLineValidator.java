0d91fe4a227401dbcff3d3b1fef2123491d119eb
wilgur513 <wilgur513@naver.com>
2021-12-09 22:39:23 +0900

    feat: validate item price is over 100 and ten times number

--- src/main/java/vendingmachine/reader/validator/ItemLineValidator.java
+++ src/main/java/vendingmachine/reader/validator/ItemLineValidator.java
@@ -4,7 +4,10 @@ import java.util.List;
 import vendingmachine.reader.ItemLineParser;
 
 public class ItemLineValidator implements Validator {
+	private static final int BOUNDARY_VALUE = 100;
 	private final NumberFormatValidator numberFormatValidator = new NumberFormatValidator();
+	private final TenTimesNumberValidator tenTimesNumberValidator = new TenTimesNumberValidator();
+	private final OverBoundaryValidator overBoundaryValidator = new OverBoundaryValidator(BOUNDARY_VALUE);
 	private final ItemLineParser parser;
 
 	public ItemLineValidator(ItemLineParser parser) {
@@ -14,15 +17,35 @@ public class ItemLineValidator implements Validator {
 	@Override
 	public boolean validate(String value) {
 		List<String[]> itemInfo = parser.parse(value);
-		return itemInfo.stream().allMatch(this::isPriceAndQuantityNumberFormat);
+		return isPriceAndQuantityNumberFormat(itemInfo) && isPriceOverBoundary(itemInfo) && isPriceTenTimesNumber(itemInfo);
+	}
+
+	private boolean isPriceTenTimesNumber(List<String[]> itemInfo) {
+		return itemInfo.stream().allMatch(this::isEachPriceTenTimesNumber);
+	}
+
+	private boolean isPriceAndQuantityNumberFormat(List<String[]> itemInfo) {
+		return itemInfo.stream().allMatch(this::isEachPriceAndQuantityNumberFormat);
+	}
+
+	private boolean isPriceOverBoundary(List<String[]> itemInfo) {
+		return itemInfo.stream().allMatch(this::isEachPriceOverBoundary);
+	}
+
+	private boolean isEachPriceAndQuantityNumberFormat(String[] item) {
+		return numberFormatValidator.validate(item[1]) && numberFormatValidator.validate(item[2]);
+	}
+
+	private boolean isEachPriceTenTimesNumber(String[] item) {
+		return tenTimesNumberValidator.validate(item[1]);
+	}
+
+	private boolean isEachPriceOverBoundary(String[] item) {
+		return overBoundaryValidator.validate(item[1]);
 	}
 
 	@Override
 	public String getErrorMessage(String value, String inputValueName) {
 		return "[ERROR] 상품명, 가격, 수량 입력 포맷이 잘못됐습니다.";
-	}
-
-	private boolean isPriceAndQuantityNumberFormat(String[] item) {
-		return numberFormatValidator.validate(item[1]) && numberFormatValidator.validate(item[2]);
 	}
 }
---
+++ src/main/java/vendingmachine/reader/validator/OverBoundaryValidator.java
@@ -0,0 +1,19 @@
+package vendingmachine.reader.validator;
+
+public class OverBoundaryValidator implements Validator{
+	private final int boundaryValue;
+
+	public OverBoundaryValidator(int boundaryValue) {
+		this.boundaryValue = boundaryValue;
+	}
+
+	@Override
+	public boolean validate(String value) {
+		return Integer.valueOf(value) >= boundaryValue;
+	}
+
+	@Override
+	public String getErrorMessage(String value, String inputValueName) {
+		return "[ERROR] " + inputValueName + "은 " + boundaryValue + "를 넘어야 합니다.";
+	}
+}
