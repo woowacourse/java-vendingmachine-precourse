6919a054ddb6c7c16ec7e5761e5360a32230746f
wannagohome <iwwuf7@gmail.com>
2021-12-09 19:29:59 +0900

    feat: handle vending machine input is not number exception

--- src/main/java/vendingmachine/Input.java
+++ src/main/java/vendingmachine/Input.java
@@ -5,8 +5,17 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
     public int requestVendingMachineHoldingAmount() {
         System.out.println(Constant.VENDING_MACHINE_HOLING_AMOUNT_REQUEST_STRING);
+        return readVendingMachineHoldingAmount();
+    }
+
+    private int readVendingMachineHoldingAmount() {
         String input = Console.readLine();
-        validateVendingMachineAmountIsNumber(input);
+        try {
+            validateVendingMachineAmountIsNumber(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(Constant.VENDING_MACHINE_REQUEST_IS_NOT_NUMBER_ERROR_STRING);
+            return readVendingMachineHoldingAmount();
+        }
         return Integer.parseInt(input);
     }
 
