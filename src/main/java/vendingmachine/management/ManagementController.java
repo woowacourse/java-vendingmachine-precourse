75ffdb42e1cfae017a766607cc4e76026ba458b4
steven <seokju73@gmail.com>
2021-12-09 18:10:31 +0900

    Feat: Add validating function for deposit in controller

---
+++ src/main/java/vendingmachine/management/ManagementController.java
@@ -0,0 +1,28 @@
+package vendingmachine.management;
+
+import vendingmachine.management.validation.CheckDepositFigure;
+import vendingmachine.view.InputView;
+import vendingmachine.view.outputView.ManagementView;
+
+public class ManagementController {
+    
+    public static void runManagement(InputView inputView) {
+    
+        ManagementView.askDepositAmout();
+        int deposit = doValidation(inputView.depositAmout());
+        
+       
+    }
+    private static int doValidation(String input) {
+        int n = 0;
+        
+        try {
+            n = CheckDepositFigure.validationFigure(input);
+            CheckDepositFigure.validationPositiveNumber(n);
+            } catch(Exception e) {
+                System.out.println(e.getMessage());
+        }
+        
+        return n;
+    }
+}
