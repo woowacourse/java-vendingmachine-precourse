e67cccf4cb2b2f4e92792e7207d560f82c5eb206
iamtaehoon <kimth9981@naver.com>
2021-12-09 16:55:11 +0900

    test(VendingMachine): 입력 금액이 0원 이상이면 되도록 조건 변경

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -1,6 +1,9 @@
 package vendingmachine.domain;
 
 public class VendingMachine {
+    public void putInitialAmount(int inputMoney) {
+
+    }
     //금액을 입력받는다
     //금액이 100원 이상 10으로 나눠떨어지는거여야 한다.
 }
--- src/test/java/vendingmachine/domain/VendingMachineTest.java
+++ src/test/java/vendingmachine/domain/VendingMachineTest.java
@@ -11,8 +11,8 @@ import org.junit.jupiter.params.provider.ValueSource;
 class VendingMachineTest {
 
     @ParameterizedTest
-    @DisplayName("10원 이상 10으로 나눠떨어지는 정상적인 금액을 입력받는다.")
-    @ValueSource(ints = {10, 100, 180, 20, 2000, 10000, 40000})
+    @DisplayName("10으로 나눠떨어지는 정상적인 금액을 입력받는다.")
+    @ValueSource(ints = {0, 10, 100, 180, 20, 2000, 10000, 40000})
     void 자판기_초기_금액_입력_정상(int inputMoney) {
         VendingMachine vm = new VendingMachine();
         vm.putInitialAmount(inputMoney);
@@ -20,8 +20,8 @@ class VendingMachineTest {
 
     @ParameterizedTest
     @DisplayName("초기 금액으로 10원 밑으로 입력받으면 에러를 반환한다.")
-    @ValueSource(ints = {9, 6, 0, -1, -10, -100, -87, -240, -900, -10000})
-    void 자판기_초기_금액_입력_동전_최소보다_적은_경우_오류() {
+    @ValueSource(ints = {-1, -10, -100, -87, -240, -900, -10000})
+    void 자판기_초기_금액_입력_0보다_적은_경우_오류() {
         VendingMachine vm = new VendingMachine();
         Assertions.assertThatThrownBy(() -> vm.putInitialAmount(inputMoney)).isInstanceOf(IllegalArgumentException.class); //TODO: 문구는 나중에 생성
     }
