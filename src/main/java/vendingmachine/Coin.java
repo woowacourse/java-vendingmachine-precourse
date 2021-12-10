24ca257982853f78f495d0dc1345f9aade61d2af
Leeyerimearth <50127628+Leeyerimearth@users.noreply.github.com>
2021-12-10 23:08:44 +0900

    feat: 보유한 동전을 랜덤으로 생성하는 기능

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,16 +1,32 @@
 package vendingmachine;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public enum Coin {
-    COIN_500(500),
-    COIN_100(100),
-    COIN_50(50),
-    COIN_10(10);
+    COIN_500(500,0),
+    COIN_100(100, 0),
+    COIN_50(50, 0),
+    COIN_10(10, 0);
 
     private final int amount;
+    private int count;
 
-    Coin(final int amount) {
+    Coin(final int amount, int count) {
         this.amount = amount;
+        this.count = count;
     }
 
     // 추가 기능 구현
+    public void makeRandom(int money) {
+        for (Coin coin : Coin.values()) {
+            int endNumber = money / coin.amount;
+            if (endNumber != 0 && coin.amount != 10) {
+                coin.count = Randoms.pickNumberInRange(0,endNumber);
+                money -= (coin.count * coin.amount);
+            }
+            if(coin.amount == 10) {
+                coin.count = money / coin.amount;
+            }
+        }
+    }
 }
