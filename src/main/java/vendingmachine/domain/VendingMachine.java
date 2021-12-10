056c4112cc80d0bf5d955ab677316bea6899e0f7
Donggyu Kim <a29661498@gmail.com>
2021-12-10 19:24:24 +0900

    feat: VendingMachine 도메인 정의
    * 자판기에 해당하는 VendingMachine 도메인을 정의하였습니다.

---
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -0,0 +1,38 @@
+package vendingmachine.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VendingMachine {
+
+    private Change change;
+    private Money money;
+    private List<Product> productList = new ArrayList<>();
+
+    public VendingMachine() {
+    }
+
+    public void inputMoeny(Money money) {
+        this.money = money;
+    }
+
+    public void createChange() {
+        //랜덤하게 생성하는 코드 필요
+    }
+
+    public void inputProducts(List<Product> productList) {
+        this.productList = productList;
+    }
+
+    public Change getChange() {
+        return change;
+    }
+
+    public Money getMoney() {
+        return money;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+}
