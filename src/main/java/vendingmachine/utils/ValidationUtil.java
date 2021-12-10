c8e6c4e0f683d37872501403787b254194471449
yoon <duddbs774@gmail.com>
2021-12-10 20:18:13 +0900

    refactor: 매직넘버 및 오류메세지 상수화

--- src/main/java/vendingmachine/utils/ValidationUtil.java
+++ src/main/java/vendingmachine/utils/ValidationUtil.java
@@ -1,34 +1,36 @@
 package vendingmachine.utils;
 
 public class ValidationUtil {
+    public static final int MULTIPLE_STANDARD_NUMBER = 10;
+    public static final int MULTIPLE_INVALID_VALUE = 0;
+    public static final int MULTIPLE_REMAINDER = 0;
+    public static final String ERROR_PREFIX = "[ERROR] ";
+    public static final String ERROR_EMPTY_MESSAGE = "값을 입력해주세요.";
+    public static final String ERROR_MULTIPLE_MESSAGE = MULTIPLE_STANDARD_NUMBER + "의 단위로 입력주세요.";
+    public static final String ERROR_DIGIT_MESSAGE = "숫자를 입력주세요.";
+
     public static void checkAmount(String value) {
+        checkIsEmpty(value);
         checkIsDigit(value);
         checkIsMultipleOfStandard(value);
     }
 
+    public static void checkIsEmpty(String value) {
+        if (value.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_PREFIX + ERROR_EMPTY_MESSAGE);
+        }
+    }
+
     public static void checkIsMultipleOfStandard(String value) {
         int number = Integer.parseInt(value);
-        if (number == 0 || number % 10 != 0) {
-            throw new IllegalArgumentException("[ERROR] 10의 단위로 입력주세요.");
+        if (number == MULTIPLE_INVALID_VALUE || number % MULTIPLE_STANDARD_NUMBER != MULTIPLE_REMAINDER) {
+            throw new IllegalArgumentException(ERROR_PREFIX + ERROR_MULTIPLE_MESSAGE);
         }
     }
 
     public static void checkIsDigit(String value) {
         if (!value.chars().allMatch(Character::isDigit)) {
-            throw new IllegalArgumentException("[ERROR] 숫자를 입력주세요.");
+            throw new IllegalArgumentException(ERROR_PREFIX + ERROR_DIGIT_MESSAGE);
         }
     }
-
-    // public static void checkProductContent(String value) {
-    //     checkIsEmptyProductContent(value);
-    //     FormatUtil.convertToProductsElement(value).forEach(elements -> {
-    //         if(elements[0])
-    //     });
-    // }
-    //
-    // private static void checkIsEmptyProductContent(String value) {
-    //     if(value.isEmpty()) {
-    //         throw new IllegalArgumentException("[ERROR] 값을 입력해주세요.");
-    //     }
-    // }
 }
