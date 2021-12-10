05ad5aab72f61ac9a4a54d95a3c5d3b3df2ae545
iamtaehoon <kimth9981@naver.com>
2021-12-10 14:23:20 +0900

    chore(ProductGenerator): 메서드 작은 단위로 분리

--- src/main/java/vendingmachine/utils/ProductValidator.java
+++ src/main/java/vendingmachine/utils/ProductValidator.java
@@ -1,6 +1,7 @@
 package vendingmachine.utils;
 
 import static vendingmachine.Constant.*;
+import static vendingmachine.utils.StringUtil.*;
 
 import java.util.ArrayList;
 
@@ -10,23 +11,38 @@ import vendingmachine.domain.Quantity;
 
 public class ProductValidator {
     public static Product validateForm(String productInfoInput) {
-        if (isEmpty(productInfoInput)) {
-            throw new IllegalArgumentException("공백은 입력될 수 없습니다.");
-        }
-        StringBuffer sb = new StringBuffer(productInfoInput);
-        if (!((sb.charAt(0) == '[') && (sb.charAt(sb.length() - 1) == ']'))) {
-            throw new IllegalArgumentException("상품의 상태는 []로 감싸져야 합니다.");
-        }
-        sb.deleteCharAt(sb.length() - 1);
-        sb.deleteCharAt(0);
+        validateInputIsEmpty(productInfoInput);
+        productInfoInput = removeBracket(productInfoInput);
+        Product product = validateProductInfo(productInfoInput);
+        return product;
+    }
 
-        // split 후 안에 세 개의 문자가 있는지 확인한다.
-        productInfoInput = sb.toString();
+    private static Product validateProductInfo(String productInfoInput) {
         String[] split = productInfoInput.split(",", -1);
         String name = validateProductName(split);
         Price price = new Price(split[1]);
         Quantity quantity = new Quantity(split[2]);
         return Product.registerProduct(name, price, quantity);
+    }
+
+    private static String removeBracket(String productInfoInput) {
+        StringBuffer temp = new StringBuffer(productInfoInput);
+        validateInputWrapBracket(temp);
+        temp.deleteCharAt(temp.length() - 1);
+        temp.deleteCharAt(0);
+        return temp.toString();
+    }
+
+    private static void validateInputWrapBracket(StringBuffer sb) {
+        if (!((sb.charAt(0) == '[') && (sb.charAt(sb.length() - 1) == ']'))) {
+            throw new IllegalArgumentException("상품의 상태는 []로 감싸져야 합니다.");
+        }
+    }
+
+    private static void validateInputIsEmpty(String productInfoInput) {
+        if (isEmpty(productInfoInput)) {
+            throw new IllegalArgumentException("공백은 입력될 수 없습니다.");
+        }
     }
 
     private static String validateProductName(String[] split) {
@@ -37,9 +53,5 @@ public class ProductValidator {
             throw new IllegalArgumentException("이름이 제대로 입력되지 않은 상품이 있습니다.");
         }
         return split[0];
-    }
-
-    private static boolean isEmpty(String name) {
-        return name == null || name.trim().isEmpty();
     }
 }
--- src/main/java/vendingmachine/utils/StringUtil.java
+++ src/main/java/vendingmachine/utils/StringUtil.java
@@ -8,6 +8,10 @@ import java.util.List;
 
 public class StringUtil {
 
+    public static boolean isEmpty(String name) {
+        return name == null || name.trim().isEmpty();
+    }
+
     public static int parseStringToInt(String inputInitialAmount) {
         if (inputInitialAmount.matches(NUMBER_REGEX)) {
             return Integer.parseInt(inputInitialAmount);
