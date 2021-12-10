36e3a49ebd7cc606eb807a7f82975bf3787e32cc
kanamycine <jinyeong5320@gmail.com>
2021-12-10 15:47:15 +0900

    feat(Message) : 메세지 utils 클래스 생성

---
+++ src/main/java/vendingmachine/utils/Message.java
@@ -0,0 +1,6 @@
+package vendingmachine.utils;
+
+public class Message {
+	public static final String  NON_NUMERIC_HOLDING_MONEY_ERROR = "[ERROR] 보유 금액은 양의 정수를 입력하셔야 합니다.";
+	public static final String  ZERO_HOLDING_MONEY_ERROR = "[ERROR] 보유 금액은 0이 아닌 양의 정수를 입력하셔야 합니다.";
+}
