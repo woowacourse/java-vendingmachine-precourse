0d506d3f0160a4b5dd1b369868eb392f038d4be8
Seungmin Lim <seungmin9066@gmail.com>
2021-12-10 14:44:22 +0900

    [추가] 컨트롤러 생성자 추가

--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -1,4 +1,15 @@
 package vendingmachine.controller;
 
+import vendingmachine.domain.VendingMachine;
+import vendingmachine.view.InputView;
+import vendingmachine.view.OutputView;
+
 public class VendingMachineController {
+
+	private final VendingMachine vendingMachine;
+
+	public VendingMachineController() {
+		OutputView.setVendingMachineMoney();
+		vendingMachine = new VendingMachine(InputView.readLineInt());
+	}
 }
