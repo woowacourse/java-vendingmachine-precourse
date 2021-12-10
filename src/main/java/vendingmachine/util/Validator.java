555aff96ec199f57134cc4f9173b5a85101617ce
Seungmin Lim <seungmin9066@gmail.com>
2021-12-10 14:24:03 +0900

    [추가] 숫자인지 검사하는 validator 추가

--- src/main/java/vendingmachine/util/Validator.java
+++ src/main/java/vendingmachine/util/Validator.java
@@ -1,2 +1,13 @@
-package vendingmachine.util;public class Validator {
+package vendingmachine.util;
+
+import static vendingmachine.constant.Constant.*;
+
+public class Validator {
+
+	public void validateNumber(String number) {
+		boolean isNumeric = number.matches(REGEX_EXPRESSION_OF_NUMBER);
+		if (!isNumeric) {
+			throw new IllegalArgumentException(VALIDATE_NUMBER_MESSAGE);
+		}
+	}
 }
