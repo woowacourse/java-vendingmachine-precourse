16ecf90c39df248aea2441cd806260261873a05d
steven <seokju73@gmail.com>
2021-12-10 14:15:41 +0900

    Feat: Add 'findByAmount' method in Coin enum class

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,5 +1,7 @@
 package vendingmachine;
 
+import java.util.Arrays;
+
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
@@ -7,10 +9,29 @@ public enum Coin {
     COIN_10(10);
 
     private final int amount;
+    private int number = 0;
 
     Coin(final int amount) {
-        this.amount = amount;
+        this.amount = amount; 
+    }
+    
+    public String getKey() {
+        return name();
+    }
+    
+    public int getAmount() {
+        return amount;
+    }
+    
+    public int getNumber() {
+        return number;
+    }
+    
+    public static Coin findByAmount(int number) {
+        return Arrays.stream(values())
+             .filter(c -> c.amount == number)
+             .findAny()
+             .orElse(null);
     }
 
-    // 추가 기능 구현
 }
