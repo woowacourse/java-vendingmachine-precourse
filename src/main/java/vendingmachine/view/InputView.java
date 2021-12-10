97be52f9907a6f5d159816f9c236cb91be5a6bf2
Seungmin Lim <seungmin9066@gmail.com>
2021-12-10 15:08:33 +0900

    [추가] 숫자를 입력받는 메서드 추가

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -2,9 +2,19 @@ package vendingmachine.view;
 
 import static camp.nextstep.edu.missionutils.Console.*;
 
+import vendingmachine.util.Validator;
+
 public class InputView {
+
+	static Validator validator = new Validator();
 
 	public static String readLineString() {
 		return readLine();
+	}
+
+	public static int readLineInt() {
+		String input = readLine();
+		validator.validateNumber(input);
+		return Integer.parseInt(input);
 	}
 }
