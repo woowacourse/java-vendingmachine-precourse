f6b1994459fd424c2af87898a5e370b9d84e902b
steven <seokju73@gmail.com>
2021-12-10 14:13:30 +0900

    Feat: Add function to split deposit into coins in management service class

---
+++ src/main/java/vendingmachine/management/ManagementService.java
@@ -0,0 +1,26 @@
+package vendingmachine.management;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import vendingmachine.Coin;
+
+public class ManagementService {
+
+    public static void generateCoins(int deposit) {
+        List<Integer> coinAmounts = Stream.of(Coin.values()).map(a -> a.getAmount()).collect(Collectors.toList());
+        
+        while(deposit >= 0) {    
+            int tmp = Randoms.pickNumberInList(coinAmounts);
+            
+            if(deposit - tmp < 0) {
+                continue;
+            }
+            
+            deposit -= tmp;
+       
+        }
+    }
+}
