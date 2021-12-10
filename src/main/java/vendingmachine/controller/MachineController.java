7d3308e4007f6898184cac8013826cd652e34e25
Gyuchool <balance0591@naver.com>
2021-12-10 18:09:03 +0900

    feat(inputView): 자판기가 보유하고 있는 금액 입력받는 기능 구현

---
+++ src/main/java/vendingmachine/controller/MachineController.java
@@ -0,0 +1,10 @@
+package vendingmachine.controller;
+
+import vendingmachine.view.InputView;
+
+public class MachineController {
+
+	public void run() {
+		InputView.getHavingMoney();
+	}
+}
---
+++ src/main/java/vendingmachine/view/InputView.java
@@ -0,0 +1,12 @@
+package vendingmachine.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+	private static final String INPUT_HAVING_MONEY = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+
+	public static String getHavingMoney() {
+		System.out.println(INPUT_HAVING_MONEY);
+		return Console.readLine();
+	}
+}
