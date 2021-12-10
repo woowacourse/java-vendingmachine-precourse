de40bc72d7d8c2d7dabfe862ebcd34597e64d859
Inhyeok Jo <alzpqm@naver.com>
2021-12-10 18:19:16 +0900

    feat(Coin.java): 동전의 금액으로 동전상수를 얻을 수 있게 getCoinFromAmount메소드 작성

--- src/main/java/vendingmachine/resource/Coin.java
+++ src/main/java/vendingmachine/resource/Coin.java
@@ -4,7 +4,9 @@ public enum Coin {
 	COIN_500(500),
 	COIN_100(100),
 	COIN_50(50),
-	COIN_10(10);
+	COIN_10(10),
+	COIN_ERROR(0);
+
 
 	private final int amount;
 
@@ -13,4 +15,19 @@ public enum Coin {
 	}
 
 	// 추가 기능 구현
+	public static Coin getCoinFromAmount(int amount) {
+		if (amount == 500) {
+			return COIN_500;
+		}
+		if (amount == 100) {
+			return COIN_100;
+		}
+		if (amount == 50) {
+			return COIN_50;
+		}
+		if (amount == 10) {
+			return COIN_10;
+		}
+		return COIN_ERROR;
+	}
 }
