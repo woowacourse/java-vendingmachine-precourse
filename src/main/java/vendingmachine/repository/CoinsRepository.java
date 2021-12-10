7bf189ca235c62f10083fb156fa36b33fc65a933
devHudi <devhudi@gmail.com>
2021-12-10 13:44:28 +0900

    refactor: 변수명을 instance 에서 coinsRepository 로 변경

--- src/main/java/vendingmachine/repository/CoinsRepository.java
+++ src/main/java/vendingmachine/repository/CoinsRepository.java
@@ -3,14 +3,14 @@ package vendingmachine.repository;
 import vendingmachine.domain.coins.Coins;
 
 public class CoinsRepository {
-	private static CoinsRepository instance = new CoinsRepository();
+	private static CoinsRepository coinsRepository = new CoinsRepository();
 	private Coins coins = Coins.create();
 
 	private CoinsRepository() {
 	}
 
 	public static CoinsRepository getInstance() {
-		return instance;
+		return coinsRepository;
 	}
 
 	public void setCoins(Coins coins) {
