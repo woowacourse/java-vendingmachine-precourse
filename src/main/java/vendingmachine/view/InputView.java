4e9bbca1ebd8d5057401785048fcb5edd84d210e
pup-paw <wldusdl0310@gmail.com>
2021-12-10 19:07:07 +0900

    feat: 상품 받아오기, 상품 정보 분리하기

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -18,7 +18,39 @@ public class InputView {
 		ExceptionController.isMultipleOfTen(holdingAmount);
 	}
 
+	public static void askProductInfo() {
+		System.out.println(Constant.ASK_PRODUCT_INFO);
+		String[] products = splitProducts(Console.readLine());
+		checkProducts(products);
+		String[][] information = checkInfoFormat(products);
+	}
 
 
+	private static void checkProducts(String[] products) {
+		for (int i = 0; i < products.length; i++) {
+			ExceptionController.isInfoFormatValidate(products[i]);
+			products[i] = removeParentheses(products[i]);
+		}
+	}
 
+	private static String[][] checkInfoFormat(String[] products) {
+		String[][] information = new String[products.length][3];
+		for (int i = 0; i < products.length; i++) {
+			information[i] = splitInfo(products[i]);
+			ExceptionController.isNumberOfInfo3(information[i]);
+		}
+		return information;
+	}
+
+	private static String[] splitProducts(String input) {
+		return input.split(";");
+	}
+
+	private static String removeParentheses(String input) {
+		return input.substring(1, input.length() - 1);
+	}
+
+	private static String[] splitInfo(String input) {
+		return input.split(",");
+	}
 }
