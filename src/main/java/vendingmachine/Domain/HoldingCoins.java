c82fe006407467a9adf11a0783489f2441d5fc2d
= <ulim130@naver.com>
2021-12-10 21:52:27 +0900

    fix : 자판기 보유 동전 출력시 요구사항과 다른 순서로 출력되는 오류 해결

--- src/main/java/vendingmachine/Domain/HoldingCoins.java
+++ src/main/java/vendingmachine/Domain/HoldingCoins.java
@@ -21,18 +21,7 @@ public class HoldingCoins {
         coins.put(c, coins.get(c) + 1);
     }
 
-    public static String printCoins() {
-        StringBuilder result = new StringBuilder();
-        for (Coin c : coins.keySet()) {
-            result.append(c.printAmount());
-            result.append(OutputConstant.COIN_STANDARD);
-            result.append(printCoinCount(c));
-            result.append("\n");
-        }
-        return result.toString();
-    }
-
-    private static String printCoinCount(Coin c) {
+    public static String printCoinCount(Coin c) {
         return coins.get(c) + OutputConstant.COIN_COUNT;
     }
 }
--- src/main/java/vendingmachine/View/CoinOutputView.java
+++ src/main/java/vendingmachine/View/CoinOutputView.java
@@ -1,12 +1,29 @@
 package vendingmachine.View;
 
 import vendingmachine.Constant.OutputConstant;
+import vendingmachine.Domain.Coin;
 import vendingmachine.Domain.HoldingCoins;
 
 public class CoinOutputView {
 
     public static void printHoldingCoins() {
         System.out.println(OutputConstant.PRINT_COIN);
-        System.out.println(HoldingCoins.printCoins());
+
+        StringBuilder result = new StringBuilder();
+        for (Coin c : Coin.values()) {
+            result.append(printCoin(c));
+        }
+
+        System.out.println(result);
+    }
+
+    private static String printCoin(Coin c) {
+        StringBuilder result = new StringBuilder();
+        result.append(c.printAmount());
+        result.append(OutputConstant.COIN_STANDARD);
+        result.append(HoldingCoins.printCoinCount(c));
+        result.append("\n");
+
+        return result.toString();
     }
 }
