3d0c68d403e56c74f70824b87aed6697782b9d79
Donggyu Kim <a29661498@gmail.com>
2021-12-10 18:17:16 +0900

    feat: Product 도메인 정의
    상품에 해당하는 Product 도메인을 정의하였습니다.

---
+++ src/main/java/vendingmachine/domain/Product.java
@@ -0,0 +1,26 @@
+package vendingmachine.domain;
+
+public class Product {
+
+    private String productName;
+    private int price;
+    private int quantity;
+
+    public Product (String productName, int price, int quantity) {
+        this.productName = productName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
