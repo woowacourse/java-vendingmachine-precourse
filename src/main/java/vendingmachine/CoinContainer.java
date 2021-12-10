fac670c71d545069e8ba5136199c0be06abff9c7
Jihyun <bjih1999@naver.com>
2021-12-11 02:10:58 +0900

    feat(CoinContainer): 입력받은 보유 금액으로 동전을 무작위로 생성
    init 메소드 구현

---
+++ src/main/java/vendingmachine/CoinContainer.java
@@ -0,0 +1,51 @@
+package vendingmachine;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CoinContainer {
+
+    private Map<Coin, Integer> remainingCoin = new HashMap<Coin, Integer>(){{
+        put(Coin.COIN_10, 0);
+        put(Coin.COIN_50, 0);
+        put(Coin.COIN_100, 0);
+        put(Coin.COIN_500, 0);
+    }};
+
+    public CoinContainer() {};
+
+    public void init(int insertedAmount) {
+        int holdingAmount = insertedAmount;
+        Coin pickedCoin;
+        List<Integer> pickableCoinAmountList = Coin.coinAmountList;
+
+        while (holdingAmount > 0) {
+            pickableCoinAmountList = updatePickableCoinAmountList(pickableCoinAmountList, holdingAmount);
+
+            pickedCoin = pickCoin(pickableCoinAmountList);
+            addRemainingCoin(pickedCoin);
+            holdingAmount -= pickedCoin.getAmount();
+        }
+    }
+
+    private List<Integer> updatePickableCoinAmountList(List<Integer> pickableCoinAmountList, int upperBoundAmount) {
+        return pickableCoinAmountList.stream()
+                .filter(amount -> amount <= upperBoundAmount)
+                .collect(Collectors.toList());
+    }
+
+    private Coin pickCoin(List<Integer> pickableCoinAmountList) {
+        int pickedAmount = Randoms.pickNumberInList(pickableCoinAmountList);
+
+        return Coin.of(pickedAmount);
+    }
+
+    private void addRemainingCoin(Coin coin) {
+        int remainingStock = remainingCoin.get(coin);
+        remainingCoin.put(coin, remainingStock + 1);
+    }
+}
