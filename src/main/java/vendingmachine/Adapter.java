f5710a74fa1194f42886e82e5c84e6500c7145c8
yoon <duddbs774@gmail.com>
2021-12-10 20:38:13 +0900

    refactor: run 메서드 기능 분리

--- src/main/java/vendingmachine/Adapter.java
+++ src/main/java/vendingmachine/Adapter.java
@@ -4,18 +4,50 @@ public class Adapter {
     private VendingMachine vendingMachine;
 
     public void run() {
+        mapToVendingMachineAmountInputPage();
+        mapToProductRegistrationPage();
+        mapToUserAmountInputPage();
+        mapToPurchasePage();
+        vendingMachine.getCoins();
+    }
+
+    private void mapToUserAmountInputPage() {
+        String value = View.inputAmount();
+        vendingMachine.createInputAmount(value);
+    }
+
+    private void mapToVendingMachineAmountInputPage() {
         String value = View.putCoinIntoVendingMachine();
         vendingMachine = new VendingMachine(value);
-        value = View.registerProduct();
-        vendingMachine.createProduct(value);
-        value = View.inputAmount();
-        vendingMachine.createInputAmount(value);
+    }
+
+    private void mapToProductRegistrationPage() {
+        String value = View.registerProduct();
+        try {
+            vendingMachine.createProduct(value);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            mapToProductRegistrationPage();
+        }
+    }
+
+    private void mapToPurchasePage() {
         boolean canBuy = vendingMachine.checkUserBalance();
         while (canBuy) {
-            value = View.buyProduct(vendingMachine.getUserBalance());
+            String value = checkValidProductName();
             vendingMachine.sell(value);
             canBuy = vendingMachine.checkUserBalance();
         }
-        vendingMachine.getCoins();
+    }
+
+    private String checkValidProductName() {
+        String value = View.buyProduct(vendingMachine.getUserBalance());
+        try {
+            vendingMachine.checkIsValidProductName(value);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return checkValidProductName();
+        }
+        return value;
     }
 }
