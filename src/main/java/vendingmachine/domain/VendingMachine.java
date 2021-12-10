75508d0052475160b0180effde103fb8bda402cc
grace_goose <choieungi@gm.gist.ac.kr>
2021-12-10 21:10:45 +0900

    feat: 잔돈을 돌려줄 때 현재 보유한 최소 개수의 동전으로 잔돈을 돌려주는 기능 구현

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -8,12 +8,12 @@ public class VendingMachine {
     private static final String ERROR_HEADER = "[ERROR] ";
     private static final String NOT_EXIST_PRODUCT = "구매할 수 있는 상품이 존재하지 않습니다. 자판기에서 구매할 수 있고 존재하는 상품을 입력해주세요. ";
 
-    private final CoinCount coinCount;
+    private final CoinCountMap coinCountMap;
     private final List<Product> products;
     private int inputMoney;
 
-    public VendingMachine(CoinCount coinCount, List<Product> products, int inputMoney) {
-        this.coinCount = coinCount;
+    public VendingMachine(CoinCountMap coinCountMap, List<Product> products, int inputMoney) {
+        this.coinCountMap = coinCountMap;
         this.products = products;
         this.inputMoney = inputMoney;
     }
@@ -25,8 +25,11 @@ public class VendingMachine {
 
     }
 
-    public CoinCount getCoinCount() {
-        return coinCount;
+    public CoinCountMap getLeftoverCash() {
+        if (inputMoney >= coinCountMap.getCoinSum()) {
+            return coinCountMap;
+        }
+        return calculateLeftoverCash(inputMoney);
     }
 
     public int getInputMoney() {
@@ -60,5 +63,30 @@ public class VendingMachine {
         }
         throw new IllegalArgumentException(ERROR_HEADER + NOT_EXIST_PRODUCT);
     }
+
+    private CoinCountMap calculateLeftoverCash(int toReturnCash ) {
+        CoinCountMap leftoverCoinCountMap = new CoinCountMap();
+        Coin[] coinArray = Coin.values();
+        int idx = 0;
+        while (toReturnCash != 0) {
+            Coin coin = coinArray[idx];
+            if (this.coinCountMap.getCoinCount().get(coin) == 0) {
+                idx += 1;
+                continue;
+            }
+            int minCoinCount = findMinCoinCount(idx, coinArray, toReturnCash);
+            toReturnCash -= minCoinCount * coin.getAmount();
+            leftoverCoinCountMap.getCoinCount().replace(coin, leftoverCoinCountMap.getCoinCount().get(coin) + minCoinCount);
+            this.coinCountMap.getCoinCount().replace(coin, this.coinCountMap.getCoinCount().get(coin) - minCoinCount);
+        }
+        return leftoverCoinCountMap;
+    }
+
+    private int findMinCoinCount(int idx, Coin[] coinArray, int toReturnCash) {
+        int coinValue = coinArray[idx].getAmount();
+        return Math.min(toReturnCash / coinValue, this.coinCountMap.getCoinCount().get(coinArray[idx]));
+
+    }
+
 
 }
