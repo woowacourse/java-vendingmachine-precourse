952fcfc36d98b52dcbcf5f4e731df0f927c0ecc3
sun <syoun602@gmail.com>
2021-12-11 03:50:02 +0900

    refactor: combine methods in InputValidator to remove redundancy

--- src/main/java/vendingmachine/util/InputValidator.java
+++ src/main/java/vendingmachine/util/InputValidator.java
@@ -21,13 +21,13 @@ public class InputValidator {
     private static final String ALLOWED_PRODUCT_PRICE_FORMAT = "^[1-9][0-9]*[0]$";
     private static final String ALLOWED_PRODUCT_QUANTITY_FORMAT = "^[1-9][0-9]*$";
     private static final String INVALID_AMOUNT_INPUT_MESSAGE = "금액은 숫자여야 합니다.";
-    private static final String INVALID_PRODUCT_INPUT_MESSAGE = "마지막 문자가 세미콜론(;)일 수 없습니다.";
+    private static final String INVALID_PRODUCT_LIST_MESSAGE = "마지막 문자가 세미콜론(;)일 수 없습니다.";
     private static final String INVALID_PRODUCT_MESSAGE = "각 상품은 [ ]로 감싸야 합니다.";
     private static final String INVALID_PRODUCT_COMPONENT_SIZE_MESSAGE = "각 상품은 [상품명,가격,수량] 세 가지와 세미콜론(;)으로 구분됩니다.";
     private static final String INVALID_EACH_PRODUCT_INPUT_MESSAGE = "각 상품의 마지막 문자가 콤마(,)일 수 없습니다.";
     private static final String INVALID_PRODUCT_NAME = "상품명은 영문, 한글, 또는 숫자로 입력해야 합니다.";
-    private static final String INVALID_PRODUCT_PRICE = "상품 가격은 숫자여야 하고, 10 단위로 나누어 떨어져야 합니다.";
-    private static final String INVALID_PRODUCT_QUANTITY = "상품 수량은 숫자여야 하고, 1개 이상이어야 합니다.";
+    private static final String INVALID_PRODUCT_PRICE = "상품 가격은 10 단위로 나누어 떨어지는 `숫자`여야 합니다.";
+    private static final String INVALID_PRODUCT_QUANTITY = "상품 수량은 1개 이상의 `숫자`여야 합니다.";
     private static final String EMPTY_PRODUCT_NAME = "상품명이 비어있습니다.";
     private static final String EMPTY_PRODUCT_PRICE = "상품 가격이 비어있습니다.";
     private static final String EMPTY_PRODUCT_QUANTITY = "상품 수량이 비어있습니다.";
@@ -45,17 +45,21 @@ public class InputValidator {
     }
 
     private static List<String> validateProductList(String input) {
-        if (input.endsWith(SEMI_COLON)) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + INVALID_PRODUCT_INPUT_MESSAGE + RETRY_MESSAGE);
-        }
-
-        List<String> productList = Arrays.asList(input.split(SEMI_COLON));
+        List<String> productList = validateLastCharacter(input, SEMI_COLON, INVALID_PRODUCT_LIST_MESSAGE);
         if (!productList.stream().allMatch(product ->
                 product.startsWith(OPEN_SQUARE_BRACKET) && product.endsWith(CLOSE_SQUARE_BRACKET))) {
             throw new IllegalArgumentException(ERROR_MESSAGE + INVALID_PRODUCT_MESSAGE + RETRY_MESSAGE);
         }
 
         return productList;
+    }
+
+    private static List<String> validateLastCharacter(String input, String lastCharacter, String givenMessage) {
+        if (input.endsWith(lastCharacter)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + givenMessage + RETRY_MESSAGE);
+        }
+
+        return Arrays.asList(input.split(lastCharacter));
     }
 
     private static List<List<String>> validateEachProduct(List<String> productList) {
@@ -68,56 +72,40 @@ public class InputValidator {
     }
 
     private static List<String> validateProductDetails(String productDetails) {
-        if (productDetails.endsWith(COMMA)) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + INVALID_EACH_PRODUCT_INPUT_MESSAGE + RETRY_MESSAGE);
-        }
-
-        List<String> productDetailList = Arrays.asList(productDetails.split(COMMA));
+        List<String> productDetailList = validateLastCharacter(productDetails, COMMA, INVALID_EACH_PRODUCT_INPUT_MESSAGE);
         if (productDetailList.size() != PRODUCT_DETAIL_SIZE) {
             throw new IllegalArgumentException(ERROR_MESSAGE + INVALID_PRODUCT_COMPONENT_SIZE_MESSAGE + RETRY_MESSAGE);
         }
+        return validateEachDetail(productDetailList);
+    }
 
+    private static List<String> validateEachDetail(List<String> productDetailList) {
         List<String> product = new ArrayList<>();
-        product.add(validateProductName(productDetailList.get(INDEX_ZERO)));
-        product.add(validateProductPrice(productDetailList.get(INDEX_ONE)));
-        product.add(validateProductQuantity(productDetailList.get(INDEX_TWO)));
+        product.add(validateEachDetail(productDetailList.get(INDEX_ZERO), EMPTY_PRODUCT_NAME,
+                ALLOWED_PRODUCT_NAME_FORMAT, INVALID_PRODUCT_NAME));
+        product.add(validateEachDetail(productDetailList.get(INDEX_ONE), EMPTY_PRODUCT_PRICE,
+                ALLOWED_PRODUCT_PRICE_FORMAT, INVALID_PRODUCT_PRICE));
+        product.add(validateEachDetail(productDetailList.get(INDEX_TWO), EMPTY_PRODUCT_QUANTITY,
+                ALLOWED_PRODUCT_QUANTITY_FORMAT, INVALID_PRODUCT_QUANTITY));
 
         return product;
     }
 
-    private static String validateProductName(String name) {
-        if (name.isEmpty()) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + EMPTY_PRODUCT_NAME + RETRY_MESSAGE);
-        }
-
-        if (!name.matches(ALLOWED_PRODUCT_NAME_FORMAT)) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + INVALID_PRODUCT_NAME + RETRY_MESSAGE);
-        }
-
+    private static String validateEachDetail(String name, String emptyMessage, String format, String invalidMessage) {
+        validateEmpty(name, emptyMessage);
+        validateRegex(name, format, invalidMessage);
         return name;
     }
 
-    private static String validateProductPrice(String price) {
-        if (price.isEmpty()) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + EMPTY_PRODUCT_PRICE + RETRY_MESSAGE);
+    public static void validateEmpty(String detail, String givenMessage) {
+        if (detail.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + givenMessage + RETRY_MESSAGE);
         }
-
-        if (!price.matches(ALLOWED_PRODUCT_PRICE_FORMAT)) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + INVALID_PRODUCT_PRICE + RETRY_MESSAGE);
-        }
-
-        return price;
     }
 
-    private static String validateProductQuantity(String quantity) {
-        if (quantity.isEmpty()) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + EMPTY_PRODUCT_QUANTITY + RETRY_MESSAGE);
+    private static void validateRegex(String name, String format, String givenMessage) {
+        if (!name.matches(format)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE + givenMessage + RETRY_MESSAGE);
         }
-
-        if (!quantity.matches(ALLOWED_PRODUCT_QUANTITY_FORMAT)) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + INVALID_PRODUCT_QUANTITY + RETRY_MESSAGE);
-        }
-
-        return quantity;
     }
 }
