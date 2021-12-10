62ebeacb7ce245b96286225b3129fca0639417d3
Donggyu Kim <a29661498@gmail.com>
2021-12-10 19:05:51 +0900

    feat: Change 도메인 정의
    * 잔돈에 대응하는 도메인 초기 정의 (이후 구체적인 로직 구현 필요)

---
+++ src/main/java/vendingmachine/domain/Change.java
@@ -0,0 +1,13 @@
+package vendingmachine.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Change {
+
+    private Map<Coin, Integer> coinMap = new HashMap<>();
+
+    public Change() {
+        //랜덤하게 동전 생성하는 코드 필요
+    }
+}
