85b59d61c96bd4e7f8d2bc2204c9a0f970d6c7e6
steven <seokju73@gmail.com>
2021-12-10 15:38:15 +0900

    Style: Change return type for 'validationFigure' method

--- src/main/java/vendingmachine/management/validation/CheckDepositFigure.java
+++ src/main/java/vendingmachine/management/validation/CheckDepositFigure.java
@@ -4,13 +4,12 @@ public class CheckDepositFigure {
     private static final String ERROR_MESSAGE_FIGURE = "[ERROR] 금액은 숫자여야 합니다.";
     private static final String ERROR_MESSAGE_POSITIVE_NUMBER = "[ERROR] 금액은 0보다 커야 합니다.";
 
-    public static int validationFigure(String input) {
+    public static void validationFigure(String input) {
         try {
             Integer.parseInt(input);
         } catch(NumberFormatException e) {
             throw new IllegalArgumentException(ERROR_MESSAGE_FIGURE);
         }
-        return Integer.parseInt(input);
     }
     
     public static void validationPositiveNumber(int deposit) {
