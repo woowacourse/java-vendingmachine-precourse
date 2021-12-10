827ca3f413cecb43a3732626bae2cdb92df4802a
asebn1 <asebn1@khu.ac.kr>
2021-12-10 14:33:15 +0900

    refactor: 구매 조건을 함수로 분리

--- src/main/java/vendingmachine/controller/PurchaseController.java
+++ src/main/java/vendingmachine/controller/PurchaseController.java
@@ -9,12 +9,26 @@ public class PurchaseController {
         product.setMinPrice();
         product.setSumCount();
 
-        while (inputMoney >= product.getMinPrice() && product.getSumCount() != 0){
+        while (checkChange(inputMoney, product.getMinPrice()) && checkCount(product.getSumCount())){
             String productName = Input.InputPurchase(inputMoney);
             inputMoney -= product.getPrice(productName);
             product.replaceProductCount(productName, product.getCount(productName)-1);
 
             product.setSumCount();
         }
+    }
+        
+    private static boolean checkChange(int remainMoney, int minPrice){
+        if (remainMoney >= minPrice){
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean checkCount(int count){
+        if (count != 0){
+            return true;
+        }
+        return false;
     }
 }
