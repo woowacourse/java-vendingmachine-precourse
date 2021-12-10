3bcfe2b039211950a5bf78be0b3425765706c276
Yunkeun <yunx2@naver.com>
2021-12-10 18:32:51 +0900

    refactor: side effect 방지를 위한 stream 사용

--- src/main/java/vendingmachine/utils/CoinCaseFactory.java
+++ src/main/java/vendingmachine/utils/CoinCaseFactory.java
@@ -1,6 +1,5 @@
 package vendingmachine.utils;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,15 +15,13 @@ public class CoinCaseFactory {
 
 	public static List<CoinCase> makeCoinsCase(int totalAmount) {
 		currentAmount = totalAmount;
-		List<CoinCase> coinsCase = new ArrayList<>();
 		List<Coin> coinTypes = Arrays.stream(Coin.values()).collect(Collectors.toList());
-		for (Coin coin : coinTypes) {
-			coinsCase.add(makeCoinCase(coin));
-		}
-		return coinsCase;
+		return coinTypes.stream()
+			.map(CoinCaseFactory::makeCoinCase)
+			.collect(Collectors.toList());
 	}
 
-	public static CoinCase makeCoinCase(Coin coin) {
+	private static CoinCase makeCoinCase(Coin coin) {
 		CoinCase coinCase = new CoinCase(coin, currentAmount);
 		currentAmount = coinCase.getCurrentAmount();
 		return coinCase;
