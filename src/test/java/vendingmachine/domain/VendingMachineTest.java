bffeba4877fa8d40dffc8fa6d0b3f9c8e277f8bd
이민재 <lmj938@naver.com>
2021-12-10 13:08:50 +0900

    test : 자판기 투입 금액에 대한 유효성 검사 테스트 코드 추가

--- src/test/java/vendingmachine/domain/VendingMachineTest.java
+++ src/test/java/vendingmachine/domain/VendingMachineTest.java
@@ -1,5 +1,6 @@
 package vendingmachine.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -8,6 +9,13 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class VendingMachineTest {
+    VendingMachine vendingMachine;
+
+    @BeforeEach
+    public void setUp() {
+        vendingMachine = new VendingMachine();
+    }
+
     @ParameterizedTest
     @ValueSource(strings = {"", "qwe", "1413"})
     public void 자판기_투입금액_예외테스트(String input) {
@@ -16,7 +24,6 @@ class VendingMachineTest {
 
     @Test
     public void 자판기_상품등록_테스트() {
-        VendingMachine vendingMachine = new VendingMachine();
         vendingMachine.addMerchandise("[사이다,2000,10];[콜라,1500,20]");
         assertThat(vendingMachine.getAllMerchandiseInfo()).isEqualTo("사이다200010,콜라150020");
     }
@@ -24,7 +31,12 @@ class VendingMachineTest {
     @ParameterizedTest
     @ValueSource(strings = {"", "[],[]", "[]", "[asd,d,10]", "[asd,20],[사이다,2000,20][콜라,2000,20]"})
     public void 자판기_상품등록_예외_테스트(String input) {
-        VendingMachine vendingMachine = new VendingMachine();
         assertThatThrownBy(() -> vendingMachine.addMerchandise(input)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "qwe"})
+    public void 자판기_투입금액_예외_테스트(String input) {
+        assertThatThrownBy(() -> vendingMachine.setInputMoney(input)).isInstanceOf(IllegalArgumentException.class);
     }
 }
