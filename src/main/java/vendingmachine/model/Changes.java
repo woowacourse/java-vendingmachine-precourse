53c71e38ca6c7310e0d2f10f9467eb43ff965fce
nailseong <nnis9857@gmail.com>
2021-12-11 03:40:58 +0900

    refactor(Changes): 더 이상 필요없는 생성자 삭제

--- src/main/java/vendingmachine/model/Changes.java
+++ src/main/java/vendingmachine/model/Changes.java
@@ -5,24 +5,7 @@ import java.util.Map;
 
 public class Changes {
 
-	private final LinkedHashMap<Coin, Integer> hashMap;
-
-	public Changes() {
-		this.hashMap = new LinkedHashMap<>();
-	}
-
-	public Changes(CoinList coinList) {
-		this.hashMap = new LinkedHashMap<>();
-
-		LinkedHashMap<Coin, Integer> coinListHashMap = coinList.getHashMap();
-		for (Map.Entry<Coin, Integer> entry : coinListHashMap.entrySet()) {
-			if (entry.getValue() == 0) {
-				continue;
-			}
-
-			hashMap.put(entry.getKey(), entry.getValue());
-		}
-	}
+	private final LinkedHashMap<Coin, Integer> hashMap = new LinkedHashMap<>();
 
 	public void addCoin(Coin coin, int numberOfCoins) {
 		hashMap.put(coin, numberOfCoins);
