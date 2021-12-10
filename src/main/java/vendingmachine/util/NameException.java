f54288268a915a2e47969ab157e2d7bf842ac9d7
shindong96 <2006peter@naver.com>
2021-12-11 03:58:07 +0900

    feat(util/NameException) : 상품 이름 관련 예외처리
    - 상품명에 공백이 있는 경우
    - 상품명에 탭이 있는 경우

--- src/main/java/vendingmachine/util/NameException.java
+++ src/main/java/vendingmachine/util/NameException.java
@@ -1,8 +1,35 @@
 package vendingmachine.util;
 
+import vendingmachine.constants.ErrorMessage;
+
 public class NameException {
 
 	public static String checkNameException(String productName) {
+		try {
+			checkSpaceInProductName(productName);
+			checkTabInProductName(productName);
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException(e.getMessage());
+		}
+
+		return productName;
+	}
+
+	public static void checkSpaceInProductName(String name) {
+		String[] tmp = name.split(" ", -1);
+
+		if (tmp.length != 1) {
+			throw new IllegalArgumentException(ErrorMessage.SPACE_IN_PRODUCT_NAME_MESSAGE);
+		}
+
+	}
+
+	public static boolean checkTabInProductName(String name) {
+		String[] tmp = name.split("\t", -1);
+
+		if (tmp.length != 1) {
+			throw new IllegalArgumentException(ErrorMessage.TAB_IN_PRODUCT_NAME_MESSAGE);
+		}
 
 	}
 }
