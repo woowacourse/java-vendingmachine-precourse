5f912492a1b35f48844d55e87f3f5ecdbd2116ef
shindong96 <2006peter@naver.com>
2021-12-11 03:49:44 +0900

    feat(model/Product) : 상품 정보가 세개가 아닐 경우 예외처리
    - name
    - price
    - quantity

--- src/main/java/vendingmachine/model/Product.java
+++ src/main/java/vendingmachine/model/Product.java
@@ -1,11 +1,15 @@
 package vendingmachine.model;
 
+import vendingmachine.util.ProductException;
+
 public class Product {
 	public String name;
 	public int price;
 	public int quantity;
 
 	public Product(String productStr) {
-		
+		String[] productInfo = productStr.split(",", -1);
+
+		ProductException.ProductInfoSize(productInfo);
 	}
 }
