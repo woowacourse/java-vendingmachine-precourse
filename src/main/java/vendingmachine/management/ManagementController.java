91e89d5b952460bb6a3a7c52cd231bb730f95b25
steven <seokju73@gmail.com>
2021-12-10 14:29:25 +0900

    Feat: Call 'generateCoins' method in Management controller

--- src/main/java/vendingmachine/management/ManagementController.java
+++ src/main/java/vendingmachine/management/ManagementController.java
@@ -7,12 +7,13 @@ import vendingmachine.view.outputView.ManagementView;
 public class ManagementController {
     
     public static void runManagement(InputView inputView) {
-    
         ManagementView.askDepositAmout();
         int deposit = doValidation(inputView.depositAmout());
+        ManagementService.generateCoins(deposit);
         
        
     }
+    
     private static int doValidation(String input) {
         int n = 0;
         
@@ -25,4 +26,5 @@ public class ManagementController {
         
         return n;
     }
+    
 }
