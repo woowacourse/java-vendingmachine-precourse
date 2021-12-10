4f290c3c3d47b82f194988b9adec13cdcc3d3c08
Seungmin Lim <seungmin9066@gmail.com>
2021-12-10 15:05:50 +0900

    [추가] 소유한 동전을 보유주는 뷰메서드 추가

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -2,8 +2,17 @@ package vendingmachine.view;
 
 import static vendingmachine.constant.Constant.*;
 
+import java.util.LinkedHashMap;
+
 public class OutputView {
 	public static void setVendingMachineMoney() {
-		System.out.println(ENTER_VENDING_MACHINE_MONEY_MESSAGE);
+		System.out.println(ENTER_VENDING_MACHINE_INITIAL_MONEY);
+	}
+
+	public static void showCoins(LinkedHashMap<Integer, Integer> coins) {
+		System.out.println(VENDING_MACHINE_COINS);
+		for (int coin : coins.keySet()) {
+			System.out.println(coin + RULES_TO_SHOW + coins.get(coin) + NUMBER);
+		}
 	}
 }
