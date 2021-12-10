a0ed1e5c19d58ba2407661e472e0d4eff21e6f29
wannagohome <iwwuf7@gmail.com>
2021-12-10 21:49:50 +0900

    feature: generate coins

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,8 +1,15 @@
 package vendingmachine;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        new Input().requestMachineMoney();
+        int machineMoney = new Input().requestMachineMoney();
+        List<Coin> coins = Coin.generateCoinsBy(machineMoney);
+
+        for (Coin coin : coins) {
+            System.out.println(coin);
+        }
     }
 }
--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,5 +1,11 @@
 package vendingmachine;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
@@ -12,5 +18,39 @@ public enum Coin {
         this.amount = amount;
     }
 
-    // 추가 기능 구현
+    public static List<Coin> generateCoinsBy(int money) {
+        List<Coin> result = new ArrayList<>();
+        while (money > getMinAmount()) {
+            int randomAmount = Randoms.pickNumberInList(Coin.getListOfAmountLessThen(money));
+            Coin randomCoin = Coin.byAmount(randomAmount);
+            if (randomCoin != null) {
+                result.add(randomCoin);
+                money -= randomCoin.amount;
+            }
+        }
+        return result;
+    }
+
+    private static Coin byAmount(int amount) {
+        return Arrays.stream(Coin.values())
+                .filter(c -> c.amount == amount)
+                .findFirst()
+                .orElse(null);
+    }
+
+    private static List<Integer> getListOfAmountLessThen(int money) {
+        Integer[] amounts = Arrays.stream(Coin.values())
+                .map(c -> c.amount)
+                .filter(i -> i < money)
+                .toArray(Integer[]::new);
+
+        return new ArrayList<>(Arrays.asList(amounts));
+    }
+
+    private static Integer getMinAmount() {
+        return Arrays.stream(Coin.values())
+                .mapToInt(c -> c.amount)
+                .min()
+                .orElse(COIN_10.amount);
+    }
 }
