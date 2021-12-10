ba16da199044e58b25fa37ebbac6cb24850cd863
pup-paw <wldusdl0310@gmail.com>
2021-12-11 00:17:06 +0900

    feat: Coin Enum 동전 단위 추가, 랜덤 동전 생성 기능

---
+++ src/main/java/vendingmachine/model/Coin.java
@@ -0,0 +1,44 @@
+package vendingmachine.model;
+
+import java.util.Arrays;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import vendingmachine.util.Constant;
+
+public enum Coin {
+	COIN_500("500원", 500),
+	COIN_100("100원", 100),
+	COIN_50("50원", 50),
+	COIN_10("10원", 10);
+
+	private final String unit;
+	private final int amount;
+
+	Coin(final String unit, final int amount) {
+		this.unit = unit;
+		this.amount = amount;
+	}
+
+	public static Coin valueOf(int amount) {
+		if (amount == 500) {
+			return COIN_500;
+		} else if (amount == 100) {
+			return COIN_100;
+		} else if (amount == 50) {
+			return COIN_50;
+		}
+		return COIN_10;
+	}
+
+	public static int pickRandom() {
+		return Randoms.pickNumberInList(
+			Arrays.asList(COIN_500.amount, COIN_100.amount, COIN_50.amount, COIN_10.amount));
+	}
+
+	public String getUnit() {
+		return this.unit;
+	}
+	public int getAmount() {
+		return this.amount;
+	}
+}
