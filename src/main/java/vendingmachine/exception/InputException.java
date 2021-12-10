520ed71e86e6028df2ec64cc31358bbd5aebb412
Gyuchool <balance0591@naver.com>
2021-12-10 21:20:46 +0900

    feat(exception): 개별상품이 대괄호로 묶여있는지 예외처리

--- src/main/java/vendingmachine/exception/InputException.java
+++ src/main/java/vendingmachine/exception/InputException.java
@@ -3,10 +3,18 @@ package vendingmachine.exception;
 public class InputException {
 
 	private static final String INPUT_HAVE_SEMICOLON = "[ERROR] ;(세미콜론)을 이용해서 상품을 구별해야 합니다.";
+	private static final String INPUT_HAVE_BRACKET = "[ERROR] 개별 상품은 [](대괄호)로 묶어야 합니다.";
 
 	public static void checkSemicolon(String input) {
 		if (!input.contains(";")) {
 			throw new IllegalArgumentException(INPUT_HAVE_SEMICOLON);
 		}
 	}
+
+	public static void checkBracket(String input) {
+		if (input.charAt(0) != '[' || input.charAt(input.length() - 1) != ']') {
+			throw new IllegalArgumentException(INPUT_HAVE_BRACKET);
+		}
+	}
+
 }
--- src/main/java/vendingmachine/validator/InputValidator.java
+++ src/main/java/vendingmachine/validator/InputValidator.java
@@ -12,6 +12,7 @@ public class InputValidator {
 
 		String[] itemList = input.split(";");
 		for (String item : itemList) {
+			InputException.checkBracket(item);
 			item = item.substring(1, item.length() - 1);
 			items.add(item);
 		}
