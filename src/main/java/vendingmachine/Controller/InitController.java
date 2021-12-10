69011f1b6942909f91130c91ec7a6d0847718a93
= <ulim130@naver.com>
2021-12-11 00:10:16 +0900

    refactor : 기능별 Service 파일 안에서 입력값 검증하도록 변경

--- src/main/java/vendingmachine/Controller/InitController.java
+++ src/main/java/vendingmachine/Controller/InitController.java
@@ -1,12 +1,10 @@
 package vendingmachine.Controller;
 
 import vendingmachine.Service.InitHoldingCoinsService;
-import vendingmachine.Service.ValidatorService;
 import vendingmachine.View.InputView;
 import vendingmachine.View.OutputView;
 
 public class InitController {
-    ValidatorService validatorService = new ValidatorService();
     InitHoldingCoinsService holdingCoinsService = new InitHoldingCoinsService();
 
     public void initVendingMachine() {
@@ -16,7 +14,7 @@ public class InitController {
     private void initHoldingCoins() {
         try {
             String holdingAmount = InputView.getHoldingAmount();
-            validatorService.isValidHoldingAmount(holdingAmount);
+            holdingCoinsService.isValidHoldingAmount(holdingAmount);
             holdingCoinsService.setHoldingCoins(Integer.parseInt(holdingAmount));
             OutputView.printHoldingCoins();
         } catch (IllegalArgumentException e) {
--- src/main/java/vendingmachine/Service/InitHoldingCoinsService.java
+++ src/main/java/vendingmachine/Service/InitHoldingCoinsService.java
@@ -1,10 +1,31 @@
 package vendingmachine.Service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import vendingmachine.Constant.ErrorConstant;
 import vendingmachine.Domain.Coin;
 import vendingmachine.Domain.HoldingCoins;
 
 public class InitHoldingCoinsService {
+
+    public void isValidHoldingAmount(String holdingAmount) {
+        if (isInteger(holdingAmount) && isInRange(Integer.parseInt(holdingAmount))) {
+            return;
+        }
+        throw new IllegalArgumentException(ErrorConstant.WrongHoldingAmount);
+    }
+
+    private boolean isInteger(String holdingAmount) {
+        try {
+            Integer.parseInt(holdingAmount);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isInRange(int holdingAmount) {
+        return (holdingAmount >= 0 && holdingAmount % 10 == 0);
+    }
 
     public void setHoldingCoins(int holdingAmount) {
         while (holdingAmount > 0) {
