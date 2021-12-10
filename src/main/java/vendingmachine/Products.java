fef7d5164495d019cbe88adec54286f11062d1b6
yoon <duddbs774@gmail.com>
2021-12-10 13:34:24 +0900

    feat: 상품등록시 입력값이  양식에 맞지 않을 경우 예외발생 및 처리 구현

--- src/main/java/vendingmachine/Products.java
+++ src/main/java/vendingmachine/Products.java
@@ -3,17 +3,38 @@ package vendingmachine;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Products {
     private final List<Product> products;
 
     public Products(String value) {
         products = new ArrayList<>();
-        Arrays.stream(value.split(";")).forEach(content -> {
-                String[] elements = content.replaceAll("[\\[\\]]","").split(",");
-                products.add(Product.of(elements[0], Integer.parseInt(elements[1]), Integer.parseInt(elements[2])));
-            }
-        );
+        convertToProductsElement(value)
+            .forEach(elements -> products.add(Product.of(elements[0], elements[1], elements[2])));
+    }
+
+    public List<String[]> convertToProductsElement(String value) {
+        return Arrays.stream(value.split(";")).map(this::convertToProductElements).collect(Collectors.toList());
+    }
+
+    public String[] convertToProductElements(String value) {
+        checkForm(value);
+        String[] elements = value.replaceAll("[\\[\\]]","").split(",");
+        checkElementCount(elements.length);
+        return elements;
+    }
+
+    private void checkForm(String value) {
+        if (value.indexOf(0) != '[' || value.indexOf(value.length() - 1) != ']') {
+            throw new IllegalArgumentException("[ERROR] 한 상품의 내용은 '[' 으로 시작해서 ']' 로 끝나야 합니다.");
+        }
+    }
+
+    private void checkElementCount(int count) {
+        if (count != 3) {
+            throw new IllegalArgumentException("[ERROR] 양식에 맞게 다시 입력주세요. 예) [콜라,1500,20];[사이다,1000,10]");
+        }
     }
 
     public void reduce(String productName) {
