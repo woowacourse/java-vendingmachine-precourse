42af02d648b8f99415ee53bfc5c481ffdf3b387c
shindong96 <2006peter@naver.com>
2021-12-11 01:31:39 +0900

    feat(constants) : input,output 메세지 추가

--- src/main/java/vendingmachine/constants/InputMessage.java
+++ src/main/java/vendingmachine/constants/InputMessage.java
@@ -1,4 +1,9 @@
 package vendingmachine.constants;
 
 public class InputMessage {
+	public static final String PREPARING_COIN_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+	public static final String INPUT_MENU_MESSAGE = "상품명과 가격, 수량을 입력해 주세요.";
+	public static final String INSERT_MONEY_MESSAGE = "투입 금액을 입력해 주세요.";
+	public static final String ORDER_MENU_NAME = "구매할 상품명을 입력해 주세요.";
+
 }
---
+++ src/main/java/vendingmachine/constants/OutputMessage.java
@@ -0,0 +1,8 @@
+package vendingmachine.constants;
+
+public class OutputMessage {
+	public static final String PREPARED_NUMBER_OF_COINS_MESSAGE = "자판기가 보유한 동전";
+	public static final String PRINT_USER_MONEY = "투입 금액: ";
+	public static final String PRINT_CHANGES = "잔돈";
+	
+}
