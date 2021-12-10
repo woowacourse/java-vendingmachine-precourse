2648295f6340d9868ee18516b3f7daff17579365
shindong96 <2006peter@naver.com>
2021-12-11 02:24:51 +0900

    feat(NumberException, Changes) : 자판기가 보유하고 있는 금액 입력하기
    - 금액이 숫자가 아닐 경우 예외처리
    - 금액이 양수가 아닐 경우 예외처리
    - 금액이 10단위로 나뉘지 않을 경우 예외처리

--- src/main/java/vendingmachine/model/Changes.java
+++ src/main/java/vendingmachine/model/Changes.java
@@ -1,5 +1,37 @@
 package vendingmachine.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+import vendingmachine.constants.ErrorMessage;
+import vendingmachine.constants.InputMessage;
+import vendingmachine.util.NumberException;
+
 public class Changes {
-	
+	private List<Integer> coinList;
+	private int changes;
+
+	public Changes() {
+		coinList = new ArrayList<Integer>();
+		changes = 0;
+	}
+
+	public void setCoinList() {
+		changes = setChanges();
+	}
+
+	public int setChanges() {
+		int money;
+		try {
+			System.out.println(InputMessage.PREPARING_COIN_MESSAGE);
+			String moneyStr = Console.readLine();
+			money = NumberException.checkMoneyException(moneyStr);
+		} catch (Exception e) {
+			System.out.println(ErrorMessage.ERROR + e.getMessage());
+			return setChanges();
+		}
+		return money;
+	}
+
 }
--- src/main/java/vendingmachine/util/NumberException.java
+++ src/main/java/vendingmachine/util/NumberException.java
@@ -1,4 +1,44 @@
 package vendingmachine.util;
 
+import vendingmachine.constants.ErrorMessage;
+
 public class NumberException {
+	public static int money = 0;
+
+	public static int checkMoneyException(String moneyStr) {
+		try {
+			money = Integer.parseInt(moneyStr);
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER_MONEY__MESSAGE);
+		}
+
+		if (!checkPositiveNumberMoneyException(money)) {
+			throw new IllegalArgumentException(ErrorMessage.NOT_POSITIVE_NUMBER_MONEY_MESSAGE);
+		}
+
+		if (!checkNumberIsDividedTen(money)) {
+			throw new IllegalArgumentException(ErrorMessage.NOT_DIVIDED_TEN_NUMBER_MESSAGE);
+		}
+
+		return money;
+	}
+
+	public static boolean checkPositiveNumberMoneyException(int number) {
+
+		if (number <= 0) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public static boolean checkNumberIsDividedTen(int number) {
+
+		if (number % 10 != 0) {
+			return false;
+		}
+
+		return true;
+	}
+
 }
