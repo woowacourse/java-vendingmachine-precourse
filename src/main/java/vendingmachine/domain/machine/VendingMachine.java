ed7e9b8758c4b8387c140bc9c8d0d56906b98925
mac_daehwan2yo <daehwan7989@naver.com>
2021-12-10 15:04:13 +0900

    feat : 자판기 생성시 ProductStore, CoinPocket 생성 구현
    ## 목적
     생성자를 통해 초기에 초기화시켜, 후에 상품이 들어오거나 동전이 들어오면 바로 저장소에 추가 및 관리가 가능하도록 한다.

--- src/main/java/vendingmachine/domain/machine/VendingMachine.java
+++ src/main/java/vendingmachine/domain/machine/VendingMachine.java
@@ -1,13 +1,20 @@
 package vendingmachine.domain.machine;
 
+import vendingmachine.domain.coin.CoinPocket;
+import vendingmachine.domain.product.ProductStore;
+
 public class VendingMachine {
     private int balance;
+    private final ProductStore productStore;
+    private final CoinPocket coinPocket;
 
     public static VendingMachine of(int balance) {
         return new VendingMachine(balance);
     }
     private VendingMachine(int balance) {
         this.balance = balance;
+        this.productStore = ProductStore.getInstance();
+        this.coinPocket = CoinPocket.getInstance();
     }
 
     // for test
