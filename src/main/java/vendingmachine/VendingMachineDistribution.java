a8158571a5d8eda98e7d017722c818912e6170d2
AlphaTechnic <alphatechnicist@gmail.com>
2021-12-11 18:41:51 +0900

    [fix] 기능 테스트를 통과하도록 동전 랜덤 생성 로직 변경
    - {500, 100, 50, 10}를 대상으로 pickNumberInList()를 사용해야함
    - 랜덤하게 동전을 택하여 동전을 생성하고, 해당 가치만큼 total 투입 총액에서 깎음
    - 0원이 되면, 분배 완료.

--- src/main/java/vendingmachine/VendingMachineDistribution.java
+++ src/main/java/vendingmachine/VendingMachineDistribution.java
@@ -1,48 +1,50 @@
 package vendingmachine;
 
-import camp.nextstep.edu.missionutils.Randoms;
+import models.Coin;
 
+import java.util.ArrayList;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInList;
 import static models.Coin.*;
 import static vendingmachine.VendingMachineMain.coin2Num;
 import static vendingmachine.VendingMachineMain.totalAmount;
 
+
 public class VendingMachineDistribution {
     public static void distributeRandomly() {
-        int remains = totalAmount;
-
-        if (COIN_500.value() != totalAmount) {
-            remains = allocTo500(remains);
-        }
-        if (COIN_100.value() != totalAmount) {
-            remains = allocTo100(remains);
-        }
-        if (COIN_50.value() != totalAmount) {
-            remains = allocTo50(remains);
-        }
-        if (COIN_10.value() != totalAmount) {
-            allocTo10(remains);
+        ArrayList<Integer> coinList = makeSeeds();
+        int total = totalAmount;
+        while (true) {
+            int seed = pickNumberInList(coinList);
+            if (seed == totalAmount || total - seed < 0) {
+                continue;
+            }
+            if (total - seed >= 0) {
+                total -= seed;
+                countUp(seed);
+            }
+            if (total == 0) {
+                break;
+            }
         }
     }
 
-    public static int allocTo500(int total) {
-        int num = Randoms.pickNumberInRange(0, total / COIN_500.value());
-        coin2Num.put(COIN_500, num);
-        return total - COIN_500.value() * num;
+    public static ArrayList<Integer> makeSeeds() {
+        ArrayList<Integer> coinList = new ArrayList<>();
+        coinList.add(500);
+        coinList.add(100);
+        coinList.add(50);
+        coinList.add(10);
+        return coinList;
     }
 
-    public static int allocTo100(int remains) {
-        int num = Randoms.pickNumberInRange(0, remains / COIN_100.value());
-        coin2Num.put(COIN_100, num);
-        return remains - COIN_100.value() * num;
-    }
-
-    public static int allocTo50(int remains) {
-        int num = Randoms.pickNumberInRange(0, remains / COIN_50.value());
-        coin2Num.put(COIN_50, num);
-        return remains - COIN_50.value() * num;
-    }
-
-    public static void allocTo10(int remains) {
-        coin2Num.put(COIN_10, remains / 10);
+    public static void countUp(int seed) {
+        Coin[] coins = new Coin[] {COIN_500, COIN_100, COIN_50, COIN_10};
+        for (Coin coin: coins) {
+            if (coin.value() == seed) {
+                coin2Num.put(coin, coin2Num.get(coin) + 1);
+                break;
+            }
+        }
     }
 }
