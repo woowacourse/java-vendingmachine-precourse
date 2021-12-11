0172ac5a85428f59257bc21025eb441a1d919b3e
hyewoncc <hyewoncc@gmail.com>
2021-12-12 03:59:42 +0900

    feat: 동전 개수가 최소가 되도록 잔돈 계산
    그리디 알고리즘을 이용해 동전 개수가 최소가 되도록 계산

---
+++ src/main/java/vendingmachine/util/CoinCalculator.java
@@ -0,0 +1,34 @@
+package vendingmachine.util;
+
+import vendingmachine.model.Coin;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+
+public class CoinCalculator {
+	public HashMap<Coin, Integer> combineCoinsByGreedy(HashMap<Coin, Integer> coins, int remainMoney) {
+		HashMap<Coin, Integer> returnCoins = new HashMap<>(Coin.getCoinTypes());
+		List<Integer> coinAmounts = Coin.getCoinAmounts();
+		for (int amount : coinAmounts) {
+			Coin coin = Coin.getCoinByAmount(amount);
+			int count = getMaxCoinCount(coin, coins.get(coin), remainMoney);
+			if (count != 0) {
+				returnCoins.put(coin, count);
+			}
+			remainMoney -= coin.getAmount() * count;
+		}
+		return returnCoins;
+	}
+
+	private int getMaxCoinCount(Coin coin, int count, int money) {
+		int max = 0;
+		for (int i = 0; i <= count; i++) {
+			if (coin.getAmount() * i <= money) {
+				max = i;
+			}
+		}
+		return max;
+	}
+}
