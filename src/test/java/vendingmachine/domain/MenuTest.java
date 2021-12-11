a1f8ee18ede5226bb4db61e49db0fbd9723133f1
JunHyeok.Bang <bangvkf3@gmail.com>
2021-12-11 20:05:05 +0900

    test: 메뉴정보 입력값 검증 및 메뉴 생성 테스트케이스 추가

--- src/test/java/vendingmachine/domain/MenuTest.java
+++ src/test/java/vendingmachine/domain/MenuTest.java
@@ -2,8 +2,11 @@ package vendingmachine.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import vendingmachine.exceptions.NotDivisibleByMinPriceCoinException;
 
 class MenuTest {
 
@@ -25,6 +28,45 @@ class MenuTest {
 		);
 
 		assertEquals("[ERROR] 상품 정보는 [상품명,가격,수량] 형태로 입력해야 합니다.", exception.getMessage());
+	}
+
+	@Test
+	void validatePriceNumber() {
+		Throwable exception = assertThrows(IllegalArgumentException.class, () ->
+			Menu.from("[상품명,오백원,4]")
+		);
+
+		assertEquals("[ERROR] 상품 가격은 정수입니다.", exception.getMessage());
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"[상품명,0,4]", "[상품명,50,4]"})
+	void validateMinPrice(String input) {
+		Throwable exception = assertThrows(IllegalArgumentException.class, () ->
+			Menu.from(input)
+		);
+
+		assertEquals("[ERROR] 상품 가격은 100원 이상입니다.", exception.getMessage());
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"[상품명,111,4]", "[상품명,1111,4]"})
+	void validateDividingByMinPriceCoin(String input) {
+		Throwable exception = assertThrows(NotDivisibleByMinPriceCoinException.class, () ->
+			Menu.from(input)
+		);
+
+		assertEquals("[ERROR] 상품 가격은 10원으로 나누어 떨어져야 합니다.", exception.getMessage());
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"[상품명,500,넷]", "[상품명,500,4개]"})
+	void validateCountNumber(String input) {
+		Throwable exception = assertThrows(IllegalArgumentException.class, () ->
+			Menu.from(input)
+		);
+
+		assertEquals("[ERROR] 상품 수량은 정수입니다.", exception.getMessage());
 	}
 
 }
