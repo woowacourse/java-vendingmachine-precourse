6ee8d66db3aa40124e2dca2c3a508d8b336b5fc2
kanamycine <jinyeong5320@gmail.com>
2021-12-11 23:14:21 +0900

    feat(OutputView) : 보유 동전 출력 로직 구현

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -1,4 +1,22 @@
 package vendingmachine.view;
 
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+
+import vendingmachine.domain.Coin;
+
+
 public class OutputView {
+	private static final String WON_FORMAT = "원 - ";
+	private static final String EA_FORMAT = "개";
+
+	public void printHoldingCoins(LinkedHashMap<Coin, Integer> coins){
+		Iterator<Coin> keys = coins.keySet().iterator();
+		while (keys.hasNext()){
+			Coin key = keys.next();
+			int value = coins.get(key);
+			System.out.println(key.getAmount() + WON_FORMAT + value + EA_FORMAT);
+		}
+		System.out.println();
+	}
 }
