2af5b8f8baf1514e8918d0f3bfd8776081d82ae9
steven <seokju73@gmail.com>
2021-12-11 20:04:50 +0900

    Feat: Add method to question insertion amount in OperationView

---
+++ src/main/java/vendingmachine/view/outputView/OperationView.java
@@ -0,0 +1,10 @@
+package vendingmachine.view.outputView;
+
+public class OperationView {
+    private static final String ASK_INSERTION_AMOUNT = "투입 금액을 입력해 주세요.";
+    
+    public static void askInsertionAmount() {
+        System.out.println(ASK_INSERTION_AMOUNT);
+    }
+
+}
