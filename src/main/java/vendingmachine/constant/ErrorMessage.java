663ba620bdd52dba2cc5ae1877e4fe40bca944ab
이민재 <lmj938@naver.com>
2021-12-12 03:08:30 +0900

    style : 메시지 출력 내용 변경

--- src/main/java/vendingmachine/constant/ErrorMessage.java
+++ src/main/java/vendingmachine/constant/ErrorMessage.java
@@ -7,7 +7,7 @@ public enum ErrorMessage {
     NOT_CORRECT_BRACKET("[ERROR] 상품 정보는 대괄호로 묶어주어야 합니다."),
     NULL_IN_LIST("[ERROR] 상품의 정보 중 누락된 값이 있습니다."),
     DUPLICATE_MERCHANDISE_EXIST("[ERROR] 중복된 상품명이 존재합니다."),
-    LACK_MONEY("[ERROR] 해당 상품을 구매하기 위한 금액이 부족합니다."),
+    LACK_MONEY("[ERROR] 입력한 상품을 구매하기 위한 금액이 부족합니다."),
     NOT_EXIST_MERCHANDISE("[ERROR] 존재하지 않는 상품명입니다."),
     SOLD_OUT("[ERROR] 해당 상품은 매진되었습니다.");
 
