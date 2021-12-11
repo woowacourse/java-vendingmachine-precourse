e0b5d97ab51f96e436b1b64366220b43ca3e7b02
iamsseaa <homingsea@gmail.com>
2021-12-11 23:12:24 +0900

    feat: 사용자가 입력한 금액이 숫자인지 확인

--- src/main/java/vendingmachine/user/UserValidation.java
+++ src/main/java/vendingmachine/user/UserValidation.java
@@ -1,4 +1,9 @@
 package vendingmachine.user;
 
 public class UserValidation {
+    public static void isNumber(String money) {
+        if (!money.matches("-?\\d+")) {
+            throw new IllegalArgumentException(InputErrorConstant.ERROR_IS_NOT_NUMBER);
+        }
+    }
 }
