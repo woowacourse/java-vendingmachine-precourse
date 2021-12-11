a6c1f8f398a846b3203381b5cae29b581cc93f89
lomuto <kangys1245@gmail.com>
2021-12-11 12:12:39 +0900

    feat: Add price field

--- src/main/java/vendingmachine/Product.java
+++ src/main/java/vendingmachine/Product.java
@@ -2,13 +2,19 @@ package vendingmachine;
 
 public class Product {
     private String name;
+    private int price;
 
-    public Product(String name) {
+    public Product(String name, int price) {
         this.name = name;
+        this.price = price;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getPrice() {
+        return price;
     }
 
     @Override
