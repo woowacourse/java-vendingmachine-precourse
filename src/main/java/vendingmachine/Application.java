bed18eb2118744e550f401b81edf848d9a24e12c
pup-paw <wldusdl0310@gmail.com>
2021-12-11 23:05:35 +0900

    feat: 자판기 전체 프로그램 실행

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,7 +1,9 @@
 package vendingmachine;
 
+import vendingmachine.controller.MachineController;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		MachineController.runMachine();
+	}
 }
