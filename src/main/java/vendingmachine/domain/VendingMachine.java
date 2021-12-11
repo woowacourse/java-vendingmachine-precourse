a0d0bd687d4ad3194301a8d552931752b171e381
JunHyeok.Bang <bangvkf3@gmail.com>
2021-12-11 21:35:19 +0900

    feat: 금액 투입 기능 테스트케이스 추가

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -6,9 +6,7 @@ import vendingmachine.domain.coin.Coins;
 import vendingmachine.domain.menu.Menus;
 
 public class VendingMachine {
-	private static final String ERROR_NOT_INTEGER = "[ERROR] 보유 금액은 정수입니다.";
-	private static final String ERROR_LT_MIN_PRICE_COIN = "[ERROR] 보유 금액은 10원 이상입니다.";
-	private static final String ERROR_NOT_DIVISIBLE_BY_MIN_PRICE_COIN = "[ERROR] 보유 금액은 10원의 배수입니다.";
+	private static final String ERROR_INPUT_MONEY_NUMBER = "[ERROR] 투입 금액은 정수입니다.";
 
 	private Coins leftCoins;
 	private Menus menus;
@@ -26,7 +24,7 @@ public class VendingMachine {
 	}
 
 	public void validateMoney(String money) {
-		validateNumber(money);
+		validateNumber(money, ERROR_INPUT_MONEY_NUMBER);
 	}
 
 	public Coins getLeftCoins() {
--- src/test/java/vendingmachine/domain/VendingMachineTest.java
+++ src/test/java/vendingmachine/domain/VendingMachineTest.java
@@ -1,5 +1,26 @@
 package vendingmachine.domain;
 
-class VendingMachineTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import vendingmachine.domain.coin.Coins;
+import vendingmachine.domain.menu.Menus;
+
+class VendingMachineTest {
+	@ParameterizedTest
+	@ValueSource(strings = {"", "오백원", "500원"})
+	void validateNumber(String input) {
+		Coins coins = Coins.generateCoinsRandomlyFromTotalAmount("1000");
+		Menus menus = Menus.from("[상품명,300,10];[상품명2,400,15]");
+
+		VendingMachine vendingMachine = new VendingMachine(coins, menus);
+
+		Throwable exception = assertThrows(IllegalArgumentException.class, () ->
+			vendingMachine.putMoney(input)
+		);
+
+		assertEquals("[ERROR] 투입 금액은 정수입니다.", exception.getMessage());
+	}
 }
