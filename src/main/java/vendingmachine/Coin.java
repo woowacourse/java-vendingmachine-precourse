7b2c40344f832416e2e56cc8010f716654624eb6
Sanggu <rkdtkdrn93@naver.com>
2021-12-12 03:46:15 +0900

    feat: 입력받은 금액만큼 동전을 무작위로 생성하는 기능 구현
    Coin.java
    - pickRandomCoin() : 각 동전의 값으로 만든 리스트에서 랜덤으로 한 값을 추출하고 그 값에 해당하는 동전을 반환하는 함수.
    - hasEnoughMoney() : 목표로 하는 값보다 동전의 값이 큰지 확인하는 함수.
    
    Machine.java
    - setWallet() : 자판기가 가지고 있는 동전을 정리하기 위해서 HashMap을 초기화 하는 함수.
    - setMachineCoin() : 입력받은 금액만큼 자판기가 가진 동전을 랜덤으로 생성하는 함수.

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,10 +1,20 @@
 package vendingmachine;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public enum Coin {
 	COIN_500(500),
 	COIN_100(100),
 	COIN_50(50),
 	COIN_10(10);
+
+	public static final List<Integer> LIST_OF_MONEY = Arrays.stream(values())
+		.map(coin -> coin.amount)
+		.collect(Collectors.toList());
 
 	private final int amount;
 
@@ -12,5 +22,26 @@ public enum Coin {
 		this.amount = amount;
 	}
 
-	// 추가 기능 구현
+	public int getAmount() {
+		return amount;
+	}
+
+	public static Coin pickRandomCoin() {
+		int amount = Randoms.pickNumberInList(LIST_OF_MONEY);
+		if (amount == COIN_500.amount) {
+			return COIN_500;
+		}
+		if (amount == COIN_100.amount) {
+			return COIN_100;
+		}
+		if (amount == COIN_50.amount) {
+			return COIN_50;
+		}
+		return COIN_10;
+	}
+
+	public static boolean hasEnoughMoney(int target, Coin coin) {
+		return target >= coin.amount;
+	}
+
 }
--- src/main/java/vendingmachine/Machine.java
+++ src/main/java/vendingmachine/Machine.java
@@ -1,18 +1,21 @@
 package vendingmachine;
 
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.stream.Collectors;
 
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
 
 public class Machine {
 
 	private static final String REQUEST_MSG_MACHINE_MONEY = "자판기가 보유하고 있는 금액을 입력해 주세요.";
 
 	private int money;
+	private HashMap<Coin, Integer> wallet;
 
 	public void run() {
 		inputMachineMoney();
+		setMachineCoin(this.money);
 	}
 
 	public void inputMachineMoney() {
@@ -24,6 +27,26 @@ public class Machine {
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			inputMachineMoney();
+		}
+		this.money = Integer.parseInt(userInput);
+	}
+
+	public void setWallet() {
+		this.wallet = new HashMap<>();
+		for (Coin coin : Arrays.stream(Coin.values()).collect(Collectors.toList())) {
+			this.wallet.put(coin, 0);
+		}
+	}
+
+	public void setMachineCoin(int money) {
+		setWallet();
+		int target = money;
+		while (target > 0) {
+			Coin coin = Coin.pickRandomCoin();
+			if (Coin.hasEnoughMoney(target,coin)) {
+				target -= coin.getAmount();
+				wallet.put(coin, wallet.get(coin) + 1);
+			}
 		}
 	}
 
