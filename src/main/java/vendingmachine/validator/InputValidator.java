dd82cd61831ab5aad220061715148771a8e174d4
Gyuchool <balance0591@naver.com>
2021-12-11 23:07:37 +0900

    refactor(inputValidator): 대괄호 제거하는 로직 정규식으로 변경

--- src/main/java/vendingmachine/validator/InputValidator.java
+++ src/main/java/vendingmachine/validator/InputValidator.java
@@ -7,12 +7,15 @@ import vendingmachine.domain.Beverages;
 import vendingmachine.exception.InputException;
 
 public class InputValidator {
-	private static final int itemIndex = 0;
+	private static final int nameIndex = 0;
 	private static final int priceIndex = 1;
 	private static final int stockIndex = 2;
 	private static final String MONEY_REGEX = "\\d+0";
 	private static final String STOCK_REGEX = "[1-9]+[0-9]*";
 	private static final int MINIMUM_MONEY_SIZE = 100;
+	private static final String BRACKET_REGEX = "\\[(.*)\\]";
+	private static final String OPEN_BRACKET_REGEX = "\\[";
+	private static final String CLOSE_BRACKET_REGEX = "\\]";
 
 	public static Beverages checkInputForm(String input) {
 		ArrayList<String> items = new ArrayList<>();
@@ -20,20 +23,19 @@ public class InputValidator {
 
 		String[] itemList = input.split(";");
 		for (String item : itemList) {
-			checkBracket(item);
-			item = item.substring(1, item.length() - 1);
-			items.add(item);
+			String itemInfo = eraseBracket(item);
+			items.add(itemInfo);
 		}
 		return splitItem(items);
 	}
 
 	private static Beverages splitItem(ArrayList<String> itemPriceStock) {
 		Beverages beverages = new Beverages();
-		for (String itemInfoList : itemPriceStock) {
-			String[] itemInfo = itemInfoList.split(",");
-			String name = itemInfo[itemIndex];
-			int price = InputValidator.checkPriceForm(itemInfo[priceIndex]);
-			int stock = InputValidator.checkStockForm(itemInfo[stockIndex]);
+		for (String beveragesInfoList : itemPriceStock) {
+			String[] beverageInfo = beveragesInfoList.split(",");
+			String name = beverageInfo[nameIndex];
+			int price = InputValidator.checkPriceForm(beverageInfo[priceIndex]);
+			int stock = InputValidator.checkStockForm(beverageInfo[stockIndex]);
 			beverages.add(new Beverage(name, price), stock);
 		}
 		return beverages;
@@ -67,10 +69,12 @@ public class InputValidator {
 		}
 	}
 
-	private static void checkBracket(String input) {
-		if (input.charAt(0) != '[' || input.charAt(input.length() - 1) != ']') {
+	private static String eraseBracket(String input) {
+
+		if (!input.matches(BRACKET_REGEX)) {
 			InputException.printNotFoundBracketError();
 		}
+		return input.replaceAll(OPEN_BRACKET_REGEX, "").replaceAll(CLOSE_BRACKET_REGEX, "");
 	}
 
 }
