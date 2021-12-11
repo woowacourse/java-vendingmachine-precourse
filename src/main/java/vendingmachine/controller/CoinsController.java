bb435cfaeb9330d53c8eabe01c06ac01eed76c34
devHudi <devhudi@gmail.com>
2021-12-12 00:13:41 +0900

    fix: 자판기 보유 금액에 대한 예외 처리 핸들링 문제 해결
    - 예외가 발생하는 부분이 try ~ catch 외부에 있어 제대로 핸들링 하지 못하는 문제를 해결한다

--- src/main/java/vendingmachine/controller/CoinsController.java
+++ src/main/java/vendingmachine/controller/CoinsController.java
@@ -14,17 +14,21 @@ public class CoinsController {
 	public void generateCoins() {
 		String input = InputView.inputVendingMachineBalance();
 
-		if (!StringUtils.isNumeric(input)) {
-			throw new NotNumericException();
-		}
-
 		try {
-			VendingMachineBalance vendingMachineBalance = VendingMachineBalance.from(Integer.parseInt(input));
-			coinsService.generateRandomCoins(vendingMachineBalance);
+			initCoins(input);
 		} catch (IllegalArgumentException e) {
 			OutputView.printError(e.getMessage());
 			generateCoins();
 		}
+	}
+
+	private void initCoins(String input) {
+		if (!StringUtils.isNumeric(input)) {
+			throw new NotNumericException();
+		}
+
+		VendingMachineBalance vendingMachineBalance = VendingMachineBalance.from(Integer.parseInt(input));
+		coinsService.generateRandomCoins(vendingMachineBalance);
 	}
 
 	public void printGeneratedCoins() {
