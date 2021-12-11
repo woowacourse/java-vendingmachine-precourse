e424ea02adc3e04331ca709871668f37e906867d
yeon-06 <rnjstldus2@hs.ac.kr>
2021-12-11 20:15:48 +0900

    feat: 상품명 입력 및 검증 구현

--- src/main/java/vendingmachine/util/Validator.java
+++ src/main/java/vendingmachine/util/Validator.java
@@ -1,9 +1,20 @@
 package vendingmachine.util;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class Validator {
-    private static String ERROR_VENDINGMACHINE_SENETENCE = "[ERROR] 올바른 자판기 보유 금액이 아닙니다.";
+    private static final String ERROR_VENDINGMACHINE_SENETENCE = "[ERROR] 올바른 금액이 아닙니다.";
+    private static final String ERROR_PRODUCT_SENETENCE = "[ERROR] 올바른 상품 정보를 입력해주십시오.";
+    private static final String DIVISOR_PRODUCT_LIST = ";";
+    private static final String DIVISOR_PRODUCT = ",";
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("^[0-9]*$");
+    private static final int DIVISOR_PRICE = 10;
+    private static final int MIN_PRICE = 100;
+    private static final char OPEN_BRACKET = '[';
+    private static final int CLOSE_BRACKET = ']';
+    private static final int NEGATIVE_NUMBER = -1;
 
     public boolean isValidVendingmachineChange(String input) {
         if (!isNumber(input)
@@ -14,9 +25,65 @@ public class Validator {
         return true;
     }
 
+    public boolean isValidProduct(String input) {
+        if (!isValidProductList(input)) {
+            throw new IllegalArgumentException(ERROR_PRODUCT_SENETENCE);
+        }
+        return true;
+    }
+
+    private boolean isValidBracket(String input) {
+        return input.charAt(0) == OPEN_BRACKET
+                && input.charAt(input.length() - 1) == CLOSE_BRACKET;
+    }
+
+    private boolean isValidProductList(String input) {
+        List<String> names = new ArrayList<>();
+
+        for (String product : input.split(DIVISOR_PRODUCT_LIST, NEGATIVE_NUMBER)) {
+            if (isStringEmpty(product) || !isValidBracket(product)) {
+                return false;
+            }
+            String[] productInfo = splitProductInfo(product);
+            if (isDuplicateName(names, productInfo[0])
+                    || !isValidPrice(productInfo[1])
+                    || !isValidStock(productInfo[2])) {
+                return false;
+            }
+            names.add(productInfo[0]);
+        }
+        return true;
+    }
+
+    private boolean isStringEmpty(String str) {
+        return str == null || str.isEmpty();
+    }
+
+    private String[] splitProductInfo (String input) {
+        return input.substring(1, input.length() - 1).split(DIVISOR_PRODUCT);
+    }
+
+    private boolean isValidPrice(String input) {
+        return isNumber(input)
+                && isNaturalNumber(input)
+                && isDivideByDivisor(input)
+                && isOverMinPrice(input);
+    }
+
+    private boolean isValidStock(String input) {
+        return isNumber(input) && isNaturalNumber(input);
+    }
+
+    private boolean isOverMinPrice(String input) {
+        return Integer.parseInt(input) >= MIN_PRICE;
+    }
+
+    private boolean isDuplicateName(List<String> names, String name) {
+        return names != null && names.contains(name);
+    }
+
     private boolean isNumber(String input) {
-        Pattern pattern = Pattern.compile("^[0-9]$");
-        return pattern.matcher(input).matches();
+        return NUMBER_PATTERN.matcher(input).matches();
     }
 
     private boolean isNaturalNumber(String input) {
@@ -24,6 +91,6 @@ public class Validator {
     }
 
     private boolean isDivideByDivisor(String input) {
-        return Integer.parseInt(input) % 10 == 0;
+        return Integer.parseInt(input) % DIVISOR_PRICE == 0;
     }
 }
--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,10 +1,15 @@
 package vendingmachine.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import vendingmachine.model.Product;
 import vendingmachine.util.Validator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputView {
-    private static String INPUT_VENDINGMACHINE_SENETENCE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    private static final String INPUT_VENDINGMACHINE_SENETENCE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    private static final String INPUT_PRODUCT_SENETENCE = "상품명과 가격, 수량을 입력해 주세요.";
 
     private final Validator validator;
 
@@ -23,5 +28,22 @@ public class InputView {
             System.out.println(e.getMessage());
             return inputVendingmachineChange();
         }
+    }
+
+    public List<Product> inputProductList() {
+        System.out.println(INPUT_PRODUCT_SENETENCE);
+        String input = Console.readLine();
+        try {
+            validator.isValidProduct(input);
+            return convertStringToProductList(input);
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputProductList();
+        }
+    }
+
+    private List<Product> convertStringToProductList(String input) {
+        return new ArrayList<>();
     }
 }
