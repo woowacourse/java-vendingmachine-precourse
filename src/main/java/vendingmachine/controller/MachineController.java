fd26e15b0137baeb494eec2b3542ed3084dd6cc9
pup-paw <wldusdl0310@gmail.com>
2021-12-11 23:03:55 +0900

    feat: 자판기 전체 프로그램 실행 기능

--- src/main/java/vendingmachine/controller/MachineController.java
+++ src/main/java/vendingmachine/controller/MachineController.java
@@ -1,14 +1,62 @@
 package vendingmachine.controller;
 
+import vendingmachine.util.Constant;
 import vendingmachine.view.InputView;
+import vendingmachine.view.OutputView;
 
 public class MachineController {
-	public static void askHoldingAmount() {
+	private static int inputAmount;
+	public static void runMachine() {
+		int holdingAmount = askHoldingAmount();
+		OutputView.showHoldingCoins(holdingAmount);
+		askProductInfo();
+		inputAmount = askInputAmount();
+		purchase();
+		OutputView.showChanges(inputAmount);
+	}
+	private static int askHoldingAmount() {
 		try {
-			InputView.askHoldingAmount();
+			return InputView.askHoldingAmount();
 		} catch (IllegalArgumentException e) {
 			System.out.println(e.getMessage());
-			askHoldingAmount();
+		}
+		return askHoldingAmount();
+	}
+
+	private static void askProductInfo() {
+		System.out.println();
+		try {
+			InputView.askProductInfo();
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			askProductInfo();
+		}
+	}
+
+	private static int askInputAmount() {
+		try {
+			System.out.println();
+			return InputView.askInputAmount();
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+		}
+		return askInputAmount();
+	}
+
+	private static int askPurchase() {
+		try {
+			System.out.println();
+			OutputView.showInputAmount(inputAmount);
+			return InputView.askPurchase();
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+		}
+		return askPurchase();
+	}
+
+	private static void purchase() {
+		while (!(InputView.isEmpty()) && InputView.getMinPrice() <= inputAmount) {
+			inputAmount -= askPurchase();
 		}
 	}
 }
