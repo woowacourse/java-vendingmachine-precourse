5e7c77945259ef62fb82556b006746dc4b352e4a
meatsby <meatsby@gmail.com>
2021-12-11 23:57:15 +0900

    style: spaces -> tab

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -3,8 +3,8 @@ package vendingmachine;
 import vendingmachine.controller.VendingMachineController;
 
 public class Application {
-    public static void main(String[] args) {
-        VendingMachineController vendingMachineController = new VendingMachineController();
-        vendingMachineController.run();
-    }
+	public static void main(String[] args) {
+		VendingMachineController vendingMachineController = new VendingMachineController();
+		vendingMachineController.run();
+	}
 }
