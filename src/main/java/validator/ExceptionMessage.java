0a284765ec7331a9591206225958a3037c3d424c
sha11 <sha1119@naver.com>
2021-12-11 11:54:10 +0900

    feat: ExceptionMessage 클래스 구현
    예외 출력을 위한 클래스 구현
    
    에러 메시지 관련클래스 구현

---
+++ src/main/java/validator/ExceptionMessage.java
@@ -0,0 +1,6 @@
+package validator;
+
+public class ExceptionMessage {
+    public static final String ERROR_PHRASE = "[ERROR]";
+    public static final String ERROR_INPUT_NUMBER ="숫자를 입력하세요.";
+}
--- src/main/java/validator/InputNumberValidator.java
+++ src/main/java/validator/InputNumberValidator.java
@@ -8,6 +8,10 @@ public class InputNumberValidator {
     }
 
     public static int isNumber(String userInput) {
-
+        try {
+            return Integer.parseInt(userInput);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException(ExceptionMessage.ERROR_PHRASE + ExceptionMessage.ERROR_INPUT_NUMBER);
+        }
     }
 }
