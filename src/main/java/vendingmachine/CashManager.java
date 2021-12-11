78087c351e17fd170057d0f3cd603bf25778403b
lomuto <kangys1245@gmail.com>
2021-12-11 14:29:09 +0900

    feat: Add CashManager deposit, withdraw

--- src/main/java/vendingmachine/CashManager.java
+++ src/main/java/vendingmachine/CashManager.java
@@ -1,2 +1,48 @@
-package vendingmachine;public class CashManager {
+package vendingmachine;
+
+import java.util.HashMap;
+
+public class CashManager {
+    private int remainCash = 0;
+    private HashMap<Coin, Integer> vault;
+
+    public CashManager(int amount) {
+        this.vault = getMinimumCoins(amount);
+    }
+
+    public int getRemainCash() {
+        return remainCash;
+    }
+
+    public void withdraw(int amount) throws MyIllegalArgumentException {
+        if (remainCash < amount) {
+            throw new MyIllegalArgumentException(
+                    String.format("Not enough remain cash. Remains: [%d]", this.remainCash)
+            );
+        }
+
+        remainCash -= amount;
+    }
+
+    public void deposit(int amount) throws MyIllegalArgumentException {
+        if(Integer.MAX_VALUE - amount < this.remainCash) {
+            throw new MyIllegalArgumentException(
+                    String.format("Cash overflow. Remains: [%d]", this.remainCash)
+            );
+        }
+
+        remainCash += amount;
+    }
+
+
+    private HashMap<Coin, Integer> getMinimumCoins(int amount) {
+        HashMap<Coin, Integer> ret = new HashMap<>();
+
+        for (Coin coin : Coin.values()) {
+            ret.put(coin, amount / coin.getAmount());
+            amount -= (amount / coin.getAmount()) * coin.getAmount();
+        }
+
+        return ret;
+    }
 }
