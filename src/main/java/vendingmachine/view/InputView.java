c7eea23124ef04268bf49241a247dc24fc0bca47
jurlring <78091011+jurlring@users.noreply.github.com>
2021-12-11 18:41:58 +0900

    feat : 상품 명과 가격, 수량 입력 받기 구현

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -2,16 +2,17 @@ package vendingmachine.view;
 
 import static camp.nextstep.edu.missionutils.Console.*;
 import static vendingmachine.exception.NumberException.*;
-import static vendingmachine.message.ViewMessage.*;
+import static vendingmachine.constant.ViewMessage.*;
+import static vendingmachine.exception.ProductException.*;
 
 public class InputView {
 
 	public static int requestPossessionMoney() {
 		System.out.println(POSSESSION_MONEY_REQUEST_MESSAGE);
-		return Integer.parseInt(inputMoney());
+		return Integer.parseInt(requestMoney());
 	}
 
-	public static String inputMoney() {
+	public static String requestMoney() {
 		while (true) {
 			String money = readLine();
 			try {
@@ -22,4 +23,19 @@ public class InputView {
 			}
 		}
 	}
+
+	public static String requestProduct() {
+		System.out.println();
+		System.out.println(PRODUCT_REQUEST_MESSAGE);
+		while (true) {
+			String products = readLine();
+			try {
+				checkProductException(products);
+				return products;
+			} catch (Exception exception) {
+				System.out.println(exception.getMessage());
+			}
+		}
+	}
+
 }
