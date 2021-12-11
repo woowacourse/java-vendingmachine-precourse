4424c6024ff14547ed2cd8a27fce239bd5928f98
summerlunaa <rhgus1085@gmail.com>
2021-12-12 06:33:15 +0900

    feat(NumberValidator): checkLowLimitOfPrice 메서드 구현

--- src/main/java/vendingmachine/validator/NumberValidator.java
+++ src/main/java/vendingmachine/validator/NumberValidator.java
@@ -3,6 +3,8 @@ package vendingmachine.validator;
 public class NumberValidator {
 	static final String MSG_NOT_INTEGER_ERROR = "[ERROR] 정수를 입력해야 한다.";
 	static final String MSG_LESS_THAN_ZERO_ERROR = "[ERROR] 0보다 크거나 같아야 한다.";
+	static final String MSG_LESS_THAN_PRICE_LIMIT_ERROR = "[ERROR] 100원 이상이어야 한다.";
+	static final int LOW_LIMIT_OF_PRICE = 100;
 
 	static public void isInteger(String number) {
 		try {
@@ -15,6 +17,12 @@ public class NumberValidator {
 	static public void isGreaterThanOrEqualToZero(int number) {
 		if (number < 0) {
 			throw new IllegalArgumentException(MSG_LESS_THAN_ZERO_ERROR);
+		}
+	}
+
+	static public void checkLowLimitOfPrice(int number) {
+		if (number < LOW_LIMIT_OF_PRICE) {
+			throw new IllegalArgumentException(MSG_LESS_THAN_PRICE_LIMIT_ERROR);
 		}
 	}
 }
