4619ce615606f3d32541bc52ef36fedd1d99e356
hyeonic <dev.hyeonic@gmail.com>
2021-12-11 22:41:21 +0900

    feat: VendingMachineController 생성자 추가

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,10 +1,13 @@
 package vendingmachine;
 
 import vendingmachine.controller.VendingMachineController;
+import vendingmachine.domain.RandomPossessionCoinsGenerator;
 
 public class Application {
     public static void main(String[] args) {
-        VendingMachineController vendingMachineController = new VendingMachineController();
+        VendingMachineController vendingMachineController = new VendingMachineController(
+            new RandomPossessionCoinsGenerator());
+
         vendingMachineController.run();
     }
 }
--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -1,11 +1,19 @@
 package vendingmachine.controller;
 
+import vendingmachine.domain.PossessionCoins;
+import vendingmachine.domain.PossessionCoinsGenerator;
 import vendingmachine.domain.PossessionMoney;
 import vendingmachine.view.InputView;
 
 public class VendingMachineController {
+    private final PossessionCoinsGenerator possessionCoinsGenerator;
+
+    public VendingMachineController(PossessionCoinsGenerator possessionCoinsGenerator) {
+        this.possessionCoinsGenerator = possessionCoinsGenerator;
+    }
 
     public void run() {
         PossessionMoney possessionMoney = InputView.getPossessionMoney();
+        PossessionCoins possessionCoins = possessionCoinsGenerator.generate(possessionMoney);
     }
 }
