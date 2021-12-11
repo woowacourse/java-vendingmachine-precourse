f5370adfc00bcb050c5d6b5f6fb1ad3aad5c4115
iamsseaa <homingsea@gmail.com>
2021-12-11 23:07:58 +0900

    feat: 사용자 금액 입력 관련 에러 문구

--- src/main/java/vendingmachine/user/InputErrorConstant.java
+++ src/main/java/vendingmachine/user/InputErrorConstant.java
@@ -1,4 +1,9 @@
 package vendingmachine.user;
 
 public class InputErrorConstant {
+    public static final String ERROR_PREFIX = "[ERROR]";
+    public static final String ERROR_IS_NOT_NUMBER = "금액은 숫자여야 합니다.";
+    public static final String ERROR_IS_NOT_POSITIVE = "금액은 양수여야 합니다.";
+    public static final String ERROR_IS_NULL = "금액을 입력하셔야 합니다.";
+    public static final String ERROR_HAS_SPACE = "금액 입력 중간에 빈칸이 포함될 수 없습니다.";
 }
