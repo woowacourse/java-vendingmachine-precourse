ae043a72a4802b3eeca50e46811ef380468485cc
pup-paw <wldusdl0310@gmail.com>
2021-12-11 23:01:14 +0900

    feat: 프로젝트 전반에 사용되는 상수 처리

---
+++ src/main/java/vendingmachine/util/Constant.java
@@ -0,0 +1,17 @@
+package vendingmachine.util;
+
+public class Constant {
+	public static final String ASK_HOLDING_AMOUNT = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+	public static final String ASK_PRODUCT_INFO = "상품명과 가격, 수량을 입력해 주세요.";
+	public static final String ASK_INPUT_AMOUNT = "투입 금액을 입력해 주세요.";
+	public static final String ASK_PRODUCT_NAME_TO_PURCHASE = "구매할 상품명을 입력해 주세요.";
+	public static final String SHOW_HOLDING_COINS = "자판기가 보유한 동전";
+	public static final String SHOW_INPUT_AMOUNT = "투입 금액: ";
+	public static final String SHOW_CHANGES = "잔돈";
+	public static final String ERROR_IS_NOT_INTEGER = "[ERROR] 숫자를 입력해 주세요.";
+	public static final String ERROR_IS_NOT_POSITIVE = "[ERROR] 양의 정수를 입력해 주세요.";
+	public static final String ERROR_IS_NOT_MULTIPLE_OF_TEN = "[ERROR] 10의 배수를 입력해 주세요.";
+	public static final String ERROR_IS_NOT_VALIDATE_INFO_FORMAT = "[ERROR] 상품 정보는 [ 과 ] 으로 묶어 입력해 주세요.";
+	public static final String ERROR_IS_NOT_3_INFO = "[ERROR] 상품의 정보는 상품명, 가격, 수량 3가지만 입력해 주세요.";
+	public static final String ERROR_DOES_NOT_EXIST = "[ERROR] 올바른 상품명을 입력해 주세요.";
+}
