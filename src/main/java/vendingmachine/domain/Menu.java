3cb0c8f9850868bdb09c332621d42a63c2718609
JunHyeok.Bang <bangvkf3@gmail.com>
2021-12-11 15:49:20 +0900

    feat: 메뉴명 검증 - 대괄호

--- src/main/java/vendingmachine/domain/Menu.java
+++ src/main/java/vendingmachine/domain/Menu.java
@@ -1,6 +1,8 @@
 package vendingmachine.domain;
 
 public class Menu {
+	private static final String ERROR_SQUARE_BRACKETS = "[ERROR] 대괄호 사이에 메뉴정보를 입력해야 합니다.";
+
 	private String name;
 	private int price;
 	private int count;
@@ -12,6 +14,22 @@ public class Menu {
 	}
 
 	public static Menu from(String menuInfo) {
+		parseMenuInfo(menuInfo);
 		return null;
+	}
+
+	private static void parseMenuInfo(String menuInfo) {
+		menuInfo = removeBrackets(menuInfo);
+	}
+
+	private static String removeBrackets(String menuInfo) {
+		validateBrackets(menuInfo);
+		return menuInfo.substring(1, menuInfo.length() - 1);
+	}
+
+	private static void validateBrackets(String menuInfo) {
+		if (!menuInfo.matches("\\[.*\\]")) {
+			throw new IllegalArgumentException(ERROR_SQUARE_BRACKETS);
+		}
 	}
 }
