8709adde749ad1f7565a575fe46aabbdb6863709
Seungmin Lim <seungmin9066@gmail.com>
2021-12-11 13:27:07 +0900

    [추가] 문제의 알고리즘 순서대로 구현

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,7 +1,14 @@
 package vendingmachine;
 
+import vendingmachine.controller.VendingMachineController;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		// TODO: 프로그램 구현
+		VendingMachineController vendingMachine = new VendingMachineController();
+		vendingMachine.addProduct();
+		vendingMachine.buyProduct();
+		vendingMachine.showChange();
+	}
+
 }
