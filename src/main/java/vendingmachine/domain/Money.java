8efa12893aefd81a22ce13d8aca1f1db8c4593c4
xfrnk2 <xfrnk2@gmail.com>
2021-12-11 23:04:23 +0900

    refactor(Money): 출력시 텍스트에 String format 적용

--- src/main/java/vendingmachine/domain/Money.java
+++ src/main/java/vendingmachine/domain/Money.java
@@ -2,7 +2,7 @@ package vendingmachine.domain;
 
 public class Money {
 	private static final String COLON = ":";
-	private static final String INSERTED_REMAINING_AMOUNT = "투입 금액 " + COLON;
+	private static final String INSERTED_REMAINING_AMOUNT = "투입 금액%s %s원";
 	private int money;
 
 	public Money(int money) {
@@ -23,6 +23,6 @@ public class Money {
 
 	@Override
 	public String toString() {
-		return INSERTED_REMAINING_AMOUNT + money + " ";
+		return String.format(INSERTED_REMAINING_AMOUNT, COLON, money);
 	}
 }
