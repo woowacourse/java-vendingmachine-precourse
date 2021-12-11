54fd4ab3f25f367a5f267310c4c48d12c9f0e128
SheepEatLion <gowjr207@gmail.com>
2021-12-11 17:18:08 +0900

    refactor : Application.java
    - 기능 분리
    - 메인에서 inputMachineOwnMoneyAndValidation() 메서드를 호출하던 코드를 inputMachineOwnMoney() 라는 새로운 메서드를 만들어 기능을 분리함으로써 각 메서드 마다 15줄 이하의 코드를 유지한다.

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -8,6 +8,10 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String machineOwnMoney = "";
+        inputMachineOwnMoney(machineOwnMoney);
+    }
+
+    public static void inputMachineOwnMoney(String machineOwnMoney){
         while(machineOwnMoney.isEmpty()) {
             try {
                 machineOwnMoney = inputMachineOwnMoneyAndValidation();
