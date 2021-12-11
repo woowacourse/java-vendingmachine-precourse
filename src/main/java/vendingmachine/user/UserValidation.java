21cb9d2288112385cd9aa9646d0ce98f9d193068
iamsseaa <homingsea@gmail.com>
2021-12-11 23:14:00 +0900

    feat: 사용자가 입력한 금액이 양수인지 확인

--- src/main/java/vendingmachine/user/UserValidation.java
+++ src/main/java/vendingmachine/user/UserValidation.java
@@ -6,4 +6,10 @@ public class UserValidation {
             throw new IllegalArgumentException(InputErrorConstant.ERROR_IS_NOT_NUMBER);
         }
     }
+
+    public static void isPositive(String money) {
+        if (Integer.parseInt(money) <= 0) {
+            throw new IllegalArgumentException(InputErrorConstant.ERROR_IS_NOT_POSITIVE);
+        }
+    }
 }
