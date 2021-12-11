63978c5a5ed51f37aec7c4eeae78d53fa7ffdcf3
hyeonic <dev.hyeonic@gmail.com>
2021-12-12 01:58:29 +0900

    feat: 투입 금액을 관리하기 위한 InvestmentMoney 추가

---
+++ src/main/java/vendingmachine/domain/InvestmentMoney.java
@@ -0,0 +1,37 @@
+package vendingmachine.domain;
+
+public class InvestmentMoney {
+    private static final int DEFAULT_INVESTMENT_MONEY = 0;
+    private static final int QUOTIENT = 10;
+    private static final int REMAINDER = 0;
+
+    private int investmentMoney;
+
+    public InvestmentMoney(String inputInvestmentMoney) {
+        validateNumberFormat(inputInvestmentMoney);
+        int investmentMoney = Integer.parseInt(inputInvestmentMoney);
+        validateNegativeNumber(investmentMoney);
+        validateDivide(investmentMoney);
+        this.investmentMoney = investmentMoney;
+    }
+
+    private void validateNumberFormat(String inputInvestmentMoney) {
+        try {
+            Integer.parseInt(inputInvestmentMoney);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNegativeNumber(int investmentMoney) {
+        if (investmentMoney < DEFAULT_INVESTMENT_MONEY) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDivide(int investmentMoney) {
+        if (investmentMoney % QUOTIENT != REMAINDER) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
