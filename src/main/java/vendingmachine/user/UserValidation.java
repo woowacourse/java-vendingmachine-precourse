abdd0625a7deb3eccefd96abaac292577840b286
iamsseaa <homingsea@gmail.com>
2021-12-11 23:17:49 +0900

    feat: 입력에 빈칸이 포함되어 있는지 확인

--- src/main/java/vendingmachine/user/UserValidation.java
+++ src/main/java/vendingmachine/user/UserValidation.java
@@ -12,4 +12,10 @@ public class UserValidation {
             throw new IllegalArgumentException(InputErrorConstant.ERROR_IS_NOT_POSITIVE);
         }
     }
+
+    public static void hasEmpty(String money) {
+        if (money.contains(InputErrorConstant.EMPTY_SPACE)) {
+            throw new IllegalArgumentException(InputErrorConstant.ERROR_HAS_SPACE);
+        }
+    }
 }
