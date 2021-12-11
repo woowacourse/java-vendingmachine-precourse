bd62501f7ac32cdf68fd54bd4212a5a5d71b8d5f
SheepEatLion <gowjr207@gmail.com>
2021-12-11 17:11:30 +0900

    feat : Application.java
    - 새로운 메서드 추가: inputMachineOwnMoneyAndValidation()
    - 사용자로부터 금액을 입력받고 (올바른 숫자, 10원 이상)에 따라 예외처리하여 IllegalArgumentException을 발생시키고 에러메시지를 출력한 뒤 사용자로부터 재입력을 받는다.

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,7 +1,27 @@
 package vendingmachine;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String machineOwnMoney = "";
+        while(machineOwnMoney.isEmpty()) {
+            try {
+                machineOwnMoney = inputMachineOwnMoneyAndValidation();
+            } catch (IllegalArgumentException e){
+                System.out.println("[ERROR] 금액은 숫자여야 합니다.");
+            }
+        }
+    }
+
+    public static String inputMachineOwnMoneyAndValidation(){
+        System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
+        String machineOwnMoney = Console.readLine();
+        String regex = "^[0-9]{2,}$";
+            if (!Pattern.matches(regex, machineOwnMoney)) throw new IllegalArgumentException();
+        return machineOwnMoney;
     }
 }
