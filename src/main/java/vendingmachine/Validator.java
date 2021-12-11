0b07524be5e21a0868a12cfc2248c57dee36fc1f
lomuto <kangys1245@gmail.com>
2021-12-11 15:46:27 +0900

    Add product validator

--- src/main/java/vendingmachine/Validator.java
+++ src/main/java/vendingmachine/Validator.java
@@ -1,15 +1,52 @@
 package vendingmachine;
 
 public class Validator {
-    public static void validateInputCash(String buffer) throws MyIllegalArgumentException {
+    private static final int MAX_CASH_DIGIT = 5;
+    private static final int MAX_PRODUCT_NAME_LENGTH = 10;
+    private static final int MAX_PRODUCT_PRICE = 100000000;
+    private static final int MAX_PRODUCT_AMOUNT = 100;
+
+    public static void validateCashInput(String buffer) throws MyIllegalArgumentException {
         for (char c : buffer.toCharArray()) {
             if (!Character.isDigit(c)) {
                 throw new MyIllegalArgumentException("Input must be a positive integer value");
             }
         }
 
-        if (buffer.length() > 5) {
+        if (buffer.length() > MAX_CASH_DIGIT) {
             throw new MyIllegalArgumentException("Input value shouldn't be larger than 99999");
+        }
+    }
+
+    public static void validateProductInput(String[] tokens) {
+        if (tokens[0].length() > MAX_PRODUCT_NAME_LENGTH) {
+            throw new MyIllegalArgumentException(
+                    String.format("Product name shouldn't be longer than %d", MAX_PRODUCT_NAME_LENGTH)
+            );
+        }
+
+        for (char c : tokens[1].toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new MyIllegalArgumentException("Price of product must be a positive integer value");
+            }
+        }
+
+        if (tokens[1].length() > MAX_PRODUCT_PRICE) {
+            throw new MyIllegalArgumentException(
+                    String.format("Product price shouldn't be higher than %d", MAX_PRODUCT_PRICE)
+            );
+        }
+
+        for (char c : tokens[2].toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new MyIllegalArgumentException("Amount of product must be a positive integer value");
+            }
+        }
+
+        if (tokens[2].length() > MAX_PRODUCT_AMOUNT) {
+            throw new MyIllegalArgumentException(
+                    String.format("Product amount shouldn't be larger than %d", MAX_PRODUCT_AMOUNT)
+            );
         }
     }
 }
