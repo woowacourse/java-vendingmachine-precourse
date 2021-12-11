46884630127254d4df3863e3f22d4f792cd00497
JunHyeok.Bang <bangvkf3@gmail.com>
2021-12-11 15:22:14 +0900

    feat: 메뉴 정보(상품명, 가격, 수량) 입력 기능 추가

--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -1,6 +1,7 @@
 package vendingmachine.controller;
 
 import vendingmachine.domain.Coins;
+import vendingmachine.domain.Menus;
 import vendingmachine.domain.VendingMachine;
 import vendingmachine.view.ErrorView;
 import vendingmachine.view.InputView;
@@ -8,11 +9,14 @@ import vendingmachine.view.OutputView;
 
 public class VendingMachineController {
 	public void init() {
-		String initialLeftMoney = InputView.getNoticeInputInitialLeftMoney();
+		String initialLeftMoney = InputView.getInitialLeftMoney();
 		try {
-			VendingMachine vendingMachine = VendingMachine.of(initialLeftMoney);
+			VendingMachine vendingMachine = VendingMachine.from(initialLeftMoney);
 			Coins leftCoins = vendingMachine.getLeftCoins();
 			OutputView.showInitialLeftCoins(leftCoins);
+
+			String menuInfos = InputView.getMenuInfo();
+			Menus menus = Menus.from(menuInfos);
 		} catch (Exception e) {
 			ErrorView.printErrorMesasge(e.getMessage());
 			init();
--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -4,9 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 	private static final String NOTICE_INPUT_INITIAL_LEFT_MONEY = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+	private static final String NOTICE_INPUT_MENU_INFOS = "상품명과 가격, 수량을 입력해 주세요.";
 
-	public static String getNoticeInputInitialLeftMoney() {
+	public static String getInitialLeftMoney() {
 		System.out.println(NOTICE_INPUT_INITIAL_LEFT_MONEY);
+		return Console.readLine();
+	}
+
+	public static String getMenuInfo() {
+		System.out.println(NOTICE_INPUT_MENU_INFOS);
 		return Console.readLine();
 	}
 }
