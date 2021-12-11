0502ea96d31338de1e5e53674d03a27702ecffd3
hyeonic <dev.hyeonic@gmail.com>
2021-12-11 22:15:05 +0900

    feat: PossessionCoin 리스트 관리를 위한 일급 컬렉션 PossessionCoins 추가

---
+++ src/main/java/vendingmachine/domain/PossessionCoins.java
@@ -0,0 +1,11 @@
+package vendingmachine.domain;
+
+import java.util.List;
+
+public class PossessionCoins {
+    List<PossessionCoin> possessionCoins;
+
+    public PossessionCoins(List<PossessionCoin> possessionCoins) {
+        this.possessionCoins = possessionCoins;
+    }
+}
