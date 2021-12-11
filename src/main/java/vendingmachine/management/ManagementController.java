51c2bf4832cc05cd1a5ee20d0e8e9dd295ec980c
steven <seokju73@gmail.com>
2021-12-11 19:50:58 +0900

    Feat: Add function to prepare inventory in management controller

--- src/main/java/vendingmachine/management/ManagementController.java
+++ src/main/java/vendingmachine/management/ManagementController.java
@@ -3,9 +3,6 @@ package vendingmachine.management;
 import java.util.Arrays;
 
 import vendingmachine.management.validation.CheckCommodityFormat;
-import vendingmachine.management.validation.CheckCommodityName;
-import vendingmachine.management.validation.CheckCommodityPrice;
-import vendingmachine.management.validation.CheckCommodityQuantity;
 import vendingmachine.management.validation.CheckDepositFigure;
 import vendingmachine.view.InputView;
 import vendingmachine.view.outputView.ManagementView;
@@ -46,9 +43,17 @@ public class ManagementController {
                 String [] commodityInfo = input.split(";");
                 CheckCommodityFormat.validation(commodityInfo);
                 Arrays.stream(commodityInfo).forEach(s -> s.substring(1, s.length()-1));
+                putCommodities(commodityInfo);
             } catch (IllegalArgumentException e){
                 System.out.println(e.getMessage());
             }
+        }
+    }
+    
+    private static void putCommodities(String [] commodityInfo) {
+        for(String s : commodityInfo) {
+            Commodity commodity = ManagementService.toCommodity(s);
+            ManagementService.addCommodityInList(commodity);
         }
     }
 }
