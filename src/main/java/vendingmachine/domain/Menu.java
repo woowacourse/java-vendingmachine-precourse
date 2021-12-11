0a820b18100a03c498d07b93e82edddc046bc004
JunHyeok.Bang <bangvkf3@gmail.com>
2021-12-11 16:07:47 +0900

    feat: 메뉴명 검증 - 상품명,가격,수량 포맷

--- src/main/java/vendingmachine/domain/Menu.java
+++ src/main/java/vendingmachine/domain/Menu.java
@@ -1,7 +1,11 @@
 package vendingmachine.domain;
 
 public class Menu {
-	private static final String ERROR_SQUARE_BRACKETS = "[ERROR] 대괄호 사이에 메뉴정보를 입력해야 합니다.";
+
+	private static final String ERROR_SQUARE_BRACKETS = "[ERROR] 대괄호 사이에 상품정보를 입력해야 합니다.";
+	private static final String ERROR_DELIMITERS = "[ERROR] 상품 정보는 [상품명,가격,수량] 형태로 입력해야 합니다.";
+
+	private static final String DELIMITER = ",";
 
 	private String name;
 	private int price;
@@ -20,6 +24,7 @@ public class Menu {
 
 	private static void parseMenuInfo(String menuInfo) {
 		menuInfo = removeBrackets(menuInfo);
+		String[] infoArray = splitMenuInfo(menuInfo);
 	}
 
 	private static String removeBrackets(String menuInfo) {
@@ -30,6 +35,17 @@ public class Menu {
 	private static void validateBrackets(String menuInfo) {
 		if (!menuInfo.matches("\\[.*\\]")) {
 			throw new IllegalArgumentException(ERROR_SQUARE_BRACKETS);
+		}
+	}
+
+	private static String[] splitMenuInfo(String menuInfo) {
+		validateDelimeters(menuInfo);
+		return menuInfo.split(DELIMITER);
+	}
+
+	private static void validateDelimeters(String menuInfo) {
+		if (!menuInfo.matches(".+,.+,.+")) {
+			throw new IllegalArgumentException(ERROR_DELIMITERS);
 		}
 	}
 }
