dddc7553d1ef11efa260c542a5e671edff947886
Leeyerimearth <50127628+Leeyerimearth@users.noreply.github.com>
2021-12-11 22:42:24 +0900

    feat: 구매할 상품이 구매 가능한지 체크하는 기능

--- src/main/java/vendingmachine/Machine.java
+++ src/main/java/vendingmachine/Machine.java
@@ -13,7 +13,17 @@ public class Machine {
         this.products = InputView.inputProductList();
         this.money = InputView.inputMoney();
         while (true) {
+            OutputView.printMoney(money);
             InputView.inputProduct();
         }
+    }
+
+    public boolean isPurchasable(String productName) {
+        for (Product product : this.products) {
+            if (product.getName().equals(productName) && product.getPrice() >= money && product.getCount() > 0) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/main/java/vendingmachine/Product.java
+++ src/main/java/vendingmachine/Product.java
@@ -10,4 +10,16 @@ public class Product {
         this.price = price;
         this.count = count;
     }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
 }
