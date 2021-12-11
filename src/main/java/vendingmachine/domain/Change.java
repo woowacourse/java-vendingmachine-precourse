fca30845820125d2d9a5b2278e9a5ce86fd5fb53
Donggyu Kim <a29661498@gmail.com>
2021-12-11 15:03:46 +0900

    feat: 자판기가 보유하고 있는 금액에 따라 랜덤하게 동전 생성
    자판기가 보유하고 있는 금액(Money) 값에 따라 랜덤하게 동전(Change) 생성하는 코드를 작성하였습니다.

--- src/main/java/vendingmachine/domain/Change.java
+++ src/main/java/vendingmachine/domain/Change.java
@@ -3,11 +3,45 @@ package vendingmachine.domain;
 import java.util.HashMap;
 import java.util.Map;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInList;
+import static vendingmachine.domain.Coin.*;
+
 public class Change {
 
     private Map<Coin, Integer> coinMap = new HashMap<>();
+    private int totalMoney;
 
-    public Change() {
-        //랜덤하게 동전 생성하는 코드 필요
+    public Change(int money) {
+        this.totalMoney = money;
+        initCoinMap();
+    }
+
+    public void createCoin() {
+        while (totalMoney > 0) {
+            int index = pickNumberInList(getCoinEnumValueList()) - 1;
+
+            Coin coin = getCoin(index);
+
+            Integer count = coinMap.get(coin);
+
+            if (checkValidCoin(coin.getAmount())) {
+                coinMap.put(coin, count++);
+            }
+        }
+    }
+
+    private boolean checkValidCoin (int coinAmount) {
+        if (coinAmount <= totalMoney) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void initCoinMap() {
+        coinMap.put(COIN_10, 0);
+        coinMap.put(COIN_50, 0);
+        coinMap.put(COIN_100, 0);
+        coinMap.put(COIN_500, 0);
     }
 }
--- src/main/java/vendingmachine/domain/Coin.java
+++ src/main/java/vendingmachine/domain/Coin.java
@@ -1,5 +1,9 @@
 package vendingmachine.domain;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
@@ -14,5 +18,29 @@ public enum Coin {
 
     public int getAmount() {
         return this.amount;
+    }
+
+    public static List<Integer> getCoinEnumValueList() {
+        List<Integer> coinList = Arrays.stream(Coin.values())
+                .map(c -> (c.getAmount()))
+                .collect(Collectors.toList());
+
+        return coinList;
+    }
+
+    public static List<Coin> getCoinEnumList() {
+        List<Coin> coinList = Arrays.
+                stream(Coin.values()).
+                collect(Collectors.toList());
+
+        return coinList;
+    }
+
+    public static Coin getCoin (int index) {
+        List<Coin> coinList = getCoinEnumList();
+
+        Coin coin = coinList.get(index);
+
+        return coin;
     }
 }
