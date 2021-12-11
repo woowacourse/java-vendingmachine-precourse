e13640e5d45424fab2189cab7cafc38ef0fd524d
steven <seokju73@gmail.com>
2021-12-11 21:37:32 +0900

    Feat: Add validation to check if commodity is in stock

--- src/main/java/vendingmachine/operation/validation/CheckCommoditySelection.java
+++ src/main/java/vendingmachine/operation/validation/CheckCommoditySelection.java
@@ -6,10 +6,17 @@ import vendingmachine.management.Commodity;
 
 public class CheckCommoditySelection {
     private static final String IS_NOT_REGISTERED = "[ERROR] 해당 상품이 존재하지 않습니다.";
+    private static final String IS_SOLD_OUT = "[ERROR] 해당 상품이 품절 되었습니다.";
     
     public static void validationIsNotRegistered(List<Commodity> inventory, String name) {
         if(!inventory.stream().anyMatch(c -> c.getName().equals(name))) {
             throw new IllegalArgumentException(IS_NOT_REGISTERED);
+        }
+    }
+    
+    public static void validationSoldOut(Commodity commodity) {
+        if(commodity.getQuantity() < 1) {
+            throw new IllegalArgumentException(IS_SOLD_OUT);
         }
     }
 }
