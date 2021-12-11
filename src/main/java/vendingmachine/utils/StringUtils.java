83b4a0f4a78575f80519d49933c02dfe8e0e114c
devHudi <devhudi@gmail.com>
2021-12-11 16:14:21 +0900

    refactor: 파라미터명 str 을 input 으로 변경

--- src/main/java/vendingmachine/utils/StringUtils.java
+++ src/main/java/vendingmachine/utils/StringUtils.java
@@ -1,10 +1,9 @@
 package vendingmachine.utils;
 
 public class StringUtils {
-	// TODO: str 보다 더 좋은 네이밍 고민 필요
-	public static boolean isNumeric(String str) {
+	public static boolean isNumeric(String input) {
 		try {
-			Integer.parseInt(str);
+			Integer.parseInt(input);
 			return true;
 		} catch (NumberFormatException e) {
 			return false;
