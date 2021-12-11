7f82bdc3247f93d997f1c96fe6485438632a7c59
jaeyoungjang2 <lion85488@gmail.com>
2021-12-11 21:07:31 +0900

    feat: 상품 목록을 입력받는 기능 생성  - 상품을 입력 받고, 입력 받은 상품이 요구 사항을 지키지 않았을 때 재 입력을 받는 로직 생성

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -39,7 +39,49 @@ public class VendingMachine {
         }
     }
 
+    public Changes changes() {
+        return changes;
+    }
 
+    public Map<Integer, Integer> getChanges() {
+        return changes.changes();
+    }
 
+    public void putMoney(String stringMoney) {
+        this.money.add(stringMoney);
+    }
 
+    public void buyMerchandise(String targetMerchandise) {
+        validTargetMerchandise(targetMerchandise);
+        merchandises.buy(targetMerchandise);
+        Integer cost = merchandises.cost(targetMerchandise);
+        money.spend(cost);
+    }
+
+    private void validTargetMerchandise(String targetMerchandise) {
+        if (!merchandises.exist(targetMerchandise)) {
+            throw new IllegalArgumentException("[ERROR] : 해당 상품은 존재하지 않습니다. 존재하는 상품을 입력 부탁드립니다.");
+        }
+    }
+
+    public boolean canBuySomething() {
+        return money.amount() >= merchandises.cheapest();
+    }
+
+    public void setMerchandise(String requireVendingMachineMerchandiseInfo) {
+        this.merchandises = new Merchandises(requireVendingMachineMerchandiseInfo);
+    }
+
+    public int money() {
+        return money.amount();
+    }
+
+    public Map<Integer, Integer> calculateChangeToCustomer() {
+
+        return changes.calculateChangeToCustomer(money.amount());
+    }
+
+    public boolean soldOut() {
+        return merchandises.soldOut();
+    }
 }
