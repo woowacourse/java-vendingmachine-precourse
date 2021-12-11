0173b1c789c8d70d05375361d8d160851f8e1520
meatsby <meatsby@gmail.com>
2021-12-11 19:48:06 +0900

    feat: 상품 목록 입력 및 예외처리 기능 구현
    - 등록할 상품 목록 입력 기능 구현
    - 예외 출력 기능 구현
      - 상품 목록 입력이 빈칸인지 검증
      - 상품 목록 입력에 ;만 입력됐는지 검증
      - 상품 정보 입력이 서식에 맞는지 검증 [한글영어숫자, 3자리 이상의 양수, 양수]
      - 상품 가격이 10원 단위로 나누어지는지 검증
      - 상품 갯수가 1개 이상인지 검증

--- src/main/java/vendingmachine/Validation.java
+++ src/main/java/vendingmachine/Validation.java
@@ -1,12 +1,29 @@
 package vendingmachine;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class Validation {
 	private static final String ERROR = "[ERROR] ";
 	private static final String NOT_NUMBER_ERROR = "금액은 숫자여야 합니다.";
 	private static final String NOT_DIVIDABLE_ERROR = "금액은 10원 단위로 나누어져야 합니다.";
 	private static final String NOT_POSITIVE_ERROR = "금액은 양수여야 합니다.";
+	private static final String BLANK = "";
+	private static final String NO_ITEMS_ERROR = "상품이 입력되지 않았습니다.";
+	private static final String ITEM_DIVIDER = ";";
+	private static final String REGEX = "\\[[a-zA-Z0-9가-힣]+,\\d{3,},\\d+\\]";
+	private static final String NOT_VALID_FORMAT = "입력 형식이 잘못되었습니다.";
+	private static final String INFO_DIVIDER = ",";
+	private static final String NOT_VALID_PRICE = "상품가격은 10원 단위로 나누어져야 합니다.";
+	private static final String NOT_ENOUGH_QUANTITY = "해당 상품이 부족합니다.";
+
 	private static final int TEN_WON = 10;
 	private static final int ZERO_WON = 0;
+	private static final int EMPTY = 0;
+	private static final int ITEM_PACKAGE = 1;
+	private static final int PRICE = 1;
+	private static final int QUANTITY = 2;
 
 	public static int isValidBalance(String input) {
 		int balance = isDigit(input);
@@ -32,6 +49,51 @@ public class Validation {
 	private static void isPositive(int balance) {
 		if (balance < ZERO_WON) {
 			throw new IllegalArgumentException(ERROR + NOT_POSITIVE_ERROR);
+		}
+	}
+
+	public static List<String> isValidItems(String input) {
+		List<String> items = isNotBlank(input);
+		isNotEmpty(items);
+		return items;
+	}
+
+	private static List<String> isNotBlank(String input) {
+		if (input.equals(BLANK)) {
+			throw new IllegalArgumentException(ERROR + NO_ITEMS_ERROR);
+		}
+		return Arrays.asList(input.split(ITEM_DIVIDER));
+	}
+
+	private static void isNotEmpty(List<String> items) {
+		if (items.size() == EMPTY) {
+			throw new IllegalArgumentException(ERROR + NO_ITEMS_ERROR);
+		}
+	}
+
+	public static List<String> isValidItem(String input) {
+		List<String> item = isInFormat(input);
+		isValidPrice(item);
+		isEnough(item);
+		return item;
+	}
+
+	private static List<String> isInFormat(String input) {
+		if (!Pattern.matches(REGEX, input)) {
+			throw new IllegalArgumentException(ERROR + NOT_VALID_FORMAT);
+		}
+		return Arrays.asList(input.substring(ITEM_PACKAGE, input.length() - ITEM_PACKAGE).split(INFO_DIVIDER));
+	}
+
+	private static void isValidPrice(List<String> item) {
+		if (Integer.parseInt(item.get(PRICE)) % TEN_WON != ZERO_WON) {
+			throw new IllegalArgumentException(ERROR + NOT_VALID_PRICE);
+		}
+	}
+
+	private static void isEnough(List<String> item) {
+		if (Integer.parseInt(item.get(QUANTITY)) <= EMPTY) {
+			throw new IllegalArgumentException(ERROR + NOT_ENOUGH_QUANTITY);
 		}
 	}
 }
--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -4,9 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 	private static final String SET_COINS_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+	private static final String SET_ITEMS_MESSAGE = "상품명과 가격, 수량을 입력해 주세요.";
 
 	public static String setCoins() {
 		System.out.println(SET_COINS_MESSAGE);
+		return Console.readLine();
+	}
+
+	public static String setItems() {
+		System.out.println();
+		System.out.println(SET_ITEMS_MESSAGE);
 		return Console.readLine();
 	}
 }
