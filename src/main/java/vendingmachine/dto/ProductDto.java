7c16a079f001888fec686b3cec3a202669bfd33d
GYUTAEKIM <kkt219a@gmail.com>
2021-12-11 21:04:04 +0900

    feat: 상품을 쉼표(,)를 기준으로 상품명, 가격, 수량으로 분리
    - 상품 문자열을 쉼표 기준으로 상품의 구성요소로 분리

--- src/main/java/vendingmachine/dto/ProductDto.java
+++ src/main/java/vendingmachine/dto/ProductDto.java
@@ -1,6 +1,11 @@
 package vendingmachine.dto;
 
+import vendingmachine.Notification;
+
 public class ProductDto {
+	private static final int PRODUCT_COMPOSITION_SIZE = 3;
+	private static final int PRODUCT_START_INDEX = 1;
+
 	private final String input;
 
 	public ProductDto(String input) {
@@ -9,5 +14,38 @@ public class ProductDto {
 
 	public void convertProducts() {
 		String[] products = input.split(";");
+		for (String product : products) {
+			convertProduct(product);
+		}
+	}
+
+	private void convertProduct(String product) {
+		product = removePattern(product);
+		String[] productElements = product.split(",");
+		validateSize(productElements.length);
+		createProduct(productElements);
+	}
+
+	private void createProduct(String[] productElements) {
+		String name = productElements[0];
+		String price = productElements[1];
+		String quantity = productElements[2];
+	}
+
+	private String removePattern(String product) {
+		validatePattern(product);
+		return product.substring(PRODUCT_START_INDEX,product.length()-1);
+	}
+
+	private void validateSize(int productSize) {
+		if(productSize != PRODUCT_COMPOSITION_SIZE) {
+			throw new IllegalArgumentException(Notification.PRODUCT_INVALID_COMPOSITION.getMessage());
+		}
+	}
+
+	private void validatePattern(String product) {
+		if(!product.startsWith("[") || !product.endsWith("]")){
+			throw new IllegalArgumentException(Notification.PRODUCT_INVALID_COMPOSITION.getMessage());
+		}
 	}
 }
