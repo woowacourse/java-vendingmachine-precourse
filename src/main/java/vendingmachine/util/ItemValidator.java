4fc9662399ce2e3d25a05f3c9924e8388e971e7b
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-12-11 14:32:56 +0900

    fix<ItemValidator>: 정규 표현식 오류 수정
    + 와 * 을 바꾸어 입력한 오류를 수정했습니다.
    가격은 3자리수 이상이고 0으로 끝나기 위해 중간 숫자에 +를 붙여줘야 합니다.
    반면 수량은 1자리수도 가능하므로 *를 붙여줍니다.

--- src/main/java/vendingmachine/util/ItemValidator.java
+++ src/main/java/vendingmachine/util/ItemValidator.java
@@ -7,7 +7,7 @@ import vendingmachine.view.ErrorMessage;
 public class ItemValidator {
 
 	public static final String ITEM_SEPARATOR = ";";
-	public static final String ITEM_FORM = "^\\[[a-zA-Z가-힣]+,[1-9][0-9]*0,[1-9][0-9]+]$";
+	public static final String ITEM_FORM = "^\\[[a-zA-Z가-힣]+,[1-9][0-9]+0,[1-9][0-9]*]$";
 
 	public static void validate(String inputString) {
 		Arrays.stream(inputString.split(ITEM_SEPARATOR))
