ecee56bb983238e8d8aca2ab10fa8f0a9b394794
steven <seokju73@gmail.com>
2021-12-11 19:10:09 +0900

    Feat: Add method to transform commodity info into 'Commodity' class

--- src/main/java/vendingmachine/management/ManagementService.java
+++ src/main/java/vendingmachine/management/ManagementService.java
@@ -1,14 +1,19 @@
 package vendingmachine.management;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import vendingmachine.Coin;
+import vendingmachine.management.validation.CheckCommodityFormat;
+import vendingmachine.management.validation.CheckCommodityName;
+import vendingmachine.management.validation.CheckCommodityPrice;
+import vendingmachine.management.validation.CheckCommodityQuantity;
 
 public class ManagementService {
-
+    
     public static void generateCoins(int deposit) {
         List<Integer> coinAmounts = Stream.of(Coin.values()).map(a -> a.getAmount()).collect(Collectors.toList());
         
@@ -24,4 +29,10 @@ public class ManagementService {
             Coin.addNumber(coinType);
         }
     }
+    
+    public static Commodity toCommodity(String input) {
+        String [] components = input.split(",");
+        
+        return new Commodity(components[0],Integer.parseInt(components[1]),Integer.parseInt(components[2]));
+    }   
 }
