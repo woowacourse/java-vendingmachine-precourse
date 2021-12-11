96c10457463d596447f794b1bdb4a11bf08f1747
steven <seokju73@gmail.com>
2021-12-11 20:34:25 +0900

    Feat: Add money insertion function in Operation controller

---
+++ src/main/java/vendingmachine/operation/OperationController.java
@@ -0,0 +1,32 @@
+package vendingmachine.operation;
+
+import vendingmachine.view.InputView;
+import vendingmachine.view.outputView.OperationView;
+import vendingmachine.common.CheckMoenyFigure;
+
+public class OperationController {
+    protected static int balance;
+    
+    public static void runOperation(InputView inputView) {
+        OperationView.askInsertionAmount();
+        doBalanceValidation(inputView);
+        
+    }
+    
+    private static void doBalanceValidation(InputView inputView) {
+        while(true) {
+            String input = inputView.insertMoney();
+            try {
+                CheckMoenyFigure.validationFigure(input);
+                balance = Integer.parseInt(input);
+                CheckMoenyFigure.validationPositiveNumber(balance);
+                CheckMoenyFigure.validationUnit(balance);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    
+    
+
+}
