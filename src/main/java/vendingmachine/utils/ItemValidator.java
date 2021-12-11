72b3907c05db7e393d335c878b0120904ab10caa
Hyunta <mohai2618@gmail.com>
2021-12-11 21:44:45 +0900

    refactor(ItemValidator) : 검증 조건들을 메서드로 분리

--- src/main/java/vendingmachine/utils/ItemValidator.java
+++ src/main/java/vendingmachine/utils/ItemValidator.java
@@ -8,17 +8,21 @@ public class ItemValidator {
 	public static final String ERROR_EMPTY_ITEM = "[ERROR] 상품을 한 개이상 등록해주세요.";
 	public static final String ERROR_NUMBER_OF_PROPERTIES = "[ERROR] 상품명과 가격, 수량 3가지 항목을 입력해주세요.";
 	public static final String ERROR_NOT_INTEGER = "[ERROR] 상품 가격, 수량은 숫자로 입력해주세요.";
-	public static final String ERROR_PRICE_UNDER_100 = "[ERROR] 상품 가격은 100원 이상으로 입력해주세요";
+	public static final String ERROR_PRICE_UNDER_MINIMUM = "[ERROR] 상품 가격은 100원 이상으로 입력해주세요";
 	public static final String ERROR_QUANTITY_NOT_POSITIVE = "[ERROR] 상품 수량은 1개 이상으로 입력해주세요";
 	public static final String ERROR_PRICE_NOT_DIVIDE_BY_10 = "[ERROR] 상품 가격은 10원으로 나누어떨어져야 합니다.";
 
 	public void validateItems(String inputValue) {
 		StringTokenizer stringTokenizer = new StringTokenizer(inputValue, ITEMS_DELIMITER);
-		if (stringTokenizer.countTokens() == 0) {
-			throw new IllegalArgumentException(ERROR_EMPTY_ITEM);
-		}
+		isNotEmpty(stringTokenizer);
 		while (stringTokenizer.hasMoreTokens()) {
 			validateItem(stringTokenizer.nextToken());
+		}
+	}
+
+	private void isNotEmpty(StringTokenizer stringTokenizer) {
+		if (stringTokenizer.countTokens() == 0) {
+			throw new IllegalArgumentException(ERROR_EMPTY_ITEM);
 		}
 	}
 
@@ -26,9 +30,11 @@ public class ItemValidator {
 		item = item.replaceAll(START_BRACKET, "").replaceAll(END_BRACKET, "");
 		StringTokenizer stringTokenizer = new StringTokenizer(item, ITEM_DELIMITER);
 		checkProperties(stringTokenizer);
+
 		String itemName = stringTokenizer.nextToken().trim();
 		String itemPrice = stringTokenizer.nextToken().trim();
 		String itemQuantity = stringTokenizer.nextToken().trim();
+
 		checkItemPrice(itemPrice);
 		checkItemQuantity(itemQuantity);
 	}
@@ -40,27 +46,39 @@ public class ItemValidator {
 	}
 
 	private void checkItemPrice(String itemPrice) {
+		isInteger(itemPrice);
+		isOverMin(itemPrice);
+		isDivideBy10(itemPrice);
+	}
+
+	private void checkItemQuantity(String itemQuantity) {
+		isInteger(itemQuantity);
+		isPositive(itemQuantity);
+	}
+
+	private void isInteger(String itemPrice) {
 		try {
-			int price = Integer.parseInt(itemPrice);
-			if (price < MINIMUM_PRICE) {
-				throw new IllegalArgumentException(ERROR_PRICE_UNDER_100);
-			}
-			if (price % 10 != 0) {
-				throw new IllegalArgumentException(ERROR_PRICE_NOT_DIVIDE_BY_10);
-			}
+			Integer.parseInt(itemPrice);
 		} catch (NumberFormatException e) {
 			throw new IllegalArgumentException(ERROR_NOT_INTEGER);
 		}
 	}
 
-	private void checkItemQuantity(String itemQuantity) {
-		try {
-			int quantity = Integer.parseInt(itemQuantity);
-			if (quantity <= 0) {
-				throw new IllegalArgumentException(ERROR_QUANTITY_NOT_POSITIVE);
-			}
-		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException(ERROR_NOT_INTEGER);
+	private void isOverMin(String itemPrice) {
+		if (Integer.parseInt(itemPrice) < MINIMUM_PRICE) {
+			throw new IllegalArgumentException(ERROR_PRICE_UNDER_MINIMUM);
+		}
+	}
+
+	private void isDivideBy10(String itemPrice) {
+		if (Integer.parseInt(itemPrice) % 10 != 0) {
+			throw new IllegalArgumentException(ERROR_PRICE_NOT_DIVIDE_BY_10);
+		}
+	}
+
+	private void isPositive(String itemQuantity) {
+		if (Integer.parseInt(itemQuantity) <= 0) {
+			throw new IllegalArgumentException(ERROR_QUANTITY_NOT_POSITIVE);
 		}
 	}
 
