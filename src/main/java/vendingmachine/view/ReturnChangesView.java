15adfcca8bb0e74ef0e057accfbe2e3b49a0c3d5
yxxnghwan <younghwan960@gmail.com>
2021-12-12 03:10:04 +0900

    feat(ReturnChangesView): 잔돈 반환
    1. 잔돈의 총 동전 숫자가 가장 적도록 계산
    2. 잔돈이 모자란 경우 가능한 만큼만 반환

--- src/main/java/vendingmachine/view/ReturnChangesView.java
+++ src/main/java/vendingmachine/view/ReturnChangesView.java
@@ -1,6 +1,10 @@
 package vendingmachine.view;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import vendingmachine.Application;
+import vendingmachine.domain.Coin;
 import vendingmachine.util.PublicConst;
 import vendingmachine.util.SystemMessage;
 
@@ -9,11 +13,36 @@ public class ReturnChangesView implements View {
 	public void show() {
 		int money = Application.controller.getMoney();
 		printMoneyAndMessage(money);
-
+		printChanges(money);
 	}
 
 	private void printMoneyAndMessage(int money) {
 		System.out.println(SystemMessage.SHOW_INPUT_MONEY + money + PublicConst.MONETARY_UNIT);
 		System.out.println(SystemMessage.RETURN_CHANGES);
+	}
+
+	private void printChanges(int money) {
+		Map<Coin, Integer> changes = new LinkedHashMap<>();
+		Map<Coin, Integer> coins = Application.controller.getCoins();
+
+		calculateChanges(coins, changes, money);
+
+		changes.forEach(Coin::printCoinAndCount);
+	}
+
+	private void calculateChanges(Map<Coin, Integer> coins, Map<Coin, Integer> changes, int money) {
+		for (Coin coin : coins.keySet()) {
+			int coinCount = coins.get(coin);
+
+			if (coinCount == 0)
+				continue;
+
+			int changeCount = Math.min(coinCount, money / coin.getAmount());
+			changes.put(coin, changeCount);
+			money -= coin.getAmount() * changeCount;
+
+			if (money == 0)
+				break;
+		}
 	}
 }
