1ba635b630d5304edf99edea5be39e6cace9cf0a
summerlunaa <rhgus1085@gmail.com>
2021-12-12 06:35:08 +0900

    feat(ProductValidator): checkIsEmptyName 메서드 구현

--- src/main/java/vendingmachine/validator/ProductValidator.java
+++ src/main/java/vendingmachine/validator/ProductValidator.java
@@ -3,6 +3,7 @@ package vendingmachine.validator;
 public class ProductValidator {
 	static final String MSG_INVALIDATE_FORMAT_ERROR = "[ERROR] 상품명, 가격, 수량은 대괄호로 감싸져 있어야 한다.";
 	static final String MSG_INFO_MISS_ERROR = "[ERROR] 상품명, 가격, 수량을 입력해야 한다.";
+	static final String MSG_EMPTY_NAME_ERROR = "[ERROR] 상품명은 공백일 수 없다.";
 	static final String PRODUCT_INPUT_PREFIX = "[";
 	static final String PRODUCT_INPUT_SUFFIX = "]";
 	static final String PRODUCT_INFO_REGEX = ",";
@@ -19,6 +20,12 @@ public class ProductValidator {
 		String [] productInfo = productInput.split(PRODUCT_INFO_REGEX);
 		if (productInfo.length != PRODUCT_INFO_LENGTH) {
 			throw new IllegalArgumentException(MSG_INFO_MISS_ERROR);
+		}
+	}
+
+	static public void checkIsEmptyName(String productName) {
+		if (productName.equals("")) {
+			throw new IllegalArgumentException(MSG_EMPTY_NAME_ERROR);
 		}
 	}
 }
