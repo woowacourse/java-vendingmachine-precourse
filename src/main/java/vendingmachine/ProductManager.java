00b842316e38cdb94c2d7b735c5ce0106a706906
lomuto <kangys1245@gmail.com>
2021-12-11 11:55:08 +0900

    feat: Add ProductManager constructor

---
+++ src/main/java/vendingmachine/ProductManager.java
@@ -0,0 +1,11 @@
+package vendingmachine;
+
+import java.util.HashMap;
+
+public class ProductManager {
+    private HashMap<String, Integer> products = new HashMap<>();
+
+    public ProductManager(){}
+
+
+}
