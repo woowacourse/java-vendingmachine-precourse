c63ba947b5c17a5714f5e9ef97f37fdf9dbb5741
AlphaTechnic <alphatechnicist@gmail.com>
2021-12-11 12:15:09 +0900

    [feat] 자판기 user가 상품을 구매하는 로직 추가

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,10 +1,25 @@
 package vendingmachine;
 
+import inputcontroller.InputGenerator;
+
+import static models.Product.totalRemains;
+import static vendingmachine.VendingMachineMain.*;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        VendingMachineMain.makeInitialCoins();
-        VendingMachineMain.makeInitialProducts();
+        makeInitialCoins();
+        makeInitialProducts();
+
+        InputGenerator.inputUserMoney();
+        while (true) {
+            buy();
+            if (userInputMoney < minCost || totalRemains == 0) {
+                int[] numOfCoin = makeChange();
+                VendingMachineUI.printChange(numOfCoin);
+                break;
+            }
+        }
     }
 }
