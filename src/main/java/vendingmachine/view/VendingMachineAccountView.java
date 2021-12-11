c1ccf818fa8468a21d30f8ec5b68876237b11764
Kangho-Noh <rkdgh98@khu.ac.kr>
2021-12-12 01:36:54 +0900

    refactor: 문자열 format 사용

--- src/main/java/vendingmachine/view/VendingMachineAccountView.java
+++ src/main/java/vendingmachine/view/VendingMachineAccountView.java
@@ -5,6 +5,7 @@ import vendingmachine.domain.VendingMachineAccount;
 
 public class VendingMachineAccountView {
 	private static final String INPUT_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+	private static final String COIN_LIST_FORMAT = "%d원 - %d개";
 
 	private static final VendingMachineAccount vendingMachineAccount = new VendingMachineAccount();
 
@@ -15,7 +16,8 @@ public class VendingMachineAccountView {
 	public static void printCoinList() {
 		for (Coin coin : Coin.values()) {
 			Integer coinCount = vendingMachineAccount.getCoinCount(coin);
-			System.out.println(coin.getAmount() + "원 - " + coinCount + "개");
+			System.out.println(String.format(COIN_LIST_FORMAT, coin.getAmount(), coinCount));
 		}
+		System.out.println();
 	}
 }
