95af7d045b90e12dc259b339d95b2aec60f37a2e
Seungmin Lim <seungmin9066@gmail.com>
2021-12-11 13:27:29 +0900

    [추가] 상수모아놓은 클래스 추가

--- src/main/java/vendingmachine/constant/Constant.java
+++ src/main/java/vendingmachine/constant/Constant.java
@@ -1,4 +1,26 @@
 package vendingmachine.constant;
 
-public class VendingMachineConstant {
+public class Constant {
+	public static final String DISTINGUISH_BETWEEN_PRODUCTS = ";";
+	public static final String DISTINGUISH_BETWEEN_PRODUCT_INFORMATION = ",";
+	public static final String RIGHT_SQUARE_BRACKETS = "\\]";
+	public static final String LEFT_SQUARE_BRACKETS = "\\[";
+	public static final String NO_STOCKS_MESSAGE = "[ERROR] 재고가 남아있지 않습니다.";
+	public static final String VALIDATE_NUMBER_MESSAGE = "[ERROR] 숫자만 입력이 가능합니다.";
+	public static final String ENTER_VENDING_MACHINE_INITIAL_MONEY = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+	public static final String VENDING_MACHINE_COINS = "자판기가 보유한 동전";
+	public static final String RULES_TO_SHOW = "원 - ";
+	public static final String WON = "원";
+	public static final String NUMBER = "개";
+	public static final String ENTER_PRODUCT_AND_NUMBER = "상품명과 가격, 수량을 입력해 주세요.";
+	public static final String ENTER_INPUT_MONEY = "투입 금액을 입력해 주세요.";
+	public static final String INPUT_MONEY = "투입 금액: ";
+	public static final String REMAINING_MONEY = "잔돈";
+	public static final String ENTER_WANT_TO_BUY_PRODUCT = "구매할 상품명을 입력해 주세요.";
+	public static final String REGEX_EXPRESSION_OF_NUMBER = "[+-]?\\d*(\\.\\d+)?";
+	public static final int INITIAL_VALUE = 0;
+	public static final int NAME = 0;
+	public static final int PRICE = 1;
+	public static final int STOCKS = 2;
+	public static final int MINIMUM_STOCKS = 1;
 }
