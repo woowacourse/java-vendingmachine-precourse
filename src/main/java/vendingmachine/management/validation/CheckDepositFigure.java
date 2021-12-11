c0d7e3c58f4bcdf8fae8e83ffa644f4f435bb4dc
steven <seokju73@gmail.com>
2021-12-11 18:03:02 +0900

    Feat: Add validating condition for deposit

--- src/main/java/vendingmachine/management/validation/CheckDepositFigure.java
+++ src/main/java/vendingmachine/management/validation/CheckDepositFigure.java
@@ -2,7 +2,8 @@ package vendingmachine.management.validation;
 
 public class CheckDepositFigure {
     private static final String ERROR_MESSAGE_FIGURE = "[ERROR] 금액은 숫자여야 합니다.";
-    private static final String ERROR_MESSAGE_POSITIVE_NUMBER = "[ERROR] 금액은 0보다 커야 합니다.";
+    private static final String ERROR_MESSAGE_POSITIVE_NUMBER = "[ERROR] 금액은 10원 이상이어야 합니다.";
+    private static final String ERROR_MESSAGE_UNIT = "[ERROR] 금액은 10으로 나누어 떨어져야 합니다.";
 
     public static void validationFigure(String input) {
         try {
@@ -13,8 +14,14 @@ public class CheckDepositFigure {
     }
     
     public static void validationPositiveNumber(int deposit) {
-        if(deposit <= 0) {
+        if(deposit < 10) {
             throw new IllegalArgumentException(ERROR_MESSAGE_POSITIVE_NUMBER);
+        }
+    }
+    
+    public static void validationUnit(int deposit) {
+        if(deposit%10 != 0) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_UNIT);
         }
     }
             
