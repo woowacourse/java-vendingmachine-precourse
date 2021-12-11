10fbad77ba0f51ab360de893718e021cae165789
minsulee <alkwen0996@naver.com>
2021-12-11 20:50:31 +0900

    feat: 상품 수량 예외처리 기능 추가

--- src/main/java/vendingmachine/domain/Product.java
+++ src/main/java/vendingmachine/domain/Product.java
@@ -11,14 +11,25 @@ public class Product {
     private final int count;
 
     public Product(final String name, final int price, final int count) {
-        validateProduct(price);
+        validateProduct(price, count);
         this.name = name;
         this.price = price;
         this.count = count;
     }
 
-    protected void validateProduct(final int price) {
+    protected void validateProduct(final int price, final int count) {
         validatePrice(price);
+        validateCount(count);
+    }
+
+    protected void validateCount(final int count) {
+        if (!isLessThanZero(count)) {
+            throw new IllegalArgumentException(ExceptionMessages.ERROR_MESSAGE_PRODUCT_COUNT_LESS_THAN_ZERO.getErrorMessage());
+        }
+    }
+
+    protected boolean isLessThanZero(final int count) {
+        return count > 0;
     }
 
     protected void validatePrice(final int price) {
--- src/main/java/vendingmachine/utils/ExceptionMessages.java
+++ src/main/java/vendingmachine/utils/ExceptionMessages.java
@@ -7,6 +7,7 @@ public enum ExceptionMessages {
     ERROR_MESSAGE_PRODUCT_INFORMATION_FORMAT("[ERROR] 상품명은 한글, 가격과 수량은 0이상의 숫자만 입력 가능합니다.\n(상품명, 가격, 수량은 쉼표(,)로, 개별 상품은 대괄호([])로 묶어 세미콜론(;)으로 구분해야 합니다.)"),
     ERROR_MESSAGE_PRODUCT_PRICE_FORMAT("[ERROR] 상품가격은 100원부터 시작하며, 10원으로 나누어 떨어져야 합니다."),
     ERROR_MESSAGE_PRODUCT_PRICE_NUMBER("[ERROR] 상품가격은 숫자만 입력 가능합니다."),
+    ERROR_MESSAGE_PRODUCT_COUNT_LESS_THAN_ZERO("[ERROR] 상품수량은 0보다 커야한다."),
     ERROR_MESSAGE_PRODUCT_NAME_KOREAN("[ERROR] 상품이름은 한글로 입력되어야 합니다."),
     ERROR_MESSAGE_INPUT_PURCHASING_COST("[ERROR] 투입 금액은 숫자만 입력 가능합니다.");
 
