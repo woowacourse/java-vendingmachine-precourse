2de764795c571ade9c8e88fd551d77e2a5204467
lomuto <kangys1245@gmail.com>
2021-12-11 16:24:26 +0900

    feat: Add product types validator

--- src/main/java/vendingmachine/Validator.java
+++ src/main/java/vendingmachine/Validator.java
@@ -1,51 +1,70 @@
 package vendingmachine;
 
 public class Validator {
-    private static final int MAX_CASH_DIGIT = 5;
+    private static final int MAX_PRODUCTS_TYPES = 10;
     private static final int MAX_PRODUCT_NAME_LENGTH = 10;
-    private static final int MAX_PRODUCT_PRICE = 100000000;
+    private static final int MAX_PRODUCT_PRICE = 10000;
     private static final int MAX_PRODUCT_AMOUNT = 100;
+    private static final int MAX_CASH = MAX_PRODUCTS_TYPES * MAX_PRODUCT_PRICE * MAX_PRODUCT_AMOUNT;
 
-    public static void validateCashInput(String buffer) throws MyIllegalArgumentException {
+    public static void validateCashInput(String buffer) throws IllegalArgumentException {
         for (char c : buffer.toCharArray()) {
             if (!Character.isDigit(c)) {
                 throw new MyIllegalArgumentException("Input must be a positive integer value");
             }
         }
 
-        if (buffer.length() > MAX_CASH_DIGIT) {
+        if (Integer.parseInt(buffer) > MAX_CASH) {
             throw new MyIllegalArgumentException("Input value shouldn't be larger than 99999");
         }
     }
 
-    public static void validateProductInput(String[] tokens) {
-        if (tokens[0].length() > MAX_PRODUCT_NAME_LENGTH) {
-            throw new MyIllegalArgumentException(
-                    String.format("Product name shouldn't be longer than %d", MAX_PRODUCT_NAME_LENGTH)
-            );
-        }
+    public static void validateProductInput(String[] tokens) throws IllegalArgumentException {
+        validateProductName(tokens[0]);
+        validateProductPrice(tokens[1]);
+        validateProductAmount(tokens[2]);
+    }
 
-        for (char c : tokens[1].toCharArray()) {
-            if (!Character.isDigit(c)) {
-                throw new MyIllegalArgumentException("Price of product must be a positive integer value");
-            }
-        }
-
-        if (Integer.parseInt(tokens[1]) > MAX_PRODUCT_PRICE) {
-            throw new MyIllegalArgumentException(
-                    String.format("Product price shouldn't be higher than %d", MAX_PRODUCT_PRICE)
-            );
-        }
-
-        for (char c : tokens[2].toCharArray()) {
+    private static void validateProductAmount(String amount) throws IllegalArgumentException {
+        for (char c : amount.toCharArray()) {
             if (!Character.isDigit(c)) {
                 throw new MyIllegalArgumentException("Amount of product must be a positive integer value");
             }
         }
 
-        if (Integer.parseInt(tokens[2]) > MAX_PRODUCT_AMOUNT) {
+        if (Integer.parseInt(amount) > MAX_PRODUCT_AMOUNT) {
             throw new MyIllegalArgumentException(
                     String.format("Product amount shouldn't be larger than %d", MAX_PRODUCT_AMOUNT)
+            );
+        }
+    }
+
+    public static void validateProductName(String name) throws IllegalArgumentException {
+        if (name.length() > MAX_PRODUCT_NAME_LENGTH) {
+            throw new MyIllegalArgumentException(
+                    String.format("Product name shouldn't be longer than %d", MAX_PRODUCT_NAME_LENGTH)
+            );
+        }
+    }
+
+    public static void validateProductPrice(String price) throws IllegalArgumentException {
+        for (char c : price.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new MyIllegalArgumentException("Price of product must be a positive integer value");
+            }
+        }
+
+        if (Integer.parseInt(price) > MAX_PRODUCT_PRICE) {
+            throw new MyIllegalArgumentException(
+                    String.format("Product price shouldn't be higher than %d", MAX_PRODUCT_PRICE)
+            );
+        }
+    }
+
+    public static void validateProductsTypes(int productTypes) {
+        if (productTypes > MAX_PRODUCT_PRICE) {
+            throw new MyIllegalArgumentException(
+                    String.format("Product types shouldn't be larger than %d", MAX_PRODUCTS_TYPES)
             );
         }
     }
