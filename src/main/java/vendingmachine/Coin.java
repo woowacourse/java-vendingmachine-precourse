b15cb6070238e1c2ccde5942e27c087059fb4955
lomuto <kangys1245@gmail.com>
2021-12-11 13:08:21 +0900

    feat: Add Coin type getter by amount

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -15,4 +15,21 @@ public enum Coin {
     public int getAmount() {
         return amount;
     }
+
+    public Coin getType(int amount) throws MyIllegalArgumentException {
+        switch (amount) {
+            case 10:
+                return COIN_10;
+            case 50:
+                return COIN_50;
+            case 100:
+                return COIN_100;
+            case 500:
+                return COIN_500;
+            default:
+                throw new MyIllegalArgumentException(
+                        String.format("Invalid unit of coin %d", amount)
+                );
+        }
+    }
 }
