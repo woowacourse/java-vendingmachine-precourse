c8ca5251fec6d98fe3b5cecae1a6197a4f170def
이민재 <lmj938@naver.com>
2021-12-12 03:12:32 +0900

    style : 메시지 출력 내용 변경

--- src/main/java/vendingmachine/constant/ErrorMessage.java
+++ src/main/java/vendingmachine/constant/ErrorMessage.java
@@ -2,7 +2,7 @@ package vendingmachine.constant;
 
 public enum ErrorMessage {
     NULL("[ERROR] 입력값이 비어있습니다."),
-    BIGGER_THAN_MIN_COIN_UNIT("[ERROR] 입력값은 10으로 나누어 떨어져야 합니다."),
+    BIGGER_THAN_MIN_COIN_UNIT("[ERROR] 입력값은 10으로 나누어 떨어지는 값이어야 합니다."),
     NOT_DIGIT("[ERROR] 입력값은 숫자로만 이루어져야 합니다."),
     NOT_CORRECT_BRACKET("[ERROR] 상품 정보는 대괄호로 묶어주어야 합니다."),
     NULL_IN_LIST("[ERROR] 상품의 정보 중 누락된 값이 있습니다."),
