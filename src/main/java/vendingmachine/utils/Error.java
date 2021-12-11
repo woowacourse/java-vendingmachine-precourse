e7658dfcc99ae279f8f554db2b3921a9885bfab8
SeungCheol <ssc6839@gmail.com>
2021-12-11 19:15:31 +0900

    feat: 상품 구매 예외 처리 구현

--- src/main/java/vendingmachine/utils/Error.java
+++ src/main/java/vendingmachine/utils/Error.java
@@ -6,4 +6,7 @@ public class Error {
 	public static String AMOUNT_NOT_POSITIVE_ERROR = "[ERROR] 0 또는 음수가 입력되었습니다. 양수를 입력하세요.";
 	public static String AMOUNT_NOT_DIVIDE_ERROR = "[ERROR] 10원으로 나누어떨어지는 금액을 입력하세요.";
 	public static String AMOUNT_MINIMUM_ERROR = "[ERROR] 상품 가격은 100원부터 시작합니다. 100원 이상의 가격을 입력하세요.";
+
+	public static String ITEM_NOT_EXIST_ERROR = "[ERROR] 해당 상품이 존재하지 않습니다. 다시 입력하세요.";
+	public static String ITEM_NOT_STOCK_ERROR = "[ERROR] 상품 재고가 없습니다. 다른 상품을 입력하세요.";
 }
---
+++ src/main/java/vendingmachine/utils/validator/PurchaseItemValidator.java
@@ -0,0 +1,31 @@
+package vendingmachine.utils.validator;
+
+import static vendingmachine.utils.Error.*;
+
+import java.util.List;
+
+import vendingmachine.domain.Items;
+
+public class PurchaseItemValidator {
+
+	public static void validateItem(Items items, String itemName) {
+		validateStatus(items, itemName);
+		validateStock(items, itemName);
+	}
+
+	private static void validateStatus(Items items, String itemName) {
+		List<String> names = items.toNames();
+
+		if (!names.contains(itemName)) {
+			throw new IllegalArgumentException(ITEM_NOT_EXIST_ERROR);
+		}
+	}
+
+	private static void validateStock(Items items, String itemName) {
+		int quantity = items.getStockByName(itemName);
+
+		if (quantity == 0) {
+			throw new IllegalArgumentException(ITEM_NOT_STOCK_ERROR);
+		}
+	}
+}
