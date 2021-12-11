358778163d7d0c75e2abadb723d1cd9d3eaab244
Leeyerimearth <50127628+Leeyerimearth@users.noreply.github.com>
2021-12-11 20:47:20 +0900

    feat: 투입한 금액의 남은 금액 출력

--- src/main/java/vendingmachine/OutputView.java
+++ src/main/java/vendingmachine/OutputView.java
@@ -5,7 +5,11 @@ public class OutputView {
     public void printMachineCoin() {
         System.out.println("자판기가 보유한 동전");
         for (Coin coin : Coin.values()) {
-            System.out.println(coin.getAmount() + "원 - "+ coin.getCount() + "개");
+            System.out.println(coin.getAmount() + "원 - " + coin.getCount() + "개");
         }
+    }
+
+    public void printMoney(int money) {
+        System.out.println("투입 금액: " + money + "원");
     }
 }
