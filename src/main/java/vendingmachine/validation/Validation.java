01657151f85321240964d67a502d26ad9882ab54
유영우 <fbduddn97@naver.com>
2021-12-11 18:20:30 +0900

    docs: 마크다운 오타 수정

--- src/main/java/vendingmachine/validation/Validation.java
+++ src/main/java/vendingmachine/validation/Validation.java
@@ -44,6 +44,7 @@ public class Validation {
 		if(product.length!=3){
 			throw new IllegalArgumentException(ErrorMessage.PRODUCT_INPUT_LENGTH_ERROR.getErrorMessage());
 		}
+
 	}
 
 	// 상품 가격이 100 이상 자연수가 아닐 경우
@@ -54,6 +55,9 @@ public class Validation {
 			throw new IllegalArgumentException(ErrorMessage.PRODUCT_PRICE_IS_NOT_MORE_THAN_100_ERROR.getErrorMessage());
 		}
 	}
+
+	// 상품 가격이 10으로 나누어 떨어지지 않는 경우, 상품명, 가격, 수량이 빈 값으로 들어오는 경우
+
 
 	// 상품 수량이 자연수가 아닐 경우
 	public static void validateProductAmount(String amount) {
