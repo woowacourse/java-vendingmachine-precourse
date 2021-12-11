41e142db8b7bf8e395abcb5ab8e639615cd253d1
JunHyeok.Bang <bangvkf3@gmail.com>
2021-12-11 20:14:03 +0900

    feat: Menus 검증 및 생성 구현

--- src/main/java/vendingmachine/domain/Menus.java
+++ src/main/java/vendingmachine/domain/Menus.java
@@ -1,8 +1,13 @@
 package vendingmachine.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Menus {
+	private static final String ERROR_DELIMITERS = "[ERROR] 각 상품 정보는 세미콜론(;)으로 구분합니다.";
+	private static final String DELIMITER = ";";
+
 	private final List<Menu> menuList;
 
 	public Menus(List<Menu> menuList) {
@@ -10,6 +15,18 @@ public class Menus {
 	}
 
 	public static Menus from(String menuInfos) {
-		return null;
+		List<Menu> menuList = new ArrayList<>();
+		validateMenuInfos(menuInfos);
+
+		Arrays.stream(menuInfos.split(DELIMITER))
+			.forEach(menuInfo -> menuList.add(Menu.from(menuInfo)));
+
+		return new Menus(menuList);
+	}
+	
+	private static void validateMenuInfos(String menuInfos) {
+		if (!menuInfos.matches("([^;];)*[^;]")) {
+			throw new IllegalArgumentException(ERROR_DELIMITERS);
+		}
 	}
 }
