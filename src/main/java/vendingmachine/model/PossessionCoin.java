fd1e9a2312c0690711ea1100835e76b1958ec20e
jurlring <78091011+jurlring@users.noreply.github.com>
2021-12-11 15:05:30 +0900

    refactor : coinMap key-> Coin대신 Coin amount로 대체

--- src/main/java/vendingmachine/model/PossessionCoin.java
+++ src/main/java/vendingmachine/model/PossessionCoin.java
@@ -8,22 +8,25 @@ import java.util.Map;
 
 public class PossessionCoin {
 
-	Map<Coin, Integer> coins = new LinkedHashMap<>();
+	Map<Integer, Integer> coins = new LinkedHashMap<>();
 
 	public PossessionCoin() {
-		coins.put(Coin.COIN_10, INITIAL_NUMBER);
-		coins.put(Coin.COIN_50, INITIAL_NUMBER);
-		coins.put(Coin.COIN_100, INITIAL_NUMBER);
-		coins.put(Coin.COIN_500, INITIAL_NUMBER);
+		coins.put(Coin.COIN_500.getAmount(), INITIAL_NUMBER);
+		coins.put(Coin.COIN_100.getAmount(), INITIAL_NUMBER);
+		coins.put(Coin.COIN_50.getAmount(), INITIAL_NUMBER);
+		coins.put(Coin.COIN_10.getAmount(), INITIAL_NUMBER);
+	}
+
+	public Map<Integer, Integer> getCoins() {
+		return coins;
 	}
 
 	public void createRandomCoins(PossessionMoney money) {
 		while (money.isBiggerThanMinimumCoin()) {
-			int randomMoney = pickNumberInList(Coin.createCoinList());
-			Coin randomCoin = Coin.getCoin(randomMoney);
-			if (money.canMoneyIntoCoin(randomMoney)) {
-				money.changeMoneyIntoCoin(randomMoney);
-				coins.put(randomCoin, coins.get(randomCoin) + 1);
+			int randomAmount = pickNumberInList(Coin.createCoinList());
+			if (money.canMoneyIntoCoin(randomAmount)) {
+				money.changeMoneyIntoCoin(randomAmount);
+				coins.put(randomAmount, coins.get(randomAmount) + 1);
 			}
 		}
 	}
