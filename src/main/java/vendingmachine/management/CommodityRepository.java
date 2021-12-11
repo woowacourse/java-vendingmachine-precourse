46041f7a37268d9310f59cd4e7df9f024ac1dcf9
steven <seokju73@gmail.com>
2021-12-11 19:31:26 +0900

    Feat: Add repository for commodity information

---
+++ src/main/java/vendingmachine/management/CommodityRepository.java
@@ -0,0 +1,14 @@
+package vendingmachine.management;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommodityRepository {
+    private static final List<Commodity> commodities = new ArrayList<>();
+
+    public static List<Commodity> getList() {
+        return commodities;
+    }
+    
+    
+}
