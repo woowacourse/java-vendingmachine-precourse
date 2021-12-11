c15ed33b4cdece2252c39fa8ba548f8ac0416719
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-12-11 23:54:09 +0900

    test<ItemValidator>: 구입 상품명 입력 검증 테스트
    - 존재하지 않는 상품명을 입력하면 예외 처리
    - 수량이 0인 상품을 입력하면 예외 처리
    
    구입 상품명 입력값 검증 테스트를 수행하였습니다.

--- src/test/java/vendingmachine/util/ItemValidatorTest.java
+++ src/test/java/vendingmachine/util/ItemValidatorTest.java
@@ -2,32 +2,46 @@ package vendingmachine.util;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+
+import vendingmachine.domain.Item;
 
 class ItemValidatorTest {
 
 	@Test
 	void 상품_입력값_예외검증() {
-		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validate("콜라,1500,20]"));
-		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validate("[콜라,1500,20"));
-		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validate("[,1500,20]"));
-		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validate("[콜라,,20]"));
-		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validate("[콜라,1500,]"));
-		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validate("[콜라1500,20]"));
-		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validate("[콜라,150020]"));
-		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validate("[10,1500,20]"));
-		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validate("[콜라,z,20]"));
-		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validate("[콜라,1500,z]"));
-		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validate("[ ,1500,20]"));
-		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validate("[콜라, ,20]"));
-		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validate("[콜라,1500, ]"));
-		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validate("[콜,라,1500, 20]"));
-		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validate("[콜라,15,00, 20]"));
-		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validate("[콜 라,1500, 20]"));
-		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validate("[콜라,15 00, 20]"));
-		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validate("[콜라,1501, 20]"));
-		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validate("[콜라,050, 20]"));
-		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validate("[콜라,150, 02]"));
-		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validate("[콜라,10, 2]"));
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateItemForm("콜라,1500,20]"));
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateItemForm("[콜라,1500,20"));
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateItemForm("[,1500,20]"));
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateItemForm("[콜라,,20]"));
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateItemForm("[콜라,1500,]"));
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateItemForm("[콜라1500,20]"));
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateItemForm("[콜라,150020]"));
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateItemForm("[10,1500,20]"));
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateItemForm("[콜라,z,20]"));
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateItemForm("[콜라,1500,z]"));
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateItemForm("[ ,1500,20]"));
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateItemForm("[콜라, ,20]"));
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateItemForm("[콜라,1500, ]"));
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateItemForm("[콜,라,1500, 20]"));
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateItemForm("[콜라,15,00, 20]"));
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateItemForm("[콜 라,1500, 20]"));
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateItemForm("[콜라,15 00, 20]"));
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateItemForm("[콜라,1501, 20]"));
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateItemForm("[콜라,050, 20]"));
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateItemForm("[콜라,150, 02]"));
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateItemForm("[콜라,10, 2]"));
+	}
+
+	@Test
+	void 상품명_입력_검증() {
+		List<Item> itemList = Item.createList(Arrays.asList("[콜라,1500,0]", "[사이다,1000,10]"));
+
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateAvailability(itemList, "환타"));
+
+		assertThrows(IllegalArgumentException.class, () -> ItemValidator.validateAvailability(itemList, "콜라"));
 	}
 }
