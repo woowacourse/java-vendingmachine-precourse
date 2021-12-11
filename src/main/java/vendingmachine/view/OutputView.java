215c5ea9f703d81ba9f3bfe317332493ed40f72d
Hyunta <mohai2618@naver.com>
2021-12-11 17:11:20 +0900

    style(Won) : 잔돈을 출력할 때 단위 "원"를 같이 출력할 수 있도록 구현

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -5,6 +5,7 @@ import vendingmachine.domain.Machine;
 
 public class OutputView {
 	public static final String CURRENT_MACHINE_COIN = "자판기가 보유한 동전";
+	public static final String WON = "원";
 
 	public static final String CURRENT_MONEY = "투입 금액: ";
 	public static final String CHANGES = "잔돈";
@@ -18,7 +19,7 @@ public class OutputView {
 
 	public void printChanges(Machine machine, int inputMoney) {
 		StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.append(CURRENT_MONEY + inputMoney).append("\n");
+		stringBuilder.append(CURRENT_MONEY + inputMoney + WON).append("\n");
 		stringBuilder.append(CHANGES).append("\n");
 		Coins changes = machine.calculateChange(inputMoney);
 		stringBuilder.append(changes.toString());
