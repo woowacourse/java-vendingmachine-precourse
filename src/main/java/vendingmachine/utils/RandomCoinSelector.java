b41bf7bc489af85b09bd3951e4153220310dd09c
JunHyeok.Bang <bangvkf3@gmail.com>
2021-12-11 13:12:18 +0900

    feat: RandomCoinSelector util 클래스 추가

---
+++ src/main/java/vendingmachine/utils/RandomCoinSelector.java
@@ -0,0 +1,42 @@
+package vendingmachine.utils;
+
+import static java.util.stream.Collectors.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import vendingmachine.Coin;
+
+public class RandomCoinSelector {
+
+	public static Coin selectCoinInAllCoins() {
+		List<Coin> allCoinList = getAllCoinList();
+		int randomAmount = Randoms.pickNumberInList(getAmountList(allCoinList));
+		return Coin.valueOf(randomAmount);
+	}
+
+	public static Coin selectCoinCheaperThanValue(int value) {
+		List<Coin> coinListCheaperThanValue = getCoinListCheaperThanValue(value);
+		int randomAmout = Randoms.pickNumberInList(getAmountList(coinListCheaperThanValue));
+		return Coin.valueOf(randomAmout);
+	}
+
+	private static List<Coin> getAllCoinList() {
+		return Arrays.asList(Coin.values());
+	}
+
+	private static List<Coin> getCoinListCheaperThanValue(int value) {
+		List<Coin> allCoins = getAllCoinList();
+		return allCoins.stream()
+			.filter(coin -> coin.getAmount() < value)
+			.collect(toList());
+	}
+
+	private static List<Integer> getAmountList(List<Coin> coins) {
+		return coins.stream()
+			.map(Coin::getAmount)
+			.collect(Collectors.toList());
+	}
+}
