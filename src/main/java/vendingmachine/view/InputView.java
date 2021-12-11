25a22874d53df2c107646fbcf45144be27cd9a10
yeon-06 <rnjstldus2@hs.ac.kr>
2021-12-11 20:24:37 +0900

    feat: 사용자 투입 금액 입력 기능 구현

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -10,6 +10,7 @@ import java.util.List;
 public class InputView {
     private static final String INPUT_VENDINGMACHINE_SENETENCE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
     private static final String INPUT_PRODUCT_SENETENCE = "상품명과 가격, 수량을 입력해 주세요.";
+    private static final String INPUT_USER_PAY_SENETENCE = "투입 금액을 입력해 주세요.";
 
     private final Validator validator;
 
@@ -21,7 +22,7 @@ public class InputView {
         System.out.println(INPUT_VENDINGMACHINE_SENETENCE);
         String input = Console.readLine();
         try {
-            validator.isValidVendingmachineChange(input);
+            validator.isValidMoney(input);
             return Integer.parseInt(input);
 
         } catch (IllegalArgumentException e) {
@@ -40,6 +41,19 @@ public class InputView {
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return inputProductList();
+        }
+    }
+
+    public int inputUserPay() {
+        System.out.println(INPUT_USER_PAY_SENETENCE);
+        String input = Console.readLine();
+        try {
+            validator.isValidMoney(input);
+            return Integer.parseInt(input);
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputUserPay();
         }
     }
 
