97b237468159cff86b6bbd2674622bd9514b8be9
hyeonic <dev.hyeonic@gmail.com>
2021-12-11 20:51:38 +0900

    feat: InputView 보유 금액 입력을 위한 메서드 추가

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,7 +1,16 @@
 package vendingmachine.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import vendingmachine.domain.PossessionMoney;
+
 public class InputView {
+    private static final String POSSESSION_MONEY_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
 
     private InputView() {
+    }
+
+    public static PossessionMoney getPossessionMoney() {
+        System.out.println(POSSESSION_MONEY_MESSAGE);
+        return new PossessionMoney(Console.readLine());
     }
 }
