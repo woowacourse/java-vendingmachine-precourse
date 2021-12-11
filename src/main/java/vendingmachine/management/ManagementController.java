1f225aa19f079f89b6ed430a5b5bb64e2997bdb5
steven <seokju73@gmail.com>
2021-12-11 18:23:45 +0900

    Feat: Add commodity preparation method with user input

--- src/main/java/vendingmachine/management/ManagementController.java
+++ src/main/java/vendingmachine/management/ManagementController.java
@@ -1,10 +1,17 @@
 package vendingmachine.management;
 
+import java.util.Arrays;
+
+import vendingmachine.management.validation.CheckCommodityFormat;
+import vendingmachine.management.validation.CheckCommodityName;
+import vendingmachine.management.validation.CheckCommodityPrice;
+import vendingmachine.management.validation.CheckCommodityQuantity;
 import vendingmachine.management.validation.CheckDepositFigure;
 import vendingmachine.view.InputView;
 import vendingmachine.view.outputView.ManagementView;
 
 public class ManagementController {
+    
     protected static int deposit;
     
     public static void runManagement(InputView inputView) {
@@ -12,7 +19,8 @@ public class ManagementController {
         doDepositValidation(inputView);
         ManagementService.generateCoins(deposit);
         ManagementView.showCoinStatus();
-       
+        ManagementView.askCommodityInfo();
+        prepareCommodity(inputView);
     }
     
     private static void doDepositValidation(InputView inputView) {
@@ -23,7 +31,20 @@ public class ManagementController {
                 CheckDepositFigure.validationFigure(input);
                 deposit = Integer.parseInt(input);
                 CheckDepositFigure.validationPositiveNumber(deposit);
+                CheckDepositFigure.validationUnit(deposit);
             } catch(IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static void prepareCommodity(InputView inputView) {
+        while(true) {
+            String input = inputView.commodityInfo();
+            
+            try {
+                String [] commodityInfo = input.split(";");
+            } catch (IllegalArgumentException e){
                 System.out.println(e.getMessage());
             }
         }
