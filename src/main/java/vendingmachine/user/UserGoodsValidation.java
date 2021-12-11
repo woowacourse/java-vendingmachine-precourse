354b8bf62bc33cecb4db8fe449e8770cd7845efa
iamsseaa <homingsea@gmail.com>
2021-12-12 08:27:29 +0900

    feat: 입력한 상품 가격이 100원 이상인지 확인

--- src/main/java/vendingmachine/user/UserGoodsValidation.java
+++ src/main/java/vendingmachine/user/UserGoodsValidation.java
@@ -1,4 +1,9 @@
 package vendingmachine.user;
 
 public class UserGoodsValidation {
+    public static void is100WonMore(String price) {
+        if (Integer.parseInt(price) >= 100) {
+            throw new IllegalArgumentException(InputErrorConstant.ERROR_IS_NOT_100WON_MORE);
+        }
+    }
 }
