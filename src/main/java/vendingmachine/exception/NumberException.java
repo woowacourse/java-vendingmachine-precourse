c97ee98c34d70db530438c4a36c6c859bdd72050
jurlring <78091011+jurlring@users.noreply.github.com>
2021-12-11 18:45:26 +0900

    fix : 의미에 맞는 상수 명으로 변경

--- src/main/java/vendingmachine/exception/NumberException.java
+++ src/main/java/vendingmachine/exception/NumberException.java
@@ -15,25 +15,25 @@ public class NumberException {
 
 	private static void checkNumberEmpty(String number) {
 		if (number == null || number.trim().isEmpty()) {
-			throw new IllegalArgumentException(NUMBER_EMPTY_ERROR_MASSAGE);
+			throw new IllegalArgumentException(NUMBER_EMPTY_MSG);
 		}
 	}
 
 	private static void checkNumberIsInteger(String number) {
 		if (!number.chars().allMatch(Character::isDigit)) {
-			throw new IllegalArgumentException(NUMBER_NOT_INTEGER_ERROR_MESSAGE);
+			throw new IllegalArgumentException(NUMBER_NOT_INTEGER_MSG);
 		}
 	}
 
 	private static void checkNegativeNumber(String number) {
 		if (Integer.parseInt(number) < DISCRIMINATION_NEGATIVE_NUMBER) {
-			throw new IllegalArgumentException(NUMBER_IS_NEGATIVE_ERROR_MESSAGE);
+			throw new IllegalArgumentException(NUMBER_NEGATIVE_MSG);
 		}
 	}
 
 	private static void checkDivisionMinimumCoinAmount(String number) {
 		if (Integer.parseInt(number) % MINIMUM_COIN_AMOUNT != 0) {
-			throw new IllegalArgumentException(NUMBER_NOT_DIVIDE_MINIMUM_COIN_AMOUNT);
+			throw new IllegalArgumentException(NUMBER_NOT_DIVIDE_MINIMUM_COIN_AMOUNT_MSG);
 		}
 	}
 }
