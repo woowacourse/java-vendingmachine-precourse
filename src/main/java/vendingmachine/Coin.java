3b9fc83382b98765dd1bf914c65345b493f37e07
jaeyoungjang2 <lion85488@gmail.com>
2021-12-11 21:14:13 +0900

    feat: 최초 동전의 종류가 담긴 리스트를 반환하는 메서드 추가

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,5 +1,9 @@
 package vendingmachine;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
@@ -13,4 +17,13 @@ public enum Coin {
     }
 
     // 추가 기능 구현
+    public static List<Integer> amounts() {
+        return Arrays.stream(values())
+            .map(coin -> coin.amount)
+            .collect(Collectors.toList());
+    }
+
+    public int getAmount() {
+        return amount;
+    }
 }
