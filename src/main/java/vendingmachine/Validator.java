d29b94447a5ca757ed74a82ceffcfc5aa7359883
wannagohome <iwwuf7@gmail.com>
2021-12-11 21:20:45 +0900

    feat: include number verification into positive number verification
     - string이 숫자인지에 대한 검증은 양수인지에 대한 검증에 포함되기 때문에 해당 검증과정을 삭제하고 양수 검증에 포함시킨다

--- src/main/java/vendingmachine/Validator.java
+++ src/main/java/vendingmachine/Validator.java
@@ -8,26 +8,27 @@ public class Validator {
         validateMachineMoneyInputIsMultipleOf10(input);
     }
 
-    private static void validateVendingMachineAmountIsNumber(String input) throws IllegalArgumentException {
-        try {
-            Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(Constant.MACHINE_MONEY_INPUT_IS_NOT_NUMBER_ERROR_STRING);
-        }
-    }
-
     private static void validateMachineMoneyInputIsPositiveNumber(String input) throws IllegalArgumentException {
-        validateVendingMachineAmountIsNumber(input);
-        if (Integer.parseInt(input) < 0) {
+        if (!isPositiveNumber(input)) {
             throw new IllegalArgumentException(Constant.MACHINE_MONEY_INPUT_IS_NOT_POSITIVE_NUMBER_ERROR_STRING);
         }
     }
 
     private static void validateMachineMoneyInputIsMultipleOf10(String input) throws IllegalArgumentException {
-        validateVendingMachineAmountIsNumber(input);
         if (Integer.parseInt(input) % 10 != 0) {
             throw new IllegalArgumentException(Constant.MACHINE_MONEY_INPUT_IS_NOT_MULTIPLE_OF_10);
         }
+    }
+
+    private static boolean isPositiveNumber(String input) throws IllegalArgumentException {
+        try {
+            if (Integer.parseInt(input) > 0) {
+                return true;
+            }
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return false;
     }
 
     public static void validateMachineProductInput(String input) throws IllegalArgumentException {
