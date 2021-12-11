535e25bef249ba4d63e7f2182f587645e65a24e5
CptBluebear <cptbluebear@naver.com>
2021-12-12 03:40:18 +0900

    feat: 상품 목록 입력 기능 추가
    InputHandler에 상품 목록 입력 기능을 추가함.
    이에 따라 입력값 검증을 위해 InputValidator에 입력 양식 검증 및
    상품명 중복을 검사하는 기능을 추가함.
    에러 메세지 출력을 위해 VendingMachineData에 문자열 데이터도 추가함.

--- src/main/java/vendingmachine/io/InputHandler.java
+++ src/main/java/vendingmachine/io/InputHandler.java
@@ -1,8 +1,16 @@
 package vendingmachine.io;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
 import camp.nextstep.edu.missionutils.Console;
 import vendingmachine.data.VendingMachineData;
 import vendingmachine.io.validator.InputValidator;
+import vendingmachine.type.Product;
 
 public class InputHandler {
 
@@ -21,10 +29,34 @@ public class InputHandler {
 		return getMoney();
 	}
 
+	public List<Product> getValidProductList() {
+		outputHandler.printMessage(VendingMachineData.INPUT_PRODUCT_INFO_MESSAGE);
+		return getProductList();
+	}
+
 	private int getMoney() {
 		String data = read();
 		InputValidator.validMoneyValue(data);
 		return Integer.parseInt(data);
 	}
 
+	private List<Product> getProductList() {
+		String[] productData = read().split(";");
+		InputValidator.validProductInformationForm(productData);
+		List<Product> productList = generateProductList(productData);
+		InputValidator.validNameNotDuplicate(productList);
+		return productList;
+	}
+
+	private List<Product> generateProductList(String[] productData) {
+		Pattern productPattern = Pattern.compile(VendingMachineData.PRODUCT_REGEX);
+		List<Product> productList = new ArrayList<Product>();
+		for (String product : productData) {
+			Matcher matcher = productPattern.matcher(product);
+			matcher.find();
+			productList.add(
+				new Product(matcher.group(1), Integer.parseInt(matcher.group(2)), Integer.parseInt(matcher.group(3))));
+		}
+		return productList;
+	}
 }
--- src/main/java/vendingmachine/io/validator/InputValidator.java
+++ src/main/java/vendingmachine/io/validator/InputValidator.java
@@ -1,8 +1,17 @@
 package vendingmachine.io.validator;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
 import vendingmachine.data.VendingMachineData;
 import vendingmachine.exception.IntegerTooSmallException;
+import vendingmachine.exception.NameDuplicatedException;
+import vendingmachine.exception.ProductInformationFormMismatchException;
 import vendingmachine.exception.ValueNotIntegerException;
+import vendingmachine.type.Product;
 
 public class InputValidator {
 
@@ -14,6 +23,29 @@ public class InputValidator {
 			}
 		} catch (NumberFormatException nfe) {
 			throw new ValueNotIntegerException(VendingMachineData.INPUT_VALUE_NOT_INTEGER_ERROR, nfe);
+		}
+	}
+
+	public static void validProductInformationForm(String[] productArray) {
+		Pattern productPattern = Pattern.compile(VendingMachineData.PRODUCT_REGEX);
+		for (String product : productArray) {
+			Matcher matcher = productPattern.matcher(product);
+			if (!matcher.find()) {
+				throw new ProductInformationFormMismatchException(VendingMachineData.PRODUCT_INFO_FORM_NOT_MATCH_ERROR);
+			}
+			if(Integer.parseInt(matcher.group(2)) <= 0) {
+				throw new IntegerTooSmallException(VendingMachineData.INPUT_VALUE_TOO_SMALL_ERROR);
+			}
+			if(Integer.parseInt(matcher.group(3)) <= 0) {
+				throw new IntegerTooSmallException(VendingMachineData.PRODUCT_QUANTITY_TOO_SMALL);
+			}
+		}
+	}
+
+	public static void validNameNotDuplicate(List<Product> productList) {
+		long distinctNameCount = productList.stream().map(Product::getName).distinct().count();
+		if(distinctNameCount != productList.size()) {
+			throw new NameDuplicatedException(VendingMachineData.PRODUCT_NAME_DUPLICATED_ERROR);
 		}
 	}
 }
