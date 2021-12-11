15e2841e2096ce4452442d7ea8b1ce346be82113
Leeyerimearth <50127628+Leeyerimearth@users.noreply.github.com>
2021-12-11 19:39:36 +0900

    feat: 상품명과 가격, 수량 입력

--- src/main/java/vendingmachine/InputView.java
+++ src/main/java/vendingmachine/InputView.java
@@ -2,10 +2,23 @@ package vendingmachine;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class InputView {
 
     public int inputMachineMoney() {
         System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
         return Integer.parseInt(Console.readLine());
+    }
+
+    public void inputProduct() {
+        System.out.println("상품명과 가격, 수량을 입력해 주세요.");
+        String inputString = Console.readLine();
+        ArrayList<String> strings = Utils.splitString(inputString);
+        ArrayList<Product> products = new ArrayList<Product>();
+        for (String str : strings) {
+            products.add(new Product(str.split(",")[0], Integer.parseInt(str.split(",")[1]), Integer.parseInt(str.split(",")[2])));
+        }
     }
 }
---
+++ src/main/java/vendingmachine/Utils.java
@@ -0,0 +1,11 @@
+package vendingmachine;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Utils {
+    public static ArrayList splitString(String inputString) {
+        String[] splits = inputString.replace("[","").replace("[","").split(";");
+        return new ArrayList<String>(Arrays.asList(splits));
+    }
+}
