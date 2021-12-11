665d76f98240e9d851aa50c131f1e92b6b654d24
yeon-06 <rnjstldus2@hs.ac.kr>
2021-12-11 20:16:12 +0900

    test: 상품명 입력 관련 테스트 코드 추가

--- src/test/java/vendingmachine/view/ValidatorTest.java
+++ src/test/java/vendingmachine/view/ValidatorTest.java
@@ -1,32 +1,64 @@
 package vendingmachine.view;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import vendingmachine.util.Validator;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ValidatorTest {
+    private final Validator validator = new Validator();
+
     @Test
-    void 문자_입력_예외_발생() {
-        Validator validator = new Validator();
-        assertThrows(IllegalArgumentException.class, () -> validator.isValidVendingmachineChange("a100"));
+    void 자판기_문자_입력_예외_발생() {
+        assertThrows(IllegalArgumentException.class,
+                () -> validator.isValidVendingmachineChange("a100"));
     }
 
     @Test
-    void 음수_입력_예외_발생() {
-        Validator validator = new Validator();
-        assertThrows(IllegalArgumentException.class, () -> validator.isValidVendingmachineChange("-1"));
+    void 자판기_음수_입력_예외_발생() {
+        assertThrows(IllegalArgumentException.class,
+                () -> validator.isValidVendingmachineChange("-1"));
     }
 
     @Test
-    void 나눠지지_않는_수_입력_예외_발생() {
-        Validator validator = new Validator();
-        assertThrows(IllegalArgumentException.class, () -> validator.isValidVendingmachineChange("11"));
+    void 자판기_나눠지지_않는_수_입력_예외_발생() {
+        assertThrows(IllegalArgumentException.class,
+                () -> validator.isValidVendingmachineChange("11"));
     }
 
     @Test
-    void 올바른_입력() {
-        Validator validator = new Validator();
-        assertThrows(IllegalArgumentException.class, () -> validator.isValidVendingmachineChange("1530"));
+    void 자판기_올바른_입력() {
+        assertThat(validator.isValidVendingmachineChange("1530"));
+    }
+
+    @Test
+    void 상품목록_올바른_입력() {
+        assertThat(validator.isValidProduct("[사이다,100,3];[콜라,500,10]"));
+    }
+    @Test
+    void 상품목록_상품금액_오류() {
+        assertThrows(IllegalArgumentException.class,
+                () -> validator.isValidProduct("[콜라,10,5]"));
+    }
+
+    @Test
+    void 상품목록_괄호_오류() {
+        assertThrows(IllegalArgumentException.class,
+                () -> validator.isValidProduct("{콜라,100,5}"));
+    }
+
+    @Test
+    void 상품목록_재고수_오류() {
+        assertThrows(IllegalArgumentException.class,
+                () -> validator.isValidProduct("[콜라,100,-1]"));
+    }
+
+    @Test
+    void 상품목록_구분선_오류() {
+        assertThrows(IllegalArgumentException.class,
+                () -> validator.isValidProduct("[콜라,100,1];"));
     }
 }
