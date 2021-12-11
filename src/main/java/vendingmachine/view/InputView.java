5bf23e9bff307f78491710d6dbc366c7be9beddf
pup-paw <wldusdl0310@gmail.com>
2021-12-11 23:11:49 +0900

    feat: 자판기 상품 재고 확인, 거래 기능 추가

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -4,21 +4,60 @@ import java.util.ArrayList;
 
 import camp.nextstep.edu.missionutils.Console;
 import vendingmachine.controller.ExceptionController;
-import vendingmachine.model.Products;
+import vendingmachine.model.Product;
 import vendingmachine.util.Constant;
 
 public class InputView {
-	public static ArrayList<Products> products;
+	public static ArrayList<Product> products;
 
-	public static void askHoldingAmount() {
-		System.out.println(Constant.ASK_HOLDING_AMOUNT);
-		String input = Console.readLine();
-		checkHoldingAmount(input);
+	public static boolean isEmpty() {
+		for (Product product : products) {
+			if (product.getCount() != 0) {
+				return false;
+			}
+		}
+		return true;
 	}
 
-	private static void checkHoldingAmount(String input) {
+	public static int getMinPrice() {
+		int minPrice = products.get(0).getPrice();
+		for (Product product : products) {
+			minPrice = Math.min(minPrice, product.getPrice());
+		}
+		return minPrice;
+	}
+
+	public static int askPurchase() {
+		System.out.println(Constant.ASK_PRODUCT_NAME_TO_PURCHASE);
+		String input = Console.readLine();
+		return checkExistence(input);
+	}
+
+	private static int checkExistence(String input) {
+		for (Product product : products) {
+			if (product.getName().equals(input)) {
+				return product.sell(product);
+			}
+		}
+		throw new IllegalArgumentException(Constant.ERROR_DOES_NOT_EXIST);
+	}
+
+	public static int askInputAmount() {
+		System.out.println(Constant.ASK_INPUT_AMOUNT);
+		String input = Console.readLine();
+		return ExceptionController.isValidNumber(input);
+	}
+
+	public static int askHoldingAmount() {
+		System.out.println(Constant.ASK_HOLDING_AMOUNT);
+		String input = Console.readLine();
+		return checkHoldingAmount(input);
+	}
+
+	private static int checkHoldingAmount(String input) {
 		int holdingAmount = ExceptionController.isValidNumber(input);
 		ExceptionController.isMultipleOfTen(holdingAmount);
+		return holdingAmount;
 	}
 
 	public static void askProductInfo() {
@@ -28,7 +67,6 @@ public class InputView {
 		String[][] information = checkInfoFormat(products);
 		inputProduct(information);
 	}
-
 
 	private static void checkProducts(String[] products) {
 		for (int i = 0; i < products.length; i++) {
@@ -50,8 +88,9 @@ public class InputView {
 		products = new ArrayList<>();
 		for (String[] info : information) {
 			int price = ExceptionController.isValidNumber(info[1]);
+			ExceptionController.isMultipleOfTen(price);
 			int count = ExceptionController.isValidNumber(info[2]);
-			products.add(new Products(info[0], price, count));
+			products.add(new Product(info[0], price, count));
 		}
 	}
 
