40fe4c855c4f6e49b0dc7a903dff7dfc48c69b81
koo <gudonghee2000@gmail.com>
2021-12-12 19:02:36 +0900

    refactor : OutputDisplay클래스 매직넘버 상수화

--- src/main/java/view/OutputDisplay.java
+++ src/main/java/view/OutputDisplay.java
@@ -4,18 +4,21 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class OutputDisplay {
+	private static final String PRICE = "원 - ";
+	private static final String COUNT = "개";
+	private static final String VENDING_MACHINE_HOLDING_COIN = "자판기가 보유한 동전";
 
 	private OutputDisplay() {
 	}
 
 	public static void showEachCoinInCoinBox(List<Integer> eachCoinPrice, List<Integer> eachCoinCount) {
 		System.out.println();
-		System.out.println("자판기가 보유한 동전");
+		System.out.println(VENDING_MACHINE_HOLDING_COIN);
 		IntStream.range(0, eachCoinCount.size())
 			.forEach(index -> showCoin(eachCoinPrice.get(index), eachCoinCount.get(index)));
 	}
 
 	private static void showCoin(int coinPrice, int coinCount) {
-		System.out.println(coinPrice + "원 - " + coinCount + "개");
+		System.out.println(coinPrice + PRICE + coinCount + COUNT);
 	}
 }
