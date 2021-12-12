ac0e633500d7fea5207bb64cddc85c3b0ea67cc3
Kim Seunglae <obey1342@gmail.com>
2021-12-12 23:12:22 +0900

    feat: 상품들을 입력받는 기능 추가

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -5,8 +5,16 @@ import static vendingmachine.resource.MessageResource.*;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+	private InputView() {
+	}
+
 	public static int haveAmount() {
 		System.out.println(INPUT_HAVE_AMOUNT_MESSAGE);
 		return Integer.parseInt(Console.readLine());
+	}
+
+	public static String inputProducts() {
+		System.out.println(INPUT_PRODUCTS_MESSAGE);
+		return Console.readLine();
 	}
 }
