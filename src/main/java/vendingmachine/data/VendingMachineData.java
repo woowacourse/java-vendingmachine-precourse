60833994acb80726957f56e9f68b24f38385d255
CptBluebear <cptbluebear@naver.com>
2021-12-12 21:47:29 +0900

    feat: 프로그램에서 사용할 문자열 추가 및 오타 수정

--- src/main/java/vendingmachine/data/VendingMachineData.java
+++ src/main/java/vendingmachine/data/VendingMachineData.java
@@ -6,10 +6,11 @@ public class VendingMachineData {
 	public static final String COIN_STATUS_MESSAGE = "자판기가 보유한 동전";
 	public static final String INPUT_PRODUCT_INFO_MESSAGE = "상품명과 가격, 수량을 입력해 주세요.";
 	public static final String INPUT_MONEY_MESSAGE = "투입 금액을 입력해 주세요.";
-	public static final String MONEY_STATUS_MESSAGE = "투입 금액: %d";
+	public static final String MONEY_STATUS_MESSAGE = "투입 금액: %d원";
 	public static final String INPUT_PRODUCT_NAME_MESSAGE = "구매할 상품명을 입력해 주세요.";
 	public static final String CHANGE_STATUS_MESSAGE = "잔돈";
 	public static final String CHANGE_STATUS_FORMAT = "%d원 - %d개";
+	public static final String BALANCE_NOT_ENOUGH_MESSAGE = "잔액이 부족합니다.";
 
 	public static final String INPUT_VALUE_NOT_INTEGER_ERROR = "[ERROR] 금액은 숫자여야 합니다.";
 	public static final String INPUT_VALUE_TOO_SMALL_ERROR = "[ERROR] 금액이 너무 작습니다.";
