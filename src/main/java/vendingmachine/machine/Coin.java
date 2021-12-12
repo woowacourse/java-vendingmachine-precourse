adf1258e0b027e6be1c87584b38953165891b03e
iamsseaa <homingsea@gmail.com>
2021-12-12 15:45:29 +0900

    feat: 동전들을 랜덤으로 하나 뽑는 함수

--- src/main/java/vendingmachine/machine/Coin.java
+++ src/main/java/vendingmachine/machine/Coin.java
@@ -1,5 +1,11 @@
 package vendingmachine.machine;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public enum Coin {
     COIN_500(500,0),
     COIN_100(100,0),
@@ -16,5 +22,23 @@ public enum Coin {
 
     public void setCoinNumber() {
         this.number++;
+    }
+
+    public List<Integer> getAllCoinsAmount() {
+        List<Integer> coin = new ArrayList<>();
+        coin.add(COIN_500.amount);
+        coin.add(COIN_100.amount);
+        coin.add(COIN_50.amount);
+        coin.add(COIN_10.amount);
+
+        return coin;
+    }
+
+    public int getRandomCoinAmount() {
+        List<Integer> coin = getAllCoinsAmount();
+
+        int pickAmount = Randoms.pickNumberInList(coin);
+
+        return pickAmount;
     }
 }
