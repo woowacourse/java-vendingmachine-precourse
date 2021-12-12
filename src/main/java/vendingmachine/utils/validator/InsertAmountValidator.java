e0ef0c2995392710459405d0f00722b7f541643c
delphox60 <delphox60@gmail.com>
2021-12-12 16:16:22 +0900

    fix(utils.validator): 투입 금액의 최대값을 넘는 입력이 통과하는 오류 수정
    10억 ~ 약 21억(int max) 사이의 수를 입력했을 때 유효성 검사를 통과하는 오류 수정
    에러 메시지 상수명 리네임

--- src/main/java/vendingmachine/utils/validator/InsertAmountValidator.java
+++ src/main/java/vendingmachine/utils/validator/InsertAmountValidator.java
@@ -2,10 +2,13 @@ package vendingmachine.utils.validator;
 
 public class InsertAmountValidator {
 
-    private static final String INPUT_IS_NOT_A_VALID_NUMBER_OR_EXCEED_MAX = "투입 금액은 10억 이하의 숫자만 입력할 수 있습니다.";
+    private static final String INPUT_IS_INVALID_NUMBER_OR_EXCEED_MAX_ERROR_MESSAGE = "투입 금액은 10억 이하의 숫자만 입력할 수 있습니다.";
+    private static final int MAX_INSERT_AMOUNT = 1000000000;
 
     public static int getValidInsertAmountValidator(final String input) {
-        int intInput = NumberValidator.getValidNumber(input, INPUT_IS_NOT_A_VALID_NUMBER_OR_EXCEED_MAX);
+        int intInput = NumberValidator.getValidNumber(input, INPUT_IS_INVALID_NUMBER_OR_EXCEED_MAX_ERROR_MESSAGE);
+        NumberValidator.validateNotExceedMaxValue(intInput, MAX_INSERT_AMOUNT,
+            INPUT_IS_INVALID_NUMBER_OR_EXCEED_MAX_ERROR_MESSAGE);
         return intInput;
     }
 }
