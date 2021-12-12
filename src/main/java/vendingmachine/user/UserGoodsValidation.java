76b0233fdd90ebf6ed94ef4eccb55063a084cdfe
iamsseaa <homingsea@gmail.com>
2021-12-12 20:17:16 +0900

    feat: 대괄호 개수가 일치하는지 확인

--- src/main/java/vendingmachine/user/UserGoodsValidation.java
+++ src/main/java/vendingmachine/user/UserGoodsValidation.java
@@ -1,4 +1,26 @@
 package vendingmachine.user;
 
 public class UserGoodsValidation {
+    public static void checkBracketNumber(String input) {
+        if (!checkBracketPairNumber(input)) {
+            throw new IllegalArgumentException(InputErrorConstant.ERROR_IS_NOT_SQUARE_BRACKETS);
+        }
+    }
+
+    public static boolean checkBracketPairNumber(String input) {
+        int countOpen = 0;
+        int countClose = 0;
+
+        for (int i=0; i<input.length(); i++) {
+            String c = Character.toString(input.charAt(i));
+
+            if (c == InputErrorConstant.SQUARE_BRACKETS_OPEN) {
+                countOpen++;
+            }
+            if (c == InputErrorConstant.SQUARE_BRACKETS_CLOSE) {
+                countClose++;
+            }
+        }
+        return countOpen == countClose;
+    }
 }
