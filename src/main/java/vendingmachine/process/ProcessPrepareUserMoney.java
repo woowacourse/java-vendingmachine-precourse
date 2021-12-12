c16edcc8397a9e71eb247ad438ceed96bfebec35
iamsseaa <homingsea@gmail.com>
2021-12-12 23:35:13 +0900

    feat: 사용자 투입 금액 입력

--- src/main/java/vendingmachine/process/ProcessPrepareUserMoney.java
+++ src/main/java/vendingmachine/process/ProcessPrepareUserMoney.java
@@ -1,4 +1,11 @@
 package vendingmachine.process;
 
+import vendingmachine.user.User;
+
 public class ProcessPrepareUserMoney {
+    public static String money;
+
+    public static void inputUserMoney() {
+        money = User.inputMoney();
+    }
 }
