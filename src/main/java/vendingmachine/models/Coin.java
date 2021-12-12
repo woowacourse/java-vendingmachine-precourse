cd6a201b19771bcf462f6340479b33ef5d25c4a9
bcc0830 <bcc0830@naver.com>
2021-12-12 17:25:58 +0900

    feat(Coin) : 현재 거스름돈을 기준으로 반환해야 하는 동전의 액면 금액을 가져오는 기능 추가

--- src/main/java/vendingmachine/models/Coin.java
+++ src/main/java/vendingmachine/models/Coin.java
@@ -15,10 +15,19 @@ public enum Coin {
 		this.amount = amount;
 	}
 
-	private static int getNowCoin(ArrayList<Item> coins, int money) {
+	private static int getRandomCoin(ArrayList<Item> coins, int money) {
 		return camp.nextstep.edu.missionutils.Randoms.pickNumberInList(coins.stream()
 			.filter(eachCoin -> eachCoin.getPrice() <= money)
 			.map(Item::getPrice)
 			.collect(Collectors.toList()));
+	}
+
+	private static int calculateReturnedCoinValue(ArrayList<Item> coinList, int money) {
+		return coinList.stream()
+			.sorted((coin1, coin2) -> coin2.getPrice() - coin1.getPrice())
+			.filter(eachCoin -> (!eachCoin.isOverThisPrice(money) && !eachCoin.isAmountZero()))
+			.findFirst()
+			.get()
+			.getPrice();
 	}
 }
