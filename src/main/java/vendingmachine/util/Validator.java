47d4989b366f5154bcea70753393d1762be73a0c
Seungmin Lim <seungmin9066@gmail.com>
2021-12-12 15:00:13 +0900

    [추가] 음수를 판단하는 validator 추가

--- src/main/java/vendingmachine/util/Validator.java
+++ src/main/java/vendingmachine/util/Validator.java
@@ -4,6 +4,15 @@ import static vendingmachine.constant.Constant.*;
 
 public class Validator {
 
+	private static final String NO_STOCKS_MESSAGE = "[ERROR] 재고가 남아있지 않습니다.";
+	private static final String VALIDATE_NUMBER_MESSAGE = "[ERROR] 숫자만 입력이 가능합니다.";
+	private static final String VALIDATE_NEGATIVE_MESSAGE = "[ERROR] 음수는 입력할 수 없습니다.";
+
+	public void validatePrice(String number) {
+		validateNumber(number);
+		validateNegative(number);
+	}
+
 	public void validateNumber(String number) {
 		boolean isNumeric = number.matches(REGEX_EXPRESSION_OF_NUMBER);
 		if (!isNumeric) {
@@ -11,5 +20,10 @@ public class Validator {
 		}
 	}
 
-	
+	public void validateNegative(String number) {
+		if (Integer.parseInt(number) < 0) {
+			throw new IllegalArgumentException(VALIDATE_NEGATIVE_MESSAGE);
+		}
+	}
+
 }
