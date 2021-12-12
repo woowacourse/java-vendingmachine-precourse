7c26c8e5228c85a4babbfb9924461a3b9e1841a7
yeongunheo <gjduddns@gmail.com>
2021-12-12 22:38:39 +0900

    feat: 입력받은 숫자가 숫자가 아닌 경우 예외를 발생하는 기능 구현

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -12,9 +12,10 @@ public class InputView {
 	private static final String BUY_PRODUCT_GUIDE_MESSEEAGE = "구매할 상품명을 입력해 주세요.";
 	private static final String PRODUCTS_DELIMITER = ";";
 	private static final int ZERO = 0;
-	private static final int LIMITATION = (int) 1e8;
+	private static final int LIMITATION = (int)1e8;
 	private static final String NEGATIVE_ERROR_MESSAGE = "[ERROR] 음수를 입력할 수 없습니다.";
 	private static final String LIMITATION_ERROR_MESSAGE = "[ERROR] 한계값을 넘는 수를 입력할 수 없습니다. (한계값: " + LIMITATION + ")";
+	private static final String NOT_NUMBER_ERROR_MESSAGE = "[ERROR] 반드시 숫자로 입력해주세요.";
 
 	private final Scanner scanner;
 
@@ -48,6 +49,7 @@ public class InputView {
 	public int validate() {
 		try {
 			String inputNumber = scanner.nextLine();
+			validateNumber(inputNumber);
 			validateLowerThanLimitationNumber(inputNumber);
 			validateNotNegativeNumber(inputNumber);
 			return Integer.parseInt(inputNumber);
@@ -62,9 +64,18 @@ public class InputView {
 			throw new IllegalArgumentException(NEGATIVE_ERROR_MESSAGE);
 		}
 	}
+
 	public void validateLowerThanLimitationNumber(String inputNumber) {
 		if (inputNumber.length() > 9) {
 			throw new IllegalArgumentException(LIMITATION_ERROR_MESSAGE);
+		}
+	}
+
+	public void validateNumber(String inputNumber) {
+		for (int i = 0; i < inputNumber.length(); i++) {
+			if (!Character.isDigit(inputNumber.charAt(i))) {
+				throw new IllegalArgumentException(NOT_NUMBER_ERROR_MESSAGE);
+			}
 		}
 	}
 }
