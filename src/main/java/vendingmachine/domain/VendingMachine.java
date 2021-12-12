44086b20bc9646711549b37baa0929253611b4ae
delphox60 <delphox60@gmail.com>
2021-12-12 12:17:21 +0900

    refactor(domain): VendingMachine class의 method 분리
    보유 금액 setter method가 다른 역할을 포함하고 있어 Constructor로 분리

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -5,9 +5,13 @@ public class VendingMachine {
     private int holdingMoney;
     private CoinCase coinCase;
 
+    public VendingMachine() {
+        this.holdingMoney = 0;
+        this.coinCase = new CoinCase();
+    }
+
     public void setHoldingMoney(final int holdingMoney) {
         this.holdingMoney = holdingMoney;
-        this.coinCase = new CoinCase();
     }
 
     public void initializeCoinCase() {
