360a58aa38e73ec966dbc18c05a2386000f85395
Byeongju-Kong <bjuuuu98@gmail.com>
2021-12-12 16:00:38 +0900

    refactor: 메인 함수 수정

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,7 +1,22 @@
 package vendingmachine;
 
+import vendingmachine.controller.VendingMachineController;
+import vendingmachine.model.change.coingenerator.CoinGenerator;
+import vendingmachine.model.change.coingenerator.RandomCoinGenerator;
+import vendingmachine.view.input.ConsoleInputView;
+import vendingmachine.view.input.InputView;
+import vendingmachine.view.output.ConsoleOutputView;
+import vendingmachine.view.output.OutputView;
+
+import java.util.function.Function;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new ConsoleInputView();
+        OutputView outputView = new ConsoleOutputView();
+        Function<Integer, CoinGenerator> coinGeneratingStrategy = RandomCoinGenerator::new;
+        VendingMachineController vendingMachineController =
+                new VendingMachineController(inputView, outputView, coinGeneratingStrategy);
+        vendingMachineController.run();
     }
 }
