dfc98d8ed4ce8661b3329fa0bfe2f044512d966a
soominsohn <soomin.sohn.dev@gmail.com>
2021-12-12 21:02:05 +0900

    feat: add generate random coin function - Coin add stock field
    - CoinService
    add exchangeMoneyToCoin function

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,16 +1,29 @@
 package vendingmachine;
 
 public enum Coin {
-    COIN_500(500),
-    COIN_100(100),
-    COIN_50(50),
-    COIN_10(10);
+    COIN_500(500, 0),
+    COIN_100(100, 0),
+    COIN_50(50, 0),
+    COIN_10(10, 0);
 
     private final int amount;
+    private int stock;
 
-    Coin(final int amount) {
+    Coin(final int amount, int stock) {
         this.amount = amount;
+        this.stock = stock;
     }
 
     // 추가 기능 구현
+    public int getAmount() {
+        return this.amount;
+    }
+
+    public int getStock() {
+        return this.stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 }
--- src/main/java/vendingmachine/service/CoinService.java
+++ src/main/java/vendingmachine/service/CoinService.java
@@ -1,15 +1,36 @@
 package vendingmachine.service;
 
-import vendingmachine.CoinInventory;
+import camp.nextstep.edu.missionutils.Randoms;
+import vendingmachine.Coin;
 import vendingmachine.view.InputView;
 
 public class CoinService {
     InputView inputView = new InputView();
 
-    public void generate(){
-      int money = Integer.parseInt(inputView.getMoney());
-      System.out.println("받은 돈: "+money);
-      //exchangeMoneyToCoin();
+    public void generate() {
+        int money = Integer.parseInt(inputView.getMoney());
+        exchangeMoneyToCoin(money);
+
+    }
+
+    public void exchangeMoneyToCoin(int money) {
+        for (Coin coin: Coin.values()){
+            if (coin.getAmount() != Coin.COIN_10.getAmount()){
+                money = generate500To100(coin ,money);
+                continue;
+            }
+            generate10(money);
+        }
+    }
+
+    public int generate500To100(Coin coin, int money){
+        int random = Randoms.pickNumberInRange(0,money/coin.getAmount());
+        coin.setStock(random);
+        return money - coin.getAmount()*random;
+    }
+
+    public void generate10(int money){
+        Coin.COIN_10.setStock(money/Coin.COIN_10.getAmount());
     }
 
 }
