533fa8cd0a97192d69b8d38716526f70ae96257c
GYUTAEKIM <kkt219a@gmail.com>
2021-12-12 16:38:26 +0900

    feat: 상황별 적절한 입력 처리 기능 구현
    - 보유금액, 상품목록, 투입금액, 구매상품명을 입력받아서 반환하도록 구현

---
+++ src/main/java/vendingmachine/processor/InputProcessor.java
@@ -0,0 +1,34 @@
+package vendingmachine.processor;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputProcessor {
+	private static final String RETENTION_MONEY_INPUT = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+	private static final String PRODUCT_COMPOSITION_INPUT = "상품명과 가격, 수량을 입력해 주세요.";
+	private static final String INSERT_MONEY_INPUT = "투입 금액을 입력해 주세요.";
+	private static final String PRODUCT_NAME_INPUT = "구매할 상품명을 입력해 주세요.";
+
+	public String readRetentionMoney() {
+		System.out.println(RETENTION_MONEY_INPUT);
+		return readLine();
+	}
+
+	public String readProductComposition() {
+		System.out.println(PRODUCT_COMPOSITION_INPUT);
+		return readLine();
+	}
+
+	public String readInsertMoney() {
+		System.out.println(INSERT_MONEY_INPUT);
+		return readLine();
+	}
+
+	public String readProductName() {
+		System.out.println(PRODUCT_NAME_INPUT);
+		return readLine();
+	}
+
+	private String readLine() {
+		return Console.readLine();
+	}
+}
