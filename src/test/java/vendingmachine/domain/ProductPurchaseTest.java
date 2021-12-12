e64ba986bed54bc45afbfdc983e152fbdef0cee8
hyeonic <dev.hyeonic@gmail.com>
2021-12-12 15:03:38 +0900

    test: ProductPurchase 검증을 위한 테스트 추가

---
+++ src/test/java/vendingmachine/domain/ProductPurchaseTest.java
@@ -0,0 +1,30 @@
+package vendingmachine.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class ProductPurchaseTest {
+
+    @DisplayName("문자열 구매 상품이 주어지고 모든 검증을 통과하면 생성된다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"콜라", "사이다"})
+    void constructor_InputNameThenString_Create(String inputProductPurchase) {
+        // given & when & then
+        assertThatCode(() -> {
+            new ProductPurchase(inputProductPurchase);
+        }).doesNotThrowAnyException();
+    }
+
+    @DisplayName("문자열 구매 상품이 비어있거나 공백이면 예외를 던진다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void constructor_InputNameThenEmptyOrBlank_ExceptionThrowna(String inputProductPurchase) {
+        // given & when & then
+        assertThatThrownBy(() -> {
+            new ProductPurchase(inputProductPurchase);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+}
