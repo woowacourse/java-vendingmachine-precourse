ed1454c51860291ca6ea1e9d27da1cb8ff27452d
delphox60 <delphox60@gmail.com>
2021-12-12 15:50:34 +0900

    feat(view): 투입 금액을 입력받는 기능 구현
    명료한 의미 전달을 위해 inputProductInfo() method 안의 변수 productInfo를 리네임

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -5,6 +5,7 @@ public class InputView {
     private static final String NEW_LINE = "\n";
     private static final String HOLDING_MONEY_INPUT_REQUEST_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
     private static final String PRODUCT_INFO_INPUT_REQUEST_MESSAGE = "상품명과 가격, 수량을 입력해 주세요.";
+    private static final String INSERT_AMOUNT_INPUT_REQUEST_MESSAGE = "투입 금액을 입력해 주세요.";
 
     public static String inputHoldingMoney() {
         System.out.println(HOLDING_MONEY_INPUT_REQUEST_MESSAGE);
@@ -15,8 +16,15 @@ public class InputView {
 
     public static String inputProductInfo() {
         System.out.println(PRODUCT_INFO_INPUT_REQUEST_MESSAGE);
-        String productInfo = camp.nextstep.edu.missionutils.Console.readLine();
+        String inputProductInfo = camp.nextstep.edu.missionutils.Console.readLine();
         System.out.print(NEW_LINE);
-        return productInfo;
+        return inputProductInfo;
+    }
+
+    public static String inputInsertAmount() {
+        System.out.println(INSERT_AMOUNT_INPUT_REQUEST_MESSAGE);
+        String inputInsertAmount = camp.nextstep.edu.missionutils.Console.readLine();
+        System.out.print(NEW_LINE);
+        return inputInsertAmount;
     }
 }
