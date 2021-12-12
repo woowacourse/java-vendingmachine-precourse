31b041c515ead3c626fafa37e7f722f7344121ab
xi_jjun <rlawowns97@naver.com>
2021-12-12 21:46:27 +0900

    test: 랜덤 동전 생성 테스트

---
+++ src/test/java/vendingmachine/domain/CoinsTest.java
@@ -0,0 +1,26 @@
+package vendingmachine.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import vendingmachine.constants.Message;
+import vendingmachine.utils.View;
+
+import java.util.Map;
+
+class CoinsTest {
+	@Test
+	void generateRandomCoins() {
+		Coins coins = new Coins();
+		final int POSSESSION = 450;
+		Map<Coin, Integer> entry = coins.createCoins(POSSESSION);
+
+		View.showCoins(entry, Message.POSSESSION_COIN);
+
+		int totalCoinAmount = 0;
+		for (Map.Entry<Coin, Integer> e : entry.entrySet()) {
+			totalCoinAmount += e.getValue() * (e.getKey().getAmount());
+		}
+
+		Assertions.assertThat(totalCoinAmount).isEqualTo(POSSESSION);
+	}
+}
