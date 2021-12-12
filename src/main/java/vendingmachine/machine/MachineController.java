773f85203f7d1aa94b406890c40d0ba1a721e6c6
iamsseaa <homingsea@gmail.com>
2021-12-12 15:54:10 +0900

    feat: MachineController 생성자 생성

--- src/main/java/vendingmachine/machine/MachineController.java
+++ src/main/java/vendingmachine/machine/MachineController.java
@@ -1,5 +1,16 @@
 package vendingmachine.machine;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
 public class MachineController {
+    Coin[] coins = Coin.values();
+    Machine machine;
+
+    MachineController(Machine machine) {
+        this.machine = machine;
+    }
+
     
 }
