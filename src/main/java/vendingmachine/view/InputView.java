d27b95ff8d7da73f02410dc06304288db4b97c9c
yeongunheo <gjduddns@gmail.com>
2021-12-12 22:20:52 +0900

    feat: 입력받은 숫자가 100,000,000을 넘을 때 예외를 발생하는 기능 구현

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -12,7 +12,7 @@ public class InputView {
 	private static final String BUY_PRODUCT_GUIDE_MESSEEAGE = "구매할 상품명을 입력해 주세요.";
 	private static final String PRODUCTS_DELIMITER = ";";
 	private static final int ZERO = 0;
-	private static final int LIMITATION = (int) 2147483647;
+	private static final int LIMITATION = (int) 1e8;
 	private static final String NEGATIVE_ERROR_MESSAGE = "[ERROR] 음수를 입력할 수 없습니다.";
 	private static final String LIMITATION_ERROR_MESSAGE = "[ERROR] 한계값을 넘는 수를 입력할 수 없습니다. (한계값: " + LIMITATION + ")";
 
@@ -48,6 +48,7 @@ public class InputView {
 	public int validate() {
 		try {
 			String inputNumber = scanner.nextLine();
+			validateLowerThanLimitationNumber(inputNumber);
 			validateNotNegativeNumber(inputNumber);
 			return Integer.parseInt(inputNumber);
 		} catch (IllegalArgumentException exception) {
@@ -59,6 +60,11 @@ public class InputView {
 	public void validateNotNegativeNumber(String inputNumber) {
 		if (Integer.parseInt(inputNumber) < ZERO) {
 			throw new IllegalArgumentException(NEGATIVE_ERROR_MESSAGE);
+		}
+	}
+	public void validateLowerThanLimitationNumber(String inputNumber) {
+		if (inputNumber.length() > 9) {
+			throw new IllegalArgumentException(LIMITATION_ERROR_MESSAGE);
 		}
 	}
 }
