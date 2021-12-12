35dea6b15c4ee24cd9b517fd7da8eb03944e2db9
Byeongju-Kong <bjuuuu98@gmail.com>
2021-12-12 22:34:21 +0900

    refactor: Coin 생성 방법 정적 팩토리로 변경

--- src/main/java/vendingmachine/model/change/vo/Coin.java
+++ src/main/java/vendingmachine/model/change/vo/Coin.java
@@ -1,12 +1,22 @@
 package vendingmachine.model.change.vo;
 
+import java.util.Arrays;
+
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
     COIN_50(50),
     COIN_10(10);
 
+    private static final String COIN_NOT_FOUND_EXCEPTION_MESSAGE = "동전은 500, 100, 50, 10원 밖에 없습니다.";
     private final int amount;
+
+    public static Coin of(final int amount) {
+        return Arrays.stream(values())
+                .filter(coin -> coin.getAmount() == amount)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(COIN_NOT_FOUND_EXCEPTION_MESSAGE));
+    }
 
     Coin(final int amount) {
         this.amount = amount;
--- src/test/java/vendingmachine/model/change/vo/CoinTest.java
+++ src/test/java/vendingmachine/model/change/vo/CoinTest.java
@@ -1,15 +1,18 @@
 package vendingmachine.model.change.vo;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CoinTest {
+    private final Coin coinOf500 = Coin.of(500);
+
     @Test
     @DisplayName("금액을 반환한다.")
     void getAmount() {
-        int actual = Coin.COIN_500.getAmount();
+        int actual = coinOf500.getAmount();
         int expected = 500;
         assertThat(actual).isEqualTo(expected);
     }
@@ -18,8 +21,16 @@ class CoinTest {
     @DisplayName("다른 금액을 받아, 동전 몇 개로 바꿀 수 있는지 반환한다.")
     void getNumberOfPossibleGeneration() {
         int givenAmount = 3100;
-        int actual = Coin.COIN_500.getNumberOfPossibleGeneration(givenAmount);
+        int actual = coinOf500.getNumberOfPossibleGeneration(givenAmount);
         int expected = 6;
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("500, 100, 50, 10 이외의 숫자로 동전을 생성하려하면 예외를 발생시킨다.")
+    void evokeExceptionByWrongCoinAmount() {
+        int wrongAmount = 600;
+        assertThatIllegalArgumentException().isThrownBy(() -> Coin.of(wrongAmount))
+                .withMessage("동전은 500, 100, 50, 10원 밖에 없습니다.");
     }
 }
