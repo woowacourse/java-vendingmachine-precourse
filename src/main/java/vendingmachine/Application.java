2a3fafd28f68e52933b01096a6dfc1106f0b2c66
yezyvibe <yeji0501@gmail.com>
2021-12-12 14:14:52 +0900

    chore: remove comments

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,13 +1,10 @@
 package vendingmachine;
 
 import vendingmachine.controller.VendingMachineController;
-import vendingmachine.domain.VendingMachine;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        VendingMachineController vendingMachineController = new VendingMachineController();
-        vendingMachineController.run();
-
-    }
+	public static void main(String[] args) {
+		VendingMachineController vendingMachineController = new VendingMachineController();
+		vendingMachineController.run();
+	}
 }
--- src/main/java/vendingmachine/domain/Coin.java
+++ src/main/java/vendingmachine/domain/Coin.java
@@ -16,8 +16,6 @@ public enum Coin {
 		this.amount = amount;
 	}
 
-	// 추가 기능 구현
-
 	public int getAmount() {
 		return amount;
 	}
