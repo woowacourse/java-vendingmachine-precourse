50233497f09711724983e76bd0ec4f8b73eaac6a
Jeong Satchel <bugod96@gmail.com>
2021-12-12 23:04:19 +0900

    feat: throw Exception if price and number input for merchandise info is not a number

--- src/main/java/vendingmachine/utils/ExceptionMessages.java
+++ src/main/java/vendingmachine/utils/ExceptionMessages.java
@@ -8,4 +8,5 @@ public class ExceptionMessages {
     public static final String HAS_ONES_NUMBER_EXCEPTION = ERROR + "1원 단위는 입력하시면 안 됩니다.";
     public static final String UNKNOWN_MERCHANDISE_NAME_EXCEPTION = ERROR + "존재하지 않는 상품명입니다.";
     public static final String SOLD_OUT_EXCEPTION = ERROR + "매진된 상품입니다.";
+    public static final String PRICE_NUMBER_NOT_NUMBER_EXCEPTION = ERROR + "상품의 가격과 수량은 숫자를 입력하셔야 합니다.";
 }
--- src/main/java/vendingmachine/validator/StringFormatValidator.java
+++ src/main/java/vendingmachine/validator/StringFormatValidator.java
@@ -1,8 +1,20 @@
 package vendingmachine.validator;
 
+import static vendingmachine.utils.ExceptionMessages.*;
+
 public class StringFormatValidator {
 
     public static void validateMerchandiseInfoFormat(String merchandiseInfo) {
-        //TODO
+        String[] infoList = merchandiseInfo.substring(1, merchandiseInfo.length() - 1).split(",");
+        validateIntType(infoList[1]);
+        validateIntType(infoList[2]);
+    }
+
+    private static int validateIntType(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(PRICE_NUMBER_NOT_NUMBER_EXCEPTION);
+        }
     }
 }
