df9329a2720bc3c905c6b9c3f9c5b5cfd763ff7c
iamsseaa <homingsea@gmail.com>
2021-12-12 14:01:46 +0900

    feat: 자판기 클래스 생성

---
+++ src/main/java/vendingmachine/machine/Machine.java
@@ -0,0 +1,13 @@
+package vendingmachine.machine;
+
+import vendingmachine.goods.Goods;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Machine {
+    public List<Goods> goods = new ArrayList<>();
+
+    Coin[] coins = Coin.values();
+
+}
