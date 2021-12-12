d0f7e5246449e3048165eb81a6342fd23d0c3b99
zmfl1230 <zmfl123097@gmail.com>
2021-12-12 21:23:11 +0900

    feat[ExceptionMessage]: define exception messages

---
+++ src/main/java/vendingmachine/utils/ExceptionMessage.java
@@ -0,0 +1,14 @@
+package vendingmachine.utils;
+
+public class ExceptionMessage {
+    public static final String INVALID_NUMBER = "[ERROR] 숫자를 입력하세요.";
+    public static final String INVALID_PRODUCT_PRICE = "[ERROR] 올바른 상품 가격이 아닙니다.";
+    public static final String DUPLICATED_PRODUCT = "[ERROR] 이미 존재하는 상품입니다.";
+    public static final String NOT_CORRECT_PRODUCT_FORMAT = "[ERROR] 올바른 상품 포맷이 아닙니다.";
+    public static final String PRODUCT_LIST_IS_NULL = "[ERROR] 빈 값입니다.";
+
+    public static final String NOT_AVAILABLE = "[ERROR] 이용가능한 상품이 아닙니다.";
+    public static final String OUT_OF_STOCK = "[ERROR] 이용가능한 재고가 없습니다.";
+    public static final String BALANCE_IS_INSUFFICIENT = "[ERROR] 진액이 부족합니다.";
+
+}
