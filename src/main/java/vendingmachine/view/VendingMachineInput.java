8f59785520ee5081ce6e5aa449c63a060e9c5336
skullKim <kyk990328@gmail.com>
2021-12-12 20:20:21 +0900

    feat(VendingMachineInput.java): 제품 정보를 입력받는 기능 완료
    자판기에서 제품 판매 전에 판매할 제품 정보를 입력받는 기능 완료

--- src/main/java/vendingmachine/view/VendingMachineInput.java
+++ src/main/java/vendingmachine/view/VendingMachineInput.java
@@ -1,11 +1,50 @@
 package vendingmachine.view;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import camp.nextstep.edu.missionutils.Console;
+import vendingmachine.models.Product;
+import vendingmachine.utils.VendingMachineMessage;
+
+/**
+ * <h1>자판기 구동에 필요한 정보를 입력받는다</h1>
+ * 자판기 구동에 필요한 정보를 입력받아 컨트롤러에 넘긴다
+ *
+ * @author yunki kim
+ * @version 1.0
+ * @since 2021-12-12(V1.0)
+ */
 
 public class VendingMachineInput {
 
 	public int inputAmountOfMoney() {
+		System.out.println(VendingMachineMessage.INPUT_AMOUNT_MONEY_IN_MACHINE);
 		final String inputtedData = Console.readLine();
 		return Integer.parseInt(inputtedData);
+	}
+
+	private ArrayList<Product> parseProductsInformation(final String productsInformation) {
+		ArrayList<Product> products = new ArrayList<>();
+		final List<String> productsList = Arrays
+			.stream(productsInformation.split(";"))
+			.collect(Collectors.toList());
+		productsList.forEach(product -> {
+			final String[] productString = product.substring(1, product.length() - 1)
+				.split(",");
+			final String name = productString[0];
+			final Integer price = Integer.parseInt(productString[1]);
+			final Integer amount = Integer.parseInt(productString[2]);
+			products.add(new Product(name, price, amount));
+		});
+		return products;
+	}
+
+	public ArrayList<Product> inputProductsInformation() {
+		System.out.println(VendingMachineMessage.INPUT_PRODUCTS_INFORMATION);
+		final String productsInformation = Console.readLine();
+		return parseProductsInformation(productsInformation);
 	}
 }
--- src/test/java/vendingmachine/view/VendingMachineInputTest.java
+++ src/test/java/vendingmachine/view/VendingMachineInputTest.java
@@ -4,10 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import vendingmachine.models.Product;
 
 @DisplayName("VendingMachineInput 클래스")
 class VendingMachineInputTest {
@@ -23,6 +31,41 @@ class VendingMachineInputTest {
 		assertEquals(Integer.parseInt(inputtedMoney),
 			vendingMachineInput.inputAmountOfMoney(),
 			"입력값은 그대로 반환되야 합니다");
+	}
+
+	@DisplayName("자판기가 가지고 있는 상품의 가격, 수량, 상품명을 입력받고 반환한다")
+	@ParameterizedTest(name = "{displayName} productsInformation={0}")
+	@ValueSource(strings = {"[콜라,1500,20]", "[콜라,1500,20];[사이다,100,10]"})
+	void inputProductsInformation(final String productsInformation) {
+		InputStream input = new ByteArrayInputStream(productsInformation.getBytes());
+		System.setIn(input);
+
+		final List<String> productsString = Arrays
+			.stream(productsInformation.split(";"))
+			.collect(Collectors.toList());
+		ArrayList<Product> products = new ArrayList<>();
+		productsString.forEach(product -> {
+			final String[] tempProduct = product.substring(1, product.length() - 1)
+				.split(",");
+			final String productionName = tempProduct[0];
+			final Integer productionPrice = Integer.parseInt(tempProduct[1]);
+			final Integer productionAmount = Integer.parseInt(tempProduct[2]);
+			products.add(new Product(productionName, productionPrice, productionAmount));
+		});
+
+		final ArrayList<Product> returnedProducts = vendingMachineInput.inputProductsInformation();
+		Iterator<Product> productsIterator = products.iterator();
+		Iterator<Product> returnedProductsIterator = returnedProducts.iterator();
+		while(productsIterator.hasNext() && returnedProductsIterator.hasNext()) {
+			final Product product1 = productsIterator.next();
+			final Product product2 = returnedProductsIterator.next();
+			assertEquals(product2.getName(), product1.getName(),
+				"제품 정보가 정상적으로 저장되지 않았다");
+			assertEquals(product2.getPrice(), product1.getPrice(),
+				"제품 정보가 정상적으로 저장되지 않았다");
+			assertEquals(product1.getAmount(), product2.getAmount(),
+				"제품 정보가 정상적으로 저장되지 않았다");
+		}
 	}
 
 }
