0c32fea6965b8949096c294bd76208a2feb1cf7b
EastHShin <xldk78@gmail.com>
2021-12-12 18:41:14 +0900

    fix(Message): 출력 메시지에 newLine 추가

--- src/main/java/vendingmachine/view/messages/Message.java
+++ src/main/java/vendingmachine/view/messages/Message.java
@@ -2,12 +2,12 @@ package vendingmachine.view.messages;
 
 public class Message {
 	public static final String INPUT_SAVED_MONEY = "자판기가 보유하고 있는 금액을 입력해 주세요.";
-	public static final String MACHINE_SAVED_COIN = "자판기가 보유한 동전";
-	public static final String INPUT_SAVED_ITEMS = "상품명과 가격, 수량을 입력해 주세요.";
-	public static final String INPUT_CUSTOMER_MONEY = "투입 금액을 입력해 주세요.";
+	public static final String MACHINE_SAVED_COIN = "\n자판기가 보유한 동전";
+	public static final String INPUT_SAVED_ITEMS = "\n상품명과 가격, 수량을 입력해 주세요.";
+	public static final String INPUT_CUSTOMER_MONEY = "\n투입 금액을 입력해 주세요.";
 	public static final String INPUT_PURCHASE_ITEM = "구매할 상품명을 입력해 주세요.";
 	public static final String REMAINING = "잔돈";
-	public static final String INPUT_MONEY = "투입 금액: ";
+	public static final String INPUT_MONEY = "\n투입 금액: ";
 	public static final String MONEY_UNIT = "원";
 	public static final String COUNT_UNIT = "개";
 	public static final String COIN_DELIMITER = " - ";
