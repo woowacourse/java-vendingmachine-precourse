e0119eb9b1c242e5da1d3a8baf13a340a33066c4
delphox60 <delphox60@gmail.com>
2021-12-12 10:26:36 +0900

    feat(utils.validator): 입력한 보유 금액이 10억 이하의 숫자가 아닌 경우 예외 처리 구현
    두 경우의 예외 처리를 따로 구현하려고 하였으나, integer overflow를 고려하여 함께 처리

--- src/main/java/vendingmachine/utils/validator/HoldingMoneyValidator.java
+++ src/main/java/vendingmachine/utils/validator/HoldingMoneyValidator.java
@@ -2,11 +2,11 @@ package vendingmachine.utils.validator;
 
 public class HoldingMoneyValidator {
 
+    public static final String HOLDING_MONEY_NOT_A_VALID_NUMBER = "[ERROR] 자판기 보유 금액은 10억 이하의 숫자만 입력할 수 있습니다.";
+    public static final int MAX_HOLDING_MONEY = 1000000000;
+
     public static int getValidHoldingMoney(final String input) {
-        try {
-            return Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
-        }
+        int intInput = NumberValidator.getValidNumber(input, HOLDING_MONEY_NOT_A_VALID_NUMBER);
+        NumberValidator.validateNotExceedMaxValue(intInput, MAX_HOLDING_MONEY, HOLDING_MONEY_NOT_A_VALID_NUMBER);
     }
 }
---
+++ src/main/java/vendingmachine/utils/validator/NumberValidator.java
@@ -0,0 +1,18 @@
+package vendingmachine.utils.validator;
+
+public class NumberValidator {
+
+    public static int getValidNumber(final String input, final String errorMessage) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
+
+    public static void validateNotExceedMaxValue(final int input, final int maxValue, final String errorMessage) {
+        if (input > maxValue) {
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
+}
