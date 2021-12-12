48baef20efe6b9be303cef12259e8b6091b9f730
delphox60 <delphox60@gmail.com>
2021-12-12 14:46:21 +0900

    feat(utils.validator): 상품명이 한글, 숫자, 영문 외의 문자를 포함할 경우 예외 처리 구현

--- src/main/java/vendingmachine/utils/validator/ProductInfoValidator.java
+++ src/main/java/vendingmachine/utils/validator/ProductInfoValidator.java
@@ -12,9 +12,11 @@ public class ProductInfoValidator {
 
     public static final String PRODUCT_INVALID_FORMAT_ERROR_MESSAGE = "상품 정보 형식이 올바르지 않습니다. \"[상품명1, 상품 가격1, 상품 수량1];[상품명2, 상품 가격2, 상품 수량2]...\" 형식으로 입력해 주세요.";
     public static final String PRODUCT_DROPPED_INFO_ERROR_MESSAGE = "누락된 정보가 있습니다. 상품명, 가격, 수량을 확인해 주세요.";
+    public static final String PRODUCT_INVALID_NAME_ERROR_MESSAGE = "상품명은 한글, 숫자, 영문으로만 이루어진 이름이어야 합니다.";
     private static final String SEMICOLON_SEPARATION_REGEX = "\\s*;\\s*";
     private static final Pattern PRODUCT_INFO_PATTERN = Pattern.compile(
         "^\\[\\s*(.*)\\s*,\\s*(.*)\\s*,\\s*(.*)\\s*\\]$");
+    private static final Pattern PRODUCT_NAME_PATTERN = Pattern.compile("^[가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9]+$");
     private static final int PRODUCT_NAME_INDEX = 1;
     private static final int PRODUCT_PRICE_INDEX = 2;
     private static final int PRODUCT_STOCK_INDEX = 3;
@@ -43,7 +45,14 @@ public class ProductInfoValidator {
 
     private static String getValidProductName(final String name) { // TODO implement Exception cases
         validateIsNotDropped(name);
+        validateName(name);
         return name;
+    }
+
+    private static void validateName(String name) {
+        if (!PRODUCT_NAME_PATTERN.matcher(name).matches()) {
+            throw new IllegalArgumentException(PRODUCT_INVALID_NAME_ERROR_MESSAGE);
+        }
     }
 
     private static int getValidProductPrice(final String price) { // TODO implement Exception cases
