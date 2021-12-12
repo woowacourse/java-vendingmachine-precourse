b8ba086036870a7dad8c75455da207eb7da8a3b7
iamsseaa <homingsea@gmail.com>
2021-12-12 22:50:23 +0900

    feat: 사용자에게 보유 금액 입력받기

--- src/main/java/vendingmachine/process/ProcessPrepare.java
+++ src/main/java/vendingmachine/process/ProcessPrepare.java
@@ -1,6 +1,14 @@
 package vendingmachine.process;
 
-public class ProcessPrepare {
+import camp.nextstep.edu.missionutils.Console;
 
+import vendingmachine.user.User;
+
+public class ProcessPrepare {
+    public static String holdingAmount;
+
+    public static void inputHoldingAmount() {
+        holdingAmount = User.inputMoney();
+    }
 
 }
