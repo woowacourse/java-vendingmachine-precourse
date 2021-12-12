d4e3f8d1aad4981f3dfa08aac1b26200b6405b5f
dongho <dongho108@naver.com>
2021-12-12 17:41:33 +0900

    refactor: 변수와 메서드이름을 더 확실하게 수정

--- src/main/java/vendingmachine/util/MapSupporter.java
+++ src/main/java/vendingmachine/util/MapSupporter.java
@@ -5,10 +5,10 @@ import java.util.Map;
 import vendingmachine.domain.Coin;
 
 public class MapSupporter {
-	public static void increaseValue(Map<Coin, Integer> map, Coin keyCoin, final int initValue, final int value) {
-		if (!map.containsKey(keyCoin)) {
-			map.put(keyCoin, initValue);
+	public static void increaseCoinCount(Map<Coin, Integer> coins, Coin keyCoin, final int initValue, final int value) {
+		if (!coins.containsKey(keyCoin)) {
+			coins.put(keyCoin, initValue);
 		}
-		map.replace(keyCoin, map.get(keyCoin), map.get(keyCoin) + value);
+		coins.replace(keyCoin, coins.get(keyCoin), coins.get(keyCoin) + value);
 	}
 }
