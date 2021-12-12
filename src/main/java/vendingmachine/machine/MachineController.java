fb4dc7c46ed142cc926b4966a71adeb1760e0486
iamsseaa <homingsea@gmail.com>
2021-12-12 16:28:46 +0900

    feat: 코인 중 랜덤하게 하나 선택

--- src/main/java/vendingmachine/machine/MachineController.java
+++ src/main/java/vendingmachine/machine/MachineController.java
@@ -2,15 +2,22 @@ package vendingmachine.machine;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MachineController {
     Coin[] coins = Coin.values();
     Machine machine;
+    public int currentAmount = 0;
 
     MachineController(Machine machine) {
         this.machine = machine;
     }
 
-    
+    public int pickRandomCoin() {
+        int random = Randoms.pickNumberInList(new ArrayList<Integer>(Arrays.asList(500,100,50,10)));
+
+        return random;
+    }
 }
