918819b4a68f43ab2f4d02498be83bf30362016d
summerlunaa <rhgus1085@gmail.com>
2021-12-13 01:44:09 +0900

    refactor(ProductValidator, Validator): 메서드 이름 변경

--- src/main/java/vendingmachine/validator/Validator.java
+++ src/main/java/vendingmachine/validator/Validator.java
@@ -16,43 +16,27 @@ public class Validator {
 		}
 	}
 
-	public static boolean isValidProductsInputFormat(List<String> productsInfo) {
-		try {
-			validateProductsInputFormat(productsInfo);
-			return true;
-		} catch (IllegalArgumentException e) {
-			System.out.println(e.getMessage());
-			return false;
-		}
-	}
-
-	private static void validateProductsInputFormat(List<String> productsInfo) {
+	public static void validateProductsInputFormat(List<String> productsInfo) {
 		for (String productInfo : productsInfo) {
-			ProductValidator.checkFormat(productInfo);
+			ProductValidator.validateFormat(productInfo);
 		}
 	}
 
-	public static boolean isValidProduct(List<List<String>> productsEachInfoList) {
-		try {
-			for (List<String> productInfo : productsEachInfoList) {
-				Validator.validateEachProduct(productInfo);
-			}
-			return true;
-		} catch (IllegalArgumentException e) {
-			System.out.println(e.getMessage());
-			return false;
+	public static void ValidateProduct(List<List<String>> productsEachInfoList) {
+		for (List<String> productInfo : productsEachInfoList) {
+			Validator.validateEachProduct(productInfo);
 		}
 	}
 
 	private static void validateEachProduct(List<String> productInfo) {
-		ProductValidator.checkInfoMiss(productInfo);
+		ProductValidator.validateInfoMiss(productInfo);
 		validateProductName(productInfo.get(0));
 		validateProductPrice(productInfo.get(1));
 		validateProductAmount(productInfo.get(2));
 	}
 
 	private static void validateProductName(String name) {
-		ProductValidator.checkIsEmptyName(name);
+		ProductValidator.validateIsEmptyName(name);
 	}
 
 	private static void validateProductPrice(String price) {
