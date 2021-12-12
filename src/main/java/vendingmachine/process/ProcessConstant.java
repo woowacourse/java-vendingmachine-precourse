b26f2522547426bd254acbf00484b4cad7b71ea8
iamsseaa <homingsea@gmail.com>
2021-12-12 17:24:40 +0900

    feat: 미션 진행 관련 문구 상수

---
+++ src/main/java/vendingmachine/process/ProcessConstant.java
@@ -0,0 +1,18 @@
+package vendingmachine.process;
+
+public class ProcessConstant {
+
+    public static final String ASK_HOLDING_AMOUNT = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    public static final String PRINT_HOLDING_AMOUNT = "자판기가 보유한 동전";
+    public static final String COIN_500 = "500원 - ";
+    public static final String COIN_100 = "100원 - ";
+    public static final String COIN_50 = "50원 - ";
+    public static final String COIN_10 = "10원 - ";
+
+    public static final String ASK_GOODS = "상품명과 가격, 수량을 입력해 주세요.";
+    public static final String ASK_USER_MONEY = "투입 금액을 입력해 주세요.";
+    public static final String USER_MONEY = "투입 금액: ";
+    public static final String ASK_BUY_GOODS_NAME = "구매할 상품명을 입력해 주세요.";
+
+    public static final String EXCHANGE = "잔돈";
+}
