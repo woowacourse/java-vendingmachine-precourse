3c0746023b7542614f8db3ef32790f77e098351e
bcc0830 <bcc0830@naver.com>
2021-12-12 18:52:20 +0900

    feat(ItemChoiceView) : 현재 투입금액에서 사고자 하는 제품의 입력을 가져오는 기능 추가

--- src/main/java/vendingmachine/view/ItemChoiceView.java
+++ src/main/java/vendingmachine/view/ItemChoiceView.java
@@ -3,7 +3,7 @@ package vendingmachine.view;
 import vendingmachine.utils.Validator;
 
 public class ItemChoiceView implements InputView {
-	public String checkAllCondtions(String nowInput) {
+	public String checkAllConditions(String nowInput) {
 		try {
 			Validator.validateNameInput(nowInput);
 			return nowInput;
@@ -11,5 +11,15 @@ public class ItemChoiceView implements InputView {
 			System.out.print(error.getMessage());
 			return BLANK_STRING;
 		}
+	}
+
+	public String getInput() {
+		OutputView.noticeBuyItemInput();
+		String nowInput = BLANK_STRING;
+		while (nowInput.isEmpty()) {
+			String userInput = camp.nextstep.edu.missionutils.Console.readLine();
+			nowInput = checkAllConditions(userInput);
+		}
+		return nowInput;
 	}
 }
