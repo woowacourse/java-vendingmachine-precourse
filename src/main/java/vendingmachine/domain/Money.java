81e101521bf90787b12e1c8788759123d5d99bc3
xfrnk2 <xfrnk2@gmail.com>
2021-12-12 21:01:54 +0900

    feat(Money): 잔돈으로 반환 가능한 동전의 수량을 구하는 기능 구현

--- src/main/java/vendingmachine/domain/Money.java
+++ src/main/java/vendingmachine/domain/Money.java
@@ -1,5 +1,8 @@
 package vendingmachine.domain;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Money {
 	private static final String COLON = ":";
 	private static final String INSERTED_REMAINING_AMOUNT = "%n투입 금액%s %s원";
@@ -13,8 +16,24 @@ public class Money {
 		reduce(moneyAmount);
 	}
 
+	public void getMoney() {
+		System.out.println(money);
+	}
+
 	public boolean payable(int moneyAmount) {
 		return moneyAmount <= money;
+	}
+
+	public Map<Integer, Integer> makeChanges(Coins coins) {
+		Map<Integer, Integer> changes = new LinkedHashMap<>();
+		for (Map.Entry<Integer, Integer> coin : coins.findAll().entrySet()) {
+			int existentCoinAmount = money / coin.getKey();
+			if (0 < existentCoinAmount && existentCoinAmount <= coin.getValue()) {
+				changes.put(coin.getKey(), existentCoinAmount);
+				reduce(coin.getKey() * existentCoinAmount);
+			}
+		}
+		return changes;
 	}
 
 	private void reduce(int moneyAmount) {
