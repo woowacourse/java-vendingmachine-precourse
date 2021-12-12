d7fa4de9b2d27aeea0dfa316b43b794dddf3edd9
overnew <ehdls1303@gmail.com>
2021-12-12 13:10:29 +0900

    feat: checkSameName() 함수 구현
    상품의 이름이 중복인지 확인
     - StringChecker.checkSameString()를 호출하여 구현

--- src/main/java/vendingmachine/utils/productchecker/NameChecker.java
+++ src/main/java/vendingmachine/utils/productchecker/NameChecker.java
@@ -2,11 +2,17 @@ package vendingmachine.utils.productchecker;
 
 import vendingmachine.utils.datatypechecker.StringChecker;
 
+import java.util.List;
+
 public class NameChecker {
     static private final String CONTENT_TYPE = "상품명";
 
     static void checkName(String name) throws IllegalArgumentException{
         StringChecker.checkStringLength(name, CONTENT_TYPE);
+    }
+
+    static void checkSameName(List<String> names) throws IllegalArgumentException{
+        StringChecker.checkSameString(names, CONTENT_TYPE);
     }
 
 }
