0d93dab4bc6f4df24f1c94d39e215654de4f1de8
summerlunaa <rhgus1085@gmail.com>
2021-12-12 20:52:49 +0900

    feat(Validator): 상품 가격의 유효성을 검증하는 기능 구현
    isValidProductsInfo, checkProductsInfo, checkEachInfo, checkProductName 메서드 구현

--- src/main/java/vendingmachine/validator/Validator.java
+++ src/main/java/vendingmachine/validator/Validator.java
@@ -1,7 +1,11 @@
 package vendingmachine.validator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Validator {
-	public static boolean checkMoneyInMachine(String moneyInVendingMachine) {
+	public static boolean isValidMoneyInMachine(String moneyInVendingMachine) {
 		try {
 			NumberValidator.isInteger(moneyInVendingMachine);
 			int intMoneyInVendingMachine = Integer.parseInt(moneyInVendingMachine);
@@ -12,5 +16,36 @@ public class Validator {
 			System.out.println(e.getMessage());
 			return false;
 		}
+	}
+
+	public static boolean isValidProductsInfo(String productsInfo) {
+		try {
+			checkProductsInfo(productsInfo);
+			return true;
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
+	}
+
+	private static void checkProductsInfo(String productsInfo) {
+		for (String productInfo : Arrays.stream(productsInfo.split(";"))
+			.map(String::trim)
+			.collect(Collectors.toList())) {
+			ProductValidator.checkFormat(productInfo);
+			checkEachInfo(productInfo);
+		}
+	}
+
+	private static void checkEachInfo(String productInfo) {
+		List<String> info = Arrays.stream(productInfo.split(","))
+			.map(String::trim)
+			.collect(Collectors.toList());
+		ProductValidator.checkInfoMiss(info);
+		checkProductName(info.get(0));
+	}
+
+	private static void checkProductName(String name){
+		ProductValidator.checkIsEmptyName(name);
 	}
 }
