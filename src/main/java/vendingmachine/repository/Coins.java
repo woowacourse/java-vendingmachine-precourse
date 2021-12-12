191d517e54966ee546c68284dd25ebe19f423b76
yoon <duddbs774@gmail.com>
2021-12-12 19:30:15 +0900

    refactor: 매직넘버 상수화 및 잔돈반환 메소드 리팩토링

--- src/main/java/vendingmachine/repository/Coins.java
+++ src/main/java/vendingmachine/repository/Coins.java
@@ -1,15 +1,22 @@
-package vendingmachine.domain;
+package vendingmachine.repository;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import vendingmachine.repository.InputAmount;
+import vendingmachine.domain.Coin;
 
 public class Coins {
+    public static final int COIN_DEFAULT_COUNT = 0;
+    public static final int SELECT_COIN_COUNT = 1;
+    public static final int EMPTY_AMOUNT = 0;
+    public static final int START_INDEX = 0;
+
     private final Map<Coin, Integer> coins = new TreeMap<>();
 
     public Coins(String value) {
@@ -18,42 +25,36 @@ public class Coins {
             int coinAmount = Randoms.pickNumberInList(makeList(amount));
             amount -= coinAmount;
             Coin coin = Coin.mapToCoin(coinAmount);
-            coins.put(coin, coins.getOrDefault(coin, 0) + 1);
-        } while (amount != 0);
+            coins.put(coin, coins.getOrDefault(coin, COIN_DEFAULT_COUNT) + SELECT_COIN_COUNT);
+        } while (amount != EMPTY_AMOUNT);
     }
 
     private List<Integer> makeList(int amount) {
-        if (amount >= 500) {
-            return Arrays.asList(500, 100, 50, 10);
+        if (Coin.COIN_500.isOrLess(amount)) {
+            return Arrays.stream(Coin.values()).map(Coin::getAmount).collect(Collectors.toList());
         }
-        if (amount >= 100) {
-            return Arrays.asList(100, 50, 10);
+        if (Coin.COIN_100.isOrLess(amount)) {
+            return Arrays.asList(Coin.COIN_100.getAmount(), Coin.COIN_50.getAmount(), Coin.COIN_10.getAmount());
         }
-        if (amount >= 50) {
-            return Arrays.asList(50, 10);
+        if (Coin.COIN_50.isOrLess(amount)) {
+            return Arrays.asList(Coin.COIN_50.getAmount(), Coin.COIN_10.getAmount());
         }
-        return Collections.singletonList(10);
+        return Collections.singletonList(Coin.COIN_10.getAmount());
     }
 
     public Map<Coin, Integer> getCoins() {
         return coins;
     }
 
-    public Map<Coin, Integer> exchange(InputAmount amount) {
+    public Map<Coin, Integer> exchange(int amount) {
         Map<Coin, Integer> userCoins = new TreeMap<>();
-        final int[] inputAmount = {Integer.parseInt(amount.toString())};
-
-        coins.forEach((coin, count) -> {
-            for (int i = 0; i < count; i++) {
-                if (!coin.isOrLess(inputAmount[0])) {
-                    break;
-                }
-                if (coin.isOrLess(inputAmount[0])) {
-                    inputAmount[0] -= coin.getAmount();
-                    userCoins.put(coin, userCoins.getOrDefault(coin, 0) + 1);
-                }
+        final int[] inputAmount = {amount};
+        coins.forEach((coin, count) -> IntStream.range(START_INDEX, count).forEach(i -> {
+            if (coin.isOrLess(inputAmount[START_INDEX])) {
+                inputAmount[START_INDEX] -= coin.getAmount();
+                userCoins.put(coin, userCoins.getOrDefault(coin, COIN_DEFAULT_COUNT) + SELECT_COIN_COUNT);
             }
-        });
+        }));
         return userCoins;
     }
 }
