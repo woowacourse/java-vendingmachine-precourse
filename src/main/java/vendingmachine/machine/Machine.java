467543fa54517434a331e42201479eb42772bf6f
iamsseaa <homingsea@gmail.com>
2021-12-12 14:04:56 +0900

    feat: 자판기 생성자 생성

--- src/main/java/vendingmachine/machine/Machine.java
+++ src/main/java/vendingmachine/machine/Machine.java
@@ -7,7 +7,13 @@ import java.util.List;
 
 public class Machine {
     public List<Goods> goods = new ArrayList<>();
-
     Coin[] coins = Coin.values();
+
+    public int holdingAmount;
+
+    public Machine(int holdingAmount, List<Goods> goods) {
+        this.goods = goods;
+        this.holdingAmount = holdingAmount;
+    }
 
 }
