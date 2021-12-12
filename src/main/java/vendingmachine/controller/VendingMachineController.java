0355f36b4ce80debf849b8c6f600673ddf1157b4
zmfl1230 <zmfl123097@gmail.com>
2021-12-12 22:16:04 +0900

    feat[VendingMachineController-Controller]: run vending machine

---
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -0,0 +1,36 @@
+package vendingmachine.controller;
+
+import vendingmachine.domain.VendingMachine;
+import vendingmachine.view.VendingMachineInput;
+import vendingmachine.view.VendingMachineOutput;
+
+
+public class VendingMachineController {
+
+    public void run() {
+        // 자판기가 보유할 금액 입력 받기
+        int amount = VendingMachineInput.insertAmount();
+        VendingMachine vendingMachine = new VendingMachine(amount);
+
+        // 자판기가 보유한 동전 출력
+        vendingMachine.printCoins();
+
+        // 자판기가 보유할 상품 입력 받기
+        String products = VendingMachineInput.insertProducts();
+        vendingMachine.insertProducts(products);
+
+        // 투입 금액 입력 받기
+        int inputMoney = VendingMachineInput.insertInputMoney();
+        vendingMachine.setInputMoney(inputMoney);
+
+        while(vendingMachine.checkPurchasePossible()) {
+            vendingMachine.printRestMoney();
+            String productName = VendingMachineInput.insertProductNameToBuy();
+            vendingMachine.reduceInputMoneyAndProductQuantityByName(productName);
+        }
+
+        vendingMachine.printRestMoney();
+        vendingMachine.printChanges();
+
+    }
+}
