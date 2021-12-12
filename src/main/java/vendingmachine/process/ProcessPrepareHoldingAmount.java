c61f7bfdd057281cfc3e0e9ec2f62a536b072b7d
iamsseaa <homingsea@gmail.com>
2021-12-12 23:25:07 +0900

    refactor: 보유 금액 입력과 상품 입력 클래스 분리

---
+++ src/main/java/vendingmachine/process/ProcessPrepareHoldingAmount.java
@@ -0,0 +1,25 @@
+package vendingmachine.process;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import vendingmachine.user.User;
+import vendingmachine.user.UserGoodsValidation;
+import vendingmachine.user.UserMoneyValidation;
+
+import java.util.List;
+
+public class ProcessPrepareHoldingAmount {
+    public static String holdingAmount;
+
+    public static void inputHoldingAmount() {
+        holdingAmount = User.inputMoney();
+    }
+
+    public static void checkHoldingAmount() {
+        UserMoneyValidation.checkUserMoneyValidation(holdingAmount);
+    }
+
+    public static int toIntegerHoldingAmount() {
+        return Integer.parseInt(holdingAmount);
+    }
+}
