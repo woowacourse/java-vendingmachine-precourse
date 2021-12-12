b19762b624a9a9cd95d6a4b9d863a9b66ebfc080
overnew <ehdls1303@gmail.com>
2021-12-12 23:17:59 +0900

    refactor: StockChecker rename
    기능과 명칭이 알맞지 않아 변경
     StockChecker -> QuantityChecker class

--- src/main/java/vendingmachine/utils/productchecker/QuantityChecker.java
+++ src/main/java/vendingmachine/utils/productchecker/QuantityChecker.java
@@ -3,11 +3,11 @@ package vendingmachine.utils.productchecker;
 import vendingmachine.utils.datatypechecker.IntegerChecker;
 import vendingmachine.utils.datatypechecker.StringChecker;
 
-public class StockChecker {
+public class QuantityChecker {
     static private final int LOW_LIMIT = 0;
     static private final String CONTENT_TYPE = "수량";
 
-    static void checkStock(String stock) throws IllegalArgumentException{
+    static void checkQuantity(String stock) throws IllegalArgumentException{
         StringChecker.checkStringLength(stock, CONTENT_TYPE);
         IntegerChecker.checkStringToInteger(stock, CONTENT_TYPE);
 
