ac068e011caa94f6750e5d7de5a1d42c72832053
Jeong Satchel <bugod96@gmail.com>
2021-12-12 23:07:33 +0900

    feat: throw Exception if number input for merchandise info is a negative number

--- src/main/java/vendingmachine/validator/StringFormatValidator.java
+++ src/main/java/vendingmachine/validator/StringFormatValidator.java
@@ -6,8 +6,12 @@ public class StringFormatValidator {
 
     public static void validateMerchandiseInfoFormat(String merchandiseInfo) {
         String[] infoList = merchandiseInfo.substring(1, merchandiseInfo.length() - 1).split(",");
+        String name = infoList[0];
         validateIntType(infoList[1]);
         validateIntType(infoList[2]);
+        int price = Integer.parseInt(infoList[1]);
+        int number = Integer.parseInt(infoList[2]);
+        validateNotNegativeNumber(number);
     }
 
     private static int validateIntType(String input) {
@@ -15,6 +19,12 @@ public class StringFormatValidator {
             return Integer.parseInt(input);
         } catch (Exception e) {
             throw new IllegalArgumentException(PRICE_NUMBER_NOT_NUMBER_EXCEPTION);
+        }
+    }
+
+    private static void validateNotNegativeNumber(int numberInput) {
+        if (numberInput < 0) {
+            throw new IllegalArgumentException(MERCHANDISE_NUMBER_NEGATIVE_NUMBER_EXCEPTION);
         }
     }
 }
