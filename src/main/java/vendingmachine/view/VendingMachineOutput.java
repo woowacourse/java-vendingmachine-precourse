393acc8efe2915f12dc6e8cb90bc3a130da5b679
zmfl1230 <zmfl123097@gmail.com>
2021-12-12 22:09:56 +0900

    feat[VendingMachineOutput-View]: define output functions - 자판기가 보유한 코인 출력 - 남은(투입) 금액 출력 - 잔돈 출력

---
+++ src/main/java/vendingmachine/view/VendingMachineOutput.java
@@ -0,0 +1,30 @@
+package vendingmachine.view;
+
+import vendingmachine.domain.Coin;
+
+import java.util.Map;
+
+public class VendingMachineOutput {
+    private static final String CONNECTION_MARK = " - ";
+    private static final String INPUT_MONEY_INSTRUCTION = "투입 금액: ";
+    private static final String CHANGES_INSTRUCTION = "잔돈";
+
+    public static void printCoinsVendingMachineHas(Map<Coin, Integer> coinMap) {
+        for(Coin coin : coinMap.keySet()) {
+            System.out.println(coin + CONNECTION_MARK + coinMap.get(coin));
+        }
+    }
+
+    public static void printRestMoney(int restMoney) {
+        System.out.println(INPUT_MONEY_INSTRUCTION + restMoney);
+    }
+
+    public static void printChange(Map<Coin, Integer> changes) {
+        System.out.println(CHANGES_INSTRUCTION);
+        for(Coin coin: changes.keySet()) {
+            System.out.println(coin + CONNECTION_MARK + changes.get(coin));
+        }
+
+    }
+
+}
