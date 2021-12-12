f8ca67bb7ce67c6b15cd234bffd73a759aac7586
bcc0830 <bcc0830@naver.com>
2021-12-12 16:42:49 +0900

    feat(Validator) : 제품 입력이 제품이름,제품가격,제품수량의 형식을 만족하는지 검사하는 기능 추가

--- src/main/java/vendingmachine/utils/Validator.java
+++ src/main/java/vendingmachine/utils/Validator.java
@@ -3,6 +3,7 @@ package vendingmachine.utils;
 public class Validator {
 	private static final String NUMBER_PATTERN = "^[0-9]+$";
 	private static final String NAME_PATTERN = "^[0-9|a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]+$";
+	private static final int ELEMENT_SIZE = 3;
 
 	private static void validatePositive(String input) throws IllegalArgumentException {
 		if (Integer.valueOf(input) <= 0) {
@@ -30,5 +31,11 @@ public class Validator {
 			throw new IllegalArgumentException(
 				Messages.ERROR_NOT_DIVIDABLE_MESSAGE.getValue() + Messages.COMMON_LINE_BREAK_MSG.getValue());
 		}
+	}
+
+	public static void validateElementSize(String[] itemInput) throws IllegalArgumentException {
+		if (itemInput.length != ELEMENT_SIZE)
+			throw new IllegalArgumentException(
+				Messages.ERROR_NOT_VALID_ELEMENT_SIZE.getValue() + Messages.COMMON_LINE_BREAK_MSG.getValue());
 	}
 }
