3fe6a232eb224dc120c29e4b5d8447088736f475
bcc0830 <bcc0830@naver.com>
2021-12-12 16:37:22 +0900

    feat(Validator) : 입력이 숫자인지 검사하는 기능 추가

--- src/main/java/vendingmachine/utils/Validator.java
+++ src/main/java/vendingmachine/utils/Validator.java
@@ -1,10 +1,18 @@
 package vendingmachine.utils;
 
 public class Validator {
+	private static final String NUMBER_PATTERN = "^[0-9]+$";
 	private static void validatePositive(String input) throws IllegalArgumentException {
 		if (Integer.valueOf(input) <= 0) {
 			throw new IllegalArgumentException(
 				Messages.ERROR_NOT_POSITIVE.getValue() + Messages.COMMON_LINE_BREAK_MSG.getValue());
+		}
+	}
+
+	private static void validateNumber(String input) throws IllegalArgumentException {
+		if (!input.matches(NUMBER_PATTERN)) {
+			throw new IllegalArgumentException(
+				Messages.ERROR_NOT_NUMBER.getValue() + Messages.COMMON_LINE_BREAK_MSG.getValue());
 		}
 	}
 }
