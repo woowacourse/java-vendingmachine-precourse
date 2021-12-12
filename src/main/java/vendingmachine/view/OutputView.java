599d2b126b58905d2e33b3f2402246d7ba060992
hyeonic <dev.hyeonic@gmail.com>
2021-12-13 00:12:08 +0900

    feat: OutputView 투입 금액 출력을 위한 메서드 추출 및 잔돈 출력 메서드 추가

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -1,10 +1,14 @@
 package vendingmachine.view;
 
+import vendingmachine.domain.Changes;
+import vendingmachine.domain.InvestmentMoney;
 import vendingmachine.domain.PossessionCoins;
 
 public class OutputView {
+    private static final String POSSESSION_COINS_MESSAGE = "\n자판기가 보유한 동전";
 
-    public static final String POSSESSION_COINS_MESSAGE = "\n자판기가 보유한 동전";
+    private static final String INVESTMENT_MONEY_FORMAT = "\n투입 금액: %s\n";
+    public static final String CHANGES_MESSAGE = "잔돈";
 
     private OutputView() {
     }
@@ -12,5 +16,14 @@ public class OutputView {
     public static void printPossessionCoins(PossessionCoins possessionCoins) {
         System.out.println(POSSESSION_COINS_MESSAGE);
         possessionCoins.getPossessionCoins().forEach(System.out::println);
+    }
+
+    public static void printInvestmentMoney(InvestmentMoney investmentMoney) {
+        System.out.printf(INVESTMENT_MONEY_FORMAT, investmentMoney);
+    }
+
+    public static void printChanges(Changes changes) {
+        System.out.println(CHANGES_MESSAGE);
+        changes.getChanges().forEach(System.out::println);
     }
 }
