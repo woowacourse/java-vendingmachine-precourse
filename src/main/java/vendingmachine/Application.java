0785137debe570abc77480bc9104d62306565dc6
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-12-13 03:17:36 +0900

    feat: 현재 자판기 보유 동전 출력 기능 구현

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,14 +1,18 @@
 package vendingmachine;
 
 import static vendingmachine.view.InputView.inputVendinMachineOwnMoney;
+import static vendingmachine.view.OutputView.printCurrentOwnCoins;
 import static vendingmachine.view.OutputView.printErrorMessage;
 
 import vendingmachine.domain.Money;
+import vendingmachine.domain.VendingMachine;
 
 public class Application {
 
     public static void main(String[] args) {
         Money money = inputVendingMachineOwnMoney();
+        VendingMachine machine = VendingMachine.createRandomVendingMachineByMoney(money);
+        printCurrentOwnCoins(machine.currentRemainCoins());
     }
 
     private static Money inputVendingMachineOwnMoney() {
--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -1,11 +1,23 @@
 package vendingmachine.view;
 
+import java.util.Map;
+import vendingmachine.domain.Coin;
+
 public class OutputView {
+
+    private static final String PRINT_VENDING_MACHINE_OWN_COINS_MESSAGE = "자판기가 보유한 동전";
 
     private OutputView() {
     }
 
     public static void printErrorMessage(Exception e) {
         System.out.println(e.getMessage());
+    }
+
+    public static void printCurrentOwnCoins(Map<Coin, Integer> coins) {
+        System.out.println(PRINT_VENDING_MACHINE_OWN_COINS_MESSAGE);
+        for (Coin coin : coins.keySet()) {
+            System.out.printf("%d원 - %d개\n", coin.amount(), coins.get(coin));
+        }
     }
 }
