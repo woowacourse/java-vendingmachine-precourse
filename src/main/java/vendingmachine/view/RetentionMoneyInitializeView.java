4f144a6d27fa74b3442562d841583219561c3aae
GYUTAEKIM <kkt219a@gmail.com>
2021-12-12 17:24:35 +0900

    refactor: 공통 뷰에 컨트롤러 권한 양도
    - 공통으로 컨트롤러가 쓰이기 떄문에 공통뷰로 컨트롤러를 양도한다.

--- src/main/java/vendingmachine/view/RetentionMoneyInitializeView.java
+++ src/main/java/vendingmachine/view/RetentionMoneyInitializeView.java
@@ -1,11 +1,8 @@
 package vendingmachine.view;
 
-import vendingmachine.VendingMachineController;
 import vendingmachine.coin.Coins;
 
-public class RetentionMoneyInitializeView extends View {
-	private final VendingMachineController controller = new VendingMachineController();
-
+public class RetentionMoneyInitializeView extends VendingMachineView {
 	public void show() {
 		String retentionMoney = inputProcessor.readRetentionMoney();
 		Coins coins = controller.addRetentionCoin(retentionMoney);
--- src/main/java/vendingmachine/view/VendingMachineView.java
+++ src/main/java/vendingmachine/view/VendingMachineView.java
@@ -1,17 +1,20 @@
 package vendingmachine.view;
 
+import vendingmachine.VendingMachineController;
 import vendingmachine.processor.InputProcessor;
 import vendingmachine.processor.OutputProcessor;
 
-public abstract class View {
+public abstract class VendingMachineView {
 	private Visible visible;
 	protected final InputProcessor inputProcessor;
 	protected final OutputProcessor outputProcessor;
+	protected final VendingMachineController controller;
 
-	public View() {
+	public VendingMachineView() {
 		this.visible = Visible.SHOW;
 		inputProcessor = new InputProcessor();
 		outputProcessor = new OutputProcessor();
+		controller = new VendingMachineController();
 	}
 
 	public void hide() {
