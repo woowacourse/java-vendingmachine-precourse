c9bf24fd51c9d739728f4dca3fc1433c06e9f47c
Chan Kim <chankim@Chanui-MacBookPro.local>
2021-12-13 06:55:59 +0900

    feat : 자판기가 보유하고 있는 금액에 맞게 동전을 무작위로 생성

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,16 +1,29 @@
 package vendingmachine;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum Coin {
-    COIN_500(500),
-    COIN_100(100),
-    COIN_50(50),
-    COIN_10(10);
+	COIN_500(500),
+	COIN_100(100),
+	COIN_50(50),
+	COIN_10(10);
 
-    private final int amount;
+	private static final Map<Integer, String> COIN_MAP = Collections.unmodifiableMap(
+		Stream.of(values()).collect(Collectors.toMap(Coin::getAmount, Coin::name)));
+	private final int amount;
 
-    Coin(final int amount) {
-        this.amount = amount;
-    }
+	Coin(final int amount) {
+		this.amount = amount;
+	}
 
-    // 추가 기능 구현
+	public int getAmount() {
+		return amount;
+	}
+
+	public static Coin of(int amount) {
+		return Coin.valueOf(COIN_MAP.get(amount));
+	}
 }
---
+++ src/main/java/vendingmachine/VendingMachineCoinGenerator.java
@@ -0,0 +1,37 @@
+package vendingmachine;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class VendingMachineCoinGenerator {
+	private static Map<Coin, Integer> vendingMachineCoinCounts = new LinkedHashMap<>();
+	private static List<Integer> coinAmounts = new ArrayList<>();
+
+	static {
+		Arrays.stream(Coin.values()).forEach(coin -> vendingMachineCoinCounts.put(coin, 0));
+		Arrays.stream(Coin.values()).map(coin -> coin.getAmount()).forEach(coinAmounts::add);
+	}
+
+	public static Map<Coin, Integer> generateVendingMachineCoin(int money) {
+		int vendingMachineMoneyRemaining = money;
+
+		while (vendingMachineMoneyRemaining > 0) {
+			int randomCoinAmount = Randoms.pickNumberInList(coinAmounts);
+			Coin coin = Coin.of(randomCoinAmount);
+
+			if (vendingMachineMoneyRemaining - randomCoinAmount < 0) {
+				continue;
+			}
+
+			vendingMachineMoneyRemaining -= randomCoinAmount;
+			vendingMachineCoinCounts.put(coin, vendingMachineCoinCounts.get(coin) + 1);
+		}
+
+		return vendingMachineCoinCounts;
+	}
+}
