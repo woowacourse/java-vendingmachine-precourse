77b2d816a963aa0e59f4ad1b9d5b44ea7476ebac
Byunghoon <nbalance__@kyonggi.ac.kr>
2021-12-12 14:02:11 +0900

    refactor: 검증 후 데이터 추가 방식으로 변경

--- src/main/java/vendingmachine/Validators.java
+++ src/main/java/vendingmachine/Validators.java
@@ -1,60 +1,30 @@
 package vendingmachine;
-import vendingmachine.model.Item;
-
-import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 public class Validators {
-
-    public static int validateIntegerString(String data) {
+    public static void validateIntegerString(String data) {
         try {
-            return Integer.parseInt(data);
+            Integer.parseInt(data);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException();
         }
     }
 
-    public static int validateAmount(String inputData) {
-        try {
-            int amount = Integer.parseInt(inputData);
-            if (amount <= 0) {
+    public static void validateAmount(String data) {
+        validateIntegerString(data);
+        if (Integer.parseInt(data) <= 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validateItem(String inputData) {
+        String[] itemDataList = inputData.split(";");
+        String pattern = "\\[[[A-Za-z가-힣0-9ㄱ-ㅎ][\\s]*]+,[0-9]+,[0-9]+\\]";
+        for (String itemData : itemDataList) {
+            if (!Pattern.matches(pattern, itemData)) {
                 throw new IllegalArgumentException();
             }
-            return amount;
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
         }
     }
 
-    private static String[] checkItemInformation(String itemString) {
-        if (itemString.length() <= 2) {
-            throw new IllegalArgumentException();
-        }
-        itemString = itemString.substring(1, itemString.length()-1);
-        String[] itemInformation = itemString.split(",");
-        if (itemInformation.length != 3) {
-            throw new IllegalArgumentException();
-        }
-        return itemInformation;
-    }
-
-    private static Item checkItemString(String itemString) throws IllegalArgumentException {
-        try {
-            String[] itemInformation = checkItemInformation(itemString);
-            String name = itemInformation[0];
-            int price = Integer.parseInt(itemInformation[1]);
-            int count = Integer.parseInt(itemInformation[2]);
-            return new Item(name, price, count);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    public static ArrayList<Item> validateItem(String inputData) throws IllegalArgumentException {
-        String[] itemStringArray = inputData.split(";");
-        ArrayList<Item> itemList = new ArrayList<Item>();
-        for (String itemString : itemStringArray) {
-            itemList.add(checkItemString(itemString));
-        }
-        return itemList;
-    }
 }
--- src/main/java/vendingmachine/controller/InputManager.java
+++ src/main/java/vendingmachine/controller/InputManager.java
@@ -1,10 +1,14 @@
 package vendingmachine.controller;
+
 import camp.nextstep.edu.missionutils.Console;
 import vendingmachine.Validators;
 import vendingmachine.model.Item;
 import vendingmachine.model.VendingMachine;
 
+import java.util.Arrays;
+
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class InputManager {
     private static final String PREFIX_ERROR = "[ERROR]";
@@ -20,7 +24,8 @@ public class InputManager {
         while (true) {
             String inputData = getUserInput("자판기가 보유하고 있는 금액을 입력해 주세요.");
             try {
-                return new VendingMachine(Validators.validateAmount(inputData));
+                Validators.validateAmount(inputData);
+                return new VendingMachine(Integer.parseInt(inputData));
             } catch (IllegalArgumentException e) {
                 System.out.println(PREFIX_ERROR + " 0 또는 자연수만 입력 가능합니다.");
             }
@@ -31,7 +36,11 @@ public class InputManager {
         while (true) {
             String inputData = getUserInput("상품명과 가격, 수량을 입력해 주세요.");
             try {
-                return Validators.validateItem(inputData);
+                Validators.validateItem(inputData);
+                return new ArrayList<Item>(Arrays.stream(inputData.split(";")).map((eachItem) -> {
+                    String[] itemData = eachItem.split(",");
+                    return new Item(itemData[0], Integer.parseInt(itemData[1]), Integer.parseInt(itemData[2]));
+                }).collect(Collectors.toList()));
             } catch (IllegalArgumentException e) {
                 System.out.println(PREFIX_ERROR + " 잘못된 상품 입력입니다.");
             }
@@ -42,7 +51,7 @@ public class InputManager {
         while (true) {
             String inputData = getUserInput("투입 금액을 입력해주세요.");
             try {
-                return Validators.validateIntegerString(inputData);
+                Validators.validateIntegerString(inputData);
             } catch (IllegalArgumentException e) {
                 System.out.println(PREFIX_ERROR + " 잘못된 금액 입력입니다.");
             }
