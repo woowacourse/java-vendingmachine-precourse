e80c0f60f4158458fd4fe7376764ca6c7aa802ca
kanamycine <jinyeong5320@gmail.com>
2021-12-12 21:09:13 +0900

    feat(Item) : Item model 클래스 추가

--- src/main/java/vendingmachine/domain/Item.java
+++ src/main/java/vendingmachine/domain/Item.java
@@ -1,4 +1,13 @@
 package vendingmachine.domain;
 
 public class Item {
+	private String name = "";
+	private int price = 0;
+	private int stock = 0;
+
+	public Item(String name, int price, int stock){
+		this.name = name;
+		this.price = price;
+		this.stock = stock;
+	}
 }
