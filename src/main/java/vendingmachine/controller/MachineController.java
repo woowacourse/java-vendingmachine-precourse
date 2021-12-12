09f516a6069f6dd6235ce9c574e75814f2b41cdb
0chil <0@chll.it>
2021-12-12 15:42:53 +0900

    feat: 절차에 의한 진행

--- src/main/java/vendingmachine/controller/MachineController.java
+++ src/main/java/vendingmachine/controller/MachineController.java
@@ -1,21 +1,28 @@
 package vendingmachine.controller;
 
 import static vendingmachine.view.InputView.*;
+import static vendingmachine.view.OutputView.*;
 
 import java.util.Arrays;
 import java.util.List;
 
+import vendingmachine.domain.Machine;
 import vendingmachine.repository.DepositRepository;
+import vendingmachine.repository.ProductRepository;
 import vendingmachine.service.MachineService;
 
 public class MachineController {
 
 	private final MachineService machineService;
 	private final DepositRepository depositRepository;
+	private final ProductRepository productRepository;
+	private final Machine machine;
 
-	public MachineController(MachineService machineService, DepositRepository depositRepository) {
-		this.machineService = machineService;
-		this.depositRepository = depositRepository;
+	public MachineController() {
+		this.depositRepository = new DepositRepository();
+		this.productRepository = new ProductRepository();
+		this.machine = new Machine();
+		this.machineService = new MachineService(depositRepository, productRepository, machine);
 	}
 
 	public void setDeposit() {
@@ -37,5 +44,17 @@ public class MachineController {
 	public void purchase() {
 		String input = inputProductName();
 		machineService.purchaseProduct(input);
+	}
+
+	public void operate() {
+		setDeposit();
+		printDeposits(depositRepository);
+		setProductList();
+		setMoney();
+		while (machineService.getAffordableList().size() > 0){
+			printMoney(machine);
+			purchase();
+		}
+		printChanges(machineService.spitChanges());
 	}
 }
