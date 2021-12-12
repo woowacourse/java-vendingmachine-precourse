5027bff9406c8e0f92a171471f26199799f324a7
xi_jjun <rlawowns97@naver.com>
2021-12-12 21:18:01 +0900

    feat: add Coins class functions
    Fields Summary
    private Map<Coin, Integer> vendingMachineRemainedCoins : Coin class를 Key값으로 하는 Map. value는 Coin의 개수를 뜻한다.
    
    Method Summary
    public Map<Coin, Integer> createCoins(int possession) : possession 만큼의 동전을 랜덤하게 생성한다. Map<동전타입, 동전개수> 형태로 return.
    private void initCoinType() : field vendingMachineRemainedCoins 의 Key를 Coin.values() 로 초기화 한다.
    private void generateRandomCoins(int possession) : possession만큼의 동전을 랜덤하게 생생하여 field 에 추가한다.
    private boolean isCompleted(int possession, int totalCoin) : totalCoin이 possession만큼 생성됐다면 true return.
    private int addRandomCoin(int limit, int generatedCoinAmount, int randomCoin)
    	이때까지 만들어진 돈의 양인 generatedCoinAmount와 현재 랜덤으로 생성된 randomCoin 의 합이 limit보다 작다면 field에 추가.
    private int incrementCoinCount(Coin coin) : coin에 해당하는 field의 key에 동전 개수 1개 추가.
    public Map<Coin, Integer> getVendingMachineRemainedCoins() : field의 vendingMachineRemainedCoins return.

---
+++ src/main/java/vendingmachine/domain/Coins.java
@@ -0,0 +1,58 @@
+package vendingmachine.domain;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import static vendingmachine.constants.Constant.*;
+
+public class Coins {
+	private final Map<Coin, Integer> vendingMachineRemainedCoins = new EnumMap<>(Coin.class);
+
+	public Map<Coin, Integer> createCoins(int possession) {
+		initCoinType();
+		generateRandomCoins(possession);
+
+		return vendingMachineRemainedCoins;
+	}
+
+	private void initCoinType() {
+		for (Coin coinType : Coin.values()) {
+			vendingMachineRemainedCoins.put(coinType, INIT_COUNT);
+		}
+	}
+
+	private void generateRandomCoins(int possession) {
+		int generatedCoinAmount = INIT_COUNT;
+
+		while (!isCompleted(possession, generatedCoinAmount)) {
+			int randomCoin = Randoms.pickNumberInList(COIN_VALUES);
+			generatedCoinAmount = addRandomCoin(possession, generatedCoinAmount, randomCoin);
+		}
+	}
+
+	private boolean isCompleted(int possession, int totalCoin) {
+		return totalCoin == possession;
+	}
+
+	private int addRandomCoin(int limit, int generatedCoinAmount, int randomCoin) {
+		if (generatedCoinAmount + randomCoin <= limit) {
+			generatedCoinAmount += randomCoin;
+
+			Coin coinType = Coin.findType(randomCoin);
+			int coinCount = incrementCoinCount(coinType);
+
+			vendingMachineRemainedCoins.put(coinType, coinCount);
+		}
+		return generatedCoinAmount;
+	}
+
+	private int incrementCoinCount(Coin coin) {
+		return vendingMachineRemainedCoins.getOrDefault(coin, INIT_COUNT) + ADD_ONE;
+	}
+
+	public Map<Coin, Integer> getVendingMachineRemainedCoins() {
+		return vendingMachineRemainedCoins;
+	}
+}
