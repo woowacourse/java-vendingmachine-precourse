045431b105d4b1f5e53945b9ffcae106df70f846
bcc0830 <bcc0830@naver.com>
2021-12-12 15:35:21 +0900

    feat(Pair) : 생성자 추가

---
+++ src/main/java/vendingmachine/models/Pair.java
@@ -0,0 +1,11 @@
+package vendingmachine.models;
+
+public class Pair {
+	private int firstValue;
+	private int secondValue;
+
+	public Pair(int firstValue, int secondValue) {
+		this.firstValue = firstValue;
+		this.secondValue = secondValue;
+	}
+}
