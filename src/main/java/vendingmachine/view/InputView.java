8e973acf30f92dfa86dd79a8940858599ff8d793
hyeonic <dev.hyeonic@gmail.com>
2021-12-12 13:13:35 +0900

    feat: InputView 투입 금액 입력을 위한 메서드 추가

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -6,15 +6,19 @@ import java.util.Arrays;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Console;
+import vendingmachine.domain.InvestmentMoney;
 import vendingmachine.domain.PossessionMoney;
 import vendingmachine.domain.Product;
 import vendingmachine.domain.Products;
 
 public class InputView {
     private static final String POSSESSION_MONEY_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+
     private static final String PRODUCT_SAVE_MESSAGE = "\n상품명과 가격, 수량을 입력해 주세요.";
     private static final String INPUT_PRODUCTS_SPLIT_REGEX = ";";
     private static final int INPUT_PRODUCTS_SPLIT_LIMIT = -1;
+
+    private static final String INVESTMENT_MONEY_MESSAGE = "투입 금액을 입력해 주세요.";
 
     private InputView() {
     }
@@ -40,5 +44,10 @@ public class InputView {
         return Arrays.stream(splitProducts)
             .map(Product::new)
             .collect(toList());
+    }
+
+    public static InvestmentMoney getInvestmentMoney() {
+        System.out.println(INVESTMENT_MONEY_MESSAGE);
+        return new InvestmentMoney(Console.readLine());
     }
 }
