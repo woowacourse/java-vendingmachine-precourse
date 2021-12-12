fbdf8a030e6440323238ce929fcf11d68b044148
BeomsooKim <kbsat@naver.com>
2021-12-12 11:54:27 +0900

    feat(vendingmachine): 보유한 금액으로 무작위로 동전을 생성하는 기능 구현

--- src/main/java/vendingmachine/domain/Changes.java
+++ src/main/java/vendingmachine/domain/Changes.java
@@ -7,5 +7,10 @@ public class Changes {
 	private Map<Coin, Integer> changes = new HashMap<>();
 
 	public Changes(int money) {
+		for (Coin coin : Coin.values()) {
+			int coinNum = coin.randomPick(money);
+			changes.put(coin, coinNum);
+			money = coin.getRemainMoney(money, coinNum);
+		}
 	}
 }
--- src/main/java/vendingmachine/domain/Coin.java
+++ src/main/java/vendingmachine/domain/Coin.java
@@ -1,18 +1,47 @@
 package vendingmachine.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public enum Coin {
-    COIN_500(500),
-    COIN_100(100),
-    COIN_50(50),
-    COIN_10(10);
+	COIN_500(500),
+	COIN_100(100),
+	COIN_50(50),
+	COIN_10(10);
 
-    private final int amount;
+	private final int amount;
 
-    Coin(final int amount) {
-        this.amount = amount;
-    }
+	Coin(final int amount) {
+		this.amount = amount;
+	}
 
-    public boolean isDivided(int money){
-        return money % amount == 0;
-    }
+	public boolean isDivided(int money) {
+		return money % amount == 0;
+	}
+
+	public int randomPick(int money) {
+		if (isTenCoin()) {
+			return money / COIN_10.amount;
+		}
+		List<Integer> pickList = fillPickList(money);
+		return Randoms.pickNumberInList(pickList);
+	}
+
+	public int getRemainMoney(int money, int coinNum) {
+		return money - (coinNum * amount);
+	}
+
+	private boolean isTenCoin() {
+		return amount == COIN_10.amount;
+	}
+
+	private List<Integer> fillPickList(int money) {
+		List<Integer> pickList = new ArrayList<>();
+		for (int i = 0; i <= money / amount; i++) {
+			pickList.add(i);
+		}
+		return pickList;
+	}
 }
