d8813c93964e06a5a18cfda4efd9fdc99c5808db
koo <gudonghee2000@gmail.com>
2021-12-12 17:14:45 +0900

    refactor : RandomCoinPriceGenerator클래스의 매직넘버 상수화

--- src/main/java/utils/generator/RandomCoinPriceGenerator.java
+++ src/main/java/utils/generator/RandomCoinPriceGenerator.java
@@ -5,11 +5,15 @@ import java.util.Arrays;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomCoinPriceGenerator {
+	private static final int COIN_500 = 500;
+	private static final int COIN_100 = 100;
+	private static final int COIN_50 = 50;
+	private static final int COIN_10 = 10;
 
 	private RandomCoinPriceGenerator() {
 	}
 
 	public static int pickRandomCoinPrice() {
-		return Randoms.pickNumberInList(Arrays.asList(10, 50, 100, 500));
+		return Randoms.pickNumberInList(Arrays.asList(COIN_10, COIN_50, COIN_100, COIN_500));
 	}
 }
