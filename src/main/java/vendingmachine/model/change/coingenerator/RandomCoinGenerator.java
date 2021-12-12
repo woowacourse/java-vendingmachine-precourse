f60a84ecbfad8a02e0a9365f67821116b2d279d7
Byeongju-Kong <bjuuuu98@gmail.com>
2021-12-12 22:46:51 +0900

    refactor: 동전 랜덤 생성 로직 변경
    ApplicationTest에선 랜덤 값으로 동전의 값이 주입되기 때문에
    자판기 보유 금액 한도내에서, 특정 동전이 만들어 질 수 있는 개수를 랜덤으로 생성하던 로직에서
    단순히, 동전의 값 (500, 100, 50, 10)이 랜덤으로 생성되는 로직으로 변경

--- src/main/java/vendingmachine/model/change/coingenerator/RandomCoinGenerator.java
+++ src/main/java/vendingmachine/model/change/coingenerator/RandomCoinGenerator.java
@@ -6,53 +6,57 @@ import java.util.Map;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 import vendingmachine.model.change.vo.Coin;
 
 public class RandomCoinGenerator implements CoinGenerator {
     private static final int INITIAL_NUMBER = 0;
     private static final int OVER_MONEY_VALUE = 0;
-    private static final int ADDING_VALUE_FOR_INCLUSION_OF_POSSIBLE_NUMBER = 1;
+    private static final int INCREASING_UNIT = 1;
+    private static final List<Integer> COIN_AMOUNTS;
+
+    static {
+        COIN_AMOUNTS = Arrays.stream(Coin.values())
+                .map(Coin::getAmount)
+                .collect(Collectors.toList());
+    }
 
     private final Map<Coin, Integer> numbersByCoin = new LinkedHashMap<>();
     private int remainingMoneyToGenerateCoin;
 
     public RandomCoinGenerator(final int limitOfMoney) {
         this.remainingMoneyToGenerateCoin = limitOfMoney;
+        initialize();
     }
 
     public Map<Coin, Integer> generate() {
-        List<Coin> coins = Arrays.asList(Coin.values());
-        initialize(coins);
         while (isNotOver()) {
-            coins.forEach(this::generateCoinOf);
+            generateRandomCoin();
         }
         return numbersByCoin;
     }
 
-    private void initialize(final List<Coin> coins) {
-        coins.forEach(coin -> numbersByCoin.put(coin, INITIAL_NUMBER));
+    private void initialize() {
+        Arrays.stream(Coin.values()).forEach(coin -> numbersByCoin.put(coin, INITIAL_NUMBER));
     }
 
     private boolean isNotOver() {
         return remainingMoneyToGenerateCoin != OVER_MONEY_VALUE;
     }
 
-    private void generateCoinOf(final Coin coin) {
-        int possibleNumberToGenerate = coin.getNumberOfPossibleGeneration(remainingMoneyToGenerateCoin);
-        int generatedCoinNumber = generateRandomNumberOfCoin(possibleNumberToGenerate);
-        int presentNumber = numbersByCoin.get(coin);
-        numbersByCoin.put(coin, generatedCoinNumber + presentNumber);
-        subtractGeneratedMoney(coin.getAmount() * generatedCoinNumber);
+    private void generateRandomCoin() {
+        int generatedCoinAmount = pickNumberInList(COIN_AMOUNTS);
+        if (canGenerateCoinOf(generatedCoinAmount)) {
+            Coin generatedCoin = Coin.of(generatedCoinAmount);
+            int presentCoinNumber = numbersByCoin.get(generatedCoin);
+            numbersByCoin.put(generatedCoin, presentCoinNumber + INCREASING_UNIT);
+            subtractGeneratedMoney(generatedCoinAmount);
+        }
     }
 
-    private int generateRandomNumberOfCoin(final int possibleNumber) {
-        List<Integer> possibleNumbers = new ArrayList<>();
-        IntStream.range(0, possibleNumber + ADDING_VALUE_FOR_INCLUSION_OF_POSSIBLE_NUMBER)
-                .forEach(possibleNumbers::add);
-        return pickNumberInList(possibleNumbers);
+    private boolean canGenerateCoinOf(int generatedCoinAmount) {
+        return generatedCoinAmount <= remainingMoneyToGenerateCoin;
     }
 
     private void subtractGeneratedMoney(int generatedMoneyValue) {
