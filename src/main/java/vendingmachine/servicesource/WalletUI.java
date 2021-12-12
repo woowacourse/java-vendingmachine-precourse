9d96626cc35f5ffaf140bbdb9e8f73cfc3445ff6
overnew <ehdls1303@gmail.com>
2021-12-12 21:28:47 +0900

    feat: printRemainInsertedBalance() 구현
    남은 투입 금액을 출력

--- src/main/java/vendingmachine/servicesource/WalletUI.java
+++ src/main/java/vendingmachine/servicesource/WalletUI.java
@@ -5,13 +5,14 @@ import vendingmachine.constants.StringConstants;
 public class WalletUI {
     private final WalletSystem wallet;
     private final String PRINT_TITLE_MESSAGE = "\n자판기가 보유한 동전";
+    private final String PRINT_INSERTED_BALANCE_MESSAGE = "투입 금액: ";
     private final String PRINT_DELIMITER = " - ";
 
     WalletUI(WalletSystem wallet){
         this.wallet = wallet;
     }
 
-    void printRemainCoins(){
+    void printAllCoins(){
         Coin[] coinTypes =  Coin.values();
         System.out.println(PRINT_TITLE_MESSAGE);
 
@@ -21,5 +22,9 @@ public class WalletUI {
         }
 
         System.out.print('\n');
+    }
+
+    void printRemainInsertedBalance(){
+        System.out.println(PRINT_INSERTED_BALANCE_MESSAGE + wallet.getInsertedBalance() + StringConstants.CURRENCY_UNIT);
     }
 }
