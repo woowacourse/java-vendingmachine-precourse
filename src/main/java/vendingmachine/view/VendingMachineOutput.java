0eac587ee8622d4a19fc06c8465d71568168cb21
zmfl1230 <zmfl123097@gmail.com>
2021-12-12 23:51:15 +0900

    fix[VendingMachineOutput-View]: fix print sentences, add input money postfix

--- src/main/java/vendingmachine/view/VendingMachineOutput.java
+++ src/main/java/vendingmachine/view/VendingMachineOutput.java
@@ -9,6 +9,7 @@ public class VendingMachineOutput {
     private static final String COINS_INSTRUCTION = "자판기가 보유한 동전";
     private static final String COINS_POSTFIX = "개";
     private static final String INPUT_MONEY_INSTRUCTION = "투입 금액: ";
+    private static final String INPUT_MONEY_POSTFIX = "원";
     private static final String CHANGES_INSTRUCTION = "잔돈";
 
     public static void printCoinsVendingMachineHas(Map<Coin, Integer> coinMap) {
@@ -20,7 +21,7 @@ public class VendingMachineOutput {
     }
 
     public static void printRestMoney(int restMoney) {
-        System.out.println(INPUT_MONEY_INSTRUCTION + restMoney);
+        System.out.println(INPUT_MONEY_INSTRUCTION + restMoney + INPUT_MONEY_POSTFIX);
     }
 
     public static void printChange(Map<Coin, Integer> changes) {
