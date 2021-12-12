5e7fe33545ee4af7000c4d1a36041f85fa3df685
jochungbeom <cndqjacndqja@gmail.com>
2021-12-12 16:43:07 +0900

    refactor: OutputView 하드코딩 제거

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -1,8 +1,11 @@
 package vendingmachine.view;
 
 public class OutputView {
+	public static final String PRINT_SCREEN_HOLDING_AMOUNT_COIN = "자판기가 보유한 동전";
+	public static final String PRINT_SCREEN_CHANGE_AMOUNT = "잔돈";
+
 	public static void printHoldingAmountCoin(String holdingAmountCoinToString) {
-		System.out.println("자판기가 보유한 동전");
+		System.out.println(PRINT_SCREEN_HOLDING_AMOUNT_COIN);
 		System.out.println(holdingAmountCoinToString);
 	}
 
@@ -11,7 +14,7 @@ public class OutputView {
 	}
 
 	public static void printChangeAmount(String changeAmountToString) {
-		System.out.println("잔돈");
+		System.out.println(PRINT_SCREEN_CHANGE_AMOUNT);
 		System.out.println(changeAmountToString);
 	}
 }
