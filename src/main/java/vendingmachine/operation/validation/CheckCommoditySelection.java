98ff94c65d56e2b5e0d60c86884839a221cf632a
steven <seokju73@gmail.com>
2021-12-13 04:33:10 +0900

    Fix: Change boolean sign in 'validationNorRegistered'

--- src/main/java/vendingmachine/operation/validation/CheckCommoditySelection.java
+++ src/main/java/vendingmachine/operation/validation/CheckCommoditySelection.java
@@ -3,14 +3,15 @@ package vendingmachine.operation.validation;
 import java.util.List;
 
 import vendingmachine.management.Commodity;
+import vendingmachine.management.CommodityRepository;
 
 public class CheckCommoditySelection {
     private static final String IS_NOT_REGISTERED = "[ERROR] 해당 상품이 존재하지 않습니다.";
     private static final String IS_SOLD_OUT = "[ERROR] 해당 상품이 품절 되었습니다.";
     private static final String IS_OUT_OF_BALANCE = "[ERROR] 남은 잔액으로 구매할 수 없는 상품입니다.";
     
-    public static void validationNotRegistered(List<Commodity> inventory, String name) {
-        if(!inventory.stream().anyMatch(c -> c.getName().equals(name))) {
+    public static void validationNotRegistered(String name) {
+        if(!CommodityRepository.getList().stream().anyMatch(c -> c.getName().equals(name))) {
             throw new IllegalArgumentException(IS_NOT_REGISTERED);
         }
     }
