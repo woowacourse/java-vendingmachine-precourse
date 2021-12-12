b4a080a578bfd39da41d84793c2129460f53ea10
zmfl1230 <zmfl123097@gmail.com>
2021-12-12 22:40:38 +0900

    feat[ExceptionMessage]: add messages about product input format

--- src/main/java/vendingmachine/utils/ExceptionMessage.java
+++ src/main/java/vendingmachine/utils/ExceptionMessage.java
@@ -4,7 +4,8 @@ public class ExceptionMessage {
     public static final String INVALID_NUMBER = "[ERROR] 숫자를 입력하세요.";
     public static final String INVALID_PRODUCT_PRICE = "[ERROR] 올바른 상품 가격이 아닙니다.";
     public static final String DUPLICATED_PRODUCT = "[ERROR] 이미 존재하는 상품입니다.";
-    public static final String NOT_CORRECT_PRODUCT_FORMAT = "[ERROR] 올바른 상품 포맷이 아닙니다.";
+    public static final String NOT_CORRECT_PRODUCT_FORMAT_SIZE = "[ERROR] 올바른 상품 포맷이 아닙니다.[상품 내용 불충분]";
+    public static final String NOT_CORRECT_PRODUCT_FORMAT_IS_NULL = "[ERROR] 올바른 상품 포맷이 아닙니다.[빈 값]";
     public static final String PRODUCT_LIST_IS_NULL = "[ERROR] 빈 값입니다.";
 
     public static final String NOT_AVAILABLE = "[ERROR] 이용가능한 상품이 아닙니다.";
