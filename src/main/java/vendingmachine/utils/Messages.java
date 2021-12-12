b41133003ce482301d6627393d2e8a8d7d74923f
bcc0830 <bcc0830@naver.com>
2021-12-12 17:07:10 +0900

    refactor(Messages) : OutputView에 필요한 메세지 상수 추가

--- src/main/java/vendingmachine/utils/Messages.java
+++ src/main/java/vendingmachine/utils/Messages.java
@@ -11,7 +11,17 @@ public enum Messages {
 	ERROR_NOT_ENOUGH_MONEY_MESSAGE("[ERROR] 금액이 부족합니다."),
 	ERROR_SOLD_OUT_MESSAGE("[ERROR] 해당 상품은 품절입니다."),
 	ERROR_NOT_DIVIDABLE_MESSAGE("[ERROR] 금액의 최소 단위는 10원 입니다."),
-	COMMON_LINE_BREAK_MSG("\n");
+	COMMON_LINE_BREAK_MSG("\n"),
+	OUTPUT_VIEW_NOTICE_COIN_INPUT_MSG("자판기가 보유하고 있는 금액을 입력해 주세요."),
+	OUTPUT_VIEW_NOTICE_ITEM_INPUT_MSG("상품명과 가격, 수량을 입력해 주세요."),
+	OUTPUT_VIEW_NOTICE_PAY_INPUT_MSG("투입 금액을 입력해 주세요."),
+	OUTPUT_VIEW_NOTICE_PAY_ITEM_INPUT_MSG("구매할 상품명을 입력해 주세요."),
+	OUTPUT_VIEW_NOW_MONEY_MSG("투입 금액: "),
+	OUTPUT_VIEW_WON_MSG("원"),
+	OUTPUT_VIEW_COIN_COUNT_MSG("개"),
+	OUTPUT_VIEW_DASH_MSG(" - "),
+	OUTPUT_VIEW_RETURN_MONEY_MSG("잔돈"),
+	OUTPUT_VIEW_INITIAL_COIN_MSG("자판기가 보유한 동전");
 
 	private final String value;
 
---
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -0,0 +1,10 @@
+package vendingmachine.view;
+
+import vendingmachine.utils.Messages;
+
+public class OutputView {
+	public static void noticeCoinInput() {
+		System.out.print(
+			Messages.OUTPUT_VIEW_NOTICE_COIN_INPUT_MSG.getValue() + Messages.COMMON_LINE_BREAK_MSG.getValue());
+	}
+}
