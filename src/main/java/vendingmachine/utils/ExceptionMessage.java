c04d8988173236c313d4c4a6c2bbd85ae954f52c
zmfl1230 <zmfl123097@gmail.com>
2021-12-12 22:57:51 +0900

    feat[ExceptionMessage]: remove unnecessary final value

--- src/main/java/vendingmachine/utils/ExceptionMessage.java
+++ src/main/java/vendingmachine/utils/ExceptionMessage.java
@@ -6,7 +6,6 @@ public class ExceptionMessage {
     public static final String DUPLICATED_PRODUCT = "[ERROR] 이미 존재하는 상품입니다.";
     public static final String NOT_CORRECT_PRODUCT_FORMAT_SIZE = "[ERROR] 올바른 상품 포맷이 아닙니다.[상품 내용 불충분]";
     public static final String NOT_CORRECT_PRODUCT_FORMAT_IS_NULL = "[ERROR] 올바른 상품 포맷이 아닙니다.[빈 값]";
-    public static final String PRODUCT_LIST_IS_NULL = "[ERROR] 빈 값입니다.";
 
     public static final String NOT_AVAILABLE = "[ERROR] 이용가능한 상품이 아닙니다.";
     public static final String OUT_OF_STOCK = "[ERROR] 이용가능한 재고가 없습니다.";
