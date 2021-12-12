150f7101ca2390a2339b1d19b0ccc5477cbe3956
bcc0830 <bcc0830@naver.com>
2021-12-12 17:09:44 +0900

    feat(OutputView) : 자판기의 제품 목록 입력을 요구하는 기능 추가

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -3,8 +3,14 @@ package vendingmachine.view;
 import vendingmachine.utils.Messages;
 
 public class OutputView {
+
 	public static void noticeCoinInput() {
 		System.out.print(
 			Messages.OUTPUT_VIEW_NOTICE_COIN_INPUT_MSG.getValue() + Messages.COMMON_LINE_BREAK_MSG.getValue());
+	}
+
+	public static void noticeItemInput() {
+		System.out.print(
+			Messages.OUTPUT_VIEW_NOTICE_ITEM_INPUT_MSG.getValue() + Messages.COMMON_LINE_BREAK_MSG.getValue());
 	}
 }
