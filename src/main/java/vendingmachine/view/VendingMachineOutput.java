4112e20632c666e694db08ca0b2e61bfafe81925
zmfl1230 <zmfl123097@gmail.com>
2021-12-12 23:44:35 +0900

    fix[VendingMachineOutput-View]: fix print sentences

--- src/main/java/vendingmachine/view/VendingMachineOutput.java
+++ src/main/java/vendingmachine/view/VendingMachineOutput.java
@@ -7,13 +7,15 @@ import java.util.Map;
 public class VendingMachineOutput {
     private static final String CONNECTION_MARK = " - ";
     private static final String COINS_INSTRUCTION = "자판기가 보유한 동전";
+    private static final String COINS_POSTFIX = "개";
     private static final String INPUT_MONEY_INSTRUCTION = "투입 금액: ";
     private static final String CHANGES_INSTRUCTION = "잔돈";
 
     public static void printCoinsVendingMachineHas(Map<Coin, Integer> coinMap) {
         System.out.println(COINS_INSTRUCTION);
         for(Coin coin : coinMap.keySet()) {
-            System.out.println(coin + CONNECTION_MARK + coinMap.get(coin));
+            // TODO: coinMap 일급 컬렉션 생성 후, toString 메서드에 재정의
+            System.out.println(coin + CONNECTION_MARK + coinMap.get(coin) + COINS_POSTFIX);
         }
     }
 
@@ -24,7 +26,8 @@ public class VendingMachineOutput {
     public static void printChange(Map<Coin, Integer> changes) {
         System.out.println(CHANGES_INSTRUCTION);
         for(Coin coin: changes.keySet()) {
-            System.out.println(coin + CONNECTION_MARK + changes.get(coin));
+            // TODO: coinMap 일급 컬렉션 생성 후, toString 메서드에 재정의
+            System.out.println(coin + CONNECTION_MARK + changes.get(coin)+ COINS_POSTFIX);
         }
 
     }
