2d64258d973b21369d86a8ffd930ab0290df0c35
iamsseaa <homingsea@gmail.com>
2021-12-12 09:35:12 +0900

    feat: 상품 리스트 생성자

--- src/main/java/vendingmachine/goods/GoodsController.java
+++ src/main/java/vendingmachine/goods/GoodsController.java
@@ -1,5 +1,13 @@
 package vendingmachine.goods;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GoodsController {
-    
+    List<Goods> goodsList = new ArrayList<>();
+
+    GoodsController(List<Goods> goodsList) {
+        this.goodsList = goodsList;
+    }
+
 }
