4e8a6042abd44fc94c47eb93732ef52b49dd1531
OzRagwort <ozragwort@gmail.com>
2021-12-13 03:51:25 +0900

    refactor(InputView): 의미가 잘못된 상수명 수정
    - RESPONSE_NOW_MONEY_MESSAGE -> REQUEST_SELL_PRODUCT_MESSAGE

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -14,7 +14,7 @@ public class InputView {
 	private static final String REQUEST_HOLDING_AMOUNT_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
 	private static final String REQUEST_REGISTER_PRODUCT_MESSAGE = "상품명과 가격, 수량을 입력해 주세요.";
 	private static final String REQUEST_INSERT_MONEY_MESSAGE = "투입 금액을 입력해 주세요.";
-	private static final String RESPONSE_NOW_MONEY_MESSAGE = "구매할 상품명을 입력해 주세요.";
+	private static final String REQUEST_SELL_PRODUCT_MESSAGE = "구매할 상품명을 입력해 주세요.";
 
 	public static RequestHoldingMoneyDto inputHoldingMoney() {
 		while (true) {
@@ -56,7 +56,7 @@ public class InputView {
 	}
 
 	public static RequestSellProductDto inputSellProduct() {
-		System.out.println(RESPONSE_NOW_MONEY_MESSAGE);
+		System.out.println(REQUEST_SELL_PRODUCT_MESSAGE);
 		Name name = new Name(readLine());
 		System.out.println();
 		return new RequestSellProductDto(name);
