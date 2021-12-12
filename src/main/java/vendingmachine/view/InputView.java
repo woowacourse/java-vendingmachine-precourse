59db8794fba39230faef06a628098c7cf6feb207
delphox60 <delphox60@gmail.com>
2021-12-12 12:52:43 +0900

    feat(view): 판매할 상품의 이름, 가격, 수량을 각각 입력받는 기능 구현

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -3,12 +3,20 @@ package vendingmachine.view;
 public class InputView {
 
     private static final String NEW_LINE = "\n";
-    private static final String HOLDING_MONEY_INPUT_INIT_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    private static final String HOLDING_MONEY_INPUT_REQUEST_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    private static final String PRODUCT_INFO_INPUT_REQUEST_MESSAGE = "상품명과 가격, 수량을 입력해 주세요.";
 
     public static String inputHoldingMoney() {
-        System.out.println(HOLDING_MONEY_INPUT_INIT_MESSAGE);
+        System.out.println(HOLDING_MONEY_INPUT_REQUEST_MESSAGE);
         String inputHoldingMoney = camp.nextstep.edu.missionutils.Console.readLine();
         System.out.print(NEW_LINE);
         return inputHoldingMoney;
+    }
+
+    public static String inputProductInfo() {
+        System.out.println(PRODUCT_INFO_INPUT_REQUEST_MESSAGE);
+        String productInfo = camp.nextstep.edu.missionutils.Console.readLine();
+        System.out.print(NEW_LINE);
+        return productInfo;
     }
 }
