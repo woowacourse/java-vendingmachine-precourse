90dc026743edfa9e1bb954f033cf1dfb7fe20df7
overnew <ehdls1303@gmail.com>
2021-12-12 15:16:18 +0900

    feat: Override hashCode(), equals()
    Product class  hashCode(), equals() override
    -Product 클래스를 HashMap의 eky로 이요할 수 있도록 override

--- src/main/java/vendingmachine/servicesource/Product.java
+++ src/main/java/vendingmachine/servicesource/Product.java
@@ -1,5 +1,7 @@
 package vendingmachine.servicesource;
 
+import java.util.Objects;
+
 public class Product {
     private String name;
     private int price;
@@ -15,6 +17,21 @@ public class Product {
 
     public int  getPrice(){
         return price;
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(name);
+    }
+
+    @Override
+    public boolean equals(Object object){
+
+        if(object instanceof Product){
+            return this.name == ((Product)object).name;
+        }
+
+        return false;
     }
 
 }
