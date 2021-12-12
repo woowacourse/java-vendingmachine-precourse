1f387beb7fe20968631187668bb32a0e883de248
jaeseo yoo <wotj102@gmail.com>
2021-12-12 14:13:24 +0900

    refactor(Coin): 불필요한 getter 메서드 삭제

--- src/main/java/vendingmachine/domain/Coin.java
+++ src/main/java/vendingmachine/domain/Coin.java
@@ -14,15 +14,11 @@ public enum Coin {
         this.amount = amount;
     }
 
-    public int getAmount() {
-        return amount;
-    }
-
     public static ArrayList<Integer> getCoins() {
         ArrayList<Integer> coins = new ArrayList<>();
         Coin[] coin = Coin.values();
         for (Coin value : coin) {
-            coins.add(value.getAmount());
+            coins.add(value.amount);
         }
         return coins;
     }
