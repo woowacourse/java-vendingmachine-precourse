dcaf40be144905992469cb388a1d72b7ce3a246b
Jeong Satchel <bugod96@gmail.com>
2021-12-12 21:15:06 +0900

    feat(Validator): throw Exception if total money input includes one wons

--- src/main/java/vendingmachine/validator/Validator.java
+++ src/main/java/vendingmachine/validator/Validator.java
@@ -4,10 +4,12 @@ public class Validator {
     private static final String ERROR = "[ERROR] ";
     private static final String NOT_NUMBER_EXCEPTION = ERROR + "숫자를 입력하셔야 합니다.";
     private static final String NEGATIVE_NUMBER_EXCEPTION = ERROR + "음수를 입력하시면 안 됩니다.";
+    private static final String HAS_ONES_NUMBER_EXCEPTION = ERROR + "1원 단위는 입력하시면 안 됩니다.";
 
     public static void validateTotalMoneyInput(String input) {
         int intInput = validateTotalMoneyInputType(input);
         validateTotalMoneyNotNegative(intInput);
+        validateTotalMoneyNotHaveOnes(intInput);
     }
 
     private static int validateTotalMoneyInputType(String input) {
@@ -21,6 +23,12 @@ public class Validator {
     private static void validateTotalMoneyNotNegative(int intInput) {
         if (intInput < 0) {
             throw new IllegalArgumentException(NEGATIVE_NUMBER_EXCEPTION);
+        }
+    }
+
+    private static void validateTotalMoneyNotHaveOnes(int intInput) {
+        if (intInput%10 > 0) {
+            throw new IllegalArgumentException(HAS_ONES_NUMBER_EXCEPTION);
         }
     }
 }
