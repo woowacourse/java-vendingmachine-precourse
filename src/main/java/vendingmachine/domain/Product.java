74bfd8048b3c11987e8ad9b1118e27b4783692c2
Donggyu Kim <a29661498@gmail.com>
2021-12-12 20:19:57 +0900

    feat: `Product` 엔티티가 상품 입력에 따라서 상품 리스트를 생성하는 기능 구현
    private로 작성된 두 메소드 splitProductInfo와 createProduct를 이용하여 상품 리스트를 만들어 반환해주는 static 메소드를 구현하였습니다.
    * splitProductInfo: 입력 문자열에 대해서 ";" 로 분리된 각각의 문자열에 대해서 3개의 정보(이름, 가격, 수량)을 분리해서 반환해줍니다.
    * createProduct: `splitProductInfo` 를 통해서 구분한 (이름, 가격, 수량)을 토대로 상품 하나를 만들어 반환해줍니다.

--- src/main/java/vendingmachine/domain/Product.java
+++ src/main/java/vendingmachine/domain/Product.java
@@ -1,5 +1,8 @@
 package vendingmachine.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Product {
 
     private String productName;
@@ -26,5 +29,42 @@ public class Product {
 
     public void reduceQuantity() {
         quantity = quantity - 1;
+    }
+
+    public static List<Product> createProductList(String input) {
+        List<Product> productList = new ArrayList<>();
+
+        String[] splitString = input.split(";");
+
+        for (String s : splitString) {
+
+            String[] resultStrings = splitProductInfo(s);
+
+            Product product = createProduct(resultStrings);
+
+            productList.add(product);
+        }
+
+        return productList;
+    }
+
+    private static String[] splitProductInfo(String splitString) {
+        String resultString = splitString.substring(0, splitString.length() -1);
+
+        String[] resultStrings = resultString.split(",");
+
+        return resultStrings;
+    }
+
+    private static Product createProduct(String[] strings) {
+        String productName = strings[0];
+
+        int price = Integer.parseInt(strings[1]);
+
+        int quantity = Integer.parseInt(strings[2]);
+
+        Product product = new Product(productName, price, quantity);
+
+        return product;
     }
 }
