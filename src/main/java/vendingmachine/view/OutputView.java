905dffc477ba862864558d7c4d19b7dc9302e5b8
Donggyu Kim <a29661498@gmail.com>
2021-12-12 20:13:57 +0900

    feat: 자판기가 반환할 잔돈을 출력하는 view 작성
    자판기가 남은 투입 금액을 출력하고 반환하는 잔돈을 출력하는 view 작성하였습니다.

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -2,6 +2,7 @@ package vendingmachine.view;
 
 import vendingmachine.domain.Change;
 import vendingmachine.domain.Coin;
+import vendingmachine.domain.VendingMachine;
 
 import java.util.Map;
 
@@ -14,6 +15,22 @@ public class OutputView {
 
         for (Coin coin : coinMap.keySet()) {
             System.out.println(coin.getAmount()+"원 - " + coinMap.get(coin)+"개");
+        }
+    }
+
+    public static void printVendingMachineChangeResult(VendingMachine vendingMachine) {
+        System.out.println("\n투입 금액: " + vendingMachine.getRestMoney());
+
+        Change change = vendingMachine.returnChange();
+
+        Map<Coin, Integer> coinMap = change.getCoinMap();
+
+        System.out.println("잔돈");
+
+        for (Coin coin : coinMap.keySet()) {
+            if (coinMap.get(coin) > 0) {
+                System.out.println(coin.getAmount()+"원 - " + coinMap.get(coin)+"개");
+            }
         }
     }
 }
