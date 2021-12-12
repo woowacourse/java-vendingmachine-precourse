c9d5bcdcafcc01f06e2baf5822bc75de9f41da56
OzRagwort <ozragwort@gmail.com>
2021-12-13 01:34:49 +0900

    feat: 기능이 동작하도록 통합

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,7 +1,17 @@
 package vendingmachine;
 
+import vendingmachine.controller.VendingMachine;
+
 public class Application {
 	public static void main(String[] args) {
-		// TODO: 프로그램 구현
+		start();
+	}
+
+	private static void start() {
+		VendingMachine vendingMachine = new VendingMachine();
+		vendingMachine.initHoldingMoney();
+		vendingMachine.showAllCoinQuantity();
+		vendingMachine.registerProducts();
+		vendingMachine.sellProduct();
 	}
 }
