0a055f445a36e11d5e144052bb85c5b235f4882c
Jeong Satchel <bugod96@gmail.com>
2021-12-12 21:11:39 +0900

    feat(Validator): throw Exception if total money input is a negative number

--- src/main/java/vendingmachine/validator/Validator.java
+++ src/main/java/vendingmachine/validator/Validator.java
@@ -1,14 +1,26 @@
 package vendingmachine.validator;
 
 public class Validator {
-    private static String ERROR = "[ERROR] ";
-    private static String NOT_NUMBER_EXCEPTION = ERROR + "숫자를 입력하셔야 합니다.";
+    private static final String ERROR = "[ERROR] ";
+    private static final String NOT_NUMBER_EXCEPTION = ERROR + "숫자를 입력하셔야 합니다.";
+    private static final String NEGATIVE_NUMBER_EXCEPTION = ERROR + "음수를 입력하시면 안 됩니다.";
 
     public static void validateTotalMoneyInput(String input) {
+        int intInput = validateTotalMoneyInputType(input);
+        validateTotalMoneyNotNegative(intInput);
+    }
+
+    private static int validateTotalMoneyInputType(String input) {
         try {
-            Integer.parseInt(input);
+            return Integer.parseInt(input);
         } catch (Exception e) {
             throw new IllegalArgumentException(NOT_NUMBER_EXCEPTION);
+        }
+    }
+
+    private static void validateTotalMoneyNotNegative(int intInput) {
+        if (intInput < 0) {
+            throw new IllegalArgumentException(NEGATIVE_NUMBER_EXCEPTION);
         }
     }
 }
