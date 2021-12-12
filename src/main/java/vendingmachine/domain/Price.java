dbebe5a903e8c042f5bec8e66ea8086a705c51f8
Wishoon <heonga2@gmail.com>
2021-12-13 02:06:17 +0900

    feat: 입력받은 상품 가격에 대한 예외 처리 추가
    Price
    - isValidateNumber : 상품 가격의 숫자 형태 유무 판단
    - isValidatePricePattern : 상품 가격의 앞자리가 0으로 시작하는지 판단
    - isValidatePrice10PercentDivision : 상품 가격이 10의 단위로 나누어지는지 판단

--- src/main/java/vendingmachine/domain/Price.java
+++ src/main/java/vendingmachine/domain/Price.java
@@ -1,6 +1,13 @@
 package vendingmachine.domain;
 
 public class Price {
+	private static final int PRICE_ZERO = 0;
+	private static final int PRICE_DIVISION_STANDARD = 10;
+	private static final String PRICE_INPUT_FIRST_ELEMENT = "0";
+	private static final String ERROR_PRICE_NOT_NUMBER = "상품 금액이 숫자가 아닙니다.";
+	private static final String ERROR_PRICE_NOT_PATTERN = "상품 금액 앞자리에는 0을 입력할 수 없습니다.";
+	private static final String ERROR_PRICE_NOT_TEN_PERCENT_DIVISION = "상품 금액은 10의 단위로 나누어 떨어져야 합니다.";
+
 	public final int price;
 
 	public Price(int price) {
@@ -8,6 +15,29 @@ public class Price {
 	}
 
 	public static Price from(String price) {
+		isValidateNumber(price);
+		isValidatePricePattern(price);
+		isValidatePrice10PercentDivision(price);
 		return new Price(Integer.parseInt(price));
+	}
+
+	private static void isValidateNumber(String price) {
+		try {
+			Integer.parseInt(price);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(ERROR_PRICE_NOT_NUMBER);
+		}
+	}
+
+	private static void isValidatePricePattern(String price) {
+		if (price.startsWith(PRICE_INPUT_FIRST_ELEMENT)) {
+			throw new IllegalArgumentException(ERROR_PRICE_NOT_PATTERN);
+		}
+	}
+
+	private static void isValidatePrice10PercentDivision(String price) {
+		if (Integer.parseInt(price) % PRICE_DIVISION_STANDARD != PRICE_ZERO) {
+			throw new IllegalArgumentException(ERROR_PRICE_NOT_TEN_PERCENT_DIVISION);
+		}
 	}
 }
