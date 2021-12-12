7db8a1a6022e3d61d5e4ac5b0099860a02dd6213
SuyeonChoi <tndus970120@gmail.com>
2021-12-13 00:16:25 +0900

    feat(MoneyValidator, ErrorMessageConstants): 입력된 금액 0 미만인 경우 예외 처리

--- src/main/java/vendingmachine/util/validator/MoneyValidator.java
+++ src/main/java/vendingmachine/util/validator/MoneyValidator.java
@@ -3,8 +3,14 @@ package vendingmachine.util.validator;
 import vendingmachine.util.ErrorMessageConstants;
 
 public class MoneyValidator {
+	private static final int POSITIVE_NUMBER_BOUNDARY = 0;
+
+	private MoneyValidator() {
+	}
+
 	public static void validate(String money) {
 		validateInteger(money);
+		validateRange(Integer.parseInt(money));
 	}
 
 	private static void validateInteger(String money) {
@@ -12,6 +18,12 @@ public class MoneyValidator {
 			Integer.parseInt(money);
 		} catch (NumberFormatException e) {
 			throw new IllegalArgumentException(ErrorMessageConstants.INTEGER_ERROR);
+		}
+	}
+
+	private static void validateRange(int money) {
+		if (money < POSITIVE_NUMBER_BOUNDARY) {
+			throw new IllegalArgumentException(ErrorMessageConstants.POSITIVE_NUMBER_EXCEPTION_MESSAGE);
 		}
 	}
 }
