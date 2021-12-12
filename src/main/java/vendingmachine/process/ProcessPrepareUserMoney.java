913c15e1f526d69b5cacc5b8d339a960d4a2de9b
iamsseaa <homingsea@gmail.com>
2021-12-12 23:36:45 +0900

    feat: 투입 금액 예외 처리

--- src/main/java/vendingmachine/process/ProcessPrepareUserMoney.java
+++ src/main/java/vendingmachine/process/ProcessPrepareUserMoney.java
@@ -1,11 +1,16 @@
 package vendingmachine.process;
 
 import vendingmachine.user.User;
+import vendingmachine.user.UserMoneyValidation;
 
 public class ProcessPrepareUserMoney {
     public static String money;
 
     public static void inputUserMoney() {
         money = User.inputMoney();
+    }
+
+    public static void checkUserMoney() {
+        UserMoneyValidation.checkUserMoneyValidation(money);
     }
 }
