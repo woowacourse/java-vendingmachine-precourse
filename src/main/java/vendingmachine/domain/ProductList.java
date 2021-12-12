2d519e5e0f659c73e5bb40937012c8f6c8d9a673
zmfl1230 <zmfl123097@gmail.com>
2021-12-12 21:37:58 +0900

    feat[ProductList-Domain]: define first class collection

---
+++ src/main/java/vendingmachine/domain/ProductList.java
@@ -0,0 +1,13 @@
+package vendingmachine.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductList {
+    List<Product> productList;
+
+    public ProductList() {
+        productList = new ArrayList<>();
+    }
+
+}
