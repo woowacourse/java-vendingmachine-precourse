d02d1514ce915b8a30b1de6bdbbb136d4035ceb0
JunHyeok.Bang <bangvkf3@gmail.com>
2021-12-12 18:24:05 +0900

    feat: 단위(원, 개) formatter

--- src/main/java/vendingmachine/utils/Formatter.java
+++ src/main/java/vendingmachine/utils/Formatter.java
@@ -1,11 +1,22 @@
 package vendingmachine.utils;
 
 public class Formatter {
+	private static final String KOREAN_CURRENCY_UNIT = "원";
+	private static final String KOREAN_ITEM_COUNT_UNIT = "원";
+
 	public static String formatToKoreanCurrencyUnit(String price) {
-		return price + "원";
+		return price + KOREAN_CURRENCY_UNIT;
 	}
 
 	public static String formatToKoreanCurrencyUnit(int price) {
-		return price + "원";
+		return price + KOREAN_CURRENCY_UNIT;
+	}
+
+	public static String formatToKoreanItemCountUnit(String count) {
+		return count + KOREAN_ITEM_COUNT_UNIT;
+	}
+
+	public static String formatToKoreanItemCountUnit(int count) {
+		return count + KOREAN_ITEM_COUNT_UNIT;
 	}
 }
