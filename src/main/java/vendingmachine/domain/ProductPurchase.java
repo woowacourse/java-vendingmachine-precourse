6cb79450655dda5d3b7951851f96b37f72fab0f2
hyeonic <dev.hyeonic@gmail.com>
2021-12-12 15:00:27 +0900

    feat: 상품 구매에 대한 책임을 가진 ProductPurchase 추가

---
+++ src/main/java/vendingmachine/domain/ProductPurchase.java
@@ -0,0 +1,31 @@
+package vendingmachine.domain;
+
+public class ProductPurchase {
+    private final String name;
+
+    public ProductPurchase(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        validateEmpty(name);
+        validateBlank(name);
+    }
+
+    private void validateEmpty(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateBlank(String name) {
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+}
