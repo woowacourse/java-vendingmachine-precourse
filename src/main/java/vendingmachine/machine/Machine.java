d0a4f5828e1a7afc3c43399fc5ab4915b7d19d65
iamsseaa <homingsea@gmail.com>
2021-12-12 16:05:45 +0900

    feat: 자판기가 보유하고 있는 잔돈 반환

--- src/main/java/vendingmachine/machine/Machine.java
+++ src/main/java/vendingmachine/machine/Machine.java
@@ -2,13 +2,13 @@ package vendingmachine.machine;
 
 import vendingmachine.goods.Goods;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Machine {
-    public List<Goods> goods = new ArrayList<>();
-    Coin[] coins = Coin.values();
-
+    public List<Goods> goods;
     public int holdingAmount;
 
     public Machine(int holdingAmount, List<Goods> goods) {
@@ -16,4 +16,7 @@ public class Machine {
         this.holdingAmount = holdingAmount;
     }
 
+    public int getHoldingAmount() {
+        return this.holdingAmount;
+    }
 }
