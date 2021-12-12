5b19e745717bed1f436c02846e6f6160581fe07c
iamsseaa <homingsea@gmail.com>
2021-12-12 09:06:15 +0900

    refactor: coin 클래스 패키지 이동

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,4 +1,4 @@
-package vendingmachine.machine;
+package vendingmachine;
 
 public enum Coin {
     COIN_500(500),
--- src/main/java/vendingmachine/goods/Goods.java
+++ src/main/java/vendingmachine/goods/Goods.java
@@ -11,5 +11,5 @@ public class Goods {
         this.quantity = quantity;
     }
 
-    
+
 }
