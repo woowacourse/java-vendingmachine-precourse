2c37724d7b706654f7462397a340c2bc79de5b70
zmfl1230 <zmfl123097@gmail.com>
2021-12-13 00:14:35 +0900

    refactor[VendingMachineController-Controller]: modularize functions

--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -9,6 +9,24 @@ public class VendingMachineController {
 
     public void run() {
         // 자판기가 보유할 금액 입력 받기
+        int amount = inputAmount();
+        VendingMachine vendingMachine = new VendingMachine(amount);
+        vendingMachine.printCoins();
+        // 자판기가 보유할 상품 입력 받기
+        inputProducts(vendingMachine);
+        // 투입 금액 입력 받기
+        int inputMoney = inputInputMoney();
+        vendingMachine.setInputMoney(inputMoney);
+        while(vendingMachine.checkPurchasePossible()) {
+            vendingMachine.printRestMoney();
+            String productName = VendingMachineInput.insertProductNameToBuy();
+            vendingMachine.reduceInputMoneyAndProductQuantityByName(productName);
+        }
+        vendingMachine.printRestMoney();
+        vendingMachine.printChanges();
+    }
+
+    private int inputAmount() {
         int amount = -1;
         do {
             try {
@@ -18,12 +36,10 @@ public class VendingMachineController {
             }
         } while (amount == -1);
 
-        VendingMachine vendingMachine = new VendingMachine(amount);
+        return amount;
+    }
 
-        // 자판기가 보유한 동전 출력
-        vendingMachine.printCoins();
-
-        // 자판기가 보유할 상품 입력 받기
+    private void inputProducts(VendingMachine vendingMachine) {
         boolean completed = false;
         String products = VendingMachineInput.insertProducts();
 
@@ -35,8 +51,9 @@ public class VendingMachineController {
                 System.out.println(e.getMessage());
             }
         } while (!completed);
+    }
 
-        // 투입 금액 입력 받기
+    private int inputInputMoney() {
         int inputMoney = -1;
         do {
             try {
@@ -46,16 +63,6 @@ public class VendingMachineController {
             }
         } while (inputMoney == -1);
 
-        vendingMachine.setInputMoney(inputMoney);
-
-        while(vendingMachine.checkPurchasePossible()) {
-            vendingMachine.printRestMoney();
-            String productName = VendingMachineInput.insertProductNameToBuy();
-            vendingMachine.reduceInputMoneyAndProductQuantityByName(productName);
-        }
-
-        vendingMachine.printRestMoney();
-        vendingMachine.printChanges();
-
+        return inputMoney;
     }
 }
