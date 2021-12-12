d8a22b9f47fb2912ecf39872bd88c16734f13a8d
zmfl1230 <zmfl123097@gmail.com>
2021-12-12 22:57:09 +0900

    feat[VendingMachineController-Controller]:add exception on input actions

--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -24,11 +24,28 @@ public class VendingMachineController {
         vendingMachine.printCoins();
 
         // 자판기가 보유할 상품 입력 받기
+        boolean completed = false;
         String products = VendingMachineInput.insertProducts();
-        vendingMachine.insertProducts(products);
+
+        do{
+            try {
+                vendingMachine.insertProducts(products);
+                completed = true;
+            }catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (!completed);
 
         // 투입 금액 입력 받기
-        int inputMoney = VendingMachineInput.insertInputMoney();
+        int inputMoney = -1;
+        do {
+            try {
+                inputMoney = VendingMachineInput.insertInputMoney();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (inputMoney == -1);
+
         vendingMachine.setInputMoney(inputMoney);
 
         while(vendingMachine.checkPurchasePossible()) {
