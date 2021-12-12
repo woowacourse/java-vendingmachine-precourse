45a83b8882f0208539ec1b0dd850b772440cdd21
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-12-12 23:12:01 +0900

    feat: VendingMachine Coins, Products 일급컬랙션 파라미터 추가

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -1,36 +1,18 @@
 package vendingmachine.domain;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
-import vendingmachine.strategy.CoinCreateStrategy;
 
 public class VendingMachine {
 
-    private final Map<Coin, Integer> coins;
+    private final Coins coins;
+    private final Products products;
 
-    public VendingMachine(Map<Coin, Integer> coins) {
+    public VendingMachine(Coins coins, Products products) {
         this.coins = coins;
-    }
-
-    public static VendingMachine createByMoney(Money money, CoinCreateStrategy strategy) {
-        Map<Coin, Integer> coins = Coin.createEmptyCoinMap();
-        while (!money.isEmpty()) {
-            Coin coin = cutOffByCoin(money, strategy);
-            coins.put(coin, coins.get(coin) + 1);
-        }
-        return new VendingMachine(coins);
-    }
-
-    private static Coin cutOffByCoin(Money money, CoinCreateStrategy strategy) {
-        try {
-            return money.cutOffByCoin(strategy.createCoin());
-        } catch (IllegalArgumentException e) {
-            return cutOffByCoin(money, strategy);
-        }
+        this.products = products;
     }
 
     public Map<Coin, Integer> currentRemainCoins() {
-        return Collections.unmodifiableMap(new HashMap<>(coins));
+        return coins.currentRemainCoins();
     }
 }
--- src/test/java/vendingmachine/domain/VendingMachineTest.java
+++ src/test/java/vendingmachine/domain/VendingMachineTest.java
@@ -1,29 +1,5 @@
 package vendingmachine.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.Map;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
 class VendingMachineTest {
 
-
-    @Test
-    @DisplayName("Money를 받아 그만큼의 coin을 가지고 있는 자판기를 생성할 수 있다.")
-    void createByMoneyTest() {
-        // given
-        Money money = Money.valueOf("100");
-        Coin coin = Coin.valueOfAmount(10);
-        VendingMachine vendingMachine = VendingMachine.createByMoney(money, () -> coin);
-
-        Map<Coin, Integer> expected = Coin.createEmptyCoinMap();
-        expected.put(coin, expected.get(coin) + 10);
-
-        // when
-        Map<Coin, Integer> result = vendingMachine.currentRemainCoins();
-
-        // then
-        assertThat(result).isEqualTo(expected);
-    }
 }
