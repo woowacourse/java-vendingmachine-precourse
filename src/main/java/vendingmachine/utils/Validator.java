4129cd86b1b67d9c55891b7acd1b148cfc2f3e38
bcc0830 <bcc0830@naver.com>
2021-12-12 16:40:03 +0900

    feat(Validator) : 입력이 특수문자 혹은 공백을 포함하는지 검사하는 기능 추가

--- src/main/java/vendingmachine/utils/Validator.java
+++ src/main/java/vendingmachine/utils/Validator.java
@@ -2,6 +2,8 @@ package vendingmachine.utils;
 
 public class Validator {
 	private static final String NUMBER_PATTERN = "^[0-9]+$";
+	private static final String NAME_PATTERN = "^[0-9|a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]+$";
+
 	private static void validatePositive(String input) throws IllegalArgumentException {
 		if (Integer.valueOf(input) <= 0) {
 			throw new IllegalArgumentException(
@@ -13,6 +15,13 @@ public class Validator {
 		if (!input.matches(NUMBER_PATTERN)) {
 			throw new IllegalArgumentException(
 				Messages.ERROR_NOT_NUMBER.getValue() + Messages.COMMON_LINE_BREAK_MSG.getValue());
+		}
+	}
+
+	private static void validateName(String input) throws IllegalArgumentException {
+		if (!input.matches(NAME_PATTERN)) {
+			throw new IllegalArgumentException(
+				Messages.ERROR_NOT_VALID_NAME.getValue() + Messages.COMMON_LINE_BREAK_MSG.getValue());
 		}
 	}
 }
