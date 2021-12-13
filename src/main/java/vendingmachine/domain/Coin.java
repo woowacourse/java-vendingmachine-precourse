cae66e7da9eeff1181ddae19beb4a8b88ab4c9d7
cjlee38 <gptpem38@gmail.com>
2021-12-14 01:10:35 +0900

    refactor: 사용하지 않는 import 제거

--- src/main/java/vendingmachine/domain/Coin.java
+++ src/main/java/vendingmachine/domain/Coin.java
@@ -3,48 +3,47 @@ package vendingmachine.domain;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
 public enum Coin {
-    COIN_500(500),
-    COIN_100(100),
-    COIN_50(50),
-    COIN_10(10);
+	COIN_500(500),
+	COIN_100(100),
+	COIN_50(50),
+	COIN_10(10);
 
-    private static final String CURRENCY = "원";
-    private static final String ERROR_NO_AMOUNT = "%d 원 동전이 없습니다.";
-    private final int amount;
+	private static final String CURRENCY = "원";
+	private static final String ERROR_NO_AMOUNT = "%d 원 동전이 없습니다.";
+	private final int amount;
 
-    Coin(final int amount) {
-        this.amount = amount;
-    }
+	Coin(final int amount) {
+		this.amount = amount;
+	}
 
-    public static int pickRandom() {
-        List<Integer> amounts = getAmountList();
-        return Randoms.pickNumberInList(amounts);
-    }
+	public static int pickRandom() {
+		List<Integer> amounts = getAmountList();
+		return Randoms.pickNumberInList(amounts);
+	}
 
-    private static List<Integer> getAmountList() {
-        return Arrays.stream(Coin.values()).map(coin -> coin.amount).collect(Collectors.toList());
-    }
+	private static List<Integer> getAmountList() {
+		return Arrays.stream(Coin.values()).map(coin -> coin.amount).collect(Collectors.toList());
+	}
 
-    public static Coin of(int amount) {
-        for (Coin coin : Coin.values()) {
-            if (amount == coin.amount) {
-                return coin;
-            }
-        }
-        throw new IllegalArgumentException(String.format(ERROR_NO_AMOUNT, amount));
-    }
+	public static Coin of(int amount) {
+		for (Coin coin : Coin.values()) {
+			if (amount == coin.amount) {
+				return coin;
+			}
+		}
+		throw new IllegalArgumentException(String.format(ERROR_NO_AMOUNT, amount));
+	}
 
-    @Override
-    public String toString() {
-        return amount + CURRENCY;
-    }
+	@Override
+	public String toString() {
+		return amount + CURRENCY;
+	}
 
-    public Money toMoney() {
-        return new Money(amount);
-    }
+	public Money toMoney() {
+		return new Money(amount);
+	}
 }
--- src/main/java/vendingmachine/service/ProductService.java
+++ src/main/java/vendingmachine/service/ProductService.java
@@ -2,7 +2,6 @@ package vendingmachine.service;
 
 import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import vendingmachine.domain.Product;
