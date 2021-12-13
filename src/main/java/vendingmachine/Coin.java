92e80bf46fb256b0877ff29c909e2ca4fa0157f3
Dong Geun <dlehdrms978@gmail.com>
2021-12-13 19:41:17 +0900

    각 동전 별로 개수 정보를 갖도록 수정

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,5 +1,8 @@
 package vendingmachine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
@@ -7,10 +10,19 @@ public enum Coin {
     COIN_10(10);
 
     private final int amount;
-
+    private int number = 0;
     Coin(final int amount) {
         this.amount = amount;
     }
 
     // 추가 기능 구현
+    public int getAmount(){
+        return this.amount;
+    }
+    public int getNumber(){
+        return this.number;
+    }
+    public void setNumber(int number){
+        this.number=number;
+    }
 }
