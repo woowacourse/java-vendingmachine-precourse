a9918df4cd43aff59011b98ce2c242564e874acf
Kim Seunglae <obey1342@gmail.com>
2021-12-13 16:31:57 +0900

    feat: 상품 가격, 수량 검증하는 기능 추가

--- src/main/java/vendingmachine/domain/Product.java
+++ src/main/java/vendingmachine/domain/Product.java
@@ -1,17 +1,34 @@
 package vendingmachine.domain;
 
+import static vendingmachine.resource.MessageResource.*;
+
+import vendingmachine.utils.NumericUtils;
+
 public class Product {
+	private static final int MIN_AMOUNT = 100;
+	private static final int DIVIDE_NUM = 10;
+
 	private final String name;
 	private final int price;
 	private final int count;
 
 	public Product(String name, int price, int count) {
+		check(price, count);
 		this.name = name;
 		this.price = price;
 		this.count = count;
 	}
 
-	public Product(String[] productSplit) {
-		this(productSplit[0], Integer.parseInt(productSplit[1]), Integer.parseInt(productSplit[2]));
+	private void check(int price, int count) {
+		if (price < MIN_AMOUNT || price % DIVIDE_NUM != 0) {
+			throw new IllegalArgumentException(ERROR_PRODUCT_PRICE_NOT_VALID);
+		}
+		if (count < 0) {
+			throw new IllegalArgumentException(ERROR_PRODUCT_COUNT_IS_NOT_POSITIVE_INT);
+		}
+	}
+
+	public Product(String[] split) {
+		this(split[0], NumericUtils.parseInt(split[1]), NumericUtils.parseInt(split[2]));
 	}
 }
--- src/main/java/vendingmachine/resource/MessageResource.java
+++ src/main/java/vendingmachine/resource/MessageResource.java
@@ -9,10 +9,11 @@ public class MessageResource {
 	public static final String OUTPUT_CHANGES = "%d원 - %d개\n";
 
 	public static final String ERROR_AMOUNT_IS_NOT_NUMERIC = "[ERROR] 금액은 숫자여야 합니다.";
-	public static final String ERROR_AMOUNT_IS_NOT_POSITIVE_INT = "[ERROR] 0보다 큰 정수여야 합니다.";
+	public static final String ERROR_AMOUNT_IS_NOT_POSITIVE_INT = "[ERROR] 금액은 0보다 큰 정수여야 합니다.";
 	public static final String ERROR_AMOUNT_IS_NOT_VALID = "[ERROR] 금액이 유효하지 않습니다.";
 	public static final String ERROR_IS_NOT_COIN_TYPE_EXIST = "[ERROR] 해당 코인 타입이 존재하지 않습니다.";
 	public static final String ERROR_INPUT_PRODUCT_NOT_VALID = "[ERROR] 상품명,가격,수량이 올바른 형식이 아닙니다.";
 	public static final String ERROR_PRODUCT_NOT_SQUARE_BRACKETS = "[ERROR] 상품입력 형식이 잘못 되었습니다.";
-
+	public static final String ERROR_PRODUCT_PRICE_NOT_VALID = "[ERROR] 상품 가격은 100원부터 시작하고 10으로 나누어 떨어져야 합니다.";
+	public static final String ERROR_PRODUCT_COUNT_IS_NOT_POSITIVE_INT = "[ERROR] 상품 수량은 0보다 커야 합니다.";
 }
