9b30a556a830611887463e7b8194cb061f404c46
Jeong Satchel <bugod96@gmail.com>
2021-12-13 20:26:36 +0900

    refactor: separate exception handling and menu construction logic from InputView to MenuService

--- src/main/java/vendingmachine/service/MenuService.java
+++ src/main/java/vendingmachine/service/MenuService.java
@@ -2,17 +2,43 @@ package vendingmachine.service;
 
 import static vendingmachine.constants.ExceptionMessages.*;
 import static vendingmachine.constants.SystemConstants.*;
+import static vendingmachine.validator.MenuInputFormatValidator.validateMenuInputFormat;
 
 import vendingmachine.domain.Menu;
 import vendingmachine.domain.Merchandise;
 import vendingmachine.view.InputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MenuService {
 
     private Menu menu;
 
     public void initializeMenu() {
-        this.menu = InputView.getMenuInput();
+        this.menu = this.getMenuInput();
+    }
+
+    private Menu getMenuInput() {
+        try {
+            String menuInput = InputView.requestMenuInput();
+            validateMenuInputFormat(menuInput);
+
+            return generateMenu(menuInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getMenuInput();
+        }
+    }
+
+    private Menu generateMenu(String menuInput) {
+        String[] merchandiseInfos = menuInput.split(SEMICOLON);
+
+        List<Merchandise> merchandiseList = new ArrayList<>();
+        for (String merchandiseInfo : merchandiseInfos) {
+            merchandiseList.add(new Merchandise(merchandiseInfo));
+        }
+        return new Menu(merchandiseList);
     }
 
     public Merchandise selectAvailableMerchandise(int moneyLeft) {
--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,16 +1,9 @@
 package vendingmachine.view;
 
 import static vendingmachine.constants.InputMessages.*;
-import static vendingmachine.constants.SystemConstants.SEMICOLON;
 import static vendingmachine.validator.NumberInputValidator.*;
-import static vendingmachine.validator.MenuInputFormatValidator.*;
 
 import camp.nextstep.edu.missionutils.Console;
-import vendingmachine.domain.Menu;
-import vendingmachine.domain.Merchandise;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class InputView {
 
@@ -19,27 +12,9 @@ public class InputView {
         return Console.readLine();
     }
 
-    public static Menu getMenuInput() {
-        try {
-            System.out.println(REQUEST_MENU_INPUT);
-            String menuInput = Console.readLine();
-            validateMenuInputFormat(menuInput);
-
-            return generateMenuFromInput(menuInput);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return getMenuInput();
-        }
-    }
-
-    private static Menu generateMenuFromInput(String menuInput) {
-        String[] merchandiseInfos = menuInput.split(SEMICOLON);
-
-        List<Merchandise> merchandiseList = new ArrayList<>();
-        for (String merchandiseInfo : merchandiseInfos) {
-            merchandiseList.add(new Merchandise(merchandiseInfo));
-        }
-        return new Menu(merchandiseList);
+    public static String requestMenuInput() {
+        System.out.println(REQUEST_MENU_INPUT);
+        return Console.readLine();
     }
 
     public static int getCustomerMoneyInput() {
