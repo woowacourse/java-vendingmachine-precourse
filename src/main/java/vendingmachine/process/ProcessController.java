74afb37bcc265371eed4921ed09ee72256e928a9
iamsseaa <homingsea@gmail.com>
2021-12-13 15:08:43 +0900

    feat: 보유 금액 랜덤 동전 생성 미션 추가

--- src/main/java/vendingmachine/process/ProcessController.java
+++ src/main/java/vendingmachine/process/ProcessController.java
@@ -4,20 +4,24 @@ import vendingmachine.machine.Machine;
 import vendingmachine.machine.MachineHoldingAmount;
 
 public class ProcessController {
-    private static int holdingAmount;
+    private int holdingAmount;
+    MachineHoldingAmount machineHoldingAmount;
 
-    public static void makeHoldingAmount() {
+    public ProcessController(MachineHoldingAmount machineHoldingAmount) {
+        this.machineHoldingAmount = machineHoldingAmount;
+    }
+
+    public void makeHoldingAmount() {
         while (true) {
             try {
                 System.out.println(ProcessConstant.ASK_HOLDING_AMOUNT);
                 holdingAmount = ProcessPrepareHoldingAmount.makeHoldingAmount();
+                break;
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
-    }
-
-    public static void makeMachineCoins(MachineHoldingAmount machineHoldingAmount) {
         machineHoldingAmount.makeCoins();
     }
+
 }
