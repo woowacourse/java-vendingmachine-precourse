54cc798786f1dcffc78c48fba8789e3136ffce82
koo <gudonghee2000@gmail.com>
2021-12-13 17:36:48 +0900

    refactor : OutputDisplay클래스의 상수를 생성하고 함수구조 변경

--- src/main/java/view/OutputDisplay.java
+++ src/main/java/view/OutputDisplay.java
@@ -4,26 +4,29 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class OutputDisplay {
+	private static final int FIRST_INDEX = 0;
 	private static final String PRICE = "원 - ";
 	private static final String COUNT = "개";
+	private static final String INSERTED_MONEY = "투입 금액: ";
+	private static final String UNIT_OF_MONEY = "원";
 	private static final String VENDING_MACHINE_HOLDING_COIN_MESSAGE = "자판기가 보유한 동전";
 
 	private OutputDisplay() {
 	}
 
-	public static void showEachCoinInCoinBox(List<Integer> eachCoinPrice, List<Integer> eachCoinCount) {
+	public static void showCoinsInCoinBox(List<Integer> priceOfCoins, List<Integer> countOfCoins) {
 		System.out.println();
 		System.out.println(VENDING_MACHINE_HOLDING_COIN_MESSAGE);
-		IntStream.range(0, eachCoinCount.size())
-			.forEach(index -> showCoin(eachCoinPrice.get(index), eachCoinCount.get(index)));
+		IntStream.range(FIRST_INDEX, countOfCoins.size())
+			.forEach(index -> showCoinInformation(priceOfCoins.get(index), countOfCoins.get(index)));
 	}
 
-	private static void showCoin(int coinPrice, int coinCount) {
+	private static void showCoinInformation(int coinPrice, int coinCount) {
 		System.out.println(coinPrice + PRICE + coinCount + COUNT);
 	}
 
-	public static void showNowUserInsertMoney(int userInsertMoney) {
+	public static void showNowInsertedMoney(int nowInsertedMoney) {
 		System.out.println();
-		System.out.println("투입 금액: " + userInsertMoney + "원");
+		System.out.println(INSERTED_MONEY + nowInsertedMoney + UNIT_OF_MONEY);
 	}
 }
