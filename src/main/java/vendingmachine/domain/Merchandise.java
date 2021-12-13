282155fcd505d6fdf507b85c0cf4a2f2a8a82f8e
Jeong Satchel <bugod96@gmail.com>
2021-12-13 21:08:20 +0900

    refactor(Merchandise): use static variable for String value and add final for unchanging variables

--- src/main/java/vendingmachine/domain/Merchandise.java
+++ src/main/java/vendingmachine/domain/Merchandise.java
@@ -4,12 +4,12 @@ import static vendingmachine.constants.SystemConstants.*;
 
 public class Merchandise {
 
-    private String name;
-    private int price;
+    private final String name;
+    private final int price;
     private int number;
 
     public Merchandise(String merchandiseInfo) {
-        String[] infoList = merchandiseInfo.substring(1, merchandiseInfo.length() - 1).split(",");
+        String[] infoList = merchandiseInfo.substring(1, merchandiseInfo.length() - 1).split(COMMA);
         this.name = infoList[NAME_IDX];
         this.price = Integer.parseInt(infoList[PRICE_IDX]);
         this.number = Integer.parseInt(infoList[NUMBER_IDX]);
