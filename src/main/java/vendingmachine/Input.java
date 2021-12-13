0639d198039121d622835c35e8cce1759ba80cf4
PanghoChao <Sobrightlf@naver.com>
2021-12-13 23:20:32 +0900

    feat(Input) : 사용자 입력 값 구현

--- src/main/java/vendingmachine/Input.java
+++ src/main/java/vendingmachine/Input.java
@@ -8,32 +8,26 @@ public class Input {
 		String MachineCashInput = Console.readLine();
 		return MachineCashInput;
 	}
-	
+
 	public static String goodsInput() {
-    	System.out.println("상품명과 가격, 수량을 입력해 주세요.");
-    	System.out.println("상품명, 가격, 수량은 쉼표로, 개별 상품은 대괄호([])로 묶어 세미콜론(;)으로 구분해 주세요.");
-    	
+		System.out.println("상품명과 가격, 수량을 입력해 주세요.");
+		System.out.println("상품명, 가격, 수량은 쉼표로, 개별 상품은 대괄호([])로 묶어 세미콜론(;)으로 구분해 주세요.");
+
 		String goodsInput = Console.readLine();
 		return goodsInput;
-		
-	}
-	
-	public static String cashInput() {
-    	System.out.println("투입 금액을 입력해 주세요.");
-    	String cashInput = Console.readLine();
-    	return cashInput;
+
 	}
 
-	
+	public static String cashInput() {
+		System.out.println("투입 금액을 입력해 주세요.");
+		String cashInput = Console.readLine();
+		return cashInput;
+	}
+
 	public static String buyGoodsInput() {
-    	System.out.println("구매할 상품명을 입력해 주세요.");
-    	String buyGoodsInput =  Console.readLine();
+		System.out.println("구매할 상품명을 입력해 주세요.");
+		String buyGoodsInput = Console.readLine();
 		return buyGoodsInput;
 	}
-	
-	
-	
 
-
-	
 }
