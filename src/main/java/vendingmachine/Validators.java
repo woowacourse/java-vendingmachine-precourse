52c411148a4b0196e37bd899896a7797c2e25b37
Byunghoon <nbalance__@kyonggi.ac.kr>
2021-12-13 15:10:46 +0900

    feat: 자판기 10원단위 금액 검증 추가

--- src/main/java/vendingmachine/Validators.java
+++ src/main/java/vendingmachine/Validators.java
@@ -4,6 +4,7 @@ import java.util.regex.Pattern;
 public class Validators {
     private static final String ITEM_PATTERN = "\\[[[A-Za-z가-힣0-9ㄱ-ㅎ][\\s]*]+,[0-9]+,[0-9]+\\]";
     private static final String SPLIT_REGEX = ";";
+    private static final int UNIT = 10;
     private static final int ZERO = 0;
 
     public static void validateIntegerString(String data) {
@@ -17,6 +18,9 @@ public class Validators {
     public static void validateAmount(String data) {
         validateIntegerString(data);
         if (Integer.parseInt(data) <= ZERO) {
+            throw new IllegalArgumentException();
+        }
+        if (Integer.parseInt(data) % UNIT != ZERO) {
             throw new IllegalArgumentException();
         }
     }
