8e5452ed80bf5e8208012565000363f23b50340d
steven <seokju73@gmail.com>
2021-12-13 16:47:51 +0900

    Feat: Add method to get least number of returning coin

--- src/main/java/vendingmachine/operation/OperationService.java
+++ src/main/java/vendingmachine/operation/OperationService.java
@@ -1,5 +1,10 @@
 package vendingmachine.operation;
 
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.Map;
+
+import vendingmachine.Coin;
 import vendingmachine.management.Commodity;
 import vendingmachine.management.CommodityRepository;
 import vendingmachine.operation.validation.CheckCommoditySelection;
@@ -13,5 +18,25 @@ public class OperationService {
 
     public static void reduceCommodityQuantity(Commodity commodity) {
         CommodityRepository.reduceQuantity(commodity);
+    }
+    
+    public static Map<Coin,Integer> getLeastNumberOfCoin(int balance) {
+        Map<Coin,Integer> coinMap = new EnumMap<>(Coin.class);
+        Coin[] coins = Coin.values();
+        int idx = 0;
+        while(balance > 0 && idx < coins.length) {
+            if(coins[idx].getNumber() == 0) {
+                idx++;
+                continue;
+            }
+            balance = reduceBalance(balance,coins[idx]);
+            coinMap.put(coins[idx], coinMap.getOrDefault(coins[idx], 0)+1);
+        }
+        return coinMap;
+    }
+
+    private static int reduceBalance(int balance, Coin coinType) {
+        Coin.deleteNumber(coinType);
+        return balance - coinType.getAmount();
     }
 }
