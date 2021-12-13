b01ddc4fafe9932e288677bb56d251108bf77ad9
Wishoon <heonga2@gmail.com>
2021-12-13 10:41:11 +0900

    feat: 입력받은 상품 수량에 대한 예외 처리 추가
    Quantity
    - isValidateNumber : 상품 수량의 숫자 형태 유무 판단

--- src/main/java/vendingmachine/domain/Quantity.java
+++ src/main/java/vendingmachine/domain/Quantity.java
@@ -1,13 +1,24 @@
 package vendingmachine.domain;
 
 public class Quantity {
+	private static final String ERROR_QUANTITY_NOT_NUMBER = "수량이 숫자가 아닙니다.";
+
 	private final int quantity;
 
-	public Quantity(int quantity) {
+	private Quantity(int quantity) {
 		this.quantity = quantity;
 	}
 
 	public static Quantity from(String quantity) {
+		isValidateNumber(quantity);
 		return new Quantity(Integer.parseInt(quantity));
+	}
+
+	private static void isValidateNumber(String quantity) {
+		try {
+			Integer.parseInt(quantity);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(ERROR_QUANTITY_NOT_NUMBER);
+		}
 	}
 }
