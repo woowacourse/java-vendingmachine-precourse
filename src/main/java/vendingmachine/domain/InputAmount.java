c0fc97c99ac80fe2baf0707910899768f81d9674
monk <2012.monk@gmail.com>
2021-12-13 17:08:27 +0900

    fix: add validate input amount

--- src/main/java/vendingmachine/domain/InputAmount.java
+++ src/main/java/vendingmachine/domain/InputAmount.java
@@ -2,9 +2,13 @@ package vendingmachine.domain;
 
 public class InputAmount {
 
+    private static final String ERR_INVALID_AMOUNT = "[ERROR] 투입금액은 0원 이상이어야합니다.";
+    private static final int MIN_AMOUNT = 1;
+
     private int amount;
 
     public InputAmount(int inputAmount) {
+        validateInputAmount(inputAmount);
         this.amount = inputAmount;
     }
 
@@ -14,5 +18,11 @@ public class InputAmount {
 
     public void consume(Product product) {
         amount -= product.getAmount();
+    }
+
+    private void validateInputAmount(int inputAmount) {
+        if (inputAmount < MIN_AMOUNT) {
+            throw new IllegalArgumentException(ERR_INVALID_AMOUNT);
+        }
     }
 }
