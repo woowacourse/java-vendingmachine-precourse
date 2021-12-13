c182d08b30844ee57289b593dd4dc211b10d927e
PanghoChao <Sobrightlf@naver.com>
2021-12-13 14:37:46 +0900

    feat(MoneyInput) : 투입금액관리클래스 생성

---
+++ src/main/java/vendingmachine/MoneyInput.java
@@ -0,0 +1,21 @@
+package vendingmachine;
+
+public class MoneyInput {
+	int money;
+	public MoneyInput(String inputMoney) {
+		Validater.isNumberCheck(inputMoney);
+		Validater.isDivideTen(inputMoney);
+		Validater.isZeroCheck(inputMoney);
+		this.money = Integer.parseInt(inputMoney);
+	}
+	
+	public int getMoney() {
+		return money;
+	}
+	//음료수 구매시 투입 금액 차감
+	public void remainMoney(int cost) {
+		this.money = this.money - cost;
+	}
+	
+	
+}
