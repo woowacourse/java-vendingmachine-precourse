70102a0ec2bfdf7c65966e6094754c22281eb566
bomi <bonita9931@gmail.com>
2021-12-13 19:17:03 +0900

    feat(vendingMachineController) : 자판기 컨트롤러 기능 정의

---
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -0,0 +1,14 @@
+package vendingmachine.controller;
+
+public class VendingMachineController {
+
+	public void runMachine() {
+		//금액 입력받기
+		//자판기에게 금액 센팅
+		//동전 생성
+		//상품명 입력받기
+		//투입 금액 입력받기
+		//물건 구매하기
+		//자판기 종료시 잔돈 계산하기
+	}
+}
