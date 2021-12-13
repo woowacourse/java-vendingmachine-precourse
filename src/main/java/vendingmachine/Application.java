c4a1b2075e22cedf4db1d44612ea85118ae9430d
KBY-TECH <soccer_by@naver.com>
2021-12-14 01:32:07 +0900

    style: VendingMachine 클래스명 변경 및 네이밍 변수,메소드 변경

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -4,7 +4,7 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        VendingMachine vendingMachine = new VendingMachine();
+        VendingMachineSystemController vendingMachine = new VendingMachineSystemController();
         vendingMachine.start();
     }
 }
--- src/main/java/vendingmachine/VendingMachineSystemController.java
+++ src/main/java/vendingmachine/VendingMachineSystemController.java
@@ -5,10 +5,10 @@ import vendingmachine.system.ValidationImplementation;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class VendingMachine {
+public class VendingMachineSystemController {
 
-    private static final String INPUT_HOLDINGMONEY_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
-    private static final String ERROR_INPUT_HOLDINGMONEY_MESSAGE = "[ERROR] 금액은 100 이상 그리고 1억 이하의 숫자이여야 하며 10으로 나누어 떨어져야 합니다.";
+    private static final String INPUT_HOLDING_MONEY_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    private static final String ERROR_INPUT_HOLDING_MONEY_MESSAGE = "[ERROR] 금액은 100 이상 그리고 1억 이하의 숫자이여야 하며 10으로 나누어 떨어져야 합니다.";
     private static final String INPUT_PRODUCT_PRICE_STOCK_MESSAGE = "상품명과 가격, 수량을 입력해 주세요.";
     private static final String ERROR_PRODUCT_PRICE_STOCK_MESSAGE = "[ERROR] [{상품명},{가격},{수량}] 형식으로 입력하셔야 하며 공백이 포함되선 안됩니다. 상품 구분자는 ';' 입니다.";
     private static final String INPUT_USER_INSERT_MONEY_MESSAGE = "투입 금액을 입력해주세요. ";
@@ -17,7 +17,7 @@ public class VendingMachine {
     private static final String ERROR_PRODUCT_NAME_TO_BUY_MESSAGE = "[ERROR] 상품명은 한글로 입력해야 합니다. 또는 존재하지 않습니다.";
     private final Validation validation;
 
-    public VendingMachine() {
+    public VendingMachineSystemController() {
         validation = new ValidationImplementation();
     }
 
@@ -25,17 +25,17 @@ public class VendingMachine {
         inputHoldingMoney();
         inputProductNameAndPriceAndStock();
         inputUserInsertMoney();
-        inputProductNameTobuy();
+        inputProductNameToBuy();
     }
 
     public void inputHoldingMoney() {
         boolean isValidInput = false;
         do {
             try {
-                printConsoleMessage(INPUT_HOLDINGMONEY_MESSAGE);
+                printConsoleMessage(INPUT_HOLDING_MONEY_MESSAGE);
                 isValidInput = validation.isValidHoldingMoney(Console.readLine());
             } catch (IllegalArgumentException e) {
-                printConsoleMessage(ERROR_INPUT_HOLDINGMONEY_MESSAGE);
+                printConsoleMessage(ERROR_INPUT_HOLDING_MONEY_MESSAGE);
                 isValidInput = false;
             }
         } while (!isValidInput);
@@ -67,7 +67,7 @@ public class VendingMachine {
         } while (!isValidInput);
     }
 
-    public void inputProductNameTobuy(){
+    public void inputProductNameToBuy(){
         boolean isValidInput = false;
         do {
             try {
