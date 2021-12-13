f270a1e592ee6f54ae62408300c5145082477607
이한울 <dkfghg3@naver.com>
2021-12-13 18:58:11 +0900

    refactor: 숫자 검증하는 isDigit 메소드 접근 제한자 public으로 변경

--- src/main/java/vendingmachine/utils/ValidateUtils.java
+++ src/main/java/vendingmachine/utils/ValidateUtils.java
@@ -5,7 +5,7 @@ public class ValidateUtils {
 	private static final int ZERO = 0;
 	private static final int DIVIDE_NUMBER = 10;
 	private static final String ERROR_INVALID_COIN = "유효하지 않은 금액입니다.";
-	private static final String ERROR_NOT_NUMBER = "금액은 숫자만 입력해야합니다.";
+	private static final String ERROR_NOT_NUMBER = "숫자만 입력해야합니다.";
 
 	public static void validateInputCoin(String inputCoin) {
 		isDigit(inputCoin);
@@ -21,7 +21,7 @@ public class ValidateUtils {
 		}
 	}
 
-	private static void isDigit(String inputCoin) {
+	public static void isDigit(String inputCoin) {
 		for (char number : inputCoin.toCharArray()) {
 			if (!Character.isDigit(number)) {
 				throw new IllegalArgumentException(ERROR_NOT_NUMBER);
