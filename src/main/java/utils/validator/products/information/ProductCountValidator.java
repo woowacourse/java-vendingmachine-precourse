85781a98bf9b88ff41169984d6fdeb37833d103f
koo <gudonghee2000@gmail.com>
2021-12-13 17:06:20 +0900

    refactor : ProductCountValidator클래스의 함수명, 상수명 변경

--- src/main/java/utils/validator/products/information/ProductCountValidator.java
+++ src/main/java/utils/validator/products/information/ProductCountValidator.java
@@ -3,36 +3,36 @@ package utils.validator.products.information;
 import java.util.regex.Pattern;
 
 public class ProductCountValidator {
-	private static final int MIN_PRODUCT_COUNT_INPUT_LENGTH = 1;
+	private static final int MIN_COUNT_INPUT_LENGTH = 1;
 	private static final String ZERO = "0";
-	private static final String COUNT_PATTERN = "^[0-9]*$";
+	private static final String COUNT_NUMBER_PATTERN = "^[0-9]*$";
 	private static final String INPUT_NOTHING_ERROR_MESSAGE = "[ERROR] 입력한 자판기 상품개수가 없다.";
-	private static final String NOT_RIGHT_COUNT_ERROR_MESSAGE = "[ERROR] 입력한 자판기 상품개수가 올바른수(양수)가 아니다.";
-	private static final String START_COUNT_ERROR_MESSAGE = "[ERROR] 입력한 자판기 상품개수가 0으로 시작한다.";
+	private static final String NOT_RIGHT_COUNT_NUMBER_ERROR_MESSAGE = "[ERROR] 입력한 자판기 상품개수가 올바른수(양수)가 아니다.";
+	private static final String START_COUNT_NUMBER_ERROR_MESSAGE = "[ERROR] 입력한 자판기 상품개수가 0으로 시작한다.";
 
 	private ProductCountValidator() {
 	}
 
-	public static boolean isValidProductCount(String productCount) {
-		if (isValidInputLength(productCount) && isRightNumber(productCount)) {
+	public static boolean isValidCount(String productCount) {
+		if (isValidInputLength(productCount) && isRightCountNumber(productCount)) {
 			return true;
 		}
 		throw new IllegalArgumentException();
 	}
 
 	private static boolean isValidInputLength(String productCount) {
-		if (productCount.length() < MIN_PRODUCT_COUNT_INPUT_LENGTH) {
+		if (productCount.length() < MIN_COUNT_INPUT_LENGTH) {
 			throw new IllegalArgumentException(INPUT_NOTHING_ERROR_MESSAGE);
 		}
 		return true;
 	}
 
-	private static boolean isRightNumber(String productCount) {
-		if (!Pattern.matches(COUNT_PATTERN, productCount)) {
-			throw new IllegalArgumentException(NOT_RIGHT_COUNT_ERROR_MESSAGE);
+	private static boolean isRightCountNumber(String productCount) {
+		if (!Pattern.matches(COUNT_NUMBER_PATTERN, productCount)) {
+			throw new IllegalArgumentException(NOT_RIGHT_COUNT_NUMBER_ERROR_MESSAGE);
 		}
 		if (productCount.startsWith(ZERO)) {
-			throw new IllegalArgumentException(START_COUNT_ERROR_MESSAGE);
+			throw new IllegalArgumentException(START_COUNT_NUMBER_ERROR_MESSAGE);
 		}
 		return true;
 	}
