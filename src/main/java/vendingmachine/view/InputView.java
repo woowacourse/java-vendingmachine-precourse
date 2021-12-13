15fac9de65f5225f7b6c9793155e5eb5f089ba3a
Seungmin Lim <seungmin9066@gmail.com>
2021-12-13 18:27:09 +0900

    [수정] 객체 사용방식이 바뀌어 그에 맞게 변경

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,7 +1,12 @@
 package vendingmachine.view;
 
 import static camp.nextstep.edu.missionutils.Console.*;
+import static vendingmachine.constant.Constant.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import vendingmachine.domain.Item;
 import vendingmachine.util.Validator;
 
 public class InputView {
@@ -12,9 +17,19 @@ public class InputView {
 		return readLine();
 	}
 
+	public static List<Item> readItems() {
+		String readLine = readLine();
+		String products = readLine.replaceAll("\\]", "").replaceAll("\\[", "");
+		List<Item> items = new ArrayList<>();
+		for (String productPriceAndStock : products.split(DISTINGUISH_BETWEEN_PRODUCTS)) {
+			String[] product = productPriceAndStock.split(DISTINGUISH_BETWEEN_PRODUCT_INFORMATION);
+			items.add(new Item(product[NAME], Integer.parseInt(product[PRICE]), Integer.parseInt(product[STOCKS])));
+		}
+		return items;
+	}
+
 	public static int readPositiveInt() {
 		try {
-			OutputView.setVendingMachineMoney();
 			String input = readLine();
 			validator.validatePrice(input);
 			return Integer.parseInt(input);
