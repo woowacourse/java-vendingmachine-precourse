3d28eaf9f0083b66cd3923a25840dcbc918d5c11
KIM <km0192@naver.com>
2021-12-13 14:01:22 +0900

    feat(utils.Validator-isDigit) : 숫자인지 확인하는 validator 클래스 추가

--- src/main/java/utils/Validator.java
+++ src/main/java/utils/Validator.java
@@ -1,9 +1,24 @@
 package utils;
 
-public class Validator { // interface 로 변경 후, 체크할 에러에 대해 각각 클래스 생성할 것!
+import java.util.stream.Stream;
 
+public interface Validator { // interface 로 변경 후, 체크할 에러에 대해 각각 클래스 생성할 것!
+
+	public boolean run(StringBuilder input);
+}
+
+class Foo implements Validator {
 	public boolean run(StringBuilder input) {
-		// 체크할 에러 확인 후 Throw IllegalArgumentException
 		return true;
+	}
+}
+
+class isDigit implements Validator {
+	public boolean run(StringBuilder input) {
+		String digitRegex = "^[1-9][\\d]*[\\d]$";
+		if (!input.toString().matches(digitRegex)) {
+			throw new IllegalArgumentException("입력값이 숫자가 아닙니다.");
+		}
+		return false;
 	}
 }
