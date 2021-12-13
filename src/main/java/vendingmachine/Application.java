4f70f82489d16219dc8581cbaaa968d156bc2269
yeongunheo <gjduddns@gmail.com>
2021-12-14 00:12:44 +0900

    refactor: Application main() 메소드가 15줄을 넘지 않도록 분리

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -11,15 +11,24 @@ import vendingmachine.domain.Money;
 import vendingmachine.domain.Products;
 
 public class Application {
+	private static VendingMachineController vendingMachineController;
+	private static Products products;
+
 	public static void main(String[] args) {
 		final Scanner scanner = new Scanner(System.in);
-		VendingMachineController vendingMachineController = new VendingMachineController(scanner);
-		int holdingMoney = vendingMachineController.scanHoldingMoney();
-		Map<Coin, Count> coins = Coins.moneyToCoins(holdingMoney);
+		vendingMachineController = new VendingMachineController(scanner);
+		final Money holdingMoney = new Money(vendingMachineController.scanHoldingMoney());
+		Map<Coin, Count> coins = Coins.moneyToCoins(holdingMoney.getMoney());
 		vendingMachineController.printHoldingCoins(coins);
 		String[] productNameAndPriceAndCnt = vendingMachineController.scanProductNameAndPriceAndCnt();
-		Products products = Products.save(productNameAndPriceAndCnt);
+		products = Products.save(productNameAndPriceAndCnt);
 		Money inputMoney = new Money(vendingMachineController.scanInputMoney());
+		inputMoney = buyProduct(inputMoney);
+		Map<Integer, Integer> changes = Coins.returnChange(inputMoney);
+		vendingMachineController.printChange(changes);
+	}
+
+	public static Money buyProduct(Money inputMoney) {
 		while (inputMoney.isRemain()) {
 			vendingMachineController.printInputMoney(inputMoney.getMoney());
 			if (products.isHigherThanMinProductPrice(inputMoney) || products.isSoldOut()) {
@@ -29,7 +38,6 @@ public class Application {
 			products = products.buy(buyProductName);
 			inputMoney = inputMoney.reduce(products.getProductPrice(buyProductName, products));
 		}
-		Map<Integer, Integer> changes = Coins.returnChange(inputMoney);
-		vendingMachineController.printChange(changes);
+		return inputMoney;
 	}
 }
