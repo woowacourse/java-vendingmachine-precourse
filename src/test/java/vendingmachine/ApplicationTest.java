feb5b5de74d0cce230aedf8f4dc3a4437a11e496
hamcheeseburger <yoo77hyeon@gmail.com>
2021-12-13 22:42:46 +0900

    test : 테스트 추가

--- src/test/java/vendingmachine/ApplicationTest.java
+++ src/test/java/vendingmachine/ApplicationTest.java
@@ -15,7 +15,7 @@ class ApplicationTest extends NsTest {
     private static final int COIN = 500;
     private static final int REMAIN_MONEY = 450;
 
-    // @Test
+     @Test
     void 기능_테스트() {
         assertRandomNumberInListTest(
             () -> {
@@ -29,7 +29,7 @@ class ApplicationTest extends NsTest {
         );
     }
 
-    // @Test
+     @Test
     void 기능_테스트2() {
         assertRandomNumberInListTest(
             () -> {
@@ -63,8 +63,8 @@ class ApplicationTest extends NsTest {
             () -> {
                 run("350", "[콜라,500,1];[사이다,100,1]", "100", "콜라", "사이다");
                 assertThat(output()).contains(
-                    "자판기가 보유한 동전", "500원 - 0개", "100원 - 0개", "50원 - 8개", "10원 - 10개",
-                    "투입 금액: 700원", "투입 금액: 200원", "잔돈", "50원 - 2개"
+                    "자판기가 보유한 동전", "500원 - 0개", "100원 - 2개", "50원 - 2개", "10원 - 5개",
+                    "투입 금액: 100원", "투입 금액: 0원", "잔돈"
                 );
             },
             500, 10, 100, 50, 100, 50, 500, 10, 10, 50, 10, 10
@@ -84,19 +84,5 @@ class ApplicationTest extends NsTest {
     @Override
     protected void runMain() {
         Application.main(new String[]{});
-    }
-
-    // @Test
-    CoinStorage 동전을_랜덤으로_생성한다() {
-        CoinStorage coinStorage = new CoinStorage(MONEY);
-        System.out.println("자판기 동전 목록");
-        System.out.println(coinStorage.toString());
-        return coinStorage;
-    }
-
-    // @Test
-    void 나누기를_테스트한다() {
-        int maxQuantity = MONEY / COIN;
-        System.out.println("maxQuantity : " + maxQuantity);
     }
 }
