5a4548e6978e58102f7b605a4fd94c9d86dc124c
seongwoo <seongwoo0513@uos.ac.kr>
2021-12-13 13:58:38 +0900

    refactor: Application의 진행 순서대로 메서드 구분

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -3,12 +3,25 @@ package vendingmachine;
 import vendingmachine.controller.VendingMachineController;
 
 public class Application {
+	private static final VendingMachineController vendingMachineController = new VendingMachineController();
+
 	public static void main(String[] args) {
-		VendingMachineController vendingMachineController = new VendingMachineController();
+		initializeVendingMachine();
+		useVendingMachine();
+		returnCharge();
+	}
+
+	private static void initializeVendingMachine() {
 		vendingMachineController.setVendingMachineCoinCounter();
 		vendingMachineController.setProducts();
+	}
+
+	private static void useVendingMachine() {
 		vendingMachineController.getUserAmount();
 		vendingMachineController.useVendingMachine();
+	}
+
+	private static void returnCharge() {
 		vendingMachineController.returnChange();
 	}
 }
