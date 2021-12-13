8ac79af81f60196f2ccecdb85b8a3491cb672ba9
wooseok <diddmlvkf@gmail.com>
2021-12-13 18:13:51 +0900

    refactor(VendingMachineController) : 투입 금액 입력을 RequestController에 있는 기능으로 활용

--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -3,7 +3,6 @@ package vendingmachine.controller;
 import vendingmachine.domain.Money;
 import vendingmachine.domain.VendingMachine;
 import vendingmachine.service.VendingMachineService;
-import vendingmachine.utils.validator.RequestValidator;
 import vendingmachine.view.ExceptionView;
 import vendingmachine.view.InputView;
 
@@ -16,9 +15,7 @@ public class VendingMachineController {
 
 	public void insertMoney() {
 		try {
-			String request = InputView.requestInsertMoney();
-			RequestValidator.isNumber(request);
-			Money insertMoney = new Money(Integer.parseInt(request));
+			Money insertMoney = RequestController.requestInsertMoney();
 			VendingMachineService.insertMoney(vendingMachine, insertMoney);
 		} catch (IllegalArgumentException e) {
 			ExceptionView.errorUI(e.getMessage());
