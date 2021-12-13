4a30297fbd960806690ed7b203c145fefc25b67d
Dong Geun <dlehdrms978@gmail.com>
2021-12-14 02:07:02 +0900

    오타 수정 완료

--- src/main/java/vendingmachine/ui/MachineUI.java
+++ src/main/java/vendingmachine/ui/MachineUI.java
@@ -1,6 +1,8 @@
 package vendingmachine.ui;
 
+import java.util.Map;
+
 public interface MachineUI {
     void showAmount();
-    void showLeftExchange();
+    void showExchange(Map<String,Integer> exchangedCoins);
 }
