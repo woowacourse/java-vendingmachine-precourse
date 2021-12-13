b2cb5abab56af08fcc410683c1666d6b69c4cba6
ayoung0073 <ayong0310@naver.com>
2021-12-14 00:59:00 +0900

    feat: 자판기 보유 동전을 생성하는 기능 구현

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -5,5 +5,7 @@ import vendingmachine.view.InputView;
 public class Application {
     public static void main(String[] args) {
         String vendingMachineMoney = InputView.inputVendingMachineMoney();
+        Coins coins = new Coins();
+        coins.makeCoinsByMoney(Integer.parseInt(vendingMachineMoney));
     }
 }
---
+++ src/main/java/vendingmachine/Coins.java
@@ -0,0 +1,30 @@
+package vendingmachine;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Coins {
+    private final Map<Coin, Integer> coins = new HashMap<>();
+
+    public Map<Coin, Integer> makeCoinsByMoney(int money) {
+        for (Coin coin : Coin.values()) {
+            int coinCount = getRandomCoinCount(money, coin);
+            coins.put(coin, coinCount);
+            money -= coin.getAmount() * coinCount;
+        }
+        return coins;
+    }
+
+    private int getRandomCoinCount(int money, Coin coin) {
+        int minCount = 0;
+        int maxCount = money / coin.getAmount();
+
+        if (coin == Coin.COIN_10) {
+            minCount = maxCount;
+        }
+
+        return Randoms.pickNumberInRange(minCount, maxCount);
+    }
+}
