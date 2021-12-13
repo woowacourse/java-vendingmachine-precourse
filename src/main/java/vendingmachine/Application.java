1fafc5a875db4c99db3bacc7a10d7eb03c12fb58
lymchgmk <lymchgmk@gmail.com>
2021-12-14 04:43:12 +0900

    Revert "feat(Application.java): 프로그램 시작 부 구현"
    This reverts commit 28931243976c4d1346c66ba191cc33e58bf8f4c0.

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -3,7 +3,5 @@ package vendingmachine;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Machine machine = new Machine();
-        machine.run();
     }
 }
