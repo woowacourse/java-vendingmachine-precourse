7d56e0c143e33e2d1b10ea8217f5a4a702ad43c8
cjlee38 <gptpem38@gmail.com>
2021-12-13 17:36:33 +0900

    feat(product): 상품후보 -> 상품목록 등록 구현

--- src/main/java/vendingmachine/domain/ProductSet.java
+++ src/main/java/vendingmachine/domain/ProductSet.java
@@ -6,12 +6,18 @@ import java.util.Set;
 public class ProductSet {
 
 	public static final String NAME = "상품목록";
-	private Set<Product> products = new LinkedHashSet<>();
+	private Set<Product> products;
 
 	public ProductSet() {
+		this.products = new LinkedHashSet<>();
 	}
 
 	public ProductSet(Set<Product> products) {
 		this.products = products;
+	}
+
+	@Override
+	public String toString() {
+		return products.toString();
 	}
 }
---
+++ src/main/java/vendingmachine/service/ProductService.java
@@ -0,0 +1,36 @@
+package vendingmachine.service;
+
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import vendingmachine.domain.Product;
+import vendingmachine.domain.ProductCandidate;
+import vendingmachine.domain.ProductSet;
+import vendingmachine.repository.ProductRepository;
+import vendingmachine.validation.DistinctValidation;
+import vendingmachine.validation.Validator;
+
+public class ProductService {
+
+	private final ProductRepository productRepository;
+
+	public ProductService(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
+
+	public String persist(List<ProductCandidate> productCandidates) {
+		List<Product> productList = toProductList(productCandidates);
+		return productRepository.save(toProductSet(productList));
+	}
+
+	private ProductSet toProductSet(List<Product> productList) {
+		List<Product> validated = Validator.validate(Product.NAME, productList, new DistinctValidation());
+		return new ProductSet(new LinkedHashSet<>(validated));
+	}
+
+	private List<Product> toProductList(List<ProductCandidate> productCandidates) {
+		return productCandidates.stream().map(ProductCandidate::toProduct).collect(Collectors.toList());
+	}
+}
