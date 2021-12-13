8a7eff84f74b8a2b65830781cbcfe740924c50bc
PanghoChao <Sobrightlf@naver.com>
2021-12-13 23:19:35 +0900

    feat(/VendingMachine) : 자판기에서 상품구매하고 잔돈 반환구현

--- src/main/java/vendingmachine/Output.java
+++ src/main/java/vendingmachine/Output.java
@@ -1,27 +1,33 @@
 package vendingmachine;
 
+import java.util.HashMap;
+
+
 public class Output {
 
-	
-	
-	//자판기가 보유한 잔돈 출력 
-	private void hasCoinPrint(/*배열값으로 받기*/ ) {
-		System.out.print("자판기가 보유한 동전");
-		// 배열 풀어서 출력하기
-		//if(int  a: )
-		System.out.println("원");
+	// 자판기가 보유한 잔돈 출력
+	static void hasCoinPrint(HashMap<Coin, Integer> coinMap) {
+		System.out.println("자판기가 보유한 동전");
+		for (Coin money : Coin.values()) {
+			System.out.println(money.getAmount() + "원 - " + coinMap.get(money) + "개");
+		}
 
-	}//현재 투입금액 상태 출력
+	}// 현재 투입금액 상태 출력
+
 	public static void inputMoneyPrint(int inputMoney) {
 		System.out.print("투입 금액: ");
 		System.out.println(inputMoney + "원");
-		//System.out.println("구매할 상품명을 입력해 주세요.");
 	}
-	
-	//거슬러주는 잔돈 출력
-	public static void changesPrint(/*배열값으로 받기*/) {
+
+	// 거슬러주는 잔돈 출력
+	public static void changesPrint(HashMap<Coin, Integer> coinMap) {
 		System.out.println("잔돈");
-		//배열을 풀어서 프린트하기 
-		
-	} 
+		for (Coin money : Coin.values()) {
+			if (coinMap.get(money) > 0) {
+				System.out.println(money.getAmount() + "원 - " + coinMap.get(money) + "개");
+			}
+		}
+		// 배열을 풀어서 프린트하기
+
+	}
 }
