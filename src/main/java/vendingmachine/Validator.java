3694e1ec4b8c74a476958a04f00c9bbbc5574409
Dong Geun <dlehdrms978@gmail.com>
2021-12-13 20:49:15 +0900

    투입 금액에 대한 입력 검증 구현 완료

--- src/main/java/vendingmachine/Validator.java
+++ src/main/java/vendingmachine/Validator.java
@@ -7,7 +7,7 @@ public class Validator {
     public void validateAmount(String amount){
         int parsedAmount = -1;
         try {
-            parsedAmount = Integer.parseInt(amount);
+            parsedAmount = Integer.parseInt(amount.trim());
         }catch (Exception e){
             System.out.println("[ERROR] 정수로 입력 해주세요");
             throw new IllegalArgumentException();
@@ -38,9 +38,24 @@ public class Validator {
             checkGoodsInfo(goodsInfo[1],goodsInfo[2]);
         }
     }
+    public void validateInputMoney(String money){
+        int parsedAmount = -1;
+        try {
+            parsedAmount = Integer.parseInt(money.trim());
+        }catch (Exception e){
+            System.out.println("[ERROR] 정수로 입력 해주세요");
+            throw new IllegalArgumentException();
+        }
+        if(parsedAmount < 10) {
+            System.out.println("[ERROR] 10보다 큰 수를 입력해 주세요");
+            throw new IllegalArgumentException();
+        }
+        if(parsedAmount % 10 !=0) {
+            System.out.println("[ERROR] 10으로 나누어 떨어지는 수를 입력해 주세요");
+            throw new IllegalArgumentException();
+        }
+    }
     private void checkGoodsInfo(String price, String number){
-        System.out.println("price = " + price);
-        System.out.println("number = " + number);
         try{
             Integer.parseInt(price.trim());
             Integer.parseInt(number.trim());
@@ -57,4 +72,5 @@ public class Validator {
             throw new IllegalArgumentException();
         }
     }
+
 }
