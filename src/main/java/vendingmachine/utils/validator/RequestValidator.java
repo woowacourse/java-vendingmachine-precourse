a21c8a5348b890354da46a597348bf71caf51cd5
wooseok <diddmlvkf@gmail.com>
2021-12-13 16:09:36 +0900

    feat(RequestValidator) : 상품 목록 정규식 구현

--- src/main/java/vendingmachine/utils/validator/RequestValidator.java
+++ src/main/java/vendingmachine/utils/validator/RequestValidator.java
@@ -1,8 +1,11 @@
 package vendingmachine.utils.validator;
 
 public class RequestValidator {
+	private static final String REGEX = "\\[[a-zA-Z0-9가-힣]+,\\d{3,}+,\\d+]";
+
 	private static final String IS_NUMBER_ERROR_MESSAGE = "입력값은 양수여야 한다.";
 	private static final String IS_EMPTY_ERROR_MESSAGE = "입력값은 빈 값이면 안된다.";
+	private static final String IS_MATCH_REGEX_TO_PRODUCT_ERROR_MESSAGE = "상품 입력 정규식에 맞아야 한다.";
 
 	public static void isNumber(String request) throws IllegalArgumentException {
 		try {
@@ -15,6 +18,12 @@ public class RequestValidator {
 	public static void isEmpty(String request) throws IllegalArgumentException {
 		if (request.isEmpty()) {
 			throw new IllegalArgumentException(IS_EMPTY_ERROR_MESSAGE);
+		}
+	}
+
+	public static void isMatchRegexToProduct(String request) throws IllegalArgumentException {
+		if (!request.matches(REGEX)) {
+			throw new IllegalArgumentException(IS_MATCH_REGEX_TO_PRODUCT_ERROR_MESSAGE);
 		}
 	}
 }
--- src/test/java/vendingmachine/utils/validator/RequestValidatorTest.java
+++ src/test/java/vendingmachine/utils/validator/RequestValidatorTest.java
@@ -2,6 +2,8 @@ package vendingmachine.utils.validator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -20,5 +22,14 @@ public class RequestValidatorTest {
 		assertThatThrownBy(() -> RequestValidator.isEmpty(""))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("입력값은 빈 값이면 안된다.");
+	}
+
+	@DisplayName("상품 목록 입력 정규식")
+	@ParameterizedTest
+	@ValueSource(strings = {"[콜라,1000,10", "콜라,1000,10]", "[콜라,100010]", "[콜라1000,10]"})
+	void isMatchRegexToProduct(String request) {
+		RequestValidator.isMatchRegexToProduct("[콜라,1000,10]");
+		assertThatThrownBy(() -> RequestValidator.isMatchRegexToProduct(request))
+			.isInstanceOf(IllegalArgumentException.class);
 	}
 }
