4b4da5f4c16d93e66b6dacd6b284beb67490bd39
cjlee38 <gptpem38@gmail.com>
2021-12-14 00:58:28 +0900

    test(changesafe): 잔돈 돌려받는 서비스 테스트 추가

---
+++ src/test/java/vendingmachine/job/ChangeBackJobTest.java
@@ -0,0 +1,28 @@
+package vendingmachine.job;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import vendingmachine.ConsoleTest;
+import vendingmachine.Rollback;
+import vendingmachine.config.JobConfig;
+
+class ChangeBackJobTest extends ConsoleTest implements Rollback {
+
+	ChangeBackJob job = JobConfig.getChangeBackJob();
+
+	@Test
+	@DisplayName("잔돈을 돌려준다.")
+	public void testChangeBackJob() {
+	    // when
+		job.execute();
+	    // then
+		assertEquals("잔돈\n"
+			+ "500원 - 0개\n"
+			+ "100원 - 4개\n"
+			+ "50원 - 1개\n"
+			+ "10원 - 0개\n", outputStream.toString());
+	}
+}
--- src/test/java/vendingmachine/service/ChangeSafeServiceTest.java
+++ src/test/java/vendingmachine/service/ChangeSafeServiceTest.java
@@ -2,35 +2,53 @@ package vendingmachine.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import vendingmachine.Rollback;
 import vendingmachine.config.ServiceConfig;
-import vendingmachine.domain.Coin;
-import vendingmachine.domain.Quantity;
+import vendingmachine.domain.Money;
 
 class ChangeSafeServiceTest implements Rollback {
 
-	ChangeSafeService changeSafeService = ServiceConfig.getChangeSafeService();
+	ChangeSafeService service = ServiceConfig.getChangeSafeService();
 
 	@Test
-	@DisplayName("코인맵으로 금고를 생성한다.")
+	@DisplayName("금액으로 잔돈 금고를 생성한다.")
 	public void testPersist() {
 		// given
-		Map<Coin, Quantity> coinMap = new LinkedHashMap<>();
-		coinMap.put(Coin.COIN_500, new Quantity(1));
-		coinMap.put(Coin.COIN_100, new Quantity(3));
-
+		Money money = new Money(1000);
 		// when
-		String persist = changeSafeService.persist(coinMap);
+		String changeSafe = service.generateChangeSafe(money);
 		// then
-		assertEquals("500원 - 1개\n"
+		assertTrue(changeSafe.contains("500원"));
+	}
+
+	@Test
+	@DisplayName("잔돈을 돌려받는다.")
+	public void testGiveChange() {
+		// given
+		Money money = new Money(300);
+		// when
+		String change = service.giveChange(money);
+		// then
+		assertEquals("500원 - 0개\n"
 			+ "100원 - 3개\n"
 			+ "50원 - 0개\n"
-			+ "10원 - 0개", persist);
+			+ "10원 - 0개", change);
+	}
+
+	@Test
+	@DisplayName("반환할 수 있는 잔돈만 반환한다.")
+	public void testGiveChangeOver() {
+		// given
+		Money money = new Money(500);
+		// when
+		String change = service.giveChange(money);
+		// then
+		assertEquals("500원 - 0개\n"
+			+ "100원 - 4개\n"
+			+ "50원 - 1개\n"
+			+ "10원 - 0개", change);
 	}
 }
