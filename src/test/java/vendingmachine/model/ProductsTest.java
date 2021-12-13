50a64b1548ed96b2bfce16397757aec1e258c133
yeon-06 <rnjstldus2@hs.ac.kr>
2021-12-13 14:45:56 +0900

    test: 메소드 변경으로 인한 테스트 코드 변경

--- src/test/java/vendingmachine/model/ProductsTest.java
+++ src/test/java/vendingmachine/model/ProductsTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class ProductsTest {
     private static Products products;
@@ -23,16 +23,21 @@ class ProductsTest {
 
     @Test
     void 상품_정상_구매() {
-        assertThat(products.buyProduct("콜라", 5000)).isTrue();
+        products.buyProduct("콜라", 5000);
+        assertThatNoException();
     }
 
     @Test
     void 투입금액_부족() {
-        assertThat(products.buyProduct("콜라", 1000)).isFalse();
+        assertThatThrownBy(() -> {
+            products.buyProduct("콜라", 1000);
+        }).isInstanceOf(IllegalStateException.class);
     }
 
     @Test
     void 존재하지_않는_상품() {
-        assertThat(products.buyProduct("유니콘", 5000)).isFalse();
+        assertThatThrownBy(() -> {
+            products.buyProduct("유니콘", 5000);
+        }).isInstanceOf(IllegalStateException.class);
     }
 }
