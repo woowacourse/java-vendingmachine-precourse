5f0661961239e44f7560eb57b5f3e322dbea9029
audgnssweet <audgnssweet@naver.com>
2021-12-13 20:24:23 +0900

    test: beverage생성시 validation의 테스트코드 작성

--- src/test/java/vendingmachine/model/BeverageTest.java
+++ src/test/java/vendingmachine/model/BeverageTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 public class BeverageTest {
 
@@ -28,6 +30,30 @@ public class BeverageTest {
 	void 음료_판매_전부다_팔려_실패() {
 		Beverage beverage = new Beverage("사이다", Money.from(1500), 0);
 		assertThatThrownBy(() -> beverage.sell(Money.from(1000)))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@ParameterizedTest
+	@NullAndEmptySource
+	void 이름이_비어있어_생성_실패(String name) {
+		assertThatThrownBy(() -> new Beverage(name, Money.from(100), 10))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	void 생성_성공() {
+		assertDoesNotThrow(() -> new Beverage("콜라", Money.from(100), 0));
+	}
+
+	@Test
+	void 가격이_너무작아_생성_실패() {
+		assertThatThrownBy(() -> new Beverage("콜라", Money.from(99), 10))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	void 개수가_너무적어_생성_실패() {
+		assertThatThrownBy(() -> new Beverage("콜라", Money.from(100), -1))
 			.isInstanceOf(IllegalArgumentException.class);
 	}
 }
