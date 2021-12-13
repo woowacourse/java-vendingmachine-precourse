ab7f2d9a0a7f8249a6554019500e79ceebf693bb
Seungmin Lim <seungmin9066@gmail.com>
2021-12-13 18:26:30 +0900

    [추가] item기능을 잘 사용할 수 있도록 여러 메서드를 제공

--- src/main/java/vendingmachine/domain/Item.java
+++ src/main/java/vendingmachine/domain/Item.java
@@ -1,2 +1,34 @@
-package vendingmachine.domain;public class Item {
+package vendingmachine.domain;
+
+public class Item {
+	private final String name;
+	private final int price;
+	private int stocks;
+
+	public Item(String name, int price, int stocks) {
+		this.name = name;
+		this.price = price;
+		this.stocks = stocks;
+	}
+
+	public static Item of(String name, int price, int stocks) {
+		return new Item(name, price, stocks);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void sell() {
+		stocks -= 1;
+	}
+
+	public boolean isNotSoldOut() {
+		return stocks != 0;
+	}
+
 }
