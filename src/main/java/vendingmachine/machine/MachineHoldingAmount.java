3ac8bfbaa45ddd48991c83e926238d1b095029d0
iamsseaa <homingsea@gmail.com>
2021-12-13 10:50:15 +0900

    feat: 자판기 보유 금액으로 잔돈 만드는 클래스 분리

---
+++ src/main/java/vendingmachine/machine/MachineHoldingAmount.java
@@ -0,0 +1,56 @@
+package vendingmachine.machine;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MachineHoldingAmount {
+    Coin[] coins = Coin.values();
+    Machine machine;
+    public int currentAmount = 0;
+
+    MachineHoldingAmount(Machine machine) {
+        this.machine = machine;
+    }
+
+    public void setCurrentAmount(int amount) {
+        this.currentAmount += amount;
+    }
+
+    public int pickRandomCoin() {
+        int random = Randoms.pickNumberInList(new ArrayList<Integer>(Arrays.asList(500,100,50,10)));
+
+        return random;
+    }
+
+    public boolean checkIsRightRandom(int amount) {
+        if (this.currentAmount + amount <= machine.getHoldingAmount()) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkIsFullCurrentAmount() {
+        return this.currentAmount == machine.getHoldingAmount();
+    }
+
+    public Coin compareCoin(int amount) {
+        for (Coin coin : coins) {
+            if (coin.getCoinAmount() == amount) {
+                return coin;
+            }
+        }
+        return coins[0];
+    }
+
+
+    public void makeCoins() {
+        while (!checkIsFullCurrentAmount()) {
+            int random = pickRandomCoin();
+            if (checkIsRightRandom(random)) {
+                compareCoin(random).setCoinNumber();
+            }
+        }
+    }
+}
