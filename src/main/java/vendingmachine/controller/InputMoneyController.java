31cff728bb423b8f3ed98dfbf621b85c43202581
yangdongjue5510 <djwhy5510@gmail.com>
2021-12-13 10:49:53 +0900

    feat: 입력된 보유금액 저장

--- src/main/java/vendingmachine/controller/InputMoneyController.java
+++ src/main/java/vendingmachine/controller/InputMoneyController.java
@@ -1,15 +1,22 @@
 package vendingmachine.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import vendingmachine.model.InputMoneyModel;
 import vendingmachine.validator.InputMoneyValidator;
 import vendingmachine.view.InputMoneyView;
 
 public class InputMoneyController {
 	public void inputVendingMachineMoney() {
 		InputMoneyView.messageInputMoney();
+		String inputMoney = inputUntilValid();
+		InputMoneyModel.saveMoney(inputMoney);
+	}
+
+	private String inputUntilValid() {
 		String inputLine;
 		do {
 			inputLine = Console.readLine();
 		} while (!InputMoneyValidator.checkInputMoney(inputLine));
+		return inputLine;
 	}
 }
---
+++ src/main/java/vendingmachine/model/InputMoneyModel.java
@@ -0,0 +1,9 @@
+package vendingmachine.model;
+
+public class InputMoneyModel {
+	private static long inputMoney;
+
+	public static void saveMoney(String string) {
+		inputMoney = Long.parseLong(string);
+	}
+}
