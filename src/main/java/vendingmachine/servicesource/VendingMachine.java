f684f2eb2ae620137e1f49481aed451d0ad7064f
overnew <ehdls1303@gmail.com>
2021-12-13 20:43:09 +0900

    feat: VendingMachine class 구현
    MachineStock, WalletSystem, WalletPrinter을 인스턴트 필드로 저장해 활용
    
    void runStart()
     - 자판기 기능 시작을 총괄하는 함수
    
    machineInitialize()
     - 초기 필드를 생성해 초기화
    
    void sellStart()
     - 재고가 없거나 잔액이 부족할때까지 물건을 판매
    
    void sellOneProduct()
     - 판매될 물건 한개를 선택 받음
    
    void sellSelectedProduct(Product selectedProduct)
     - 선택된 물건을 판매하는 기능

---
+++ src/main/java/vendingmachine/servicesource/VendingMachine.java
@@ -0,0 +1,48 @@
+package vendingmachine.servicesource;
+
+import vendingmachine.utils.moneychecker.BalanceInputChecker;
+
+public class VendingMachine {
+    private MachineStock stock;
+    private WalletSystem wallet;
+    private WalletPrinter walletPrinter;
+
+    public void runStart(){
+        machineInitialize();
+        sellStart();
+    }
+
+    private void machineInitialize(){
+        wallet = new WalletSystem(BalanceInputChecker.getBalance());
+        walletPrinter = wallet.getWalletPrinter();
+
+        stock = new MachineStock();
+    }
+
+    private void sellStart(){
+        wallet.setInsertedBalance();
+
+        while(!stock.checkIsOutOfAllStock() && wallet.haveEnoughInsertedBalanceToPurchase(stock.getCheapestPrice())){
+            sellOneProduct();
+        }
+
+        wallet.returnChangeByRemainCoins();
+    }
+
+    private void sellOneProduct(){
+        Product selectedProduct;
+        walletPrinter.printRemainInsertedBalance();
+
+        do{
+            selectedProduct = stock.getSelectedProduct();
+        }while(!wallet.canBuySelectedProduct(selectedProduct));
+
+        sellSelectedProduct(selectedProduct);
+    }
+
+    private void sellSelectedProduct(Product selectedProduct){
+        wallet.withdrawInsertedBalance(selectedProduct.getPrice());
+        stock.takeOutSelectedProduct(selectedProduct);
+    }
+
+}
