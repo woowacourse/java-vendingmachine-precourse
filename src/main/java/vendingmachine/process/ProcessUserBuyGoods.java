114e6d3ebd8f82f1b65afe1dbeee4717f2a44c88
iamsseaa <homingsea@gmail.com>
2021-12-13 13:01:40 +0900

    feat: 투입 금액 입력 관련 모든 처리 과정

--- src/main/java/vendingmachine/process/ProcessUserBuyGoods.java
+++ src/main/java/vendingmachine/process/ProcessUserBuyGoods.java
@@ -1,4 +1,18 @@
 package vendingmachine.process;
 
 public class ProcessUserBuyGoods {
+    // 반복해서 투입 금액 받아서 물건 사기
+    // 투입 금액 예외처리 - ProcessPrepareUserMoney
+    // 상품명 받으면 예외처리
+    // README.md 에서 상품 구매 관련 상품 기능 구현
+    // 인자로 goods list 받아서 상품 개수 줄이기
+    //
+
+    public static int userMoney;
+
+    public static void makeUserMoney() {
+        ProcessPrepareUserMoney.inputUserMoney();
+        ProcessPrepareUserMoney.checkUserMoney();
+        userMoney = ProcessPrepareUserMoney.toIntegerUserMoney();
+    }
 }
