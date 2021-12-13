4cb8463700177bb47f2a375e7a4ca26cacc93a8d
yukong <kyb062666@gmail.com>
2021-12-13 15:25:13 +0900

    chore(Print): 사용하지 않는 메소드 및 상수 삭제

--- src/main/java/vendingmachine/view/Print.java
+++ src/main/java/vendingmachine/view/Print.java
@@ -9,10 +9,6 @@ public class Print {
 
 	private static final String INSERT_PRODUCTS = "상품명과 가격, 수량을 입력해 주세요.";
 
-	// TODO: 사용하지 않는 메소드 및 상수 삭제
-	private static final String PRODUCT_NOT_SAVE = "해당 	상품의 정보가 올바르지 않아 등록되지 않았습니다 : ";
-	private static final String PRODUCT_DUPLICATE = "해당 상품의 이름이 이전에 등록된 상품의 이름과 동일하여 등록되지 않았습니다 : ";
-
 	private static final String INSERT_MONEY = "투입 금액을 입력해 주세요.";
 	private static final String REMAIN_MONEY = "투입 금액: ";
 
@@ -42,14 +38,6 @@ public class Print {
 	public static void printInsertProducts() {
 		System.out.println();
 		System.out.println(INSERT_PRODUCTS);
-	}
-
-	public static void printProductNotSave(String name) {
-		System.out.println(PRODUCT_NOT_SAVE + name);
-	}
-
-	public static void printDuplicatedProductName(String name) {
-		System.out.println(PRODUCT_DUPLICATE + name);
 	}
 
 	public static void printInsertMoney() {
