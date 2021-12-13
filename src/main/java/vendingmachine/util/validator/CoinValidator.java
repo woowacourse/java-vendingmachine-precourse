d45a8cfa077356d08357b9d51df240598c7603d6
dongho <dongho108@naver.com>
2021-12-13 23:37:50 +0900

    style: 빈 라인 제거

--- src/main/java/vendingmachine/util/validator/CoinValidator.java
+++ src/main/java/vendingmachine/util/validator/CoinValidator.java
@@ -1,7 +1,6 @@
 package vendingmachine.util.validator;
 
 public class CoinValidator extends Validator {
-
 	public static void isRightCoin(String input) throws IllegalArgumentException {
 		isEmpty(input);
 		isNumber(input);
