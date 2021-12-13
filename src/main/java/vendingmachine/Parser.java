474d2eef37df3e7ae6ffe4e810256dc91fe33ba0
Dong Geun <dlehdrms978@gmail.com>
2021-12-13 20:49:43 +0900

    파싱의 결과 타입을 list에서 map으로 변경

--- src/main/java/vendingmachine/Parser.java
+++ src/main/java/vendingmachine/Parser.java
@@ -1,21 +1,25 @@
 package vendingmachine;
 
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Parser {
     public Parser(){
 
     }
-    public List<Goods> parseGoods(String input){
-        List<Goods> goodsList = new ArrayList<>();
+    public Map<String,Goods> parseGoods(String input){
+        Map<String,Goods> goodsList = new HashMap<>();
         String[] list = input.split(";");
         for(String goodsInfo : list){
             goodsInfo = goodsInfo.replace("[","");
             goodsInfo = goodsInfo.replace("]","");
             String[] goodsInfoList = goodsInfo.split(",");
-            Goods goods = new Goods(goodsInfoList[0],Integer.parseInt(goodsInfoList[1]),Integer.parseInt(goodsInfoList[2]));
-            goodsList.add(goods);
+            String goodsName = goodsInfoList[0];
+            int price = Integer.parseInt(goodsInfoList[1]);
+            int number = Integer.parseInt(goodsInfoList[2]);
+            Goods goods = new Goods(goodsName,price,number);
+            goodsList.put(goodsName,goods);
         }
         return goodsList;
     }
