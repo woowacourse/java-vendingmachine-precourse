eb7d6a97c629a374294d9422e5f372572a7578eb
KIM <km0192@naver.com>
2021-12-13 16:50:26 +0900

    refactor(Application) : 상품리스트 생성까지 Application에 반영

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -6,12 +6,15 @@ import userInterface.ProductListManager;
 public class Application {
     public static void main(String[] args) {
         /* TODO : 프로그램 구현
-         * 3. 상품 리스트 생성 (입력 받기 -> 에러체크 넘을때까지 반복 -> 생성)
          * 4. 투입 금액 (입력 받기 -> 에러체크 넘을때까지 반복 -> 생성)
          * 5. 자판기 돌리기 (남은금액 계산 -> 주문받기(에러체크->반복) -> 잔고 출력)
          * 6. 잔여 금액 반환(출력)
          */
-        ProductListManager productListManager = new ProductListManager();
+        Coins coinList = new Coins();
+        CoinListManager coinListManager = new CoinListManager(coinList);
+        coinListManager.makeRandomList();
+        coinListManager.printCoinList();
+        Products productList = new ProductListManager().getList();
 
     }
 }
