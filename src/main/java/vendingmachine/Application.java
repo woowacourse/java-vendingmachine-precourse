45d957f79276b281bc9b7bb0e9ceef676d53a7de
her0807 <her0807@naver.com>
2021-12-13 17:40:19 +0900

    feat: 자판기 동작 시작 기능
    - MachineController 를 통해서 운영
    - run() 으로 시작
    - buyCycle() 판매할 수 있는 상황까지 구매 요청
    - buy() 실질적인 구매 기능
    - initChange() 초기에 잔동 생성

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,7 +1,11 @@
 package vendingmachine;
 
+import vendingmachine.controller.MachineController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        MachineController machine = new MachineController();
+        machine.run();
     }
 }
---
+++ src/main/java/vendingmachine/controller/MachineController.java
@@ -0,0 +1,51 @@
+package vendingmachine.controller;
+
+import vendingmachine.model.Machine;
+import vendingmachine.model.coin.Change;
+import vendingmachine.model.coin.MachineMoney;
+import vendingmachine.model.drink.DrinkMapper;
+import vendingmachine.model.drink.Drinks;
+import vendingmachine.model.user.ChoiceDrink;
+import vendingmachine.model.user.UserMoney;
+import vendingmachine.util.Message;
+import vendingmachine.view.Input;
+import vendingmachine.view.Output;
+
+public class MachineController {
+    private Change change;
+
+    public void run() {
+        initChange();
+        Drinks drinks = Input.drinks(new DrinkMapper());
+        UserMoney userMoney = Input.userMoney();
+        Machine machine = new Machine(drinks, userMoney);
+        buyCycle(machine);
+        Output.changeStatus(change.createChange(userMoney));
+    }
+
+    private void buyCycle(Machine machine) {
+        while (machine.isContinue()) {
+            buy(machine);
+        }
+        Output.joinMessage(Message.USER_MONEY_STATUS, machine.toMoney());
+        Output.guideMessage(Message.CHANGE_STATUS);
+    }
+
+    private void buy(Machine machine) {
+        try {
+            Output.joinMessage(Message.USER_MONEY_STATUS, machine.toMoney());
+            ChoiceDrink buyItem = Input.choiceDrink();
+            machine.buy(buyItem);
+        } catch (IllegalArgumentException e) {
+            Output.errorMessage(e.getMessage());
+            buy(machine);
+        }
+    }
+
+    private void initChange() {
+        MachineMoney machineMoney = Input.machineMoney();
+        this.change = new Change(machineMoney);
+        Output.guideMessage(Message.COIN_STATUS);
+        Output.changeStatus(change.getCoins());
+    }
+}
