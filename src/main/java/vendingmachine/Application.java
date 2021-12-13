9849d7c17f10e98c2979a99efd06749ce03597af
chawani <60432062+chawani@users.noreply.github.com>
2021-12-13 16:02:08 +0900

    feat: 자판기 셋팅 및 작동 구현

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,7 +1,22 @@
 package vendingmachine;
 
+import vendingmachine.controller.VendingMachineController;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	private static final VendingMachineController controller = new VendingMachineController();
+
+	public static void main(String[] args) {
+		settingVendingMachine();
+		operateMachine();
+	}
+
+	private static void settingVendingMachine() {
+		controller.settingMachineChanges();
+		controller.settingMerchandise();
+	}
+
+	private static void operateMachine() {
+		controller.putMoneyIntoMachine();
+		controller.chooseUntilReturnBalance();
+	}
 }
