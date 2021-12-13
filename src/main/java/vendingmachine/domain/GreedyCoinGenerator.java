908f096eb57c4dd24cfb24bf5765a1b4ea11481c
cjlee38 <gptpem38@gmail.com>
2021-12-14 01:12:50 +0900

    chore: 삭제된 파일 track

---
+++ src/main/java/vendingmachine/domain/GreedyCoinGenerator.java
@@ -0,0 +1,10 @@
+package vendingmachine.domain;
+
+import java.util.Map;
+
+public class GreedyCoinGenerator implements CoinGenerator {
+	@Override
+	public Map<Coin, Quantity> generate(Money money) {
+		return null;
+	}
+}
