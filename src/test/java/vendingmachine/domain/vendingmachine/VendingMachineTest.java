1e22328305ceb6d2d1e57a8cac920cd7c58b3672
GYUTAEKIM <kkt219a@gmail.com>
2021-12-13 17:38:07 +0900

    test: 자판기 사용 테스트코드 작성
    - 자판기를 직접사용하며 정상적으로 작동하는지 테스트코드를 작성

---
+++ src/test/java/vendingmachine/domain/vendingmachine/VendingMachineTest.java
@@ -0,0 +1,36 @@
+package vendingmachine.domain.vendingmachine;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import vendingmachine.domain.coin.Coins;
+import vendingmachine.domain.coin.RandomCoinGenerator;
+import vendingmachine.domain.money.Money;
+import vendingmachine.domain.product.Product;
+import vendingmachine.domain.product.Products;
+
+class VendingMachineTest {
+	Products products = Products.from();
+
+	public void initializeProducts() {
+		products.add(Product.of("사이다","1000","2"));
+		products.add(Product.of("환타","1250","10"));
+		products.add(Product.of("밀키스","1700","10"));
+		products.add(Product.of("콜라","1500","10"));
+	}
+
+	@Test
+	public void useVendingMachine() {
+		initializeProducts();
+		VendingMachine vendingMachine = new VendingMachine();
+		vendingMachine.addCoinAll(new RandomCoinGenerator().generate(Money.of(490)));
+		vendingMachine.addProductAll(products);
+		vendingMachine.insert(Money.of(3000));
+		Assertions.assertTrue(vendingMachine.isPurchasable());
+		vendingMachine.purchase("사이다");
+		vendingMachine.purchase("사이다");
+		Assertions.assertFalse(vendingMachine.isPurchasable());
+		Coins coins = vendingMachine.returnChange();
+		System.out.println(coins);
+	}
+}
