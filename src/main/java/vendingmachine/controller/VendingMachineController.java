6521f5ca6456004d9b825acf4e8ed6c3d43a5feb
Wishoon <heonga2@gmail.com>
2021-12-13 14:13:06 +0900

    feat: 자판기의 상품들 입력과 생성의 컨트롤러 기능 추가

--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -1,6 +1,7 @@
 package vendingmachine.controller;
 
 import vendingmachine.domain.coin.Coins;
+import vendingmachine.domain.product.Products;
 import vendingmachine.view.InputView;
 import vendingmachine.view.OutputView;
 
@@ -14,6 +15,17 @@ public class VendingMachineController {
 		} catch (IllegalArgumentException e) {
 			OutputView.printErrorMessage(e);
 			return registerCoins();
+		}
+	}
+
+	private Products registerProducts() {
+		try {
+			final Products products = Products.from(InputView.getInputProducts());
+			OutputView.printLineSeparator();
+			return products;
+		} catch (IllegalArgumentException e) {
+			OutputView.printErrorMessage(e);
+			return registerProducts();
 		}
 	}
 }
