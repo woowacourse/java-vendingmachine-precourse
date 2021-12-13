e0431ac55c98b5970f6802e54470926f868b3ae2
KIM <km0192@naver.com>
2021-12-13 15:20:15 +0900

    feat(Products) : 최저가 상품 구하는 메소드 getMinPrice

--- src/main/java/vendingmachine/Products.java
+++ src/main/java/vendingmachine/Products.java
@@ -1,2 +1,18 @@
-package utils.validator;public class Products {
+package vendingmachine;
+
+public class Products extends Remaining<Product> {
+	private int minimumPrice = Integer.MAX_VALUE;
+
+	public int getMinPrice() {
+		minimumPrice = Integer.MAX_VALUE;
+		this.keySet().forEach(this::changeMinPrice);
+		return minimumPrice;
+	}
+
+	private void changeMinPrice(Product product) {
+		int thisPrice = product.getPrice();
+		if (thisPrice <= minimumPrice && !isEmpty(product)) {
+			minimumPrice = thisPrice;
+		}
+	}
 }
