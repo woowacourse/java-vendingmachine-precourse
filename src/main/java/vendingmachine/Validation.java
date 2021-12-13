28346c1dad7aaf2e8c1708c8665fb58d8ef44f2d
SheepEatLion <gowjr207@gmail.com>
2021-12-14 00:21:55 +0900

    refactor : Validation.java
    - 역할 분리
    입력받은 상품명을 파싱하는 것 또한 검증의 과정이라고 판단하여 기존의 한 메서드에서 진행하던 파싱을 stringParsingToList() 메서드를 추가하여 기능을 분리하고 검증 또한 inputProductValidation() 메서드를 추가하여 분리하였음.

--- src/main/java/vendingmachine/Validation.java
+++ src/main/java/vendingmachine/Validation.java
@@ -1,12 +1,14 @@
 package vendingmachine;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
-import static vendingmachine.Application.USER_MONEY;
-
 public class Validation {
-
+    static int PRODUCT_NAME = 0;
+    static int PRODUCT_PRICE = 1;
+    static int PRODUCT_AMOUNT = 2;
+    static boolean VALIDATION_SUCCESS = true;
 
     public static void productNotFound(Product purchaseProductObject){
         if(purchaseProductObject == null){
@@ -18,35 +20,58 @@ public class Validation {
             throw new IllegalArgumentException("[ERROR] 상품의 재고가 없습니다.");
         }
     }
-    public static void userMoneyNotEnough(Product purchaseProductObject) {
-        if (purchaseProductObject.getPrice() > USER_MONEY) {
+    public static void userMoneyNotEnough(Product purchaseProductObject, int userOwnMoney) {
+        if (purchaseProductObject.getPrice() > userOwnMoney) {
             throw new IllegalArgumentException("[ERROR] 투입 금액이 부족합니다.");
         }
     }
 
-    public static boolean isUserMoneyAndMachineInventoryEnough(ArrayList<Product> products){
-        System.out.println("투입 금액: " + USER_MONEY + "원");
+    public static boolean isUserMoneyAndMachineInventoryEnough(ArrayList<Product> products, int userOwnMoney){
         boolean enough = false;
-        int amountCount = 0;
+        int vendingMachineInventory = 0;
+        View.noticeMsgOnUserOwnMoney(userOwnMoney);
         for(Product product: products){
-            if(product.getPrice() <= USER_MONEY){    // 현재 가진 금액으로 살 수 있는 음료가 하나라도 있는지 확인
+            if(product.getPrice() <= userOwnMoney){    // 현재 가진 금액으로 살 수 있는 음료가 하나라도 있는지 확인
                 enough = true;
             }
-            amountCount += product.getAmount();     // 자판기의 재고가 하나라도 있는지 확인
+            vendingMachineInventory += product.getAmount();     // 자판기의 재고가 하나라도 있는지 확인
         }
-        if(amountCount == 0){
+        if(vendingMachineInventory == 0){
             enough = false;
         }
         return enough;
     }
 
-    public static void userMoneyValidation(String userMoney){
-        String regex = "^[0-9]*$";
-        if (!Pattern.matches(regex, userMoney)) throw new IllegalArgumentException("[ERROR] 금액은 숫자여야 합니다.");
+    public static void userMoneyValidation(String userOwnMoney){
+        String regex = "^[0-9]{2,}$";
+        if (!Pattern.matches(regex, userOwnMoney)) throw new IllegalArgumentException("[ERROR] 금액은 숫자여야 합니다.");
     }
 
-    public static void inputMachineOwnMoneyAndValidation(String machineOwnMoney){
+    public static void machineOwnMoneyValidation(String machineOwnMoney){
         String regex = "^[0-9]{2,}$";
         if (!Pattern.matches(regex, machineOwnMoney)) throw new IllegalArgumentException("[ERROR] 금액은 숫자여야 합니다.");
     }
+
+    public static void inputProductValidation(ArrayList<String> stringList){
+        String regex = "^[0-9]*$";
+        for(int i = 0; i < stringList.size() - 2; i += 3){
+            if (!Pattern.matches(regex, stringList.get(i+PRODUCT_PRICE))) throw new IllegalArgumentException("[ERROR] 가격은 숫자여야 합니다.");
+            if (!Pattern.matches(regex, stringList.get(i+PRODUCT_AMOUNT))) throw new IllegalArgumentException("[ERROR] 수량은 숫자여야 합니다.");
+            if (stringList.get(i+PRODUCT_AMOUNT).equals("0")) throw new IllegalArgumentException("[ERROR] 수량은 한 개 이상이어야 합니다.");
+        }
+    }
+
+    public static ArrayList<String> stringParsingToList(String productString){
+        ArrayList<String> totalProduct = new ArrayList<>();
+        if(!productString.startsWith("[") || !productString.endsWith("]")) throw new IllegalArgumentException("[ERROR] 상품 정보 입력 형식이 올바르지 않습니다.");
+        for(String pickOneProduct: productString.split(";")){
+            String[] oneProductSplit = pickOneProduct.split(",");
+            if(oneProductSplit.length != 3) throw new IllegalArgumentException("[ERROR] 상품 정보 입력 형식이 올바르지 않습니다.");
+            oneProductSplit[PRODUCT_NAME] = oneProductSplit[PRODUCT_NAME].substring(1);
+            oneProductSplit[PRODUCT_AMOUNT] = oneProductSplit[PRODUCT_AMOUNT].substring(0, oneProductSplit[PRODUCT_AMOUNT].length()-1);
+            totalProduct.addAll(Arrays.asList(oneProductSplit));
+        }
+        return totalProduct;
+    }
+
 }
