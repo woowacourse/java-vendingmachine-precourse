259458d93413d617ee78b801b45334931d39ed58
koo <gudonghee2000@gmail.com>
2021-12-13 17:27:46 +0900

    refactor : InputDisplay클래스의 상수를 생성하고 함수명 변경

--- src/main/java/view/InputDisplay.java
+++ src/main/java/view/InputDisplay.java
@@ -1,26 +1,29 @@
 package view;
 
 public class InputDisplay {
-	private static final String ASK_INPUT_VENDING_MACHINE_CHANGE_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+	private static final String ASK_INPUT_CHANGE_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+	private static final String ASK_INPUT_PRODUCTS_MESSAGE = "상품명과 가격, 수량을 입력해 주세요.";
+	private static final String ASK_INPUT_INSERTED_MONEY_MESSAGE = "투입 금액을 입력해 주세요.";
+	private static final String ASK_INPUT_PRODUCT_TO_BUY_MESSAGE = "구매할 상품명을 입력해 주세요.";
 
 	private InputDisplay() {
 	}
 
-	public static void askInputVendingMachineChange() {
-		System.out.println(ASK_INPUT_VENDING_MACHINE_CHANGE_MESSAGE);
+	public static void askInputChange() {
+		System.out.println(ASK_INPUT_CHANGE_MESSAGE);
 	}
 
-	public static void askInputVendingMachineProduct() {
+	public static void askInputProducts() {
 		System.out.println();
-		System.out.println("상품명과 가격, 수량을 입력해 주세요.");
+		System.out.println(ASK_INPUT_PRODUCTS_MESSAGE);
 	}
 
-	public static void askInputUserInsertMoney() {
+	public static void askInputInsertedMoney() {
 		System.out.println();
-		System.out.println("투입 금액을 입력해 주세요.");
+		System.out.println(ASK_INPUT_INSERTED_MONEY_MESSAGE);
 	}
 
-	public static void askInputProductNameToBuy() {
-		System.out.println("구매할 상품명을 입력해 주세요.");
+	public static void askInputProductToBuy() {
+		System.out.println(ASK_INPUT_PRODUCT_TO_BUY_MESSAGE);
 	}
 }
