a4eb0293111532e5f63b5b6056df3398bce8567e
summerlunaa <rhgus1085@gmail.com>
2021-12-13 18:07:12 +0900

    refactor: CoinsController 삭제 후 VendingMachineController에 병합

--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -1,9 +1,27 @@
 package vendingmachine.controller;
 
+import vendingmachine.domain.Coins;
+import vendingmachine.domain.Products;
+import vendingmachine.domain.VendingMachine;
 import vendingmachine.validator.Validator;
 import vendingmachine.view.Input;
 
 public class VendingMachineController {
+	private VendingMachine vendingMachine;
+
+	public VendingMachineController() {
+		Coins coins = getCoins(getMoneyInMachine());
+		Products products = ProductsController.getProducts();
+		int inputMoney = getInputMoney();
+		vendingMachine = new VendingMachine(coins, products, inputMoney);
+	}
+
+	private static Coins getCoins(int moneyInMachine) {
+		Coins coins = new Coins(moneyInMachine);
+		coins.makeRandomNumberOfCoins();
+		return coins;
+	}
+
 	private static int getMoneyInMachine() {
 		int moneyInMachine;
 		do {
