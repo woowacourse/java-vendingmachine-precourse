3e3320db751c0ee9ff1af7444647d540e227a114
skullKim <kyk990328@gmail.com>
2021-12-14 00:34:15 +0900

    refactor(VendingMachineOutput.java): 결과 데이터를 받는 방식 수정
    결과를 출력하기 위해 결과를 받는 방식 수정
    
    BREAKING CHANGE: 결과를 받는 방식을 getter에서 파라미터로 수정
    
    as-is:
    public class VendingMachineOutput {
        private VendingMachine vendingMachine;
        public VendingMachine(final VendingMachine vendingMachine) {
            this.vendingMachine = vendingMachine;
        }
        public void printCoinTypesAmount() {
            HashMap<Integer, Integer> coinTypesAmount =
                vendingMachine.getCoinTypesAmount();
            VendingMachineMessage.printCoinTypesAmount(cointTypesAmount);
        }
        public void printUserInputtedMoney() {
            final int userInputtedMoney =
                vendingMachine.getUserInputtedMoney();
            VendingMachineMessage.printUserInputtedMoney(userInputtedMoney);
        }
        //...
    }
    
    to-be:
    public class VendingMachineOutput {
        public void printCoinTypesAmount(HashMap<Integer, Integer> coinTypesAmount) {
            VendingMachineMessage.printCoinTypesAmount(cointTypesAmount);
        }
        public void printUserInputtedMoney(final int userInputtedMoney) {
            VendingMachineMessage.printUserInputtedMoney(userInputtedMoney);
        }
        //...
    }

--- src/main/java/vendingmachine/view/VendingMachineOutput.java
+++ src/main/java/vendingmachine/view/VendingMachineOutput.java
@@ -2,32 +2,23 @@ package vendingmachine.view;
 
 import java.util.HashMap;
 
-import vendingmachine.models.VendingMachine;
 import vendingmachine.utils.VendingMachineMessage;
 
 /**
  * <h1>자판기 로직 수행 결과를 출력한다</h1>
  *
  * @author yunki kim
- * @version 1.0
+ * @version 2.0
  * @since 2021-12-13(V1.0)
  */
 
 public class VendingMachineOutput {
 
-	private VendingMachine vendingMachine;
-
-	public VendingMachineOutput(final VendingMachine vendingMachine) {
-		this.vendingMachine = vendingMachine;
-	}
-
-	public void printCoinTypesAmount() {
-		HashMap<Integer, Integer> coinTypesAmount = vendingMachine.getCoinTypesAmount();
+	public void printCoinTypesAmount(final HashMap<Integer, Integer> coinTypesAmount) {
 		VendingMachineMessage.printCoinTypesAmount(coinTypesAmount);
 	}
 
-	public void printUserInputtedMoney() {
-		final int userInputtedMoney = vendingMachine.getUserInputtedMoney();
+	public void printUserInputtedMoney(final int userInputtedMoney) {
 		VendingMachineMessage.printUserInputtedMoney(userInputtedMoney);
 	}
 
--- src/test/java/vendingmachine/view/VendingMachineOutputTest.java
+++ src/test/java/vendingmachine/view/VendingMachineOutputTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -23,17 +25,24 @@ class VendingMachineOutputTest {
 	@DisplayName("자판기 내부에 있는 동전 리스트를 출력한다")
 	@Test
 	void outputCoinTypesAmount() {
-		vendingMachineOutput.printCoinTypesAmount();
+		HashMap<Integer, Integer> coinTypesAmount = new HashMap<>();
+		coinTypesAmount.put(Coin.COIN_500.getAmount(), 1);
+		coinTypesAmount.put(Coin.COIN_100.getAmount(), 1);
+		coinTypesAmount.put(Coin.COIN_50.getAmount(), 1);
+		coinTypesAmount.put(Coin.COIN_10.getAmount(), 1);
+
+		vendingMachineOutput.printCoinTypesAmount(coinTypesAmount);
 		verify(vendingMachineOutput, times(1))
-			.printCoinTypesAmount();
+			.printCoinTypesAmount(coinTypesAmount);
 	}
 
 	@DisplayName("사용자가 제품 구매를 위해 투입한 금액을 출력한다")
-	@Test
-	void outputUserInputtedMoney() {
-		vendingMachineOutput.printUserInputtedMoney();
+	@ParameterizedTest(name = "{displayName} userInputtedMoney={0}")
+	@ValueSource(ints={100})
+	void outputUserInputtedMoney(final int userInputtedMoney) {
+		vendingMachineOutput.printUserInputtedMoney(userInputtedMoney);
 		verify(vendingMachineOutput, times(1))
-			.printUserInputtedMoney();
+			.printUserInputtedMoney(userInputtedMoney);
 	}
 
 	@DisplayName("잔돈내역을 출력한다")
