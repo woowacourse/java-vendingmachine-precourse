eea94e8789c34141df5ced70fa89a5fdc632b516
xi_jjun <rlawowns97@naver.com>
2021-12-13 16:13:30 +0900

    feat: add View functions
    UI logic 담당하는 class
    
    Method Summary
    public static void showCoins(Map<Coin, Integer> coins, String message)
            : message를 화면에 출력한 뒤, 동전들의 타입과 개수정보인 coins를 화면에 출력
    
            parameters
                    Map<Coin, Integer> coins : 출력할 동전들의 정보를 갖고 있다.
                    String message : 어떤 메세지를 출력할 것인지에 대한 정보를 갖고 있다.
    
    public static void showMoney(int money) : money를 출력형식에 맞게 화면에 출력. ex) 투입 금액: {money}원
    public static void showCoin(Coin coinType, int count)
            : 1개 동전의 타입의 양과 개수 정보를 출력.
    
            parameters
                    Coin coinType : 동전의 타입 정보를 갖고 있는 Enum객체.
                    int count : 해당 타입의 동전이 몇개 있는지에 대한 개수 정보.
    
    public static void showAskMessage(String askMessage) : askMessage에 해당하는 질문 메세지 문구를 출력.
    public static void showErrorMessage(String errorMessage) : errorMessage에 해당하는 에러 메세지 문구를 출력.

---
+++ src/main/java/vendingmachine/utils/View.java
@@ -0,0 +1,35 @@
+package vendingmachine.utils;
+
+import static vendingmachine.constants.Message.*;
+import static vendingmachine.utils.Print.*;
+
+import java.util.Map;
+
+import vendingmachine.domain.Coin;
+
+public class View {
+	private View() {
+
+	}
+
+	public static void showCoins(Map<Coin, Integer> coins, String message) {
+		showMessage(message);
+		coins.forEach(View::showCoin);
+		showMessage(ENTER_LINE);
+	}
+
+	public static void showMoney(int money) {
+		showMessage(INPUT_COST + money + WON);
+	}
+
+	public static void showCoin(Coin coinType, int count) {
+		showMessage(coinType.getAmount() + WON + SPACE + count + COUNT);
+	}
+
+	public static void showAskMessage(String askMessage) {
+		showMessage(askMessage);
+	}
+	public static void showErrorMessage(String errorMessage) {
+		showMessage(errorMessage);
+	}
+}
