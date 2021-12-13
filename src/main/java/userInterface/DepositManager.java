8a22ef90b0fcf53fd28b9f32311a4d9da189e15a
KIM <km0192@naver.com>
2021-12-13 17:02:01 +0900

    feat(DepositManager) : 투입금액 생성, 잔액 출력, 차감

---
+++ src/main/java/userInterface/DepositManager.java
@@ -0,0 +1,34 @@
+package userInterface;
+
+import utils.InputManager;
+import vendingmachine.Amount;
+import utils.validator.isAmount;
+
+public class DepositManager {
+	private Amount deposit;
+
+	public DepositManager() {
+		System.out.println("투입 금액을 입력해 주세요.");
+		initDeposit();
+	}
+
+	private void initDeposit() {
+		isAmount validator = new isAmount();
+		String input = new InputManager().getStringWithInput(validator);
+		int amount = Integer.parseInt(input);
+		deposit = new Amount(amount);
+	}
+
+	public void printDeposit() {
+		System.out.printf("투입 금액 : %d원\n", deposit.get());
+	}
+
+	public boolean deduct(int price) {
+		int depositInt = deposit.get();
+		if (depositInt <= price) {
+			throw new IllegalArgumentException("[ERROR] 투입 금액보다 큰 상품을 구매할 수 없습니다.");
+		}
+		deposit = new Amount(depositInt - price);
+		return true;
+	}
+}
--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -2,11 +2,11 @@ package vendingmachine;
 
 import userInterface.CoinListManager;
 import userInterface.ProductListManager;
+import userInterface.DepositManager;
 
 public class Application {
     public static void main(String[] args) {
         /* TODO : 프로그램 구현
-         * 4. 투입 금액 (입력 받기 -> 에러체크 넘을때까지 반복 -> 생성)
          * 5. 자판기 돌리기 (남은금액 계산 -> 주문받기(에러체크->반복) -> 잔고 출력)
          * 6. 잔여 금액 반환(출력)
          */
@@ -15,6 +15,7 @@ public class Application {
         coinListManager.makeRandomList();
         coinListManager.printCoinList();
         Products productList = new ProductListManager().getList();
-
+        DepositManager depositManager = new DepositManager();
+        depositManager.printDeposit();
     }
 }
