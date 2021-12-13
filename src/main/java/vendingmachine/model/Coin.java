77024e8f62eadd23a86b4aa63178ecf1eb99a9cd
audgnssweet <audgnssweet@naver.com>
2021-12-13 19:51:18 +0900

    refactor: Coin의 매직넘버와 매직리터럴을 상수로 치환

--- src/main/java/vendingmachine/model/Coin.java
+++ src/main/java/vendingmachine/model/Coin.java
@@ -8,19 +8,22 @@ public enum Coin implements Comparable<Coin> {
 	COIN_50(50),
 	COIN_10(10);
 
+	private static final int INITIAL_COUNT = 0;
+	private static final String AMOUNT_MISMATCH_ERROR = "[ERROR] 존재하지 않는 동전입니다.";
+
 	private final Money amount;
 	private int count;
 
 	Coin(final int amount) {
 		this.amount = Money.from(amount);
-		this.count = 0;
+		this.count = INITIAL_COUNT;
 	}
 
 	public static Coin from(Money amount) {
 		return Arrays.stream(values())
 			.filter(coin -> coin.isAmountExactly(amount))
 			.findAny()
-			.orElseThrow(() -> new IllegalArgumentException("[ERROR]"));
+			.orElseThrow(() -> new IllegalArgumentException(AMOUNT_MISMATCH_ERROR));
 	}
 
 	private boolean isAmountExactly(Money amount) {
@@ -30,7 +33,7 @@ public enum Coin implements Comparable<Coin> {
 	//테스팅을 위한 함수
 	protected static void resetForTest() {
 		Arrays.stream(Coin.values())
-			.forEach(coin -> coin.count = 0);
+			.forEach(coin -> coin.count = INITIAL_COUNT);
 	}
 
 	public void addOne() {
@@ -61,6 +64,6 @@ public enum Coin implements Comparable<Coin> {
 	}
 
 	public boolean isRemain() {
-		return count > 0;
+		return count > INITIAL_COUNT;
 	}
 }
