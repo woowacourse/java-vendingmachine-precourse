7e4c3abf893f319706d691ba3a3e84e4615c061e
Wishoon <heonga2@gmail.com>
2021-12-13 16:11:25 +0900

    feat: 자판기 보유 금액이 10의 단위로 나누어 떨어지는지 확인하는 기능 추가

--- src/main/java/vendingmachine/domain/coin/Coins.java
+++ src/main/java/vendingmachine/domain/coin/Coins.java
@@ -11,15 +11,24 @@ public class Coins {
 	private static final int COIN_AMOUNT_ZERO = 0;
 	private static final int COIN_DEFAULT_QUANTITY = 0;
 	private static final int COIN_QUANTITY_PLUS = 1;
+	private static final int COINS_NUM_DIVISION_STANDARD = 10;
+	private static final String ERROR_COINS_AMOUNT_NOT_TEN_PERCENT_DIVISION = "보유금액은 10의 단위로 나누어 떨어져야 합니다.";
 
-	final Map<Coin, Integer> coins;
+	public final Map<Coin, Integer> coins;
 
 	public Coins(Map<Coin, Integer> coins) {
 		this.coins = coins;
 	}
 
 	public static Coins from(int coinsAmount) {
+		isValidateCoinsAmount10PercentDivision(coinsAmount);
 		return new Coins(generateRandomCoins(coinsAmount));
+	}
+
+	private static void isValidateCoinsAmount10PercentDivision(int coinsAmount) {
+		if (coinsAmount % COINS_NUM_DIVISION_STANDARD != COIN_AMOUNT_ZERO) {
+			throw new IllegalArgumentException(ERROR_COINS_AMOUNT_NOT_TEN_PERCENT_DIVISION );
+		}
 	}
 
 	private static Map<Coin, Integer> generateRandomCoins(int inputCoinsAmount) {
