599dd7d17e6ee5ff8bb5e0b53ae0bf01882b0b1e
koo <gudonghee2000@gmail.com>
2021-12-13 17:57:24 +0900

    refactor : ProductBox클래스 변수명, 함수명, 함수구조 변경

--- src/main/java/model/ProductBox.java
+++ src/main/java/model/ProductBox.java
@@ -5,48 +5,53 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
-public class VendingMachineProducts {
+public class ProductBox {
 	private static final String COMMA = ",";
 	private static final int PRODUCT_NAME_INDEX = 0;
 	private static final int PRODUCT_PRICE_INDEX = 1;
 	private static final int PRODUCT_COUNT_INDEX = 2;
 
-	private final List<Product> products;
+	private final List<Product> productBox;
 
-	public VendingMachineProducts(List<String> vendingMachineProducts) {
-		this.products = makeProducts(vendingMachineProducts);
+	public ProductBox(List<String> products) {
+		this.productBox = makeBox(products);
 	}
 
-	private List<Product> makeProducts(List<String> vendingMachineProducts) {
-		return vendingMachineProducts.stream()
+	private List<Product> makeBox(List<String> products) {
+		return products.stream()
 			.map(this::makeProduct)
 			.collect(Collectors.toList());
 	}
 
-	private Product makeProduct(String vendingMachineProduct) {
-		List<String> productInformation = Arrays.stream(vendingMachineProduct.split(COMMA))
-			.collect(Collectors.toList());
+	private Product makeProduct(String product) {
+		List<String> productInformation = Arrays.stream(product.split(COMMA)).collect(Collectors.toList());
 		return new Product(productInformation.get(PRODUCT_NAME_INDEX),
 			Integer.parseInt(productInformation.get(PRODUCT_PRICE_INDEX)),
 			Integer.parseInt(productInformation.get(PRODUCT_COUNT_INDEX)));
 	}
 
-	public boolean hasAnyProduct() {
-		return products.stream().filter(product -> !product.isSoldOut()).count() > 0;
+	public boolean hasProduct() {
+		return productBox.stream().filter(product -> !product.isSoldOut()).count() > 0;
 	}
 
-	public int bringMinimumProductPrice() {
-		Comparator<Product> comparatorByPrice = Comparator.comparingInt(Product::bringPrice);
-		return products.stream().max(comparatorByPrice).orElseThrow(NoSuchElementException::new).bringPrice();
+	public int getMinimumProductPrice() {
+		Comparator<Product> comparatorByPrice = Comparator.comparingInt(Product::getPrice);
+		return productBox.stream().max(comparatorByPrice).orElseThrow(NoSuchElementException::new).getPrice();
 	}
 
-	public int sellProduct(String productName) {
-		Product soldProduct = products.stream()
+	public void sellProduct(String productName) {
+		findSoldProduct(productName).reduceCount();
+	}
+
+	public int giveSoldProductPrice(String productName) {
+		return findSoldProduct(productName).getPrice();
+	}
+
+	private Product findSoldProduct(String productName) {
+		return productBox.stream()
 			.filter(product -> product.isSameName(productName))
 			.collect(Collectors.toList())
 			.get(0);
-		return soldProduct.reduceCount();
 	}
 }
