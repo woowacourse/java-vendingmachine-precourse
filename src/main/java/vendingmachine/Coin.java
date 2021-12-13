f3419e60bfc21ed99614489f2b38d2dfc63253b2
RIANAEH <ririhan217@gmail.com>
2021-12-13 22:44:04 +0900

    feat(Coin): 값 리스트 얻기, 값으로 찾기 기능 추가

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,5 +1,9 @@
 package vendingmachine;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
@@ -14,5 +18,20 @@ public enum Coin {
 
     public int getAmount() {
         return amount;
+    }
+
+    public static List<Integer> getCoinValueList() {
+        return Arrays.stream(Coin.values())
+            .map(Coin::getAmount)
+            .collect(Collectors.toList());
+    }
+
+    public static Coin valueOf(int coinValue) {
+        for (Coin coin : Coin.values()) {
+            if (coin.getAmount() == coinValue) {
+                return coin;
+            }
+        }
+        return null;
     }
 }
