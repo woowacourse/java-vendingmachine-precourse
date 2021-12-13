8298bcfb6b27238f4d6cf058c3e027ad5b5d39ef
Kim Seunglae <obey1342@gmail.com>
2021-12-13 15:18:54 +0900

    feat: 상품을 저장할 Product 추가

---
+++ src/main/java/vendingmachine/domain/Product.java
@@ -0,0 +1,13 @@
+package vendingmachine.domain;
+
+public class Product {
+	private final String name;
+	private final int price;
+	private final int count;
+
+	public Product(String name, int price, int count) {
+		this.name = name;
+		this.price = price;
+		this.count = count;
+	}
+}
