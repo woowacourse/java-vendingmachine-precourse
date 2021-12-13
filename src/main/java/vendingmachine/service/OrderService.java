1b15f1703e1ce9ea5097187e531b0eb3c874fd91
soominsohn <soomin.sohn.dev@gmail.com>
2021-12-13 21:17:14 +0900

    feat: add subtract stock and money function and add exception handling function

--- src/main/java/vendingmachine/service/OrderService.java
+++ src/main/java/vendingmachine/service/OrderService.java
@@ -10,12 +10,15 @@ public class OrderService {
     OutputView outputView = new OutputView();
     InputView inputView = new InputView();
     OrderValidator orderValidator = new OrderValidator();
+    Products products;
     Money money;
 
     public void get(Products products) {
+        this.products = products;
         outputView.printOrderMoney();
         getUserMoney();
-
+        getProduct();
+        //printCoin();
     }
 
     public void getUserMoney() {
@@ -25,9 +28,55 @@ public class OrderService {
                 orderValidator.isValidMoney(tempMoney);
                 money = new Money(Integer.parseInt(tempMoney));
                 return;
-            } catch (IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
+
+    public void getProduct() {
+        while (true) {
+            outputView.printInputMoney(money.getRemainder());
+            Product userProduct = getOrderFromUser();
+            subtract(userProduct, money);
+            // if (isValidCoin()) {
+            //      continue;}
+        }
+        // break;
+    }
+
+    public void subtract(Product userProduct, Money money) {
+        userProduct.subtractStock();
+        money.subtract(userProduct.getPrice());
+    }
+
+    public Product getOrderFromUser() {
+        while (true) {
+            outputView.printProduct();
+            try {
+                String userOrder = inputView.getOrder();
+                isValidOrder(userOrder);
+                return products.getProduct(userOrder);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public boolean isValidOrder(String userOrder) {
+        orderValidator.isExistProduct(userOrder, products);
+        isPossiblePrice(userOrder);
+        orderValidator.hasStock(userOrder, products);
+        return true;
+    }
+
+    public boolean isPossiblePrice(String userOrder) {
+        for (Product product : products.getProducts()) {
+            if (product.getName().equals(userOrder)) {
+                orderValidator.isPossiblePrice(product, money);
+            }
+        }
+        return true;
+    }
+
 }
