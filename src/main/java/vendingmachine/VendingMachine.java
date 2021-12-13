78c25fbd11d16e7ed181952558d26d5d708af1cf
khj1 <74335027+khj1@users.noreply.github.com>
2021-12-14 01:52:49 +0900

    fix: 수요 상품 재고 부족 예외 처리 누락 수정
    수요량이 재고를 초과하는 경우에 대한 예외 처리 로직 중 일부가 누락되어 수정했습니다.

--- src/main/java/vendingmachine/VendingMachine.java
+++ src/main/java/vendingmachine/VendingMachine.java
@@ -1,9 +1,11 @@
 package vendingmachine;
 
 public class VendingMachine {
+    private static final int ZERO = 0;
+
     private final Products products;
     private final Coins holdingCoins;
-    private int inputAmount = 0;
+    private int inputAmount = ZERO;
 
     public VendingMachine(String products, int holdingMoney) {
         this.products = new Products(products);
@@ -19,8 +21,21 @@ public class VendingMachine {
         this.inputAmount = inputAmount;
     }
 
+    private void validatePurchasable(int restAmount) {
+        if (isExceedRestAmount(restAmount)) {
+            throw new IllegalArgumentException("잔액을 초과하여 구매할 수 없습니다.");
+        }
+    }
+
+    private boolean isExceedRestAmount(int restAmount) {
+        return restAmount < ZERO;
+    }
+
     private int deductFromInputAmount(int totalPrice) {
         int restAmount = inputAmount - totalPrice;
+        validatePurchasable(restAmount);
+
         return restAmount;
     }
+
 }
