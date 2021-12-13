26eb3219d6cf88d94b39a0cd742467b10aee0310
Kim Seunglae <obey1342@gmail.com>
2021-12-13 22:45:28 +0900

    feat: 잔돈 출력 하는 기능 추가

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -20,4 +20,23 @@ public class OutputView {
 	public static void printCurrentMoney(int cuurentAmount) {
 		System.out.printf(OUTPUT_INPUT_CURRENT_AMOUNT, cuurentAmount);
 	}
+
+	public static void printChanges(List<Coin> coinList) {
+		for (int coin : Coin.getCoinList()) {
+			sumCoinType(coinList, coin);
+		}
+	}
+
+	private static void sumCoinType(List<Coin> coinList, int amount) {
+		int count = 0;
+		for (Coin coin : coinList) {
+			if (coin.getAmount() == amount) {
+				count++;
+			}
+		}
+
+		if (count != 0) {
+			System.out.printf(OUTPUT_CHANGES, amount, count);
+		}
+	}
 }
