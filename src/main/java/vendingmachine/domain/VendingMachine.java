d5ff23806ac8a0c5894981312ff0017336266f42
summerlunaa <rhgus1085@gmail.com>
2021-12-13 19:24:35 +0900

    fear(VendingMachine): checkTermination 메소드 구현

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -1,13 +1,23 @@
 package vendingmachine.domain;
 
 public class VendingMachine {
-	private Coins coins;
-	private Products products;
-	private int inputMoney;
+	private final Coins coins;
+	private final Products products;
+	private final int inputMoney;
 
 	public VendingMachine (Coins coins, Products products, int inputMoney){
 		this.coins = coins;
 		this.products = products;
 		this.inputMoney = inputMoney;
+	}
+
+	public boolean checkTermination() {
+		if (products.isLessThanMinPrice(inputMoney)){
+			return true;
+		}
+		if (products.getTotalAmount() == 0) {
+			return true;
+		}
+		return false;
 	}
 }
