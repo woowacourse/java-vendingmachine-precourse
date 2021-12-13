92a62428dd4ec1fcf42fbf82ba0ea56fc371535d
bomi <bonita9931@gmail.com>
2021-12-13 19:28:53 +0900

    feat : 자판기의 입력을 담당하는 InputView 클래스 구현

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -4,11 +4,27 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 	private static final String INPUT_MACHINE_MONEY = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+	private static final String INPUT_MACHINE_PRODUCT = "상품명과 가격, 수량을 입력해 주세요.";
+	private static final String INPUT_BUYING_MONEY = "투입 금액을 입력해 주세요.";
+	private static final String INPUT_BUYING_PRODUCT = "구매할 상품명을 입력해 주세요.";
 
 	public String enterMachineMoney() {
 		System.out.println(INPUT_MACHINE_MONEY);
-		String machineMoney = Console.readLine();
+		return Console.readLine();
+	}
 
-		return machineMoney;
+	public String enterMachineProduct() {
+		System.out.println(INPUT_MACHINE_PRODUCT);
+		return Console.readLine();
+	}
+
+	public String enterBuyingMoney() {
+		System.out.println(INPUT_BUYING_MONEY);
+		return Console.readLine();
+	}
+
+	public String enterBuyingProduct() {
+		System.out.println(INPUT_BUYING_PRODUCT);
+		return Console.readLine();
 	}
 }
