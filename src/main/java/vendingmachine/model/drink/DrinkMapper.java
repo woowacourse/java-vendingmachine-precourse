1b903278c44f5dbae784a64836f9bc716e60339d
her0807 <her0807@naver.com>
2021-12-13 12:15:14 +0900

    feat: 상품명 중복 얘외 로직 추가
    - isDuplicationName 에서 예외 처리 수행

--- src/main/java/vendingmachine/model/drink/DrinkMapper.java
+++ src/main/java/vendingmachine/model/drink/DrinkMapper.java
@@ -1,17 +1,22 @@
 package vendingmachine.model.drink;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import vendingmachine.util.Constant;
-import vendingmachine.util.ErrorMessage;
+import vendingmachine.util.Message;
 
 public class DrinkMapper {
 
     public List<Drink> createDrinks(String inputDrinks) {
         List<Drink> drinks = new ArrayList<>();
+
         removeBracket(inputDrinks);
         String[] contents = splitDrinks(inputDrinks);
+        validNames(contents);
+
         for (String content : contents) {
             String[] drink = content.split(Constant.DRINK_CONTENT_SPLIT_WORD);
             drinks.add(createDrink(drink));
@@ -31,10 +36,25 @@ public class DrinkMapper {
 
     private Drink createDrink(String[] content) {
         if (content.length != Constant.CHECK_ARR_HAS_ALL_CONTENT)
-            throw new IllegalArgumentException(ErrorMessage.DRINK_CONTENT_ERROR);
+            throw new IllegalArgumentException(Message.DRINK_CONTENT_ERROR);
 
         return new Drink(content[Constant.NAME_INDEX],
             content[Constant.PRICE_INDEX],
             content[Constant.QUANTITY_INDEX]);
+    }
+
+    private boolean isDuplicationName(String[] contents) {
+        Set<String> names = new HashSet<>();
+        for (String content : contents) {
+            int nameLength = content.indexOf(Constant.DRINK_CONTENT_SPLIT_WORD);
+            names.add(content.substring(nameLength));
+        }
+        return names.size() != contents.length;
+    }
+
+    private void validNames(String[] names) {
+        if (isDuplicationName(names)) {
+            throw new IllegalArgumentException(Message.DRINK_CONTENT_DUPLICATION_ERROR);
+        }
     }
 }
