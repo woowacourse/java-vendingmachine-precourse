0c6c7016d774d8971b2dd8474ea967519e21cd40
her0807 <her0807@naver.com>
2021-12-13 17:24:10 +0900

    refactor: 현재 돈으로 음료를 살 수 있는가 검증을 drinks 로 옮김
    - 투입 금액에서 음료 리스트를 가지고 있는 부분이 이상해서 변경

--- src/main/java/vendingmachine/model/user/UserMoney.java
+++ src/main/java/vendingmachine/model/user/UserMoney.java
@@ -1,12 +1,30 @@
 package vendingmachine.model.user;
 
-import vendingmachine.model.drink.Drinks;
+import vendingmachine.model.drink.Drink;
+import vendingmachine.util.Constant;
 
 public class UserMoney {
     private int amount;
 
-    public UserMoney(String inputMoney, Drinks drinks) {
-        new UserMoneyValidator(inputMoney, drinks);
+    public UserMoney(String inputMoney) {
+        new UserMoneyValidator(inputMoney);
         this.amount = Integer.parseInt(inputMoney);
+    }
+
+    public void decAmount(Drink drink) {
+        amount -= drink.getPrice();
+    }
+
+    public boolean canBuy(Drink price) {
+        return this.amount >= price.getPrice();
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Constant.COIN_UNIT, amount);
     }
 }
--- src/main/java/vendingmachine/model/user/UserMoneyValidator.java
+++ src/main/java/vendingmachine/model/user/UserMoneyValidator.java
@@ -1,31 +1,21 @@
 package vendingmachine.model.user;
 
-import vendingmachine.model.drink.Drink;
-import vendingmachine.model.drink.Drinks;
-import vendingmachine.util.ErrorMessage;
+import vendingmachine.util.Message;
 import vendingmachine.util.exception.MoneyValidator;
 
 public class UserMoneyValidator extends MoneyValidator {
 
-    public UserMoneyValidator(String money, Drinks drinks) {
+    public UserMoneyValidator(String money) {
         moneyValid(money);
-        overMinPrice(toInt(money), drinks);
     }
 
     @Override
     public void moneyValid(String money) {
         if (!isNumeric(money)) {
-            throw new IllegalArgumentException(ErrorMessage.USER_MONEY_STRING_ERROR);
+            throw new IllegalArgumentException(Message.USER_MONEY_STRING_ERROR);
         }
         if (!isUnitSplit(toInt(money))) {
-            throw new IllegalArgumentException(ErrorMessage.USER_MONEY_UNIT_ERROR);
-        }
-    }
-
-    private void overMinPrice(int money, Drinks drinks) {
-        Drink minPrice = drinks.findMinPriceDrink();
-        if (!minPrice.isOverPrice(money)) {
-            throw new IllegalArgumentException(ErrorMessage.USER_MONEY_NEED_MORE_MONEY);
+            throw new IllegalArgumentException(Message.USER_MONEY_UNIT_ERROR);
         }
     }
 }
