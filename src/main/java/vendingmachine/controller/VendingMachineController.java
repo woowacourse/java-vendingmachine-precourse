8c6b16647c1c3f041651cc4a37cd89b5513f6740
Kim Seunglae <obey1342@gmail.com>
2021-12-13 23:39:41 +0900

    feat: 자판기 전체 로직을 실행하는 기능 추가

---
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -0,0 +1,60 @@
+package vendingmachine.controller;
+
+import vendingmachine.domain.Changes;
+import vendingmachine.domain.Products;
+import vendingmachine.domain.VendingMachine;
+import vendingmachine.view.InputView;
+import vendingmachine.view.OutputView;
+
+public class VendingMachineController {
+	private final Products products;
+	private final Changes changes;
+	private VendingMachine vendingMachine;
+
+	public VendingMachineController() {
+		this.changes = initChanges();
+		OutputView.printVendingmachineChanges(changes);
+		this.products = initProducts();
+		this.vendingMachine = initVendingMachine();
+	}
+
+	public void run() {
+		while (products.hasNotProductsCount() &&
+			products.canBuyCurrentAmount(vendingMachine.getCurrentAmount())) {
+			OutputView.printCurrentMoney(vendingMachine.getCurrentAmount());
+			products.buyProduct(InputView.inputBuyProduct(), vendingMachine);
+		}
+		OutputView.printCurrentMoney(vendingMachine.getCurrentAmount());
+		OutputView.printChanges(changes.giveChanges(vendingMachine.getCurrentAmount()));
+	}
+
+	private Changes initChanges() {
+		try {
+			String haveAmount = InputView.haveAmount();
+			return new Changes(haveAmount);
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			return initChanges();
+		}
+	}
+
+	private Products initProducts() {
+		try {
+			String inputProduts = InputView.inputProducts();
+			return new Products(inputProduts);
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			return initProducts();
+		}
+	}
+
+	private VendingMachine initVendingMachine() {
+		try {
+			String inputAmount = InputView.inputAmount();
+			return new VendingMachine(inputAmount);
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			return initVendingMachine();
+		}
+	}
+}
