e373f1afad8d2ddd3f68e2641c5aa97363c62922
lymchgmk <lymchgmk@gmail.com>
2021-12-14 04:43:15 +0900

    Revert "feat(Coin.java): 금액으로 랜덤한 코인 뭉치 생성 구현"
    This reverts commit 996fd0de1ec53caac4d9e540230aa21cdb517c36.

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,10 +1,5 @@
 package vendingmachine;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import camp.nextstep.edu.missionutils.Randoms;
-
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
@@ -18,25 +13,4 @@ public enum Coin {
     }
 
     // 추가 기능 구현
-    public static HashMap<Integer, Integer> randomGenerate(int money) {
-        ArrayList<Integer> coinKeys = coinKeys();
-        HashMap<Integer, Integer> randomCoins = new HashMap<>();
-        while (money != 0) {
-            int pickedCoin = Randoms.pickNumberInList(coinKeys);
-            if (pickedCoin <= money) {
-                int count = randomCoins.getOrDefault(pickedCoin, 0);
-                randomCoins.put(pickedCoin, count+1);
-                money -= pickedCoin;
-            }
-        }
-        return randomCoins;
-    }
-
-    private static ArrayList<Integer> coinKeys() {
-        ArrayList<Integer> coinKeys = new ArrayList<>();
-        for (Coin coin : Coin.values()) {
-            coinKeys.add(coin.amount);
-        }
-        return coinKeys;
-    }
 }
