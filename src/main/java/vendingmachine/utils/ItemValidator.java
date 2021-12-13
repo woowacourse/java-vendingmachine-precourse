af6ae6f3d59cff397bf4f265d3c1e40f67fec5d9
Hyunta <mohai2618@gmail.com>
2021-12-13 11:46:22 +0900

    feat(ItemValidator) : 상품명이 중복될 경우 예외처리하는 기능 구현

--- src/main/java/vendingmachine/utils/ItemValidator.java
+++ src/main/java/vendingmachine/utils/ItemValidator.java
@@ -2,15 +2,20 @@ package vendingmachine.utils;
 
 import static vendingmachine.utils.Constant.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class ItemValidator {
 	public static final String ERROR_EMPTY_ITEM = "[ERROR] 상품을 한 개이상 등록해주세요.";
 	public static final String ERROR_NUMBER_OF_PROPERTIES = "[ERROR] 상품명과 가격, 수량 3가지 항목을 입력해주세요.";
+	public static final String ERROR_ITEM_NAME_DUPLICATE = "[ERROR] 중복되는 상품명이 있습니다.";
 	public static final String ERROR_NOT_INTEGER = "[ERROR] 상품 가격, 수량은 숫자로 입력해주세요.";
 	public static final String ERROR_PRICE_UNDER_MINIMUM = "[ERROR] 상품 가격은 100원 이상으로 입력해주세요";
 	public static final String ERROR_QUANTITY_NOT_POSITIVE = "[ERROR] 상품 수량은 1개 이상으로 입력해주세요";
 	public static final String ERROR_PRICE_NOT_DIVIDE_BY_10 = "[ERROR] 상품 가격은 10원으로 나누어떨어져야 합니다.";
+
+	private static List<String> itemNameList = new ArrayList<>();
 
 	public void validateItems(String inputValue) {
 		StringTokenizer stringTokenizer = new StringTokenizer(inputValue, ITEMS_DELIMITER);
@@ -35,6 +40,7 @@ public class ItemValidator {
 		String itemPrice = stringTokenizer.nextToken().trim();
 		String itemQuantity = stringTokenizer.nextToken().trim();
 
+		checkItemName(itemName);
 		checkItemPrice(itemPrice);
 		checkItemQuantity(itemQuantity);
 	}
@@ -43,6 +49,10 @@ public class ItemValidator {
 		if (stringTokenizer.countTokens() != NUMBER_OF_ITEM_PROPERTIES) {
 			throw new IllegalArgumentException(ERROR_NUMBER_OF_PROPERTIES);
 		}
+	}
+
+	private void checkItemName(String itemName) {
+		isDuplicateItemName(itemName);
 	}
 
 	private void checkItemPrice(String itemPrice) {
@@ -54,6 +64,13 @@ public class ItemValidator {
 	private void checkItemQuantity(String itemQuantity) {
 		isInteger(itemQuantity);
 		isPositive(itemQuantity);
+	}
+
+	private void isDuplicateItemName(String itemName) {
+		if (itemNameList.contains(itemName)) {
+			throw new IllegalArgumentException(ERROR_ITEM_NAME_DUPLICATE);
+		}
+		itemNameList.add(itemName);
 	}
 
 	private void isInteger(String inputValue) {
