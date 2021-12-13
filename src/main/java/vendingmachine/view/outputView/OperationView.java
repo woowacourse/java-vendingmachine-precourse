362b368b717f625d9b7798af6bfad2b1c07bdf45
steven <seokju73@gmail.com>
2021-12-13 17:00:39 +0900

    Feat: Add output method to show balacne to be returned

--- src/main/java/vendingmachine/view/outputView/OperationView.java
+++ src/main/java/vendingmachine/view/outputView/OperationView.java
@@ -1,10 +1,17 @@
 package vendingmachine.view.outputView;
 
+import java.util.Map;
+
+import vendingmachine.Coin;
+
 public class OperationView {
     private static final String ASK_INSERTION_AMOUNT = "투입 금액을 입력해 주세요.";
     private static final String SHOW_BALANCE = "투입 금액: ";
     private static final String WON = "원";
     private static final String ASK_COMMODITY = "구매할 상품명을 입력해 주세요.";
+    private static final String BALANCE = "잔돈";
+    private static final String NUMBER = "개";
+    private static final String HYPHEN = " - ";
     
     public static void askInsertionAmount() {
         System.out.println(ASK_INSERTION_AMOUNT);
@@ -17,5 +24,14 @@ public class OperationView {
     public static void askCommodity() {
         System.out.println(ASK_COMMODITY);
     }
-
+    
+    public static void showBalanceReturn(Map<Coin,Integer> map) {
+        System.out.println(BALANCE);
+        for(Coin c : map.keySet()) {
+            if(map.get(c) == 0) {
+                continue;
+            }
+            System.out.println(c.getAmount() + WON + HYPHEN + map.get(c) + NUMBER);
+        }
+    }
 }
