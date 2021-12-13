1c6d68ddd6e4b7692d6ba322667e0d55086f8f09
yeon-06 <rnjstldus2@hs.ac.kr>
2021-12-13 15:56:34 +0900

    refactor: 자판기 모델 로직 개선
    - 사용자 투입 금액 필드 추가에 따른 일부 로직 변경
    - 거스름돈 출력 방식 변경
    - 종료 조건 검사 로직 추가

--- src/main/java/vendingmachine/model/VendingMachine.java
+++ src/main/java/vendingmachine/model/VendingMachine.java
@@ -8,28 +8,42 @@ import java.util.Map.Entry;
 public class VendingMachine {
     private Products products;
     private Money changes;
+    private int payment;
 
     public VendingMachine(int totalMoney, Products products) {
         this.changes = new Money(totalMoney);
         this.products = products;
     }
 
-    public String buyProduct(String name, int payment) {
+    public void pay(int payment) {
+        this.payment = payment;
+    }
+
+    public boolean isExit() {
+        return products.getMinPrice() > payment || products.isAllSoldOut();
+    }
+
+    public void buyProduct(String name) {
         try {
-            products.buyProduct(name, payment);
+            System.out.println("투입 금액: "+payment);
+            int productPrice = products.buyProduct(name, payment);
+            payment -= productPrice;
+
         } catch (IllegalStateException e) {
             System.out.println(e.getMessage());
         }
-        Map<Coin, Integer> coinMap = changes.getChanges(payment);
-        return getChangeString(coinMap);
     }
 
-    private String getChangeString(Map<Coin, Integer> changes) {
+    public String getChangeString() {
+        Map<Coin, Integer> coins = changes.getChanges(payment);
         StringBuilder stringBuilder = new StringBuilder("잔돈\n");
-        for(Entry<Coin, Integer> change: changes.entrySet()) {
-            stringBuilder.append(change.getKey().toString())
-                    .append("\n - ")
-                    .append(change.getValue())
+        for(Entry<Coin, Integer> coin: coins.entrySet()) {
+            if(coin.getValue() == 0) {
+                continue;
+            }
+            stringBuilder.append(coin.getKey().getAmount())
+                    .append(" - ")
+                    .append(coin.getValue())
                     .append("개\n");
         }
         return stringBuilder.toString();
