e6f31eb6ca4ade69333b9dd6a35ed8c2d6acd09f
KIM <km0192@naver.com>
2021-12-13 14:20:48 +0900

    feat(Amount) : 금액 클래스 : 숫자인지, 음수인지 확인(utils.validator.isAmount)

---
+++ src/main/java/utils/validator/isAmount.java
@@ -0,0 +1,16 @@
+package utils.validator;
+
+public class isAmount implements Validator{
+	public boolean run(StringBuilder input) {
+		new isDigit().run(input);
+		if (isNegative(input)) {
+			throw new IllegalArgumentException("[ERROR] 음수단위의 금액은 사용할 수 없습니다.");
+		}
+		return true;
+	}
+
+	private boolean isNegative(StringBuilder input) {
+		int amount = Integer.parseInt(input.toString());
+		return amount >= 0;
+	}
+}
--- src/main/java/vendingmachine/Amount.java
+++ src/main/java/vendingmachine/Amount.java
@@ -1,2 +1,18 @@
-package vendingmachine;public class Amount {
+package vendingmachine;
+
+import utils.validator.isAmount;
+
+public class Amount {
+	private int amount;
+
+	Amount(int amount) {
+		String amountStr = Integer.toString(amount);
+		StringBuilder amountSB = new StringBuilder(amountStr);
+		new isAmount().run(amountSB);
+		this.amount = amount;
+	}
+
+	public int get() {
+		return amount;
+	}
 }
