5a2636b9848c9e8d989ddb87f092873b3ad4e42c
wooseok <diddmlvkf@gmail.com>
2021-12-13 16:42:47 +0900

    feat(Products) : 상품 리스트를 가진 상품 목록 구현

--- src/main/java/vendingmachine/domain/Products.java
+++ src/main/java/vendingmachine/domain/Products.java
@@ -1,2 +1,15 @@
-package vendingmachine.domain;public class Products {
+package vendingmachine.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Products {
+	private List<Product> products = new ArrayList<>();
+
+	public Products() {}
+
+	public void add(Product product) {
+		products.add(product);
+	}
+
 }
