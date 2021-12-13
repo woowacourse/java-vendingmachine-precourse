616e7cc752a1a547ac68eaea79ce20f1b617a5a4
xi_jjun <rlawowns97@naver.com>
2021-12-13 18:56:22 +0900

    test: returnChange method test

---
+++ src/test/java/vendingmachine/service/ChangeServiceTest.java
@@ -0,0 +1,34 @@
+package vendingmachine.service;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import vendingmachine.domain.Coin;
+
+class ChangeServiceTest {
+	@Test
+	void changeTest() {
+		ChangeService changeService = new ChangeService();
+
+		// given
+		Map<Coin, Integer> coins = new EnumMap<>(Coin.class);
+		coins.put(Coin.COIN_500, 1);
+		coins.put(Coin.COIN_100, 3);
+		coins.put(Coin.COIN_50, 4);
+		coins.put(Coin.COIN_10, 2);
+
+		int remainedMoney = 450;
+
+		// when
+		Map<Coin, Integer> change = changeService.returnChange(coins, remainedMoney);
+
+		// then
+		for (Map.Entry<Coin, Integer> en : change.entrySet()) {
+			System.out.println("Key : Value == " + en.getKey() + " : " + en.getValue());
+			assertThat(en.getValue()).isEqualTo(3);
+		}
+	}
+}
