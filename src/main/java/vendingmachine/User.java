7c859f2ff732b04853d8a88e8b167c1a400b5153
sejung-hong <sejungh@naver.com>
2021-12-13 16:28:52 +0900

    feat : 자판기 보유 금액 예외 사항

--- src/main/java/vendingmachine/User.java
+++ src/main/java/vendingmachine/User.java
@@ -5,6 +5,35 @@ import camp.nextstep.edu.missionutils.Console;
 public class User {
     public int InputMachineCoin(){
         String machineCoinString = Console.readLine();
+        try{
+            isInteger(machineCoinString);
+            isPositive(machineCoinString);
+            isDivide(machineCoinString);
+        }catch(IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return InputMachineCoin();
+        }
+
         return Integer.parseInt(machineCoinString);
+    }
+
+    private void isInteger(String machineCoinString){
+        try{
+            Integer.parseInt(machineCoinString);
+        }catch (Exception e){
+            throw new IllegalArgumentException("[ERROR] 금액은 숫자여야 합니다.");
+        }
+    }
+
+    private void isPositive(String machineCoinString){
+        if(Integer.parseInt(machineCoinString) < 0){
+            throw new IllegalArgumentException("[ERROR] 금액은 0이상이어야 합니다.");
+        }
+    }
+
+    private void isDivide(String machineCoinString){
+        if(Integer.parseInt(machineCoinString)%10 != 0){
+            throw new IllegalArgumentException("[ERROR] 금액은 10원으로 나누어떨어져야 합니다.");
+        }
     }
 }
