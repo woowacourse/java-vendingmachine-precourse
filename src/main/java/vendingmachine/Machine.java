03a4be2805f96eb74275eaacdcb5d63a2b5bb97b
Dong Geun <dlehdrms978@gmail.com>
2021-12-13 19:40:23 +0900

    자판기 기능 - 보유하고 있는 금액을 사용자 입력으로 받고 동전을 무작위로 생성한다.

---
+++ src/main/java/vendingmachine/Machine.java
@@ -0,0 +1,61 @@
+package vendingmachine;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import vendingmachine.ui.MachineUI;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Machine {
+
+    private final MachineUI machineUI;
+    private final Validator validator;
+    private int amount = 0;
+    private Coin coins;
+    public Machine(MachineUI machineUI,Validator validator){
+        this.machineUI = machineUI;
+        this.validator = validator;
+    }
+    public void operate(){
+        getVendingMachineAmount();
+        generateRandomCoins();
+        machineUI.showAmount();
+    }
+    private void getVendingMachineAmount(){
+
+        while (true){
+            System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
+            String amountInput = Console.readLine();
+            try{
+                validator.validateAmount(amountInput);
+                amount = Integer.parseInt(amountInput);
+                break;
+            }catch (Exception e){
+                System.out.println();
+            }
+        }
+
+    }
+    private void generateRandomCoins(){
+        int leftAmount = amount;
+        leftAmount = setCoins(leftAmount,Coin.COIN_500.getAmount());
+        if(leftAmount == 0) return;
+        leftAmount = setCoins(leftAmount,Coin.COIN_100.getAmount());
+        if(leftAmount == 0) return;
+        leftAmount = setCoins(leftAmount,Coin.COIN_50.getAmount());
+        if(leftAmount == 0) return;
+        Coin.COIN_10.setNumber(leftAmount/Coin.COIN_10.getAmount());
+    }
+    private int setCoins(int leftAmount, int coin){
+        int bound = leftAmount / coin;
+        int number = 0;
+        if( bound >=1){
+            number = Randoms.pickNumberInList(IntStream.range(0,bound).boxed().collect(Collectors.toList()));
+            Coin.valueOf("COIN_"+coin).setNumber(number);
+        }
+        return leftAmount  - coin * number;
+    }
+
+}
