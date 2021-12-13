462c62e044dd7f739d30740dd4b564705c86c7ea
cjlee38 <gptpem38@gmail.com>
2021-12-14 00:48:47 +0900

    feat(deposit): 금액 투입 service/repository 구현

---
+++ src/test/java/vendingmachine/repository/DepositRepositoryTest.java
@@ -0,0 +1,26 @@
+package vendingmachine.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import vendingmachine.Rollback;
+import vendingmachine.config.RepositoryConfig;
+import vendingmachine.domain.Money;
+
+class DepositRepositoryTest implements Rollback {
+
+	DepositRepository repository = RepositoryConfig.getDepositRepository();
+
+	@Test
+	@DisplayName("금액을 저장한다.")
+	public void testSave() {
+	    // given
+		Money money = new Money(1000);
+	    // when
+		String save = repository.save(money);
+		// then
+		assertEquals("1000원", money.toString());
+	}
+}
---
+++ src/test/java/vendingmachine/service/DepositServiceTest.java
@@ -0,0 +1,26 @@
+package vendingmachine.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import vendingmachine.Rollback;
+import vendingmachine.config.ServiceConfig;
+import vendingmachine.domain.Money;
+
+class DepositServiceTest implements Rollback {
+
+	DepositService service = ServiceConfig.getDepositService();
+
+	@Test
+	@DisplayName("금액을 저장한다.")
+	public void testDeposit() {
+	    // given
+		Money money = new Money(1000);
+		// then
+		String deposit = service.deposit(money);
+		// then
+		assertEquals("1000원", deposit);
+	}
+}
