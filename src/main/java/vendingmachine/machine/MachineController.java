dc9e90b57652d87f9b2ff21310028a488b98ab8e
iamsseaa <homingsea@gmail.com>
2021-12-13 10:50:44 +0900

    feat: 잔돈 생성과 잔돈 반환과 클래스 분리

--- src/main/java/vendingmachine/machine/MachineController.java
+++ src/main/java/vendingmachine/machine/MachineController.java
@@ -13,44 +13,5 @@ public class MachineController {
     MachineController(Machine machine) {
         this.machine = machine;
     }
-
-    public void setCurrentAmount(int amount) {
-        this.currentAmount += amount;
-    }
-
-    public int pickRandomCoin() {
-        int random = Randoms.pickNumberInList(new ArrayList<Integer>(Arrays.asList(500,100,50,10)));
-
-        return random;
-    }
-
-    public boolean checkIsRightRandom(int amount) {
-        if (this.currentAmount + amount <= machine.getHoldingAmount()) {
-            return true;
-        }
-        return false;
-    }
-
-    public boolean checkIsFullCurrentAmount() {
-        return this.currentAmount == machine.getHoldingAmount();
-    }
-
-    public Coin compareCoin(int amount) {
-        for (Coin coin : coins) {
-            if (coin.getCoinAmount() == amount) {
-                return coin;
-            }
-        }
-        return coins[0];
-    }
-
-
-    public void makeCoins() {
-        while (!checkIsFullCurrentAmount()) {
-            int random = pickRandomCoin();
-            if (checkIsRightRandom(random)) {
-                compareCoin(random).setCoinNumber();
-            }
-        }
-    }
+    
 }
