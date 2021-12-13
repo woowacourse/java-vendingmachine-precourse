c768a568c0f9c02d1715ff564f7a2c418c67659d
xi_jjun <rlawowns97@naver.com>
2021-12-13 15:54:02 +0900

    feat: add OrderService functions
    사용자가 상품에 대한 주문을 하게 해주는 서비스 class
    
    Field Summary
    private final UserService userService : UserService를 선언하여 주문할 Item에 대한 정보를 입력받는다
    
    Method Summary
    public int orderItem(Items forSaleItems, int userMoney)
            : 주문목록에서 사용자의 돈으로 주문을 하게 해주는 메서드.
    
            parameters
                    Items forSaleItems : 주문할 수 있는 Item들의 목록 객체.
                    int userMoney : 사용자가 투입한 금액의 잔량.
            return
                    해당 Item을 주문하고 남은 금액을 반환한다.
    
    public boolean canPurchaseAnyItems(int userMoney, Items forSaleItems)
    	: 주문목록에서 현재 userMoney로 살 수 있는 물품이 있는지 확인해주는 메서드
    
    	parameters
    		Items forSaleItems : 주문목록
    		int userMoney : 사용자가 투입한 금액의 잔량.
    	return
    		forSaleItems에 userMoney로 주문할 수 있고 해당 Item의 재고량이 1이상이라면, true return.

---
+++ src/main/java/vendingmachine/service/OrderService.java
@@ -0,0 +1,28 @@
+package vendingmachine.service;
+
+import static vendingmachine.utils.View.showErrorMessage;
+
+import vendingmachine.domain.Item;
+import vendingmachine.domain.Items;
+
+public class OrderService {
+	private final UserService userService = new UserService();
+
+	public int orderItem(Items forSaleItems, int userMoney) {
+		while (true) {
+			try {
+				String orderItemName = userService.getPurchasingItemName(forSaleItems.getForSaleItemsList(), userMoney);
+				Item orderItem = forSaleItems.findItemByName(orderItemName);
+
+				return orderItem.order(userMoney);
+			} catch (IllegalArgumentException e) {
+				showErrorMessage(e.getMessage());
+			}
+		}
+	}
+
+	public boolean canPurchaseAnyItems(int userMoney, Items forSaleItems) {
+		return forSaleItems.getForSaleItemsList().stream()
+				.anyMatch(item -> item.isEnoughMoney(userMoney) && !item.isRunOutOfStock());
+	}
+}
