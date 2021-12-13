a5f65d44936e28e1524fbbec152c9eab6e0dc749
Dong Geun <dlehdrms978@gmail.com>
2021-12-14 02:09:25 +0900

    애플리케이션 실행 코드

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,7 +1,11 @@
 package vendingmachine;
 
+import vendingmachine.ui.MachineUIImpl;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Machine machine = new Machine(new MachineUIImpl(), new Validator());
+        machine.operate();
     }
 }
