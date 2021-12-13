3153b05f0f437813329474a480136083f38e62eb
monk <2012.monk@gmail.com>
2021-12-13 17:22:55 +0900

    feat: add get user input with prompt

--- src/main/java/vendingmachine/utils/UserInput.java
+++ src/main/java/vendingmachine/utils/UserInput.java
@@ -8,37 +8,47 @@ import vendingmachine.validator.InputValidator;
 
 public class UserInput {
 
+    private static final String PROMPT_HOLDING_AMOUNT = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    private static final String PROMPT_PRODUCTS_INFO = "상품명과 가격, 수량을 입력해 주세요.";
+    private static final String PROMPT_INPUT_AMOUNT = "투입 금액을 입력해 주세요.";
+    private static final String PROMPT_ORDER_PRODUCT = "구매할 상품명을 입력해 주세요.";
+
     public static List<Product> getProductList() {
         return getValidInput(
-            () -> ProductParser.parse(Console.readLine())
+            () -> ProductParser.parse(getInputWithPrompt(PROMPT_PRODUCTS_INFO))
         );
     }
 
     public static String getProductOrder() {
         return getValidInput(() -> {
-            String input = Console.readLine();
+            String input = getInputWithPrompt(PROMPT_ORDER_PRODUCT);
             InputValidator.validateNonSpecialChar(input);
             return input;
         });
     }
 
     public static int getValidInputAmount() {
-        return getValidNumber();
+        return getValidNumberWithPrompt(PROMPT_INPUT_AMOUNT);
     }
 
-    public static int getValidCash() {
-        return getValidNumber();
+    public static int getValidHoldingAmount() {
+        return getValidNumberWithPrompt(PROMPT_HOLDING_AMOUNT);
     }
 
-    private static int getValidNumber() {
+    private static int getValidNumberWithPrompt(String prompt) {
         return getValidInput(() -> {
-            String input = Console.readLine();
+            String input = getInputWithPrompt(prompt);
             InputValidator.validateNumeric(input);
             return Integer.parseInt(input);
         });
     }
 
-    public static <T> T getValidInput(Supplier<T> supplier) {
+    private static String getInputWithPrompt(String prompt) {
+        System.out.println(prompt);
+        return Console.readLine();
+    }
+
+    private static <T> T getValidInput(Supplier<T> supplier) {
         try {
             return supplier.get();
         } catch (IllegalArgumentException e) {
