a5dab59a0ab17211a3fa8b9e688378aca2a56d5f
nayoung <evelyn82ny@gmail.com>
2021-12-14 00:28:52 +0900

    test: 상품 데이터 추가 및 구매 테스트 작성
    - 중복 테스트인 itemServiceTest.java 삭제

---
+++ src/test/java/vendingmachine/item/ItemControllerTest.java
@@ -0,0 +1,29 @@
+package vendingmachine.item;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import vendingmachine.exception.NotEnoughStockException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemControllerTest {
+
+    public static ItemController itemController = ItemController.getInstance();
+
+    @BeforeAll
+    public static void beforeAll() {
+        itemController.addItem("[콜라,1500,1];[사이다,1000,10]");
+    }
+
+    @Test
+    public void addItemTest() {
+        assertDoesNotThrow(() -> itemController.addItem("[젤리,100,3]"));
+        assertThrows(IllegalArgumentException.class, () -> itemController.addItem("[사탕,1r4,45]"));
+    }
+
+    @Test
+    public void purChaseTest() {
+        assertDoesNotThrow(() -> itemController.purChaseItem("콜라", 1));
+        assertThrows(NotEnoughStockException.class, () -> itemController.purChaseItem("사이다", 11));
+    }
+}
