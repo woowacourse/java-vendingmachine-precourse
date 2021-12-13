5eb6e6256c130bfcbfdb394f4209012d390aa48b
Kim Seunglae <obey1342@gmail.com>
2021-12-13 21:34:05 +0900

    feat: 현재 금액 출력하는 기능 추가

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -8,9 +8,16 @@ import vendingmachine.domain.Changes;
 import vendingmachine.domain.Coin;
 
 public class OutputView {
+	private OutputView() {
+	}
+
 	public static void printVendingmachineChanges(Changes changes) {
 		List<Coin> coins = changes.orderByCoin();
 		coins.forEach(coin ->
 			System.out.printf(OUTPUT_CHANGES, coin.getAmount(), changes.getCoinMapValue(coin)));
+	}
+
+	public static void printCurrentMoney(int cuurentAmount) {
+		System.out.printf(OUTPUT_INPUT_CURRENT_AMOUNT, cuurentAmount);
 	}
 }
