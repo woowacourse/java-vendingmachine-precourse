da0a09fc9ff0ed290d6b17aa7f50c106a6fb5217
koo <gudonghee2000@gmail.com>
2021-12-13 16:59:38 +0900

    refactor : ProductNameValidator클래스의 함수명 변경

--- src/main/java/utils/validator/products/information/ProductNameValidator.java
+++ src/main/java/utils/validator/products/information/ProductNameValidator.java
@@ -1,13 +1,13 @@
 package utils.validator.products.information;
 
 public class ProductNameValidator {
-	private static final int MIN_PRODUCT_NAME_INPUT_LENGTH = 1;
+	private static final int MIN_NAME_INPUT_LENGTH = 1;
 	private static final String INPUT_NOTHING_ERROR_MESSAGE = "[ERROR] 입력한 자판기 상품명이 없다.";
 
 	private ProductNameValidator() {
 	}
 
-	public static boolean isValidProductName(String productName) {
+	public static boolean isValidName(String productName) {
 		if (isValidInputLength(productName)) {
 			return true;
 		}
@@ -15,7 +15,7 @@ public class ProductNameValidator {
 	}
 
 	private static boolean isValidInputLength(String productName) {
-		if (productName.length() < MIN_PRODUCT_NAME_INPUT_LENGTH) {
+		if (productName.length() < MIN_NAME_INPUT_LENGTH) {
 			throw new IllegalArgumentException(INPUT_NOTHING_ERROR_MESSAGE);
 		}
 		return true;
