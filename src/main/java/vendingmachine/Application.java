d8f4c5362f62c760a71c3fc9b3ed948dfa2f9cb3
SheepEatLion <gowjr207@gmail.com>
2021-12-14 00:33:06 +0900

    refactor(main) : Application.java
    - 로직 수정
    기존에 다른 클래스들을 껍데기만 만들어두고 static만 사용하던 로직을 모두 수정하면서 객체를 생성하는 로직이 메인에 추가됌.

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,29 +1,14 @@
 package vendingmachine;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.*;
-import java.util.regex.Pattern;
-
-import static vendingmachine.Validation.*;
-import static vendingmachine.VendingMachine.startVendingMachine;
 
 public class Application {
-    static int USER_MONEY = 0;
-    static int MACHINE_OWN_MONEY = 0;
-    static int PRODUCT_NAME = 0;
-    static int PRODUCT_PRICE = 1;
-    static int PRODUCT_AMOUNT = 2;
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        MACHINE_OWN_MONEY = inputMachineOwnMoney();
-        setRandomCountToEachCoin();
-        printCurrentMachineCoin();
-        ArrayList<Product> products = inputProductNamePriceAmount();
-        insertMoneyByUser();
-        startVendingMachine(products);
+        VendingMachine vendingMachine = new VendingMachine();
+        vendingMachine.setRandomValueToEachCoin();
+        ArrayList<Product> productList = vendingMachine.insertProductToVendingMachine();
+        User user = new User();
+        vendingMachine.startVendingMachine(productList, user);
     }
-
 }
