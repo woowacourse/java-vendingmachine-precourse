9c72f5f2650a54748e6083d58370d1d5025d8c70
harinn1014 <gkfla7555@gmail.com>
2021-12-14 23:45:03 +0900

    feat(Application): add main controller to start
    <description>
    main()에서 controller를 만들어 실행하는 코드 추가

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,7 +1,11 @@
 package vendingmachine;
 
+import vendingmachine.controller.MainController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    	MainController mainController= new MainController();
+    	mainController.start();
     }
 }
