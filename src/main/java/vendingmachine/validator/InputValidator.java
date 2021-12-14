f1345f08ef6394d9811a2aaaebe552f775e2f6fa
harinn1014 <gkfla7555@gmail.com>
2021-12-14 21:20:46 +0900

    feat(InputValidator): create validator folder and add class
    <description>
    - 유효성 검사를 하기 위한 클래스
    - 공통: null값인지 검사
    
    - 상품 목록
    1. 대괄호로 묶여있는가
    2. 개별 상품은 ,쉼표로 구분되어 있는가
    3. 이미 등록된 상품인가
    
    - 지폐
    1. 100원 이상인가
    2. 10으로 나누어 떨어지는가
    
    - 숫자
    1. 양수인가
    
    - 상품 구매 시 발생 가능
    1. 투입 금액이 충분한가
    2. 입력한 상품이 자판기에 존재하는가
    3. 재고가 있는가?

---
+++ src/main/java/vendingmachine/validator/InputValidator.java
@@ -0,0 +1,94 @@
+package vendingmachine.validator;
+
+import java.util.Optional;
+
+import vendingmachine.domain.Product;
+import vendingmachine.domain.VendingMachine;
+import vendingmachine.utils.Constant;
+import vendingmachine.utils.ExceptionMessage;
+import static vendingmachine.utils.Constant.ZERO;
+
+public class InputValidator {
+
+	public void isNull(String value) {
+		if (value.isEmpty()) {
+			throw new IllegalArgumentException(ExceptionMessage.NULL_VALUE);
+		}
+	}
+
+	// 상품 목록
+	public String isRightFormat(String value) {
+		if (!value.startsWith("[") || !value.endsWith("]")) {
+			throw new IllegalArgumentException(ExceptionMessage.NOT_EXIST_BRACKETS);
+		}
+		return value.replace("[", "").replace("]", "");
+	}
+
+	public void isRightPattern(String value) {
+		if (value.split(",").length!=Constant.ELEMENT) {
+			throw new IllegalArgumentException(ExceptionMessage.MISMATCH_FORMAT);
+		}
+	}
+
+	public void isDuplicate(VendingMachine machine) {
+		if (machine.getProducts().stream().distinct().count() < machine.getProducts().size()) {
+			throw new IllegalArgumentException(ExceptionMessage.DUPLICATE_PRODUCT);
+		}
+	}
+
+	// 지폐
+	public int validateMoney(String input) {
+		int money = isPositiveNumber(input);
+
+		isOverMoneyLimit(money);
+		isPossibleMod(money);
+
+		return money;
+	}
+
+	public void isOverMoneyLimit(int money) {
+		if (money < Constant.LIMIT_PAY) {
+			throw new IllegalArgumentException(ExceptionMessage.UNDER_LIMIT_PAY);
+		}
+	}
+
+	public void isPossibleMod(int money) {
+		if (money % Constant.UNIT_MOD != 0) {
+			throw new IllegalArgumentException(ExceptionMessage.NOT_MOD);
+		}
+	}
+
+	// 숫자
+	public int isPositiveNumber(String value) {
+		try {
+			int number = Integer.parseInt(value);
+			if (number <= ZERO) {
+				throw new IllegalArgumentException();
+			}
+			return number;
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException(ExceptionMessage.NOT_POSITIVE);
+		}
+	}
+
+	// 상품 구매 시 발생 가능
+	public void isEnoughMoney(int money, Product product) {
+		if (money < product.getPrice()) {
+			throw new IllegalArgumentException(ExceptionMessage.NOT_ENOUGH_MONEY);
+		}
+	}
+
+	public Optional<Product> isExist(String name, VendingMachine machine) {
+		Optional<Product> product = machine.getProducts().stream().filter(p -> p.getName().equals(name)).findFirst();
+		if (!product.isPresent()) {
+			throw new IllegalArgumentException(ExceptionMessage.NOT_EXIST_PRODUCT);
+		}
+		return product;
+	}
+
+	public void isStock(String name, VendingMachine machine) {
+		if (machine.isSoldOut(name)) {
+			throw new IllegalArgumentException(ExceptionMessage.SOLD_OUT_PRODUCT);
+		}
+	}
+}
