1fcda9ab0ab99c19a92c8dce590797758db93f6c
koo <gudonghee2000@gmail.com>
2021-12-14 16:35:35 +0900

    refactor : Product클래스의 매직넘버를 상수화하고 변수, 함수명을 변경

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,32 +1,37 @@
 package model;
 
 public class Product {
+	private static final int MIN_STOCK_COUNT = 1;
+	private static final int MINUS_STOCK_COUNT = 1;
+	private static final int NO_STOCK_COUNT = 0;
+	private static final String NO_PRODUCT_STOCK_ERROR_MESSAGE = "[ERROR] 입력한 구매 상품명의 상품 재고가 없다.";
+
 	private final String name;
 	private final int price;
-	private int count;
+	private int stock;
 
-	public Product(String name, int price, int count) {
+	public Product(String name, int price, int stock) {
 		this.name = name;
 		this.price = price;
-		this.count = count;
+		this.stock = stock;
 	}
 
 	public int getPrice() {
 		return price;
 	}
 
-	public void reduceCount() {
-		if (count <= 0) {
-			throw new IllegalArgumentException("[ERROR] 입력한 구매 상품명의 상품 재고가 없다.");
+	public void reduceStock() {
+		if (stock < MIN_STOCK_COUNT) {
+			throw new IllegalArgumentException(NO_PRODUCT_STOCK_ERROR_MESSAGE);
 		}
-		count -= 1;
+		stock -= MINUS_STOCK_COUNT;
 	}
 
 	public boolean isSoldOut() {
-		return count == 0;
+		return stock == NO_STOCK_COUNT;
 	}
 
-	public boolean isSameName(String productName) {
+	public boolean hasSameName(String productName) {
 		return name.equals(productName);
 	}
 }
