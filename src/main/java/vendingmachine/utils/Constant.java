c5c167bddaf3b8d421dbfac4d577b9f0886b6fd2
harinn1014 <gkfla7555@gmail.com>
2021-12-14 20:18:16 +0900

    feat(Constant): create utils folder and Constant class
    <description>
    - 변하지 않는 값들을(상수나 예외 메시지) 관리할 utils folder를 생성
    - Constant: 상수들을 담은 클래스를 생성하고 상수 값 추가

---
+++ src/main/java/vendingmachine/utils/Constant.java
@@ -0,0 +1,27 @@
+package vendingmachine.utils;
+
+public class Constant {
+	public static final int NAME_POSITION = 0;
+	public static final int PRICE_POSITION = 1;
+	public static final int STOCK_POSITION = 2;
+	
+	public static final int ZERO = 0;
+	public static final int LIMIT_PAY = 100;
+	public static final int UNIT_MOD = 10;
+	public static final int ELEMENT = 3;
+
+	public static final String DELIMITER_PRODUNT = ";";
+	public static final String DELIMITER_CONTENT = ",";
+	public static final String UNIT_PAY = "원";
+	public static final String BETWEEN_UNIT = " - ";
+	public static final String UNIT_COUNT = "개";
+
+	public static final String INPUT_AMOUNT = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+	public static final String VENDINGMACHINE_COIN = "자판기가 보유한 동전";
+	public static final String INPUT_PRODUCTS = "상품명과 가격, 수량을 입력해 주세요.";
+	public static final String INPUT_MONEY = "투입 금액을 입력해 주세요.";
+	public static final String INFORMATION_PAY = "투입 금액: ";
+	public static final String INPUT_PRODUCT_NAME = "구매할 상품명을 입력해 주세요.";
+	public static final String RESULT_CHANGE = "잔돈";
+
+}
