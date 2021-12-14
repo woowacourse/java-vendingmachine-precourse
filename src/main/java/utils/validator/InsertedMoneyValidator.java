7fad1eb16f2ee71d52819f73d42df714879da3c7
koo <gudonghee2000@gmail.com>
2021-12-14 16:06:49 +0900

    feat : 입력한 투입 금액이 10으로 나누어 떨어지는지 검사하는 기능 생성

--- src/main/java/utils/validator/InsertedMoneyValidator.java
+++ src/main/java/utils/validator/InsertedMoneyValidator.java
@@ -3,6 +3,8 @@ package utils.validator;
 import java.util.regex.Pattern;
 
 public class InsertedMoneyValidator {
+	private static final int MINIMUM_MONEY_UNIT = 10;
+	private static final int RIGHT_NUMBER = 0;
 	private static final int MIN_INSERTED_MONEY_INPUT_LENGTH = 1;
 	private static final String ZERO = "0";
 	private static final String BLANK = " ";
@@ -11,12 +13,14 @@ public class InsertedMoneyValidator {
 	private static final String HAS_BLANK_ERROR_MESSAGE = "[ERROR] 입력한 투입 금액에 공백이 있다.";
 	private static final String NOT_RIGHT_INSERTED_MONEY_ERROR_MESSAGE = "[ERROR] 입력한 투입 금액이 올바른수(양수)가 아니다.";
 	private static final String START_INSERTED_MONEY_NUMBER_ERROR_MESSAGE = "[ERROR] 입력한 투입 금액이 0으로 시작한다.";
+	private static final String NOT_RIGHT_INSERTED_MONEY_UNIT_ERROR_MESSAGE = "[ERROR] 입력한 투입 금액이 10으로 나누어 떨어지지 않는다.";
 
 	private InsertedMoneyValidator() {
 	}
 
 	public static int checkValidInsertedMoney(String insertedMoney) {
-		if (isValidInputLength(insertedMoney) && !hasBlankInInput(insertedMoney) && isRightMoneyNumber(insertedMoney)) {
+		if (isValidInputLength(insertedMoney) && !hasBlankInInput(insertedMoney) && isRightMoneyNumber(insertedMoney)
+			&& isDivisibleByMinimumMoneyUnit(insertedMoney)) {
 			return Integer.parseInt(insertedMoney);
 		}
 		throw new IllegalArgumentException();
@@ -42,6 +46,13 @@ public class InsertedMoneyValidator {
 		}
 		if (insertedMoney.startsWith(ZERO)) {
 			throw new IllegalArgumentException(START_INSERTED_MONEY_NUMBER_ERROR_MESSAGE);
+		}
+		return true;
+	}
+
+	private static boolean isDivisibleByMinimumMoneyUnit(String insertedMoney) {
+		if (Integer.parseInt(insertedMoney) % MINIMUM_MONEY_UNIT != RIGHT_NUMBER) {
+			throw new IllegalArgumentException(NOT_RIGHT_INSERTED_MONEY_UNIT_ERROR_MESSAGE);
 		}
 		return true;
 	}
