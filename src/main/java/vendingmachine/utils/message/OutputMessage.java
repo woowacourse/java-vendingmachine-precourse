0cc854be1ff89ce8609448e04bd90129b4151e02
nayoung <evelyn82ny@gmail.com>
2021-12-14 15:36:11 +0900

    feat: 출력 메시지 추가

--- src/main/java/vendingmachine/utils/message/OutputMessage.java
+++ src/main/java/vendingmachine/utils/message/OutputMessage.java
@@ -2,5 +2,13 @@ package vendingmachine.utils.message;
 
 public class OutputMessage {
 
-    public static final String CHANGE_FORMAT = "amount원 - count개";
+    public static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
+    public static final String COIN_FORMAT = "amount원 - count개";
+    public static final String VENDING_MACHINE_BALANCE_INPUT = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    public static final String VENDING_MACHINE_COIN_PRINT_TITLE = "자판기가 보유한 동전";
+    public static final String ITEM_INPUT = "상품명과 가격, 수량을 입력해 주세요.";
+    public static final String CUSTOMER_BALANCE_INPUT = "투입 금액을 입력해 주세요.";
+    public static final String CUSTOMER_BALANCE_PRINT_FORMAT = "투입 금액: balance원";
+    public static final String PURCHASE_ITEM_NAME = "구매할 상품평을 입력해 주세요.";
+    public static final String CHANGE = "잔돈";
 }
