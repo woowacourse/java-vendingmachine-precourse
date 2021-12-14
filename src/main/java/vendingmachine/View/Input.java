9fc6b2f501109b6e0dfa0e38ba9428ac328d6f2c
sejung-hong <sejungh@naver.com>
2021-12-14 20:59:34 +0900

    fix : Input 클래스 생성 콘솔에서 입력받는 클래스

---
+++ src/main/java/vendingmachine/View/Input.java
@@ -0,0 +1,9 @@
+package vendingmachine.View;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+    public static String ConsoleInput(){
+        return Console.readLine();
+    }
+}
