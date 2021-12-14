211f43acb10da3c79f1a50bd8aa3ddf534f3f755
hwihwi99 <chamgrace@naver.com>
2021-12-14 21:42:06 +0900

    feat : 메인함수 구현

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,7 +1,11 @@
 package vendingmachine;
 
+import vendingmachine.machine.MachineController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        MachineController machineController = new MachineController();
+        machineController.initMachine();
     }
 }
