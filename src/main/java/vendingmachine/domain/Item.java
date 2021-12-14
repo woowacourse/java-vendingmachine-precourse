484085c2a82443274324a8a665417774dcd78729
xfrnk2 <xfrnk2@gmail.com>
2021-12-14 19:32:08 +0900

    refactor(Item): 예외 처리 메시지를 구체화

--- src/main/java/vendingmachine/domain/Item.java
+++ src/main/java/vendingmachine/domain/Item.java
@@ -12,8 +12,13 @@ public class Item {
 	private static final int COST_INDEX = 1;
 	private static final int AMOUNT_INDEX = 2;
 
+	private static final String COST = "가격";
+	private static final String AMOUNT = "수량";
 	private static final String ERROR = "[ERROR] ";
-	private static final String INVALID_FORMAT_ERROR = ERROR + "잘못된 포맷의 입력입니다.";
+	private static final String ERROR_INVALID_NUMBER_OF_TYPE = ERROR + "상품 정보는 %s, %s, %s의 %d 단위로 입력해야 합니다.";
+	private static final String ERROR_NON_DIGIT = ERROR + "상품 %s의 %s이 숫자여야 합니다.";
+	private static final String ERROR_NOT_DIVISIBLE_BY_TEN = ERROR + "상품 %s의 %s이 %d으로 나누어 떨어져야 합니다.";
+	private static final String ERROR_INVALID_DIGIT_RANGE = ERROR + "상품 %s의 %s이 %d 이상이여야 합니다.";
 
 	private final String name;
 	private final int cost;
@@ -22,32 +27,33 @@ public class Item {
 	public Item(final List<String> detailContainer) {
 		checkLength(detailContainer);
 		this.name = detailContainer.get(NAME_INDEX);
-		this.cost = isNumber(detailContainer.get(COST_INDEX));
-		this.amount = isNumber(detailContainer.get(AMOUNT_INDEX));
+		this.cost = isNumber(detailContainer.get(COST_INDEX), COST);
+		this.amount = isNumber(detailContainer.get(AMOUNT_INDEX), AMOUNT);
 		validateCost(cost);
 		validateAmount(amount);
 	}
 
 	private void checkLength(final List<String> detailContainer) {
 		if (detailContainer.size() != NUMBER_OF_TYPE) {
-			throw new IllegalArgumentException(INVALID_FORMAT_ERROR);
+			throw new IllegalArgumentException(
+				String.format(ERROR_INVALID_NUMBER_OF_TYPE, "상품명", COST, AMOUNT, NUMBER_OF_TYPE));
 		}
 	}
 
-	private int isNumber(final String value) {
+	private int isNumber(final String value, final String type) {
 		try {
 			return Integer.parseInt(value);
 		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException(INVALID_FORMAT_ERROR);
+			throw new IllegalArgumentException(String.format(ERROR_NON_DIGIT, name, type));
 		}
 	}
 
-	private void validateCost(final int cost) {
+	private void validateCost(int cost) {
 		if (!isValidCostRange(cost)) {
-			throw new IllegalArgumentException(INVALID_FORMAT_ERROR);
+			throw new IllegalArgumentException(String.format(ERROR_INVALID_DIGIT_RANGE, name, COST, COST_LOWER_BOUND));
 		}
 		if (!isDivisibleByTen(cost)) {
-			throw new IllegalArgumentException(INVALID_FORMAT_ERROR);
+			throw new IllegalArgumentException(String.format(ERROR_NOT_DIVISIBLE_BY_TEN, name, COST, TEN));
 		}
 	}
 
@@ -61,7 +67,7 @@ public class Item {
 
 	private void validateAmount(final int amount) {
 		if (!isValidAmountRange(amount)) {
-			throw new IllegalArgumentException(INVALID_FORMAT_ERROR);
+			throw new IllegalArgumentException(ERROR_INVALID_DIGIT_RANGE);
 		}
 	}
 
