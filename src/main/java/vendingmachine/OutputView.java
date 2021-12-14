2a160aa51ebc10a48798fef262add6f70c672193
khj1 <74335027+khj1@users.noreply.github.com>
2021-12-14 18:48:32 +0900

    feat: 잔돈을 출력하는 기능 추가
    - 반환되는 잔돈을 문자열로 출력하는 기능을 추가했습니다.

--- src/main/java/vendingmachine/OutputView.java
+++ src/main/java/vendingmachine/OutputView.java
@@ -8,4 +8,8 @@ public class OutputView {
     public static void printInputAmount(VendingMachine vendingMachine) {
         System.out.println(vendingMachine.toStringInputAmount());
     }
+
+    public static void printChanges(VendingMachine vendingMachine) {
+        System.out.println(vendingMachine.toStringChanges());
+    }
 }
