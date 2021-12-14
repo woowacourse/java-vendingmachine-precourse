1ebabc4dab77e8c81bfc7be0492ce393ff0dc350
xi_jjun <rlawowns97@naver.com>
2021-12-14 21:18:05 +0900

    refactor: 필요없는 메서드 제거와 Item 생성해주는 메서드 추가
    Added Method
    
    Items에서는 Item의 목록만 생성해야 한다고 생각했다. Item의 생성 자체는 Item의 책임이라 생각하여 추가.
    public static Item registerItem(String[] detailInfo, List<Item> registeredItems)
            : Item 객체를 생성하여 반환.
    
            parameters
                    String[] detailInfo : Item에 들어갈 정보 배열. 이름, 가격, 재고량.
                    List<Item> registeredItems : 이미 등록된 Item 객체 리스트.
            return
                    detailInfo가 적절한지 검증한 뒤 적절하다면 해당 정보로 만든 Item 객체 반환.
    
    private static void checkValidation(String registerName, int price, int stock, List<Item> registeredItems)
            : Validator 객체를 사용하여 Item에 들어갈 정보가 적절한지 판단. 적절하지 않다면 IllegalArgumentException.
    
    OrderService에서 계속 구입할 수 있는지에 대한 판단을 해주는 canPurchaseAnyItems에서 사용하기 위해 생성.
    객체에 메세지를 던지는 형태로 구현하기 위함.
    public boolean canPurchase(int userMoney) : 해당 Item의 재고량이 남아있고 userMoney로 해당 Item을 살 수 있다면 true return.
    
    Items와 Validator에서 Item객체로 메세지를 던지게 하기 위해 생성
    public boolean isSameName(String name) : name이 해당 Item의 이름과 같으면 true return.
    
    Removed Method
    
    객체에 메세지를 던져서 협력하게 하기 위해서 getter 모두 삭제.

--- src/main/java/vendingmachine/domain/Item.java
+++ src/main/java/vendingmachine/domain/Item.java
@@ -1,9 +1,12 @@
 package vendingmachine.domain;
 
-import vendingmachine.constants.Constant;
-
 import static vendingmachine.constants.Constant.*;
 import static vendingmachine.constants.Message.*;
+
+import java.util.List;
+
+import vendingmachine.utils.Parser;
+import vendingmachine.utils.Validator;
 
 public class Item {
 	private final String name;
@@ -16,29 +19,40 @@ public class Item {
 		this.stock = stock;
 	}
 
+	public static Item registerItem(String[] detailInfo, List<Item> registeredItems) {
+		String name = detailInfo[0];
+		int price = Parser.makeInteger(detailInfo[1]);
+		int stock = Parser.makeInteger(detailInfo[2]);
+		checkValidation(name, price, stock, registeredItems);
+
+		return new Item(name, price, stock);
+	}
+
+	private static void checkValidation(String registerName, int price, int stock, List<Item> registeredItems) {
+		Validator.validateAddingItemName(registerName, registeredItems);
+		Validator.validatePrice(price);
+		Validator.validateStock(stock);
+	}
+
+	public boolean canPurchase(int userMoney) {
+		return isEnoughMoney(userMoney) && !isRunOutOfStock();
+	}
+
 	public int order(int userMoney) {
 		if (isEnoughMoney(userMoney)) {
 			decreaseStockCount();
 			return userMoney - price;
 		}
 
-		return userMoney;
+		throw new IllegalArgumentException(ERROR_NOT_ENOUGH_MONEY);
 	}
 
 	private boolean isEnoughMoney(int userMoney) {
 		return userMoney >= price;
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public int getPrice() {
-		return price;
-	}
-
-	public int remainedStock() {
-		return stock;
+	public boolean isSameName(String name) {
+		return this.name.equals(name);
 	}
 
 	public void decreaseStockCount() {
