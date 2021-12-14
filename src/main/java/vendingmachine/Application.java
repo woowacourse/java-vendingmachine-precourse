0f712ff399ac80efdfc0a805a9e4b96c705c6d5e
woojaang <woojaang@naver.com>
2021-12-14 21:31:18 +0900

    View, Controller객체 생성

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,7 +1,15 @@
 package vendingmachine;
 
 public class Application {
+    public static void startApplication(Controller controller) {
+
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        View view = new View();
+        VendingMachine vendingMachine = new VendingMachine();
+        Controller controller = new Controller(vendingMachine, view);
+
+        startApplication(controller);
     }
 }
---
+++ src/main/java/vendingmachine/Controller.java
@@ -0,0 +1,29 @@
+package vendingmachine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Controller {
+    private static List<Coin> coins;
+    private View view;
+    private VendingMachine vendingMachine;
+
+    public Controller(VendingMachine vendingMachine, View view) {
+        this.view = view;
+        this.vendingMachine = vendingMachine;
+        this.coins = makeCoins();
+    }
+
+    public static List<Coin> makeCoins() {
+        List<Coin> coins = new ArrayList<>();
+        coins.add(Coin.COIN_500);
+        coins.add(Coin.COIN_100);
+        coins.add(Coin.COIN_50);
+        coins.add(Coin.COIN_10);
+        return coins;
+    }
+
+    public List<Coin> getCoins() {
+        return this.coins;
+    }
+}
