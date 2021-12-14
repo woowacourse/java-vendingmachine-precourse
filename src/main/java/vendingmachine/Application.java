9b966859428d5903ac3f948c51c22c1510550dc0
skennetho <thrkd613@naver.com>
2021-12-14 17:39:21 +0900

    feat: add 자판기 시작 기능 추가

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,7 +1,10 @@
 package vendingmachine;
 
+import vendingmachine.controller.MachineCore;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        new MachineCore().startMachine();
     }
 }
