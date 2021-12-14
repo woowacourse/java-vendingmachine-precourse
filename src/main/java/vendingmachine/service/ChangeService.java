734bab4378ef82d39639aa7b9ca16ba375c27d75
Kangho-Noh <rkdgh98@khu.ac.kr>
2021-12-14 14:08:36 +0900

    fix(ChangeService): 잔돈 구하는 로직 수정
    - 기존: for문에서 Map의 key를 가져오는 형태
    - 변경: Coin의 enum.values를 사용하여 가져옴
    - 기존의 형태는 key를 내림차순으로 가져오지 않아서 잔돈을 잘못 출력하는 경우 발생
    - 변경 후 500원 부터 내림차순으로 구해 그리디 알고리즘에 알맞게 잔돈을 구한다.

--- src/main/java/vendingmachine/service/ChangeService.java
+++ src/main/java/vendingmachine/service/ChangeService.java
@@ -2,8 +2,8 @@ package vendingmachine.service;
 
 import java.util.Map;
 
-import vendingmachine.repository.Change;
 import vendingmachine.domain.Coin;
+import vendingmachine.repository.Change;
 import vendingmachine.repository.UserAccount;
 import vendingmachine.repository.VendingMachineAccount;
 
@@ -12,9 +12,10 @@ public class ChangeService {
 	public static void calculateChange() {
 		int account = UserAccount.getAccount();
 		Map<Coin, Integer> allCoinCount = VendingMachineAccount.getAllCoinCount();
-		for (Coin coin : allCoinCount.keySet()) {
-			int coinValue = coin.getAmount();
+
+		for (Coin coin : Coin.values()) {
 			int maxCoin = calculateMaxCoin(coin, allCoinCount, account);
+			int coinValue = coin.getAmount();
 			account -= coinValue * maxCoin;
 		}
 	}
