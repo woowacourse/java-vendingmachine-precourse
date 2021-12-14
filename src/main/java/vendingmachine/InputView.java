c3f652ff1fd996736ab1909f5247e2130fc28283
khj1 <74335027+khj1@users.noreply.github.com>
2021-12-14 19:52:47 +0900

    refactor: 입력 값이 숫자인지 검증하는 기능 추가
    - 투입 금액, 자판기 보유 금액 입력 값에 대한 숫자 검증 로직을 추가했습니다.

--- src/main/java/vendingmachine/InputView.java
+++ src/main/java/vendingmachine/InputView.java
@@ -7,10 +7,15 @@ public class InputView {
     private static final String INPUT_PRODUCTS_INFO_MESSAGE = "상품명과 가격, 수량을 입력해 주세요.";
     private static final String INPUT_INPUT_AMOUNT_MESSAGE = "투입 금액을 입력해 주세요.";
     private static final String INPUT_DEMAND_PRODUCT_NAME = "구매할 상품명을 입력해 주세요.";
+    private static final String ERROR_NOT_NUMERIC_AMOUNT_INPUT_MESSAGE = "금액에는 정수를 입력해주세요.";
 
-    public static String getHoldingAmount() {
+    public static int getHoldingAmount() {
         System.out.println(INPUT_HOLDING_AMOUNTS_MESSAGE);
-        return Console.readLine();
+        String holdingAmountString = Console.readLine();
+
+        validateAmountIsNumeric(holdingAmountString);
+
+        return Integer.parseInt(holdingAmountString);
     }
 
     public static String getProductsInfo() {
@@ -18,13 +23,23 @@ public class InputView {
         return Console.readLine();
     }
 
-    public static String getInputAmount() {
+    public static int getInputAmount() {
         System.out.println(INPUT_INPUT_AMOUNT_MESSAGE);
-        return Console.readLine();
+        String inputAmountString = Console.readLine();
+
+        validateAmountIsNumeric(inputAmountString);
+
+        return Integer.parseInt(inputAmountString);
     }
 
     public static String getDemandProductName() {
         System.out.println(INPUT_DEMAND_PRODUCT_NAME);
         return Console.readLine();
+    }
+
+    private static void validateAmountIsNumeric(String holdingAmountString) {
+        if (!StringUtils.isNumeric(holdingAmountString)) {
+            throw new IllegalArgumentException(ERROR_NOT_NUMERIC_AMOUNT_INPUT_MESSAGE);
+        }
     }
 }
