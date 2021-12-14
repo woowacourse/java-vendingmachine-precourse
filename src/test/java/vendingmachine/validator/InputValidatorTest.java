4ebd6332b43c38fec42de42a4e2a111e664fd501
Gyuchool <balance0591@naver.com>
2021-12-14 23:00:30 +0900

    test(validator): 금액이 100원 이상 10원으로 나누어 떨어질때는 예외가 발생하지 않는지 검증 테스트

--- src/test/java/vendingmachine/validator/InputValidatorTest.java
+++ src/test/java/vendingmachine/validator/InputValidatorTest.java
@@ -29,6 +29,17 @@ class InputValidatorTest {
 		assertThatThrownBy(
 			() -> InputValidator.checkInputForm(input)
 		).isInstanceOf(IllegalArgumentException.class);
-
 	}
+
+	@ParameterizedTest
+	@DisplayName("금액이 100원 이상 10원으로 나누어 떨어질때는 예외가 발생하지 않는다. - 성공")
+	@ValueSource(strings = {"1500", "100"})
+	void checkMoneyForm(String input) {
+		//given
+		//when then
+		assertThatCode(
+			() -> InputValidator.checkMoneyForm(input)
+		).doesNotThrowAnyException();
+	}
+
 }
