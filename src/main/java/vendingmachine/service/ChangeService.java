88bbe867ec379007ce90470361bf1dacc819a56b
xi_jjun <rlawowns97@naver.com>
2021-12-14 22:26:13 +0900

    refactor: divide method
    returnChange 메서드가 너무 길어서 메서드로 나눴다.
    
    makeChange() : Change Map을 만들어준다.
    
    getChangeMoney : Change Map을 하나 추가해주고 돌려줘야 할 나머지 돈을 반환한다.
    
    canMakeChange : 해당 CoinType으로 Change를 만들 수 있다면 true return.
    
    isEnoughCoins : 해당 가치의 Coin을 돌려줄 수 있을 정도로 changeMoney가 크다면 true return.
    
    hasNoCoins : coin의 개수가 0개이면 true return.
    
    getRemainedChange : change로 줄 coin의 양을 계산한 뒤 남은 change금액 반환.

--- src/main/java/vendingmachine/service/ChangeService.java
+++ src/main/java/vendingmachine/service/ChangeService.java
@@ -3,30 +3,57 @@ package vendingmachine.service;
 import java.util.EnumMap;
 import java.util.Map;
 
+import vendingmachine.constants.Constant;
 import vendingmachine.domain.Coin;
+
+import static vendingmachine.constants.Constant.NO_COIN_COUNT;
 
 public class ChangeService {
 	public Map<Coin, Integer> returnChange(Map<Coin, Integer> vendingMachineCoins, int remainedMoney) {
 		Map<Coin, Integer> change = new EnumMap<>(Coin.class);
 
-		getChange(vendingMachineCoins, change, remainedMoney);
+		makeChange(vendingMachineCoins, change, remainedMoney);
 
 		return change;
 	}
 
-	private void getChange(Map<Coin, Integer> vendingMachineCoins, Map<Coin, Integer> change, int changeMoney) {
+	private void makeChange(Map<Coin, Integer> vendingMachineCoins, Map<Coin, Integer> change, int changeMoney) {
 		for (Map.Entry<Coin, Integer> coin : vendingMachineCoins.entrySet()) {
 			int coinAmount = coin.getKey().getAmount();
 			int coinCount = coin.getValue();
-			if (coinCount == 0 || changeMoney / coinAmount == 0) {
-				continue;
-			} else if (changeMoney / coinAmount > coinCount) {
-				change.put(coin.getKey(), coinCount);
-				changeMoney = changeMoney - (coinAmount * coinCount);
-				continue;
+			int neededCoinCount = changeMoney / coinAmount;
+
+			if (canMakeChange(changeMoney, coinAmount, coinCount)) {
+				changeMoney = getChangeMoney(change, changeMoney, coin, coinAmount, coinCount, neededCoinCount);
 			}
-			change.put(coin.getKey(), changeMoney / coinAmount);
-			changeMoney = changeMoney - (coinAmount * (changeMoney / coinAmount));
 		}
+	}
+
+	private int getChangeMoney(Map<Coin, Integer> change, int changeMoney, Map.Entry<Coin, Integer> coin, int coinAmount, int coinCount, int neededCoinCount) {
+		if (neededCoinCount > coinCount) {
+			change.put(coin.getKey(), coinCount);
+			changeMoney = getRemainedChange(changeMoney, coinAmount, coinCount);
+			return changeMoney;
+		}
+
+		change.put(coin.getKey(), neededCoinCount);
+		changeMoney = getRemainedChange(changeMoney, coinAmount, neededCoinCount);
+		return changeMoney;
+	}
+
+	private boolean canMakeChange(int changeMoney, int coinAmount, int coinCount) {
+		return !hasNoCoins(coinCount) && isEnoughCoinAmount(changeMoney, coinAmount);
+	}
+
+	private boolean isEnoughCoinAmount(int changeMoney, int amount) {
+		return changeMoney / amount > NO_COIN_COUNT;
+	}
+
+	private boolean hasNoCoins(int coinCount) {
+		return coinCount == NO_COIN_COUNT;
+	}
+
+	private int getRemainedChange(int changeMoney, int coinAmount, int count) {
+		return changeMoney - (coinAmount * count);
 	}
 }
