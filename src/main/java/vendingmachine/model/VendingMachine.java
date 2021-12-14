e3fec7f1012f0db5f21bad835e76a3f0e5630ac2
khj1 <74335027+khj1@users.noreply.github.com>
2021-12-14 23:00:57 +0900

    refactor: 투입 금액 출력 로직 이전
    투입 금액을 출력하는 UI 로직을 OutputView 클래스로 이전했습니다.

--- src/main/java/vendingmachine/model/VendingMachine.java
+++ src/main/java/vendingmachine/model/VendingMachine.java
@@ -4,9 +4,7 @@ public class VendingMachine {
     private static final int ZERO = 0;
     private static final String EXCEED_REST_AMOUNT_MESSAGE = "잔액을 초과하여 구매할 수 없습니다.";
     private static final String NOT_DIVIDED_INTO_SEMICOLON_MESSAGE = "상품 정보는 세미콜론으로 구분되어야 합니다.";
-    private static final String TO_STRING_INPUT_AMOUNT_PREFIX = "투입 금액: ";
     private static final String WRONG_SEMICOLON = "][";
-    private static final String AMOUNT_UNIT = "원";
 
     private final Products products;
     private final HoldingCoins holdingCoins;
@@ -16,6 +14,10 @@ public class VendingMachine {
         validateDividedIntoSemicolon(productsInfo);
         this.products = new Products(productsInfo);
         this.holdingCoins = holdingCoins;
+    }
+
+    public int getInputAmount() {
+        return inputAmount;
     }
 
     public void buy(String productName) {
@@ -31,18 +33,12 @@ public class VendingMachine {
         return products.isPurchasable(inputAmount);
     }
 
-    public String toStringChanges() {
-        return new Change(inputAmount, holdingCoins).toString();
-    }
-
-    public String toStringInputAmount() {
-        return new StringBuilder()
-                .append(TO_STRING_INPUT_AMOUNT_PREFIX).append(inputAmount).append(AMOUNT_UNIT)
-                .toString();
-    }
-
     public boolean isRestQuantity(String productName, int productQuantity) {
         return products.findByName(productName).isRestQuantity(productQuantity);
+    }
+
+    public Change getChange() {
+        return new Change(inputAmount, holdingCoins);
     }
 
     private void validateDividedIntoSemicolon(String productsString) {
--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -7,6 +7,8 @@ public class OutputView {
     private static final String CHANGE = "잔돈";
     private static final String HOLDING_COINS = "자판기가 보유한 동전";
     private static final String ERROR_PREFIX = "[ERROR] ";
+    private static final String AMOUNT_UNIT = "원";
+    private static final String TO_STRING_INPUT_AMOUNT_PREFIX = "투입 금액: ";
 
     public static void printHoldingCoins(HoldingCoins holdingCoins) {
         System.out.println(HOLDING_COINS);
@@ -14,12 +16,12 @@ public class OutputView {
     }
 
     public static void printInputAmount(VendingMachine vendingMachine) {
-        System.out.println(vendingMachine.toStringInputAmount());
+        System.out.println(TO_STRING_INPUT_AMOUNT_PREFIX + vendingMachine.getInputAmount() + AMOUNT_UNIT);
     }
 
     public static void printChanges(VendingMachine vendingMachine) {
         System.out.println(CHANGE);
-        System.out.println(vendingMachine.toStringChanges());
+        System.out.println(vendingMachine.getChange());
     }
 
     public static void printError(Exception e) {
