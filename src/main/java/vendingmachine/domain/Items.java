02e7c442d498051730f687569611dc638ef2dd04
xi_jjun <rlawowns97@naver.com>
2021-12-14 21:23:44 +0900

    refactor: Item객체로 메세지 던지게끔 바꾸고 Item생성해주고 검증하는 책임을 제거
    i.getName().equals(name) 은 객체에 메세지를 던지는 형태가 아니다.
    따라서 Item에 isSameName(String name)이라는 메서드를 생성하여 Items에서 name이라는 문자열 메세지를 던져주면,
    Item의 메서드가 책임을 지고 같은 이름인지 알려주게 했다.
    
    Item을 검증하는 Validator에서 Item을 생성하여 Item List에 추가하게 했다. 그러나 이는 Validator의 책임과는 맞지
    않다고 생각함과 동시에 Item의 생성또한 Items에서 할 일이 아니라고 판단하여 Item에게 모두 맡겼다.

--- src/main/java/vendingmachine/domain/Items.java
+++ src/main/java/vendingmachine/domain/Items.java
@@ -6,17 +6,12 @@ import static vendingmachine.utils.Parser.parsingItemInfo;
 import java.util.ArrayList;
 import java.util.List;
 
-import vendingmachine.utils.Validator;
-
 public class Items {
 	private final List<Item> forSaleItems = new ArrayList<>();
 
-	public Items() {
-	}
-
 	public Item findItemByName(String name) {
 		return forSaleItems.stream()
-				.filter(i -> i.getName().equals(name))
+				.filter(i -> i.isSameName(name))
 				.findFirst().orElse(ELEMENT_NOT_FOUNDED);
 	}
 
@@ -25,10 +20,11 @@ public class Items {
 	}
 
 	public Items createForSaleItems(String[] itemsInfo) {
-		for (String info : itemsInfo) {
-			String[] parsedItemInfo = parsingItemInfo(info);
-			Item validItem = Validator.validateItemInfo(parsedItemInfo, forSaleItems);
-			forSaleItems.add(validItem);
+		for (String itemInfo : itemsInfo) {
+			String[] parsedItemInfo = parsingItemInfo(itemInfo);
+			Item registeredItem = Item.registerItem(parsedItemInfo, forSaleItems);
+
+			forSaleItems.add(registeredItem);
 		}
 
 		return this;
