ff904d87abfb06bdc3501efe8cac69ed99e2fcce
harinn1014 <gkfla7555@gmail.com>
2021-12-14 19:23:04 +0900

    feat(Product): create Product class and add getter method
    <description>
    자판기에 입고될 상품의 정보를 저장하는 객체 Product class 생성
    기본 생성자를 만들고 getter method를 추가함.

---
+++ src/main/java/vendingmachine/domain/Product.java
@@ -0,0 +1,27 @@
+package vendingmachine.domain;
+
+public class Product {
+	private String name;
+	private int price;
+	private int stock;
+
+	public Product(String name, int price, int stock) {
+		this.name = name;
+		this.price = price;
+		this.stock = stock;
+	}
+
+	/**** getter ****/
+	public String getName() {
+		return name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+}
