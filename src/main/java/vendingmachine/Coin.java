36d495fc1d882eae8bc2699d31eba99c681553b6
YJ <soltwood6862@naver.com>
2021-12-14 23:48:41 +0900

    feat: coin 클래스 동전 리스트 메소드 추가

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,5 +1,9 @@
 package vendingmachine;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
@@ -13,4 +17,10 @@ public enum Coin {
     }
 
     // 추가 기능 구현
+    public static List<Integer> coinList() {
+        return Arrays.stream(values())
+                .map(coin -> coin.amount)
+                .collect(Collectors.toList());
+    }
+
 }
