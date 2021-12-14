dd28e69d88a2c983fc39a53d0fe89f4dcae83751
usuyn <usuyn000@gmail.com>
2021-12-14 19:36:10 +0900

    feat(Validation): check item information form

--- src/main/java/vendingmachine/domain/Validation.java
+++ src/main/java/vendingmachine/domain/Validation.java
@@ -1,7 +1,12 @@
 package vendingmachine.domain;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.StringTokenizer;
+
 public class Validation {
     private final int ZERO = 0;
+    private final int FIRST = 1;
     private final int DIVISOR = 10;
     private String errorMessage;
 
@@ -38,9 +43,50 @@ public class Validation {
 
     }
 
+    public StringTokenizer getItemToken(String itemInput) {
+        return new StringTokenizer(itemInput, Text.SEMICOLON);
+    }
+
+    public String getItemElement(String REGEX, String item) {
+        Pattern pattern = Pattern.compile(REGEX);
+        Matcher matcher = pattern.matcher(item);
+        matcher.find();
+
+        return matcher.group(FIRST);
+    }
+
+    private void checkItemInputForm(String itemInput) {
+        int lastIndex = itemInput.length() - 1;
+
+        if (itemInput.charAt(ZERO) == Text.SEMICOLON.charAt(ZERO)
+                || itemInput.charAt(lastIndex) == Text.SEMICOLON.charAt(ZERO)) {
+            throwException(ErrorMessage.ITEM_INPUT_FORM);
+        }
+
+    }
+
+    private void checkItemInformationForm(String itemInput) {
+        StringTokenizer items = getItemToken(itemInput);
+
+        while (items.hasMoreTokens()) {
+
+            if (!items.nextToken().matches(Text.REGEX_ITEM_FORM)) {
+                throwException(ErrorMessage.ITEM_INPUT_FORM);
+            }
+
+        }
+
+    }
+
     public void isValidBalanceInput(String balance) {
         checkEmptyInput(balance);
         checkBalanceOnlyNumber(balance);
         checkBalanceRange(balance);
+    }
+
+    public void isValidItemInput(String itemInput) {
+        checkEmptyInput(itemInput);
+        checkItemInputForm(itemInput);
+        checkItemInformationForm(itemInput);
     }
 }
