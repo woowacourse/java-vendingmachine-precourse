90f5ac8a1fdf7c321c67d04706063c4c56dd975c
Ohzzi <clover7even@naver.com>
2021-12-14 18:55:44 +0900

    comment: 클래스 역할에 대한 주석 추가

--- src/main/java/vendingmachine/exception/RequestChangesException.java
+++ src/main/java/vendingmachine/exception/RequestChangesException.java
@@ -1,5 +1,8 @@
 package vendingmachine.exception;
 
+/**
+ * 잔돈 반환 요구가 들어왔을 때 발생시키는 예외
+ */
 public class RequestChangesException extends RuntimeException {
 	public RequestChangesException() {
 		super();
--- src/main/java/vendingmachine/utils/DataParser.java
+++ src/main/java/vendingmachine/utils/DataParser.java
@@ -1,5 +1,9 @@
 package vendingmachine.utils;
 
+/**
+ * String -> int 변환할 때 사용할 클래스
+ * NumberFormatException catch 해서 IllegalArgumentException으로 바꿔서 던져준다.
+ */
 public class DataParser {
 	public static int parseInt(String string, String errorMessage) {
 		try {
