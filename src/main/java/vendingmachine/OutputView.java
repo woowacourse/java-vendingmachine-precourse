fb72ed983742266de599c44605ee8ca4ad38e502
khj1 <74335027+khj1@users.noreply.github.com>
2021-12-14 19:50:54 +0900

    feat: 에러문구 출력 기능 추가
    OutputView를 통해 에러 문구를 출력할 수 있도록 기능을 추가했습니다.

--- src/main/java/vendingmachine/OutputView.java
+++ src/main/java/vendingmachine/OutputView.java
@@ -3,10 +3,11 @@ package vendingmachine;
 public class OutputView {
     private static final String CHANGE = "잔돈";
     private static final String HOLDING_COINS = "자판기가 보유한 동전";
+    private static final String ERROR_PREFIX = "[ERROR]";
 
-    public static void printHoldingCoins(VendingMachine vendingMachine) {
+    public static void printHoldingCoins(HoldingCoins holdingCoins) {
         System.out.println(HOLDING_COINS);
-        System.out.println(vendingMachine.toStringHoldingCoins());
+        System.out.println(holdingCoins);
     }
 
     public static void printInputAmount(VendingMachine vendingMachine) {
@@ -16,5 +17,9 @@ public class OutputView {
     public static void printChanges(VendingMachine vendingMachine) {
         System.out.println(CHANGE);
         System.out.println(vendingMachine.toStringChanges());
+    }
+
+    public static void printError(Exception e) {
+        System.out.println(ERROR_PREFIX + e.getMessage());
     }
 }
