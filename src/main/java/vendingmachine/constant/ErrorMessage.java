43b4a6a04a7ca5ee5423457a5ac38c5a59608ede
seongwoo <seongwoo0513@uos.ac.kr>
2021-12-14 23:14:50 +0900

    refactor: 잘못된 금액 입력 시 출력되는 에러 메시지 수정

--- src/main/java/vendingmachine/constant/ErrorMessage.java
+++ src/main/java/vendingmachine/constant/ErrorMessage.java
@@ -1,7 +1,7 @@
 package vendingmachine.constant;
 
 public class ErrorMessage {
-	public static final String INPUT_IS_NOT_INTEGER_ERROR_MESSAGE = "[ERROR] 입력되는 수는 2,147,483,647이하의 정수여야합니다.\n";
+	public static final String INPUT_IS_NOT_INTEGER_ERROR_MESSAGE = "[ERROR] 입력되는 수는 2,147,483,647이하의 양의 정수여야합니다.\n";
 	public static final String LESS_THAN_CERTAIN_COIN_ERROR = "[ERROR] 금액은 %d원보다 커야 합니다.\n";
 	public static final String NOT_DIVISIBLE_BY_SMALLEST_COIN_ERROR_MESSAGE = "[ERROR] 입력받은 금액이 %d원으로 나누어지떨어지지 않습니다.\n";
 	public static final String PRODUCT_IS_NOT_MATCHED_REGEX_ERROR_MESSAGE = "[ERROR] 상품은 [상품명,가격,수량]의 형식으로 입력하세요.\n";
