c6a23ecdeafa258c3b5104d800f347eb48a0c814
cjlee38 <gptpem38@gmail.com>
2021-12-14 21:24:36 +0900

    style(validation): 정규표현식 에러 시 에러 메시지 수정

--- src/main/java/vendingmachine/validation/RegexMatchValidation.java
+++ src/main/java/vendingmachine/validation/RegexMatchValidation.java
@@ -3,12 +3,12 @@ package vendingmachine.validation;
 import java.util.regex.Matcher;
 
 public class RegexMatchValidation implements Validation<Matcher> {
-	private static final String FORMAT = "%s '%s'은 잘못된 입력입니다.(예시. '[콜라,1000,10]')";
+	private static final String FORMAT = "%s 의 입력이 잘못되었습니다. (예시. '[콜라,1000,10]')";
 
 	@Override
 	public void validate(Matcher target, String name) throws IllegalArgumentException {
 		if (!target.matches()) {
-			throw new IllegalArgumentException(String.format(FORMAT, name, target));
+			throw new IllegalArgumentException(String.format(FORMAT, name));
 		}
 	}
 }
