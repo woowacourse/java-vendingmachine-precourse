fc771cac442cd6ab1be2e28a722864aa60a11cc6
YuJeong <yjfflq4872@gmail.com>
2021-12-14 09:54:47 +0900

    feat: 차감된 투입 금액 출력 기능 구현

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -3,18 +3,27 @@ package vendingmachine.view;
 import java.util.LinkedHashMap;
 
 public class OutputView {
-	private static final String GENERATED_COIN_MSG = "자판기가 보유한 동전";
-	private static final String COIN_WON_MSG = "원 - ";
+	private static final String DISPLAY_MACHINE_COIN_MSG = "자판기가 보유한 동전";
+	private static final String DISPLAY_USER_MONEY_MSG = "투입 금액";
+	private static final String COIN_UNIT_WON = "원";
 	private static final String COIN_COUNT_UNIT = "개";
 
 	public void displayGeneratedCoin(LinkedHashMap<Integer, Integer> coinCount) {
 		StringBuilder sb = new StringBuilder();
-		sb.append("\n").append(GENERATED_COIN_MSG);
+		sb.append("\n").append(DISPLAY_MACHINE_COIN_MSG);
 
 		coinCount.forEach((coin, count) -> {
-			sb.append("\n").append(coin.toString()).append(COIN_WON_MSG)
+			sb.append("\n").append(coin.toString()).append(COIN_UNIT_WON).append(" - ")
 				.append(count.toString()).append(COIN_COUNT_UNIT);
 		});
+
+		System.out.println(sb);
+	}
+
+	public void displayUserMoney(int change) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("\n").append(DISPLAY_USER_MONEY_MSG).append(": ")
+			.append(Integer.toString(change)).append(COIN_UNIT_WON);
 
 		System.out.println(sb);
 	}
