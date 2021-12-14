2b0ddc910e1d5e18ff25a026c333d947d2359858
khj1 <74335027+khj1@users.noreply.github.com>
2021-12-14 22:08:17 +0900

    feat: 공백 입력 시 예외 처리 기능 추가

--- src/main/java/vendingmachine/InputView.java
+++ src/main/java/vendingmachine/InputView.java
@@ -8,11 +8,13 @@ public class InputView {
     private static final String INPUT_INPUT_AMOUNT_MESSAGE = "투입 금액을 입력해 주세요.";
     private static final String INPUT_DEMAND_PRODUCT_NAME = "구매할 상품명을 입력해 주세요.";
     private static final String ERROR_NOT_NUMERIC_AMOUNT_INPUT_MESSAGE = "금액에는 정수를 입력해주세요.";
+    private static final String ERROR_EMPTY_INPUT_MESSAGE = "값을 입력해주세요.";
 
     public static int getHoldingAmount() {
         System.out.println(INPUT_HOLDING_AMOUNTS_MESSAGE);
         String holdingAmountString = Console.readLine();
 
+        validateIsNotNull(holdingAmountString);
         validateAmountIsNumeric(holdingAmountString);
 
         return Integer.parseInt(holdingAmountString);
@@ -20,13 +22,18 @@ public class InputView {
 
     public static String getProductsInfo() {
         System.out.println(INPUT_PRODUCTS_INFO_MESSAGE);
-        return Console.readLine();
+        String productsInfo = Console.readLine();
+
+        validateIsNotNull(productsInfo);
+
+        return productsInfo;
     }
 
     public static int getInputAmount() {
         System.out.println(INPUT_INPUT_AMOUNT_MESSAGE);
         String inputAmountString = Console.readLine();
 
+        validateIsNotNull(inputAmountString);
         validateAmountIsNumeric(inputAmountString);
 
         return Integer.parseInt(inputAmountString);
@@ -34,7 +41,17 @@ public class InputView {
 
     public static String getDemandProductName() {
         System.out.println(INPUT_DEMAND_PRODUCT_NAME);
-        return Console.readLine();
+        String productName = Console.readLine();
+
+        validateIsNotNull(productName);
+
+        return productName;
+    }
+
+    private static void validateIsNotNull(String input) {
+        if (StringUtils.isEmpty(input)) {
+            throw new IllegalArgumentException(ERROR_EMPTY_INPUT_MESSAGE);
+        }
     }
 
     private static void validateAmountIsNumeric(String holdingAmountString) {
--- src/main/java/vendingmachine/StringUtils.java
+++ src/main/java/vendingmachine/StringUtils.java
@@ -4,4 +4,8 @@ public class StringUtils {
     public static boolean isNumeric(String string) {
         return string.chars().allMatch(Character::isDigit);
     }
+
+    public static boolean isEmpty(String string) {
+        return string == null || string.equals("");
+    }
 }
