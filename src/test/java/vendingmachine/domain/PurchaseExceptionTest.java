f5d342780871a7bc0a4faca9a92fa862953bb022
yukong <kyb062666@gmail.com>
2021-12-14 21:16:45 +0900

    test: 구매할 상품 예외 테스트 케이스 추가

--- src/test/java/vendingmachine/domain/PurchaseExceptionTest.java
+++ src/test/java/vendingmachine/domain/PurchaseExceptionTest.java
@@ -10,10 +10,10 @@ public class PurchaseExceptionTest extends BasicTest {
 	public String money;
 	public String selectedProduct;
 
-	private PurchaseExceptionTest(String selectedProduct, String errorMessage) {
+	private PurchaseExceptionTest(String products, String money, String selectedProduct, String errorMessage) {
 		this.moneyOfChanges = "450";
-		this.products = "[콜라,1500,20];[사이다,1000,10]";
-		this.money = "3000";
+		this.products = products;
+		this.money = money;
 		this.selectedProduct = selectedProduct;
 		this.errorMessage = errorMessage;
 	}
@@ -21,7 +21,15 @@ public class PurchaseExceptionTest extends BasicTest {
 	public static ArrayList<PurchaseExceptionTest> getPurchaseExceptionTest() {
 		ArrayList<PurchaseExceptionTest> purchaseExceptionTests = new ArrayList<>(MAX_TESTCASE);
 
-		purchaseExceptionTests.add(new PurchaseExceptionTest("스프라이트", NOT_EXIST_PRODUCT_NAME));
+		purchaseExceptionTests.add(new PurchaseExceptionTest(
+			"[콜라,1500,20];[사이다,1000,10]", "3000", "스프라이트", NOT_EXIST_PRODUCT_NAME
+		));
+		purchaseExceptionTests.add(new PurchaseExceptionTest(
+			"[콜라,1500,0];[사이다,1000,0]", "3000", "콜라", NOT_EXIST_PRODUCT
+		));
+		purchaseExceptionTests.add(new PurchaseExceptionTest(
+			"[콜라,1500,20];[사이다,1000,10]", "1200", "콜라", LOWER_MONEY
+		));
 
 		return purchaseExceptionTests;
 	}
