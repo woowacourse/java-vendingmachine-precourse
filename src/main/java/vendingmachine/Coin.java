6816f98120b5a16e93c23bc63944238790caf66c
woojaang <woojaang@naver.com>
2021-12-14 21:05:28 +0900

    동전금액과 갯수 설정하기,받아오기

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,16 +1,28 @@
 package vendingmachine;
 
 public enum Coin {
-    COIN_500(500),
-    COIN_100(100),
-    COIN_50(50),
-    COIN_10(10);
+    COIN_500(500, 0),
+    COIN_100(100, 0),
+    COIN_50(50, 0),
+    COIN_10(10, 0);
 
     private final int amount;
+    private int numberOfCoins;
 
-    Coin(final int amount) {
+    Coin(final int amount, int numberOfCoins) {
         this.amount = amount;
+        this.numberOfCoins = numberOfCoins;
     }
 
-    // 추가 기능 구현
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setNumberOfCoins(int numberOfCoins) {
+        this.numberOfCoins = numberOfCoins;
+    }
+
+    public int getNumberOfCoins() {
+        return numberOfCoins;
+    }
 }
