157d90bb68865e88c3d228d118fa6afa594b4293
harinn1014 <gkfla7555@gmail.com>
2021-12-14 23:34:37 +0900

    feat(InputService): add read method about product list
    <description>
    자판기에 들어갈 상품 목록들을 읽어와서 유효성검사를 하고 리스트로 반환하는 함수 구현

--- src/main/java/vendingmachine/service/InputService.java
+++ src/main/java/vendingmachine/service/InputService.java
@@ -1,6 +1,11 @@
 package vendingmachine.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import vendingmachine.domain.Product;
 import vendingmachine.domain.VendingMachine;
+import vendingmachine.utils.Constant;
 import vendingmachine.validator.InputValidator;
 import vendingmachine.view.InputView;
 import vendingmachine.view.OutputView;
@@ -19,6 +24,39 @@ public class InputService {
 				System.out.println(e.getMessage());
 			}
 		}
+	}
+
+	public List<Product> readProducts() {
+		while (true) {
+			try {
+				OutputView.print_inputProduct();
+				return makeProductList(InputView.getProducts());
+			} catch (IllegalArgumentException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+
+	public List<Product> makeProductList(String[] inputArray) {
+		List<Product> productList = new ArrayList<>();
+		for (int i = 0; i < inputArray.length; i++) {
+			inputArray[i] = validator.isRightFormat(inputArray[i]);
+			validator.isRightPattern(inputArray[i]);
+
+			productList.add(makeProduct(inputArray[i].split(Constant.DELIMITER_CONTENT)));
+		}
+		return productList;
+	}
+
+	public Product makeProduct(String[] input) {
+		for (int i = 0; i < Constant.ELEMENT; i++) {
+			validator.isNull(input[i]);
+		}
+
+		String name = input[Constant.NAME_POSITION];
+		int price = validator.validateMoney(input[Constant.PRICE_POSITION]);
+		int stock = validator.isPositiveNumber(input[Constant.STOCK_POSITION]);
+		return new Product(name, price, stock);
 	}
 
 	public int readMoney() {
