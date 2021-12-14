66638c1cebe6933bc846fa7d89d6fb2e28c32efd
HYUNAHSHIM <hyunahshim@naver.com>
2021-12-14 18:53:42 +0900

    feat: 상품명과 가격, 수량을 입력받는 기능

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,13 +1,14 @@
 package vendingmachine;
 
+import java.util.ArrayList;
+
 public class Application {
     public static int getInitCoin() {
+        System.out.println(Constants.coinInitMsg);
         while (true) {
             try {
                 String input = camp.nextstep.edu.missionutils.Console.readLine();
-
                 InputChecker.checkNumber(input);
-
                 return Integer.parseInt(input);
             } catch (IllegalArgumentException exception) {
                 System.out.println(exception.getMessage());
@@ -24,10 +25,33 @@ public class Application {
         }
     }
 
+    public static ArrayList<Product> getInitProduct() {
+        System.out.println();
+        System.out.println(Constants.productInitMsg);
+        while (true) {
+            try {
+                String input = camp.nextstep.edu.missionutils.Console.readLine();
+                return generateProducts(input);
+            } catch (IllegalArgumentException exception) {
+                System.out.println(exception.getMessage());
+                continue;
+            }
+        }
+    }
+
+    public static ArrayList<Product> generateProducts(String input) {
+        ArrayList<Product> products = new ArrayList<>();
+        String[] splited = input.split(";");
+        // for (String s: splited) {
+        //
+        // }
+        return products;
+    }
+
     public static void main(String[] args) {
-        System.out.println(Constants.coinInitMsg);
         int initCoin = getInitCoin();
         VendingMachine vm = new VendingMachine(initCoin);
         printCoinStatus();
+        ArrayList<Product> products = getInitProduct();
     }
 }
---
+++ src/main/java/vendingmachine/Product.java
@@ -0,0 +1,13 @@
+package vendingmachine;
+
+public class Product {
+	private String name;
+	private int price;
+	private int count;
+
+	public Product(String name, int price, int count) {
+		this.name = name;
+		this.price = price;
+		this.count = count;
+	}
+}
