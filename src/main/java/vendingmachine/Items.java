21e80d778c371ca96e4128dfd1ccd82a892b42a0
inbriz <adoredia@naver.com>
2021-12-14 10:30:09 +0900

    feat: 상품 클래스 생성 판매 관련 메소드 구현

---
+++ src/main/java/vendingmachine/Items.java
@@ -0,0 +1,25 @@
+package vendingmachine;
+
+public class Items {
+	private String name;
+	private int price;
+	private int stock;
+
+	Items(String name, String price, String stock) {
+		this.name = name;
+		this.price = Integer.parseInt(price);
+		this.stock = Integer.parseInt(stock);
+	}
+
+	public boolean sellItem() {
+		if (stock == 0) {
+			return false;
+		}
+		stock--;
+		return true;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+}
