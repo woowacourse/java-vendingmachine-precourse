ce2a20d653e288887d2f466709d4e542bd5fec3c
sinb57 <sinb57@naver.com>
2021-12-14 12:47:37 +0900

    feat: 프로그램 실행부 구현

---
+++ src/main/java/vendingmachine/AppConfig.java
@@ -0,0 +1,34 @@
+package vendingmachine;
+
+import vendingmachine.controller.MachineController;
+import vendingmachine.controller.MachineControllerImpl;
+import vendingmachine.service.MachineService;
+import vendingmachine.service.MachineServiceImpl;
+import vendingmachine.view.InputView;
+import vendingmachine.view.OutputView;
+import vendingmachine.view.reader.ConsoleReader;
+import vendingmachine.view.reader.Reader;
+
+public class AppConfig {
+
+	private static final AppConfig instance = new AppConfig();
+
+	public final Reader reader;
+	public final OutputView outputView;
+	public final InputView inputView;
+	public final MachineService machineService;
+	public final MachineController machineController;
+
+	public AppConfig() {
+		this.reader = new ConsoleReader();
+		this.outputView = new OutputView();
+		this.inputView = new InputView(reader, outputView);
+		this.machineService = new MachineServiceImpl();
+		this.machineController = new MachineControllerImpl(machineService, inputView, outputView);
+	}
+
+	public static AppConfig getInstance() {
+		return instance;
+	}
+
+}
--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,7 +1,32 @@
 package vendingmachine;
 
+import vendingmachine.controller.MachineController;
+
 public class Application {
+
+    private final AppConfig appConfig = AppConfig.getInstance();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application application = new Application();
+        application.run();
     }
+
+    public void run() {
+        MachineController machineController = appConfig.machineController;
+        initVendingMachine(machineController);
+        purchaseProduct(machineController);
+    }
+
+    private void initVendingMachine(MachineController machineController) {
+        machineController.fillWithCoins();
+        machineController.showCoinsOfMachine();
+        machineController.registerProducts();
+    }
+
+    private void purchaseProduct(MachineController machineController) {
+        machineController.depositMoney();
+        machineController.purchaseProducts();
+        machineController.refundMoneyOfUser();
+    }
+
 }
