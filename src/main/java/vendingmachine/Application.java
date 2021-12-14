cd3547c421c20314952bfba9a95ee3ee43c4268b
bonkwon <rnjsqhsrnjs@naver.com>
2021-12-14 22:47:41 +0900

    feature : 잔돈 출력기능 구현

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -9,6 +9,7 @@ public class Application {
             vendingMachine.buy(InputReceiver.getProductName(vendingMachine));
         }
 
+        VendingMachinePrinter.printChanges(vendingMachine.calculateChanges());
     }
 
 }
