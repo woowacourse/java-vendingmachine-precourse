248da3cbbaf0ddb0949bcc26799aa6daf4c851fd
harinn1014 <gkfla7555@gmail.com>
2021-12-14 19:20:53 +0900

    feat(Coin): add getter method
    <description>
    getter method 추가
    : getAmount(), getAmountList()

--- src/main/java/vendingmachine/domain/Coin.java
+++ src/main/java/vendingmachine/domain/Coin.java
@@ -1,5 +1,9 @@
 package vendingmachine.domain;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
@@ -11,6 +15,14 @@ public enum Coin {
     Coin(final int amount) {
         this.amount = amount;
     }
-
-    // 추가 기능 구현
+    
+    public int getAmount() {
+    	return amount;
+    }
+        
+    public static List<Integer> getAmountList(){
+    	return Stream.of(Coin.values())
+    			.map(Coin::getAmount)
+    			.collect(Collectors.toList());
+    }
 }
