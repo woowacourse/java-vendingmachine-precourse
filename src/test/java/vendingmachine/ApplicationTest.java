2bb237c9e1d831e7d61c19bef0bd9bb194b6ce00
skennetho <thrkd613@naver.com>
2021-12-14 18:07:43 +0900

    test: add 자판기의 추가 예외 항목들

--- src/test/java/vendingmachine/ApplicationTest.java
+++ src/test/java/vendingmachine/ApplicationTest.java
@@ -34,6 +34,72 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void test_try_buying_soldout() {
+        assertRandomNumberInListTest(
+            () -> {
+                run("1050", "[콜라,1500,1];[사이다,1300,0]", "2800", "사이다", "콜라");
+                assertThat(output()).contains(
+                    "\"사이다\"은(는) 현재 구매 불가능한 상태입니다."
+                );
+            },
+            100, 100, 100, 100, 500, 100, 50
+        );
+    }
+
+    @Test
+    void test_wrong_product_name() {
+        assertRandomNumberInListTest(
+            () -> {
+                run("450", "[콜라,1500,20];[사이다,1000,10]", "3000", "콜,러", "킬]라", "콜라", "사이다");
+                assertThat(output()).contains(
+                    "[ERROR] 상품명에는 ','나 ';'는 불가합니다.",
+                    "[ERROR] \"킬]라\"라는 이름의 상품이 존재하지 않습니다."
+                );
+            },
+            100, 100, 100, 100, 50
+        );
+    }
+
+    @Test
+    void test_wrong_price() {
+        assertRandomNumberInListTest(
+            () -> {
+                run("450", "[콜라,1500,1];[사이다,1300,10]", "-100", "3000", "콜라", "사이다");
+                assertThat(output()).contains(
+                    "[ERROR] 양의 정수를 입력해주세요.", "잔돈", "100원 - 2개"
+                );
+            },
+            100, 100, 100, 100, 50
+        );
+    }
+
+    @Test
+    void test_zero_change() {
+        assertRandomNumberInListTest(
+            () -> {
+                run("450", "[콜라,1500,1];[사이다,1300,1]", "2800", "콜라", "사이다");
+                assertThat(output()).contains(
+                    "투입 금액: 0원"
+                );
+            },
+            100, 100, 100, 100, 50
+        );
+    }
+
+    @Test
+    void test_zero_stock() {
+        assertRandomNumberInListTest(
+            () -> {
+                run("1050", "[콜라,1500,0];[사이다,1300,0]", "2800", "콜라");
+                assertThat(output()).contains(
+                    "투입 금액: 2800원"
+                );
+            },
+            100, 100, 100, 100, 500, 100, 50
+        );
+    }
+
     @Override
     protected void runMain() {
         Application.main(new String[]{});
