074679384667793f9d6740ea8bad8a61c46da063
hyeonic <dev.hyeonic@gmail.com>
2021-12-14 19:08:50 +0900

    refactor: PossessionMoney 에러 문구 수정

--- src/main/java/vendingmachine/domain/possessionmoney/PossessionMoney.java
+++ src/main/java/vendingmachine/domain/possessionmoney/PossessionMoney.java
@@ -6,7 +6,7 @@ public class PossessionMoney {
 
     private static final String VALID_NUMBER_FORMAT = "[ERROR] 보유 금액은 숫자여야 합니다.";
     private static final String VALID_NEGATIVE_NUMBER = "[ERROR] 보유 금액은 음수가 될 수 없습니다.";
-    private static final String VALID_DIVIDE = "[ERROR] 금액은 10으로 나누어 떨어져야 합니다.";
+    private static final String VALID_DIVIDE = "[ERROR] 보유 금액은 10으로 나누어 떨어져야 합니다.";
 
     private int possessionMoney;
 
