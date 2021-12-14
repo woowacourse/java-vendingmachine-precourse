c5da3186b49a1c87379f75f7826c091ae10e7f31
sejung-hong <sejungh@naver.com>
2021-12-14 23:39:37 +0900

    feat : output 메시지 변수명 지정

--- src/main/java/vendingmachine/View/Output.java
+++ src/main/java/vendingmachine/View/Output.java
@@ -37,4 +37,16 @@ public class Output {
         System.out.println(MSG_INPUT_PURCHASE);
     }
 
+    public static void MSGChanges() {
+        System.out.println(MSG_CHANGES);
+    }
+
+    public static void PrintChanges(Map<Coin, Integer> changes){
+        for(Map.Entry<Coin,Integer> m : changes.entrySet()){
+            if(m.getValue() != 0) {
+                System.out.println(m.getKey().getAmount() + WON + DIVISION + m.getValue() + NUM);
+            }
+        }
+    }
+
 }
