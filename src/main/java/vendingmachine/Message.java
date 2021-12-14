0c2ac005e9080b3222ddd48d23d25d41b74b46e6
erie0210 <eriee0210@gmail.com>
2021-12-14 23:01:44 +0900

    feat: 실행에 필요한 message, validation 파일 추가

--- src/main/java/vendingmachine/Message.java
+++ src/main/java/vendingmachine/Message.java
@@ -7,4 +7,8 @@ public class Message {
     public static String INPUT_USER_MONEY = "투입 금액을 입력해 주세요.";
     public static String INPUT_PROD_NAME = "구매할 상품명을 입력해 주세요.";
     public static String USER_MONEY = "투입 금액: ";
+    public static String REST_MONEY = "잔돈";
+
+
+    public static String ERROR_INPUT_VEND_MONEY = "[ERROR]";
 }
