fdebe79ebd279a575abf4bb5e4a60b9a9c5f0bf7
harinn1014 <gkfla7555@gmail.com>
2021-12-14 20:02:40 +0900

    feat(VendingMachine): add methods to make random coins and add product
    <description>
    다음의 기능에 따른 함수를 추가함.
    - 상품을 추가하는 함수
    - 보유하고 있는 잔액에 맞게 랜덤으로 동전을 생성하는 함수

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -3,6 +3,10 @@ package vendingmachine.domain;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import static vendingmachine.utils.Constant.ZERO;
 
 public class VendingMachine {
 	private static final HashMap<Integer, Integer> coins = new HashMap<>();
@@ -11,22 +15,44 @@ public class VendingMachine {
 
 	public VendingMachine(int amount) {
 		changeAmount = amount;
+		makeCoins();
 	}
-
+	
 	public void initProducts() {
 		products.clear();
 	}
-
+	
 	public int getChange() {
 		return changeAmount;
 	}
-
-	public HashMap<Integer, Integer> getCoins() {
+	
+	public HashMap<Integer, Integer> getCoins(){
 		return coins;
 	}
-
-	public List<Product> getProducts() {
+	
+	public List<Product> getProducts(){
 		return products;
+	}
+
+	/**** action ****/
+	public void addProduct(Product item) {
+		products.add(item);
+	}
+
+	public void makeCoins() {
+		int sumAmount = 0;
+		while (sumAmount < changeAmount) {
+			int coin = Randoms.pickNumberInList(coinFilter(sumAmount));
+			sumAmount += coin;
+			coins.put(coin, coins.getOrDefault(coin, ZERO) + 1);
+		}
+	}
+	
+	public List<Integer> coinFilter(int sumAmount) {
+		return Coin.getAmountList()
+				.stream()
+				.filter(amount -> amount <= (changeAmount-sumAmount))
+				.collect(Collectors.toList());
 	}
 
 }
