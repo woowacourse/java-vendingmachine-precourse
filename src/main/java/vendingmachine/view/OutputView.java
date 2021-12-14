78156e496b73cb67079cf3844183f388677cccf4
YuJeong <yjfflq4872@gmail.com>
2021-12-14 10:16:44 +0900

    feat: 반환된 잔돈 출력 기능 구현

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -5,6 +5,7 @@ import java.util.LinkedHashMap;
 public class OutputView {
 	private static final String DISPLAY_MACHINE_COIN_MSG = "자판기가 보유한 동전";
 	private static final String DISPLAY_USER_MONEY_MSG = "투입 금액";
+	private static final String DISPLAY_CHANGE_MSG = "잔돈";
 	private static final String COIN_UNIT_WON = "원";
 	private static final String COIN_COUNT_UNIT = "개";
 
@@ -13,17 +14,31 @@ public class OutputView {
 		sb.append("\n").append(DISPLAY_MACHINE_COIN_MSG);
 
 		coinCount.forEach((coin, count) -> {
-			sb.append("\n").append(coin.toString()).append(COIN_UNIT_WON).append(" - ")
-				.append(count.toString()).append(COIN_COUNT_UNIT);
+			sb.append(makeDisplayCoinMsg(coin, count));
 		});
 
 		System.out.println(sb);
 	}
 
+	private StringBuilder makeDisplayCoinMsg(Integer coin, Integer count) {
+		return new StringBuilder().append("\n").append(coin.toString()).append(COIN_UNIT_WON)
+			.append(" - ").append(count.toString()).append(COIN_COUNT_UNIT);
+	}
+
 	public void displayUserMoney(int change) {
+		String msg = "\n" + DISPLAY_USER_MONEY_MSG + ": " + Integer.toString(change) + COIN_UNIT_WON;
+		System.out.println(msg);
+	}
+
+	public void displayChange(LinkedHashMap<Integer, Integer> change) {
 		StringBuilder sb = new StringBuilder();
-		sb.append("\n").append(DISPLAY_USER_MONEY_MSG).append(": ")
-			.append(Integer.toString(change)).append(COIN_UNIT_WON);
+		sb.append(DISPLAY_CHANGE_MSG);
+
+		change.forEach((coin, count) -> {
+			if (count > 0) {
+				sb.append(makeDisplayCoinMsg(coin, count));
+			}
+		});
 
 		System.out.println(sb);
 	}
