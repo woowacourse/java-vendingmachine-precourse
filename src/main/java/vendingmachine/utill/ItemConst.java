7b254ce81546c6e361225b09e12ad008658ec8ad
YuJeong <yjfflq4872@gmail.com>
2021-12-14 18:33:54 +0900

    refactor: ItemConst 파일 패키지 변경

---
+++ src/main/java/vendingmachine/utill/ItemConst.java
@@ -0,0 +1,11 @@
+package vendingmachine.utill;
+
+public class ItemConst {
+	public static final String OPEN_BRACKET = "[";
+	public static final String CLOSE_BRACKET = "]";
+	public static final String DELIMITER = ",";
+
+	public static final int NAME_INDEX = 0;
+	public static final int PRICE_INDEX = 1;
+	public static final int QUANTITY_INDEX = 2;
+}
