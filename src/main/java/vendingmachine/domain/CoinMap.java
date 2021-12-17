0197ecec83b833c2dc9f4a24a4ecbbd768e8a229
zmfl1230 <zmfl123097@gmail.com>
2021-12-17 18:04:55 +0900

    refactor[CoinMap-Domain]: create first class collection for coin map

---
+++ src/main/java/vendingmachine/domain/CoinMap.java
@@ -0,0 +1,57 @@
+package vendingmachine.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import vendingmachine.utils.CommonConstant;
+import vendingmachine.view.VendingMachineOutput;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class CoinMap {
+    private Map<Coin, Integer> coinMap;
+
+    public CoinMap() {
+        coinMap = new LinkedHashMap<>();
+    }
+
+    public void updateValue(Coin coin, int value) {
+        coinMap.put(coin, value);
+    }
+
+
+    public void generateNumberOfCoinsRandomly(int amount) {
+        coinMap = new LinkedHashMap<>();
+
+        for (Coin coin : Coin.values()) {
+            int randomValue = Randoms.pickNumberInRange(0, amount/coin.getValue());
+            amount -= randomValue * coin.getValue();
+            updateValue(coin, randomValue);
+        }
+
+        if (amount > CommonConstant.DEFAULT_AMOUNT) {
+            for (Coin coin : Coin.values()) {
+                int quotient = amount/coin.getValue();
+                amount -= quotient * coin.getValue();
+                updateValue(coin, coinMap.get(coin) + quotient);
+            }
+        }
+    }
+
+    public int calculateValueUsingQuotient(Coin coin, int quotient) {
+        if (quotient > coinMap.get(coin)) return coinMap.get(coin);
+        return quotient;
+    }
+
+    public void printCoins() {
+        VendingMachineOutput.printCoinsVendingMachineHas(coinMap);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for(Coin coin : coinMap.keySet()) {
+            stringBuilder.append(coin).append(CommonConstant.CONNECTION_MARK).append(coinMap.get(coin)).append(CommonConstant.COINS_POSTFIX);
+        }
+        return stringBuilder.toString();
+    }
+}
