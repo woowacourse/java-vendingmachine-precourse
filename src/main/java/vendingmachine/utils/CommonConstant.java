c1cc1952527419f8f69ef56dd076fd73bb831fa9
zmfl1230 <zmfl123097@gmail.com>
2021-12-17 18:03:43 +0900

    refactor[CommonConstant]: gather common constants on one file

---
+++ src/main/java/vendingmachine/utils/CommonConstant.java
@@ -0,0 +1,9 @@
+package vendingmachine.utils;
+
+public class CommonConstant {
+    public static final int DEFAULT_AMOUNT = 0;
+
+    public static final String CONNECTION_MARK = " - ";
+    public static final String COINS_POSTFIX = "개";
+    public static final String INPUT_MONEY_POSTFIX = "원";
+}
