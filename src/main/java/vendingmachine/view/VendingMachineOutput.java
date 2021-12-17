a8a14c6966c6c1f552afa626f9dd83e361492b59
zmfl1230 <zmfl123097@gmail.com>
2021-12-17 18:07:16 +0900

    refactor[VendingMachineOutput-View]: modify output function related to coin map

--- src/main/java/vendingmachine/view/VendingMachineOutput.java
+++ src/main/java/vendingmachine/view/VendingMachineOutput.java
@@ -1,34 +1,28 @@
 package vendingmachine.view;
 
 import vendingmachine.domain.Coin;
+import vendingmachine.utils.CommonConstant;
 
 import java.util.Map;
 
 public class VendingMachineOutput {
-    private static final String CONNECTION_MARK = " - ";
     private static final String COINS_INSTRUCTION = "자판기가 보유한 동전";
-    private static final String COINS_POSTFIX = "개";
     private static final String INPUT_MONEY_INSTRUCTION = "투입 금액: ";
-    private static final String INPUT_MONEY_POSTFIX = "원";
     private static final String CHANGES_INSTRUCTION = "잔돈";
 
     public static void printCoinsVendingMachineHas(Map<Coin, Integer> coinMap) {
         System.out.println(COINS_INSTRUCTION);
-        for(Coin coin : coinMap.keySet()) {
-            // TODO: coinMap 일급 컬렉션 생성 후, toString 메서드에 재정의
-            System.out.println(coin + CONNECTION_MARK + coinMap.get(coin) + COINS_POSTFIX);
-        }
+        System.out.println(coinMap);
     }
 
     public static void printRestMoney(int restMoney) {
-        System.out.println(INPUT_MONEY_INSTRUCTION + restMoney + INPUT_MONEY_POSTFIX);
+        System.out.println(INPUT_MONEY_INSTRUCTION + restMoney + CommonConstant.INPUT_MONEY_POSTFIX);
     }
 
     public static void printChange(Map<Coin, Integer> changes) {
         System.out.println(CHANGES_INSTRUCTION);
         for(Coin coin: changes.keySet()) {
-            // TODO: coinMap 일급 컬렉션 생성 후, toString 메서드에 재정의
-            System.out.println(coin + CONNECTION_MARK + changes.get(coin)+ COINS_POSTFIX);
+            System.out.println(coin + CommonConstant.CONNECTION_MARK + changes.get(coin)+ CommonConstant.COINS_POSTFIX);
         }
 
     }
