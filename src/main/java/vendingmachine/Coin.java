3e9070ed74cb3cfd28c7437cf1fed2537d7d90da
jmj005 <jmj00005@naver.com>
2022-11-25 14:20:37 +0900

    feat(Coin): 자판기 보유 금액 무작위 생성 기능 구현

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,5 +1,15 @@
 package vendingmachine;
 
+import static java.util.Arrays.*;
+
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
@@ -7,10 +17,22 @@ public enum Coin {
     COIN_10(10);
 
     private final int amount;
+    private static final List<Integer> amountList = stream(Coin.values()).map(value -> value.amount)
+            .collect(Collectors.toList());
 
     Coin(final int amount) {
         this.amount = amount;
     }
 
-    // 추가 기능 구현
+    public static Map<Integer, Integer> getVendingMachineCoins(int vendingMachineCoin) {
+        Map<Integer, Integer> vendingMachineCoins  = new TreeMap<>(Collections.reverseOrder());
+        while (vendingMachineCoin > 0) {
+            int amount = Randoms.pickNumberInList(amountList);
+            if (vendingMachineCoin >= amount) {
+                vendingMachineCoins.put(amount, vendingMachineCoins.getOrDefault(amount, 0) + 1);
+                vendingMachineCoin -= amount;
+            }
+        }
+        return vendingMachineCoins;
+    }
 }
