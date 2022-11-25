b64d864c54ba6c209c7780fc254aaca2b16674ef
jmj005 <jmj00005@naver.com>
2022-11-25 16:40:16 +0900

    feat(InputView): 투입 금액 입력 기능 구현

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,25 +1,10 @@
 package vendingmachine.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.List;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class InputView {
 
-    private int vendingMachineCoin;
-
-    public int inputVendingMachineCoinProcess() {
-        try {
-            vendingMachineCoin = inputVendingMachineCoin();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            inputVendingMachineCoinProcess();
-        }
-        return vendingMachineCoin;
-    }
-
-    private int inputVendingMachineCoin() {
+    public int inputVendingMachineCoin() {
         System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
         String vendingMachineCoin = Console.readLine();
         validateVendingMachineCoin(vendingMachineCoin);
@@ -32,11 +17,11 @@ public class InputView {
         }
     }
 
-    public List<String> inputProducts() {
+    public String inputProducts() {
         System.out.println("상품명과 가격, 수량을 입력해 주세요.");
         String products = Console.readLine();
         validateProducts(products);
-        return Arrays.stream(products.split(";")).collect(Collectors.toList());
+        return products;
     }
 
     private void validateProducts(String products) {
@@ -44,6 +29,19 @@ public class InputView {
             if (!product.matches("^\\[[가-힣a-zA-Z]+,+[0-9]+,+[0-9]+\\].*")) {
                 throw new IllegalArgumentException("[ERROR] 입력 포맷이 잘못되었습니다.");
             }
+        }
+    }
+
+    public int inputMoney() {
+        System.out.println("투입 금액을 입력해 주세요.");
+        String money = Console.readLine();
+        validateMoney(money);
+        return Integer.parseInt(money);
+    }
+
+    private void validateMoney(String money) {
+        if (money.matches("^[0-9]$")) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
         }
     }
 }
