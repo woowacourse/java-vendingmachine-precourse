84cb4793acec933256986636ea763e37dc7abc9b
jmj005 <jmj00005@naver.com>
2022-11-27 02:56:26 +0900

    refactor(Messages): 줄 바꿈 추가

--- src/main/java/vendingmachine/Messages.java
+++ src/main/java/vendingmachine/Messages.java
@@ -2,13 +2,14 @@ package vendingmachine;
 
 public class Messages {
     public static final String INPUT_VENDING_MACHINE_COIN = "자판기가 보유하고 있는 금액을 입력해 주세요.";
-    public static final String INPUT_PRODUCTS = "상품명과 가격, 수량을 입력해 주세요.";
-    public static final String INPUT_MONEY = "투입 금액을 입력해 주세요.";
+    public static final String INPUT_PRODUCTS = System.lineSeparator() + "상품명과 가격, 수량을 입력해 주세요.";
+    public static final String INPUT_MONEY = System.lineSeparator() + "투입 금액을 입력해 주세요.";
     public static final String INPUT_PRODUCT_NAME = "구매할 상품명을 입력해 주세요.";
-    public static final String OUTPUT_MONEY = "투입 금액: %d" + System.lineSeparator();
+    public static final String OUTPUT_VENDING_MACHINE_COIN = System.lineSeparator() + "자판기가 보유한 동전";
+    public static final String OUTPUT_MONEY = System.lineSeparator() + "투입 금액: %d원" + System.lineSeparator();
     public static final String OUTPUT_CHARGE = "잔돈";
     public static final String WON_AND_HYPHEN = "원 - ";
-    public static final String GAE = "개"
+    public static final String GAE = "개";
     public static final String ERROR_INPUT_VENDING_MACHINE_COIN = "숫자만 입력 가능합니다.";
     public static final String ERROR_INPUT_PRODUCTS = "입력 포맷이 잘못되었습니다.";
     public static final String ERROR_INPUT_MONEY = "숫자만 입력 가능합니다.";
