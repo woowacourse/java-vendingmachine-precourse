03e0ef98c241098b82a3f2d888ece39e4778d42a
jmj005 <jmj00005@naver.com>
2022-11-27 23:55:12 +0900

    refactor(Coin): Coin refactoring

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -2,7 +2,6 @@ package vendingmachine;
 
 import static java.util.Arrays.*;
 
-
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Collections;
 import java.util.List;
@@ -11,21 +10,30 @@ import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public enum Coin {
-    COIN_500(500),
-    COIN_100(100),
-    COIN_50(50),
-    COIN_10(10);
+    COIN_500(500, 0),
+    COIN_100(100, 0),
+    COIN_50(50, 0),
+    COIN_10(10, 0);
 
-    private final int amount;
     private static final List<Integer> amountList = stream(Coin.values()).map(value -> value.amount)
             .collect(Collectors.toList());
+    private final int amount;
+    private final int count;
 
-    Coin(final int amount) {
+    Coin(final int amount, int count) {
         this.amount = amount;
+        this.count = count;
     }
 
-    public static Map<Integer, Integer> getVendingMachineCoins(int vendingMachineCoin) {
+    public static Map<Integer, Integer> initVendingMachineCoins(int vendingMachineCoin) {
         Map<Integer, Integer> vendingMachineCoins  = new TreeMap<>(Collections.reverseOrder());
+        for (Coin coin : Coin.values()) {
+            vendingMachineCoins.put(coin.amount, coin.count);
+        }
+        return getVendingMachineCoins(vendingMachineCoin, vendingMachineCoins);
+    }
+
+    public static Map<Integer, Integer> getVendingMachineCoins(int vendingMachineCoin, Map<Integer, Integer> vendingMachineCoins) {
         while (vendingMachineCoin > 0) {
             int amount = Randoms.pickNumberInList(amountList);
             if (vendingMachineCoin >= amount) {
