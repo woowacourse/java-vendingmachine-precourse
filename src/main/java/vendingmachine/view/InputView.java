eb32acbc2726a029a162d2065eadf4596a7e2468
HaODay <ycis5013@naver.com>
2022-12-03 04:54:32 +0900

    feat(InputView): 자판기가 보유하고 있는 금액 입력 받고 검증하는 기능 구현

---
+++ src/main/java/vendingmachine/view/InputView.java
@@ -0,0 +1,30 @@
+package vendingmachine.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static int readBalance() {
+        System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
+        int balance = 0;
+        try {
+            balance = validateNum();
+            if (balance % 10 != 0) {
+                throw new IllegalArgumentException("보유 금액은 10의 배수여야 합니다.");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            balance = readBalance();
+        }
+        return balance;
+    }
+
+
+    private static int validateNum() throws IllegalArgumentException {
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("보유 금액은 숫자여야 합니다.");
+        }
+    }
+}
