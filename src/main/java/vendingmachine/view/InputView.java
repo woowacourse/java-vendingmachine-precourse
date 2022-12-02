ec3b9f6eeff550be515b0ad37097ff129c27a281
HaODay <ycis5013@naver.com>
2022-12-03 05:05:58 +0900

    feat(InputView): 투입 금액 입력 받고 검증하는 기능 구현

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -24,12 +24,26 @@ public class InputView {
         return Console.readLine();
     }
 
+    public static int readAmountOfInput() {
+        System.out.println("\n투입 금액을 입력해 주세요.");
+        int amountOfInput = 0;
+        try {
+            amountOfInput = validateNum();
+            if (amountOfInput < 0) {
+                throw new IllegalArgumentException("투입 금액은 음수일 수 없습니다.");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            amountOfInput = readAmountOfInput();
+        }
+        return amountOfInput;
+    }
 
     private static int validateNum() throws IllegalArgumentException {
         try {
             return Integer.parseInt(Console.readLine());
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("보유 금액은 숫자여야 합니다.");
+            throw new IllegalArgumentException("입력값은 숫자여야 합니다.");
         }
     }
 }
