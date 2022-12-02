bacd922225f05bb566d076e56dcb6468d019eae7
HaODay <ycis5013@naver.com>
2022-12-03 05:02:30 +0900

    feat(VendingMachineController): 입력 받은 상품 정보 저장하는 기능 구현

---
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -0,0 +1,37 @@
+package vendingmachine.controller;
+
+import vendingmachine.model.Balance;
+import vendingmachine.model.Product;
+import vendingmachine.model.Validator;
+import vendingmachine.view.InputView;
+import vendingmachine.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+public class VendingMachineController {
+
+    private static Map<String, Product> products = new HashMap<>();
+    private static int amountOfInput;
+    
+
+    public static void saveProducts() {
+        try {
+            String input = InputView.readProductInfo();
+            String[] str = input.split(";");
+            for (String s : str) {
+                String[] productInfo = s.substring(1, s.length() - 1).split(",");
+                Validator.validateProduct(productInfo, products);
+                products.put(productInfo[0], new Product(productInfo[1], productInfo[2]));
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            products = new HashMap<>();
+            saveProducts();
+        }
+    }
+}
+
