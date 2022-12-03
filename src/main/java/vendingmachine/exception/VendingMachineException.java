6650ec621c1dac1e1c4ce3779379dccf25a54629
nahyeon <whskgus12@nate.com>
2022-12-03 14:45:42 +0900

    feat(exception): 상품 추가 입력 양식이 잘못됬을 때
    상품 추가 입력 양식이 잘못됬을 때 IllegalArgumentException을 발생합니다.  Regex를 이용하여 입력 양식에 부합하는지 판단했습니다.

--- src/main/java/vendingmachine/exception/VendingMachineException.java
+++ src/main/java/vendingmachine/exception/VendingMachineException.java
@@ -1,14 +1,16 @@
 package vendingmachine.exception;
 
 public class VendingMachineException {
-    private final String ONLY_NUMBER = "^[0-9]$";
 
-    public void validInputMoney(String money){
-        validVendingMachineMoney(money);
-    }
-
-    private void validVendingMachineMoney(String money){
+     public void validMoneyInputForm(String money){
+        final String ONLY_NUMBER = "^[0-9]*$";
         if(!money.matches(ONLY_NUMBER))
             throw new IllegalArgumentException("[ERROR] 금액은 숫자만 입력하세요.");
+    }
+
+    public void validGoodsInputForm(String goods){
+        final String GOODS_INPUT_FORM = "\\[[가-힇]*,?[0-9]*,?[0-9]*\\];?";
+        if(goods.replaceAll(GOODS_INPUT_FORM,"").length()!=0)
+            throw new IllegalArgumentException("[ERROR] 상품명, 가격, 수량은 쉼표로, 개별 상품은 대괄호([])로 묶어주세요.");
     }
 }
