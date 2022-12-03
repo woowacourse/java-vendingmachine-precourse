263b09d733caa932029b63c4d075d5f4fc29710b
rvlwldev <rvlwlasd77@gmail.com>
2022-12-03 18:12:43 +0900

    feat: main 메소드 작성

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,7 +1,10 @@
 package vendingmachine;
 
+import vendingmachine.Domain.Machine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Machine machine = new Machine();
+        machine.buyProducts();
     }
 }
