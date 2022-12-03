0f93c2b46771eca8a1a4433a7764f9514d997c14
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-12-03 14:03:05 +0900

    feat: create askInputPricePrint method

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -2,10 +2,13 @@ package vendingmachine.view;
 
 public class OutputView {
     public void askPricePrint(){
-       printMessage("자판기가 보유하고 있는 금액을 입력해주세요.");
+       printMessage("자판기가 보유하고 있는 금액을 입력해 주세요.");
     }
     public void askProductPrint(){
-        printMessage("상품가격과 가격, 수량을 입력해주세요.");
+        printMessage("상품가격과 가격, 수량을 입력해 주세요.");
+    }
+    public void askInputPricePrint(){
+        printMessage("투입 금액을 입력해 주세요.");
     }
     public void printMessage(String input){
         System.out.println(input);
