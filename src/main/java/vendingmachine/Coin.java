1420216cf6c9c17aee2e04abe9354603c43f2f5c
nahyeon <whskgus12@nate.com>
2022-12-03 15:24:52 +0900

    feat(vendingmachine): 자판기 금액 생성하기
    자판기 금액을 생성할 때 Coin 클래스를 이용하기 위해 , amount 변수를 도입하여 자판기에 생성된 코인의 개수를 Enum Class에 등록합니다.

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,16 +1,37 @@
 package vendingmachine;
 
+import java.util.Arrays;
+
 public enum Coin {
-    COIN_500(500),
-    COIN_100(100),
-    COIN_50(50),
-    COIN_10(10);
+    COIN_500(500,0),
+    COIN_100(100,0),
+    COIN_50(50,0),
+    COIN_10(10,0);
 
     private final int amount;
+    private int count;
 
-    Coin(final int amount) {
+    Coin(final int amount,final int count) {
         this.amount = amount;
+        this.count = count;
     }
 
+
     // 추가 기능 구현
+    public int getCount(){
+        return count;
+    }
+
+    public int getAmount(){
+        return amount;
+    }
+
+    public void addCoin(){
+        count++;
+    }
+
+    public Coin getCoin(int amount){
+        return Arrays.stream(values()).filter( a -> a.getAmount() == amount).findAny().orElse(null);
+    }
+
 }
