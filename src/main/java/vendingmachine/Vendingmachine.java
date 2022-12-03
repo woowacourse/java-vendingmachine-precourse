cad8893e4fec6bdb01ed081013f7b52c14cc2feb
glass <glass7312@gmail.com>
2022-12-03 21:06:22 +0900

    feat:잔돈 계산 기능

--- src/main/java/vendingmachine/Vendingmachine.java
+++ src/main/java/vendingmachine/Vendingmachine.java
@@ -1,31 +1,42 @@
 package vendingmachine;
 
-import java.io.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Vendingmachine {
-    // public static String status = "operating";
-    private List<String> products_list;
-    private int Amount = 0;
-    InputView inputview = new InputView();
-    OutputView outputview=new OutputView();
+    public static String status = "operating";
+    private List<Product> products;
+    private int Amount;
+    private List<String> purchased_products;
+    private final InputView inputview = new InputView();
+    private final OutputView outputview = new OutputView();
     List<Integer> own_coins;
 
     public Vendingmachine() {
+        own_coins = setCoins(Integer.parseInt(inputview.readCoins()));
+        outputview.totalConins(own_coins);
+        products = set_vending_products(inputview.readproduct_list(inputview.read()));
+        Amount = inputview.readbuyingAmount();
+
+        //
+        do {
+            outputview.printAmount(Amount);
+            purchased_products.add(inputview.purchaseproduct());
+        } while (status.equals("operationg"));
+
+
         //own_coins= Collections.emptyList();
         // vending_products= inputview.readproduct_list(inputview.read());
     }
 
-    public List<Products> set_vending_products(List<String> products_format) {
-        List<Products> products = new ArrayList<>();
+    public List<Product> set_vending_products(List<String> products_format) {
+        List<Product> products = new ArrayList<>();
         String[] list;
         for (String option : products_format) {
             list = option.split(",");
 
-            products.add(new Products(list[0], list[1], list[2]));
+            products.add(new Product(list[0], list[1], list[2]));
         }
         return products;
     }
@@ -35,21 +46,18 @@ public class Vendingmachine {
         int hund_d = (Amount % 500) / 100;
         int fif_ten = (Amount % 100) / 50;
         int ten_d = (Amount % 50) / 10;
-       // if (!own_coins.isEmpty()) {
-         //   own_coins.clear();
+        // if (!own_coins.isEmpty()) {
+        //   own_coins.clear();
         //}
         own_coins = new ArrayList<>(Arrays.asList(f_hund, hund_d, fif_ten, ten_d));
         return own_coins;
     }
 
-    public void totalConins() {
-        System.out.print("자판기가 보유한 동전" + "\n");
-
+    public void calculate() {
+        for (Product product : products) {
+            if (purchased_products.contains(product.getname())) {
+                Amount = Amount - product.getprice();
+            }
+        }
     }
-    public void setAmount(int Amount){
-        this.Amount=Amount;
-    }
-
-
-
 }
