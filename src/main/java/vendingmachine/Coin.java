cfe5b3d1c85f6b27f22610f74dfede177d941333
Choi-JJunho <junho5336@gmail.com>
2022-12-03 16:55:33 +0900

    feat(Coin): of Method 추가

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,5 +1,9 @@
 package vendingmachine;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
@@ -10,6 +14,25 @@ public enum Coin {
 
     Coin(final int amount) {
         this.amount = amount;
+    }
+
+    public static Coin of(int amount) {
+        for (Coin coin : values()) {
+            if (coin.amount == amount) {
+                return coin;
+            }
+        }
+        throw new IllegalArgumentException("[ERROR]");
+    }
+
+    public int getAmount() {
+        return this.amount;
+    }
+
+    public static List<Integer> getCoins() {
+        return Arrays.stream(Coin.values())
+                .map(Coin::getAmount)
+                .collect(Collectors.toList());
     }
 
     // 추가 기능 구현
