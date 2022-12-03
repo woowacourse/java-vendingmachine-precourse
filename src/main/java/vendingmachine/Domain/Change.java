79fd6e67b0ff05ba279914f1856ea0ac7f89beeb
rvlwldev <rvlwlasd77@gmail.com>
2022-12-03 14:56:01 +0900

    feat: Coin 클래스의 초기화 구현

--- src/main/java/vendingmachine/Domain/Change.java
+++ src/main/java/vendingmachine/Domain/Change.java
@@ -1,9 +1,48 @@
 package vendingmachine.Domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import static vendingmachine.Constant.Coin.*;
 
 public class Change {
 
-    private List<Integer> coins;
+    private final HashMap<Integer, Integer> coins = new HashMap<>();
+
+    Change(int money) {
+        coins.put(COIN_10.getAmount(), 0);
+        coins.put(COIN_50.getAmount(), 0);
+        coins.put(COIN_100.getAmount(), 0);
+        coins.put(COIN_500.getAmount(), 0);
+
+        createCoins(money);
+    }
+
+    private void createCoins(int money) {
+        List<Integer> coinKeys = new ArrayList<>(coins.keySet());
+        int randomCost = Randoms.pickNumberInList(coinKeys);
+        int sum = getSum();
+
+        while (sum != money) {
+            if (sum + randomCost < money) {
+                coins.put(randomCost, coins.get(randomCost));
+                sum = getSum();
+            }
+            randomCost = Randoms.pickNumberInList(coinKeys);
+        }
+    }
+
+    public int getSum() {
+        List<Integer> coinKeys = new ArrayList<>(coins.keySet());
+        int sum = 0;
+
+        for (int key : coinKeys) sum += coins.get(key);
+
+        return sum;
+    }
+
 
 }
