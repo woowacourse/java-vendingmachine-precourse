76aaa1400b7ee17423d4edb5a5891507d369532d
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-12-03 14:26:31 +0900

    feat: create isDivideMinCoin method

---
+++ src/main/java/vendingmachine/enums/Coin.java
@@ -0,0 +1,27 @@
+package vendingmachine.enums;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public enum Coin {
+    COIN_500(500),
+    COIN_100(100),
+    COIN_50(50),
+    COIN_10(10);
+
+    private final int amount;
+
+    Coin(final int amount) {
+        this.amount = amount;
+    }
+
+    // 추가 기능 구현
+    public static boolean isDivideMinCoin(String input) {
+        if (Integer.parseInt(input) % COIN_10.amount == 0) return true;
+        return false;
+    }
+}
