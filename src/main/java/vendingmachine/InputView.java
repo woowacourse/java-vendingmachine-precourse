5c48d9ab6ca20889e143bdf4eac603f532d1bc60
glass <glass7312@gmail.com>
2022-12-03 17:00:43 +0900

    feat:자판기의 제품 목록을 입력 받는 기능

--- src/main/java/vendingmachine/InputView.java
+++ src/main/java/vendingmachine/InputView.java
@@ -3,17 +3,27 @@ package vendingmachine;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class InputView {
-    public String readCoins(){
+    public String readCoins() {
         System.out.print("자판기가 보유하고 있는 금액을 입력하세요" + "\n");
         return Console.readLine();
     }
 
-    public int readbuyingAmount(){
-        System.out.print("투입 금액을 입력해주세요" +"\n");
+    public int readbuyingAmount() {
+        System.out.print("투입 금액을 입력해주세요" + "\n");
         return Integer.parseInt(Console.readLine());
+    }
+
+    public List<String> readproduct_list() {
+        System.out.print("상품명과 가격, 수량을 입력해주세요" + "\n");
+        List<String> list = new ArrayList<>();
+        String something = Console.readLine();
+        String[] buying = something.split(";");
+        Collections.addAll(list, buying);
+        return list;
     }
 }
 
