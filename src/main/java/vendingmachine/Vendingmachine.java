fd4753d95fd17916f89874023719e56a07f49a53
glass <glass7312@gmail.com>
2022-12-03 18:17:19 +0900

    feat:자판기의 동전 갯수를 설정하는 기능

--- src/main/java/vendingmachine/Vendingmachine.java
+++ src/main/java/vendingmachine/Vendingmachine.java
@@ -7,19 +7,20 @@ import java.util.Collections;
 import java.util.List;
 
 public class Vendingmachine {
-   // public static String status = "operating";
+    // public static String status = "operating";
     private List<String> products_list;
-
-
-    InputView inputview=new InputView();
+    private int Input_Amount = 0;
+    InputView inputview = new InputView();
+    List<Integer> own_coins;
 
     public Vendingmachine() {
         //own_coins= Collections.emptyList();
         // vending_products= inputview.readproduct_list(inputview.read());
+
     }
 
     public List<Products> set_vending_products(List<String> products_format) {
-        List<Products> products=new ArrayList<>();
+        List<Products> products = new ArrayList<>();
         String[] list;
         for (String option : products_format) {
             list = option.split(",");
@@ -28,5 +29,18 @@ public class Vendingmachine {
         }
         return products;
     }
+
+    public void setCoins(int Amount) {
+        int f_hund = (Amount % 1000) / 500;
+        int hund_d = (Amount % 500) / 100;
+        int fif_ten = (Amount % 100) / 50;
+        int ten_d = (Amount % 50) / 10;
+        if (!own_coins.isEmpty()) {
+            own_coins.clear();
+        }
+        own_coins = new ArrayList<>(Arrays.asList(f_hund, hund_d, fif_ten, ten_d));
+
+    }
+
 
 }
