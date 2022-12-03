03d89f2995143237126d547bc8b75b642578f42b
nahyeon <whskgus12@nate.com>
2022-12-03 22:30:14 +0900

    refactor(goods): 함수 기능 분리하기
    GoodsManager 클래스를 사용하기 위해선 상품리스트와 상품의 최소 가격은 필수이므로 생성자를 통해 상품 리스트와 최소 가격 설정 후 사용할 수 있도록 제한합니다. 따라서 기존 makeGoodList에서 사용한 minPriceGoodsList 함수를 생성자로 옮깁니다.

--- src/main/java/vendingmachine/goods/GoodsManager.java
+++ src/main/java/vendingmachine/goods/GoodsManager.java
@@ -1,28 +1,30 @@
 package vendingmachine.goods;
 
+import vendingmachine.exception.VendingMachineException;
+
 import java.util.HashMap;
 
 public class GoodsManager {
 
-    private HashMap<String, Goods> goodsInfo = new HashMap<String, Goods>();
+    private HashMap<String, Goods> goodsInfo = new HashMap<>();
     private int minPrice = Integer.MAX_VALUE;
 
-    public void makeGoodsList(String goodsList) {
-        String[] subList = goodsList.split(";");
-        String[] goodsInformation;
-
-        for (String item : subList) {
-            item = item.replace("[", "").replace("]", "");
-            goodsInformation = item.split(",");
-
-            Goods goods = new Goods();
-            goods.makeGoodsInfo(goodsInformation);
-            goodsInfo.put(goodsInformation[0], goods);
-        }
+    public GoodsManager(String goodsList){
+        makeGoodsInformation(goodsList);
         minPriceGoodsList();
     }
 
-    public void minPriceGoodsList() {
+    private void makeGoodsInformation(String goodsList) {
+        String[] itemList = goodsList.split(";");
+        for (String item : itemList) {
+            String[] goodsInformation = item.replace("[", "").replace("]", "").split(",");
+            validGoodsPrice(goodsInformation[1]); // 가격 유효성 검증
+            Goods goods = new Goods(goodsInformation);
+            goodsInfo.put(goodsInformation[0],goods);
+        }
+    }
+    
+    private void minPriceGoodsList() {
         for (Goods g : goodsInfo.values())
             if (g.getPrice() < minPrice) minPrice = g.getPrice();
     }
@@ -31,7 +33,12 @@ public class GoodsManager {
         Goods goods = goodsInfo.get(purchaseGoods);
         if (goods.getAmount() != 0)
             goods.deleteAmount();
-        return money -= goods.getPrice();
+        return money - goods.getPrice();
+    }
+
+    private void validGoodsPrice(String price){
+        VendingMachineException vendingMachineException = new VendingMachineException();
+        vendingMachineException.validGoodsMoneyType(Integer.parseInt(price));
     }
 
     public boolean validPurchase(String purchaseGoods, int money) {
