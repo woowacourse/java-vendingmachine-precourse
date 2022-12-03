6da733a8700f3695c313e6c4d47aa81a8dbf0192
rvlwldev <rvlwlasd77@gmail.com>
2022-12-03 14:18:23 +0900

    feat: Machine 클래스의 기본 동작 메소드 작성

--- src/main/java/vendingmachine/Domain/Machine.java
+++ src/main/java/vendingmachine/Domain/Machine.java
@@ -1,10 +1,37 @@
 package vendingmachine.Domain;
 
+import vendingmachine.Util.View;
+
+import java.util.List;
+
 public class Machine {
 
-    private Change change;
+    private final View view = new View();
 
-    public void turnOn () {
+    private Change change;
+    private List<Product> products;
+
+
+    public void turnOn() {
+        change = new Change(view.getInitChanges());
+        products = view.getInitProducts();
+    }
+
+    public void buyProducts() {
+        int money = view.getPurchaseMoney();
+
+        while (isPurchasable(money, change)) {
+            products = view.buyProduct(products);
+        }
+
+        result();
+    }
+
+    private boolean isPurchasable(int money, Change change) {
+        return false;
+    }
+
+    private void result() {
 
     }
 }
