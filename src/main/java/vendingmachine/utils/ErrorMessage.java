8e011ea73941e5c2c0ad5b30c4947fdf6c5fc4de
Choi-JJunho <junho5336@gmail.com>
2022-12-03 17:27:54 +0900

    feat(ErrorMessage): 에러 메시지 추가

---
+++ src/main/java/vendingmachine/utils/ErrorMessage.java
@@ -0,0 +1,21 @@
+package vendingmachine.utils;
+
+public enum ErrorMessage {
+    PRODUCT_AMOUNT_NOT_DIVIDE_10("금액은 10으로 나누어 떨어져야합니다."),
+    PRODUCT_AMOUNT_LESS_THAN_0("금액은 0 이상이여야합니다."),
+    PRODUCT_AMOUNT_LESS_THAN_100("상품 금액은 100원 이상이여야합니다."),
+    NOT_ENOUGH_CASH("해당 상품을 구매하기위한 금액이 부족합니다."),
+    NOT_EXIST_PRODUCT("존재하지 않는 상품입니다."),
+    ;
+
+    private final String prefix = "[ERROR] ";
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return prefix + this.message;
+    }
+}
