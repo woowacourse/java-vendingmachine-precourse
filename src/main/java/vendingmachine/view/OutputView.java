0985e8d368a56b3748b9f52134f6a85ae8348350
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-12-03 14:05:05 +0900

    feat: create askBuyProductPrint method

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -10,6 +10,11 @@ public class OutputView {
     public void askInputPricePrint(){
         printMessage("투입 금액을 입력해 주세요.");
     }
+    public void askBuyProductPrint(int hasPrice){
+        StringBuilder print = new StringBuilder("투입 금액: "+hasPrice+"원\n");
+        print.append("구매할 상품명을 입력해 주세요.\n");
+        printMessage(print.toString());
+    }
     public void printMessage(String input){
         System.out.println(input);
     }
