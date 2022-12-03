5f7ed370b28d9921e2c53c8a32101d715b151288
nahyeon <whskgus12@nate.com>
2022-12-04 00:37:30 +0900

    fix(exception): 상품 등록 양식 Regex 수정
    상품 등록 양식 Regex가 제대로 처리되지않아 수정합니다.

--- src/main/java/vendingmachine/exception/VendingMachineException.java
+++ src/main/java/vendingmachine/exception/VendingMachineException.java
@@ -26,7 +26,7 @@ public class VendingMachineException {
     }
 
     public void validGoodsInputForm(String goods) {
-        final String GOODS_INPUT_FORM = "\\[.*,?[0-9]*,?[0-9]*\\];?";
+        final String GOODS_INPUT_FORM = "\\[[0-9가-힇A-Za-z]+,?[0-9]*,?[0-9]*\\];?";
 
         if (goods.replaceAll(GOODS_INPUT_FORM, "").length() != 0)
             throw new IllegalArgumentException("[ERROR] 상품명, 가격, 수량은 쉼표로, 개별 상품은 대괄호([])로 묶어주세요.");
