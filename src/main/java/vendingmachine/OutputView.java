19bb44e542117b14f4939c6d424bde2a7b502ca9
glass <glass7312@gmail.com>
2022-12-03 20:45:29 +0900

    feat:투입금액을 출력하는 기능

--- src/main/java/vendingmachine/OutputView.java
+++ src/main/java/vendingmachine/OutputView.java
@@ -5,9 +5,13 @@ import java.util.List;
 public class OutputView {
 
     public void totalConins(List<Integer> own_coins) {
+        System.out.print("자판기가 보유한 동전" + "\n");
         System.out.print(Coin.COIN_500.getAmount() + "원" + " - " + own_coins.get(0) + "개" + "\n");
         System.out.print(Coin.COIN_100.getAmount() + "원" + " - " + own_coins.get(1) + "개" + "\n");
         System.out.print(Coin.COIN_50.getAmount() + "원" + " - " + own_coins.get(2) + "개" + "\n");
         System.out.print(Coin.COIN_10.getAmount() + "원" + " - " + own_coins.get(3) + "개" + "\n");
+    }
+    public void printAmount(int Amount){
+            System.out.print("투입 금액:"+ Amount);
     }
 }
