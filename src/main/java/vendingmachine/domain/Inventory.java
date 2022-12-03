3ba4ab31fba9f3f58c3566b5faea5af34d418ed2
Choi-JJunho <junho5336@gmail.com>
2022-12-03 16:53:46 +0900

    feat(Inventory): Inventory 객체 추가

---
+++ src/main/java/vendingmachine/domain/Inventory.java
@@ -0,0 +1,50 @@
+package vendingmachine.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static vendingmachine.utils.ErrorMessage.NOT_EXIST_PRODUCT;
+
+public class Inventory {
+    private final List<Product> products = new ArrayList<>();
+
+    public void add(Product product) {
+        this.products.add(product);
+    }
+
+    public void clear() {
+        this.products.clear();
+    }
+
+    public Product get(String name) {
+        return products.stream()
+                .filter(o -> o.getName().equals(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(NOT_EXIST_PRODUCT.getMessage()));
+    }
+
+    public Product consume(String name) {
+        Product product = this.get(name);
+        product.consume();
+        return product;
+    }
+
+    public int getMinAmount() {
+        return products.stream()
+                .mapToInt(Product::getAmount)
+                .min()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR]"));
+    }
+
+    public boolean isEmpty() {
+        int total = products.stream()
+                .mapToInt(Product::getTotal)
+                .sum();
+        return total == 0;
+    }
+
+    public boolean contains(String name) {
+        return products.stream()
+                .anyMatch(product -> product.getName().equals(name));
+    }
+}
