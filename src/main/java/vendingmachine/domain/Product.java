b62d7dc74fbc7af1f2e68f00a120f27b784effb8
Choi-JJunho <junho5336@gmail.com>
2022-12-03 16:53:28 +0900

    feat(Product): Product 객체 추가

---
+++ src/main/java/vendingmachine/domain/Product.java
@@ -0,0 +1,66 @@
+package vendingmachine.domain;
+
+import static vendingmachine.utils.ErrorMessage.*;
+
+public class Product {
+    private final String name;
+    private final int amount;
+    private int total;
+
+    Product(String name, int amount, int total) {
+        validateAmount(amount);
+        validateTotal(total);
+        this.name = name;
+        this.amount = amount;
+        this.total = total;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    private void validateAmount(int amount) {
+        if (isNotDivide10(amount)) {
+            throw new IllegalArgumentException(PRODUCT_AMOUNT_NOT_DIVIDE_10.getMessage());
+        }
+
+        if (isLessThen100(amount)) {
+            throw new IllegalArgumentException(PRODUCT_AMOUNT_LESS_THAN_100.getMessage());
+        }
+    }
+
+    private void validateTotal(int total) {
+        if (isLessThen0(total)) {
+            throw new IllegalArgumentException(PRODUCT_AMOUNT_LESS_THAN_0.getMessage());
+        }
+    }
+
+    private boolean isLessThen0(int number) {
+        return number <= 0;
+    }
+
+    private boolean isLessThen100(int number) {
+        return number < 100;
+    }
+
+    private boolean isNotDivide10(int number) {
+        return number % 10 != 0;
+    }
+
+    public void consume() {
+        if (isLessThen0(total)) {
+            throw new IllegalArgumentException(NOT_EXIST_PRODUCT.getMessage());
+        }
+
+        this.total--;
+    }
+
+}
