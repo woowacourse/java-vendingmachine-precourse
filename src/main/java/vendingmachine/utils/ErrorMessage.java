a7d65c9ff498cee6b9538f5a63bcee36c5f242a0
Choi-JJunho <junho5336@gmail.com>
2022-12-03 18:41:03 +0900

    feat(ErrorMessage): 에러메시지 추가

--- src/main/java/vendingmachine/utils/ErrorMessage.java
+++ src/main/java/vendingmachine/utils/ErrorMessage.java
@@ -6,7 +6,10 @@ public enum ErrorMessage {
     PRODUCT_AMOUNT_LESS_THAN_100("상품 금액은 100원 이상이여야합니다."),
     NOT_ENOUGH_CASH("해당 상품을 구매하기위한 금액이 부족합니다."),
     NOT_EXIST_PRODUCT("존재하지 않는 상품입니다."),
-    ;
+    COIN_NOT_NUMBER("금액은 숫자여야합니다."),
+    EXCHANGE_OUT_OF_RANGE("자판기가 가질 수 있는 금액은 0 ~ 100,000,000 입니다."),
+    PRODUCT_FORMAT_NOT_MATCH("입력형태가 올바르지않습니다.\n" +
+            "example : [콜라,1500,20];[사이다,1000,10]");
 
     private final String prefix = "[ERROR] ";
     private final String message;
