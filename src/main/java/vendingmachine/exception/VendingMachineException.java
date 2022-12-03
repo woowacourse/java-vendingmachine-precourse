28633f4894914616e9902b7dd84941b1fab409d2
nahyeon <whskgus12@nate.com>
2022-12-03 14:55:26 +0900

    feat(exception): 상품 금액이 100원 이상 10원 단위가 아닐 때
    사용자가 등록한 상품 금액이 100원부터 시작하지 않고, 10원 단위가 아니라면 예외를 발생시킵니다.

--- src/main/java/vendingmachine/exception/VendingMachineException.java
+++ src/main/java/vendingmachine/exception/VendingMachineException.java
@@ -2,15 +2,23 @@ package vendingmachine.exception;
 
 public class VendingMachineException {
 
-     public void validMoneyInputForm(String money){
+    public void validMoneyInputForm(String money) {
         final String ONLY_NUMBER = "^[0-9]*$";
-        if(!money.matches(ONLY_NUMBER))
+        if (!money.matches(ONLY_NUMBER))
             throw new IllegalArgumentException("[ERROR] 금액은 숫자만 입력하세요.");
     }
 
-    public void validGoodsInputForm(String goods){
+    public void validGoodsInputForm(String goods) {
         final String GOODS_INPUT_FORM = "\\[[가-힇]*,?[0-9]*,?[0-9]*\\];?";
-        if(goods.replaceAll(GOODS_INPUT_FORM,"").length()!=0)
+        if (goods.replaceAll(GOODS_INPUT_FORM, "").length() != 0)
             throw new IllegalArgumentException("[ERROR] 상품명, 가격, 수량은 쉼표로, 개별 상품은 대괄호([])로 묶어주세요.");
+    }
+
+    public void validGoodsMoneyType(int money) {
+        if (money < 100)
+            throw new IllegalArgumentException("[ERROR] 상품 가격은 100원부터 시작합니다.");
+
+        if (money % 10 != 0)
+            throw new IllegalArgumentException("[ERROR] 상품 가격은 10원 단위로 입력하세요.");
     }
 }
