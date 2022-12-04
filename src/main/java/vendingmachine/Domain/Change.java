626042b4d2ac092b9935f548c7635c62ae5b945e
rvlwldev <rvlwlasd77@gmail.com>
2022-12-04 17:47:03 +0900

    refactor: Change 클래스 필드의 자료형을 LinkedHashMap 으로 변경

--- src/main/java/vendingmachine/Domain/Change.java
+++ src/main/java/vendingmachine/Domain/Change.java
@@ -1,44 +1,40 @@
 package vendingmachine.Domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import vendingmachine.Constant.Coin;
+
+import java.util.LinkedHashMap;
 
 
 public class Change {
 
-    private final HashMap<Integer, Integer> coins;
+    private final LinkedHashMap<Coin, Integer> coins;
 
-    public Change(HashMap<Integer, Integer> coins) {
+    public Change(LinkedHashMap<Coin, Integer> coins) {
         this.coins = coins;
     }
 
-    public HashMap<Integer, Integer> getCoins() {
+    public LinkedHashMap<Coin, Integer> getCoins() {
         return coins;
     }
 
-    public HashMap<Integer, Integer> getChanges(int money) {
-        HashMap<Integer, Integer> changes = new HashMap<>();
+    public LinkedHashMap<Coin, Integer> getChanges(int money) {
+        LinkedHashMap<Coin, Integer> changes = new LinkedHashMap<>();
 
-        List<Integer> coinUnits = new ArrayList<>(this.coins.keySet());
-        coinUnits.sort(Collections.reverseOrder());
-
-        for (int unit : coinUnits) {
+        for (Coin unit : this.coins.keySet()) {
             int count = getChangeCoinCount(unit, money);
-            money -= unit * count;
+            money -= unit.getAmount() * count;
             changes.put(unit, count);
         }
 
         return changes;
     }
 
-    private int getChangeCoinCount(int unit, int money) {
+    private int getChangeCoinCount(Coin unit, int money) {
         int count = 0;
 
-        while (money >= unit) {
+        while (money >= unit.getAmount()) {
             if (coins.get(unit) > 0) {
-                money -= unit;
+                money -= unit.getAmount();
                 count++;
 
                 coins.put(unit, coins.get(unit) - 1);
