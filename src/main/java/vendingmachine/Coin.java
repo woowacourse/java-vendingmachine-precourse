443073f56d46e5ad9fa55cc542ed5973e8c0600c
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-12-04 17:27:27 +0900

    docs: 기능목록 작성

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,5 +1,12 @@
 package vendingmachine;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
@@ -8,9 +15,22 @@ public enum Coin {
 
     private final int amount;
 
+
     Coin(final int amount) {
         this.amount = amount;
     }
 
     // 추가 기능 구현
+    public int getAmount(){
+        return this.amount;
+    }
+
+    public static List<Integer> getCoins(){
+        return Arrays.stream(Coin.values())
+                .map(Coin::getAmount)
+                .collect(Collectors.toList());
+    }
+    public static int getRandomCoin(){
+        return Randoms.pickNumberInList(getCoins());
+    }
 }
