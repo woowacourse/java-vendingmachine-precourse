5805eadf0c000213c0c973dc4b18da27c2cdbdd2
glass <glass7312@gmail.com>
2022-12-04 11:45:01 +0900

    feat:main

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,7 +1,13 @@
 package vendingmachine;
 
+import java.io.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Vendingmachine vendingmachine=new Vendingmachine();
+
+
+
     }
 }
