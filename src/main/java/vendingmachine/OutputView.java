c5d70b593aa0f89bafae2021c6f8217f692edd64
glass <glass7312@gmail.com>
2022-12-04 11:42:35 +0900

    refactor:프린트 메세지 분리

--- src/main/java/vendingmachine/OutputView.java
+++ src/main/java/vendingmachine/OutputView.java
@@ -5,13 +5,20 @@ import java.util.List;
 public class OutputView {
 
     public void totalConins(List<Integer> own_coins) {
-        System.out.print("자판기가 보유한 동전" + "\n");
+
         System.out.print(Coin.COIN_500.getAmount() + "원" + " - " + own_coins.get(0) + "개" + "\n");
         System.out.print(Coin.COIN_100.getAmount() + "원" + " - " + own_coins.get(1) + "개" + "\n");
         System.out.print(Coin.COIN_50.getAmount() + "원" + " - " + own_coins.get(2) + "개" + "\n");
         System.out.print(Coin.COIN_10.getAmount() + "원" + " - " + own_coins.get(3) + "개" + "\n");
     }
-    public void printAmount(int Amount){
-            System.out.print("투입 금액:"+ Amount);
+
+    public void printAmount(int Amount) {
+        System.out.print("투입 금액: " + Amount + "원" + "\n");
+    }
+    public void vending_changes(){
+        System.out.print("자판기가 보유한 동전" + "\n");
+    }
+    public void changes(){
+        System.out.print("잔돈" + "\n");
     }
 }
