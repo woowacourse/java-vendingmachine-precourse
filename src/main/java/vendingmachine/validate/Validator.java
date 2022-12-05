a3912dda2ec4ef29a54d1a3fbd28f9c8777b97b6
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-12-05 15:54:18 +0900

    feat: create inputProducts method

--- src/main/java/vendingmachine/validate/Validator.java
+++ src/main/java/vendingmachine/validate/Validator.java
@@ -3,11 +3,28 @@ package vendingmachine.validate;
 import vendingmachine.util.ErrorMessage;
 
 public class Validator {
-    public static void inputDigitalValidator(String input){
+    private final String SEPARATE = ",";
+    private final String FORMAT_PREFIX = "[";
+    private final String FORMAT_SUFFIX = "]";
+
+    public void inputDigitalValidator(String input){
         try{
             Integer.parseInt(input);
         }catch (NumberFormatException e){
             throw new IllegalArgumentException(ErrorMessage.INPUT_NOT_DIGIT.getMessage());
         }
+    }
+    public void productFormatValidator(String input){
+        if(!(input.startsWith(FORMAT_PREFIX) && input.endsWith(FORMAT_SUFFIX)))
+            throw new IllegalArgumentException(ErrorMessage.PRODUCT_FORMAT_EXCEPTION.getMessage());
+    }
+    public void productInputSizeValidator(String input){
+        if(input.split(SEPARATE).length != 3)
+            throw new IllegalArgumentException(ErrorMessage.PRODUCT_FORMAT_EXCEPTION.getMessage());
+    }
+
+    public void productCountValidator(String input) {
+        if(Integer.valueOf(input) < 0)
+            throw new IllegalArgumentException(ErrorMessage.PRODUCT_COUNT_UNDER_ZERO.getMessage());
     }
 }
--- src/main/java/vendingmachine/view/View.java
+++ src/main/java/vendingmachine/view/View.java
@@ -2,12 +2,17 @@ package vendingmachine.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import vendingmachine.domain.Change;
+import vendingmachine.domain.Product;
 import vendingmachine.util.Message;
 import vendingmachine.validate.Validator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static vendingmachine.validate.Validator.*;
 
 public class View {
+    Validator validate = new Validator();
     public void printAskInputChange(){
         System.out.println(Message.MACHINE_CHANGE.getMessage());
     }
@@ -16,12 +21,42 @@ public class View {
         Change change;
         String inputChange = Console.readLine();
         try{
-            inputDigitalValidator(inputChange);
+            validate.inputDigitalValidator(inputChange);
             change = new Change(Integer.parseInt(inputChange));
         }catch (IllegalArgumentException e){
             System.out.println(e.getMessage());
             return inputChange();
         }
         return change;
+    }
+
+    public void printAskProducts(){
+        System.out.println(Message.PRODUCT_LIST.getMessage());
+    }
+
+    public List<Product> inputProducts(){
+        printAskProducts();
+        String inputProducts = Console.readLine();
+        List<Product> productsMaker;
+        try {
+            productsMaker = new ArrayList<>();
+            addProduct(inputProducts, productsMaker);
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return inputProducts();
+        }
+        return  productsMaker;
+    }
+    private void addProduct(String inputProducts, List<Product> productsMaker){
+        String[] products = inputProducts.split(";");
+        for (String product : products) {
+            validate.productFormatValidator(product);
+            validate.productInputSizeValidator(product);
+            String[] productValue = product.split(",");
+            validate.inputDigitalValidator(productValue[1]);
+            validate.inputDigitalValidator(productValue[2]);
+            validate.productCountValidator(productValue[2]);
+            productsMaker.add(new Product(productValue[0], Integer.parseInt(productValue[1]), Integer.parseInt(productValue[2])));
+        }
     }
 }
