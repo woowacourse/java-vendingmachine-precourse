bb8c761a8db42d8cfcb8e76471686cf6e50fd5aa
rvlwldev <rvlwlasd77@gmail.com>
2022-12-05 19:47:13 +0900

    fix: Product 클래스에서 자료형 변경 (일급컬렉션)

--- src/main/java/vendingmachine/Domain/Product.java
+++ src/main/java/vendingmachine/Domain/Product.java
@@ -1,31 +1,52 @@
 package vendingmachine.Domain;
 
+import vendingmachine.Constant.ProductSeparator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Product {
 
-    private final String name;
-    private final int price;
-    private int count;
+    private final HashMap<String, HashMap<String, Integer>> product;
 
-    public Product(String name, int price, int count) {
-        this.name = name;
-        this.price = price;
-        this.count = count;
+    public Product() {
+        this.product = new HashMap<>();
     }
 
-    public String getName() {
-        return name;
+    public void addProduct(String name, int price, int count) {
+        this.product.put(name, new HashMap<>() {{
+            put(ProductSeparator.PRICE, price);
+            put(ProductSeparator.COUNT, count);
+        }});
     }
 
-    public int getPrice() {
-        return price;
+    public List<String> getNames() {
+        return new ArrayList<>(this.product.keySet());
     }
 
-    public int getCount() {
-        return count;
+    public int getPrice(String name) {
+        return this.product.get(name).get(ProductSeparator.PRICE);
     }
 
-    public void sold () {
-        this.count--;
+    public int getCount(String name) {
+        return this.product.get(name).get(ProductSeparator.COUNT);
     }
+
+    public void sold(String name) {
+        int count = this.product.get(name).get(ProductSeparator.COUNT) - 1;
+        this.product.get(name).put(ProductSeparator.COUNT, count);
+    }
+
+    public int getCheapestPrice() {
+        int cheapestPrice = Integer.MAX_VALUE;
+
+        for (String name : getNames()) {
+            cheapestPrice = Math.min(getPrice(name), cheapestPrice);
+        }
+
+        return cheapestPrice;
+    }
+
 
 }
