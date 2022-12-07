8b8208b27705201d63ec71ad8c281c38b18419c7
Eunkeee <eunkeeeee@gmail.com>
2022-12-07 22:20:54 +0900

    refactor(Util): 하드코딩 제거

--- src/main/java/vendingmachine/util/Util.java
+++ src/main/java/vendingmachine/util/Util.java
@@ -4,17 +4,29 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Util {
+    private enum Regex {
+        SPACE(" "), NO_SPACE(""),
+        SQUARE_BRACKETS_START("["), SQUARE_BRACKETS_END("]"),
+        COMMA(",");
+
+        private final String regex;
+
+        Regex(String regex) {
+            this.regex = regex;
+        }
+    }
 
     public static String removeSpace(String input) {
-        return input.replaceAll(" ", "");
+        return input.replaceAll(Regex.SPACE.regex, Regex.NO_SPACE.regex);
     }
 
     public static String removeDelimiters(String input) {
-        return input.replace("[", "").replace("]", "");
+        return input.replace(Regex.SQUARE_BRACKETS_START.regex, Regex.NO_SPACE.regex)
+                .replace(Regex.SQUARE_BRACKETS_END.regex, Regex.NO_SPACE.regex);
     }
 
     public static List<String> splitByComma(String input) {
-        return Arrays.asList(Util.removeSpace(input).split(","));
+        return Arrays.asList(Util.removeSpace(input).split(Regex.COMMA.regex));
     }
 
     public static List<String> formatProductInfo(String input) {
