64228110c3a07db1735e3807ae366e72b5464261
Eunkeee <eunkeeeee@gmail.com>
2022-12-07 15:59:02 +0900

    feat(MachineMoneyValidator 10으로 나누어 떨어지지 않는 경우 예외 처리

--- src/main/java/vendingmachine/util/ExceptionMessage.java
+++ src/main/java/vendingmachine/util/ExceptionMessage.java
@@ -3,7 +3,8 @@ package vendingmachine.util;
 public enum ExceptionMessage {
 
     INVALID_NOT_NUMERIC("자연수만 입력 가능합니다."),
-    INVALID_OUT_OF_INT_RANGE("입력 범위를 초과하였습니다.");
+    INVALID_OUT_OF_INT_RANGE("입력 범위를 초과하였습니다."),
+    INVALID_UNIT("10원으로 나누어 떨어지는 수를 입력해 주세요.");
 
     public static final String BASE_MESSAGE = "[ERROR] %s";
     private final String message;
--- src/main/java/vendingmachine/util/validator/MachineMoneyValidator.java
+++ src/main/java/vendingmachine/util/validator/MachineMoneyValidator.java
@@ -1,11 +1,30 @@
 package vendingmachine.util.validator;
 
+import vendingmachine.util.ExceptionMessage;
 import vendingmachine.util.Util;
 
 public class MachineMoneyValidator extends Validator {
+    private enum Value {
+        MIN_UNIT(10);
+
+        private final int value;
+
+        Value(int value) {
+            this.value = value;
+        }
+    }
+
     @Override
     public void validate(String input) throws IllegalArgumentException {
         String machineMoney = Util.removeSpace(input);
         validateNumeric(machineMoney);
+        validateRange(machineMoney);
+        validateUnit(machineMoney);
+    }
+
+    private static void validateUnit(String machineMoney) {
+        if (Integer.parseInt(machineMoney) % Value.MIN_UNIT.value != 0) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_UNIT.getMessage());
+        }
     }
 }
