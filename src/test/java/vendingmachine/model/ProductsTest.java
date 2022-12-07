4035038d396a96d9d251399da2887f08d46d7ec8
Eunkeee <eunkeeeee@gmail.com>
2022-12-07 21:46:03 +0900

    feat(Products): 최저가 찾기 테스트 코드 작성

---
+++ src/test/java/vendingmachine/model/ProductsTest.java
@@ -0,0 +1,16 @@
+package vendingmachine.model;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ProductsTest {
+    private Products products = Products.from(Arrays.asList("[콜라,1000,20]", "[사이다,2000,10]", "[환타,1000,15]"));
+
+
+    @Test
+    void 최저가_찾기() {
+        Assertions.assertEquals(1000, products.findMinimumPrice());
+    }
+    
+}
