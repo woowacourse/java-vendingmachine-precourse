14fcd46a02d5b9a9b7cf9cfa5c9e71d163e4c1d3
BuildTools <unconfigured@null.spigotmc.org>
2022-12-08 10:16:10 +0900

    feat: 추가 skeleton

---
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -0,0 +1,14 @@
+package vendingmachine.controller;
+
+import vendingmachine.view.InputView;
+import vendingmachine.view.OutputView;
+
+public class VendingMachineController {
+
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    public void runVendingMachine() {
+
+    }
+}
