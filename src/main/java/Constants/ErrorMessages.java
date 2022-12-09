59c877f306509271515fc587ddf36944adec4ceb
hdsung98 <hdsung98@gmail.com>
2022-12-09 21:38:38 +0900

    feat: InputView 구현

--- src/main/java/Constants/ErrorMessages.java
+++ src/main/java/Constants/ErrorMessages.java
@@ -3,8 +3,7 @@ package Constants;
 public class ErrorMessages {
     public static final String ERROR_NOTHING_ENTERED = "[ERROR] 아무것도 입력되지 않았습니다.";
     public static final String ERROR_NOT_ARABIC_NUMBER = "[ERROR] 아라비아 숫자만 입력하세요.";
-    public static final String ERROR_NOT_POSITIVE_OR_DIVIDED_BY_TEN = "[ERROR] 10의 배수인 양수가 아닙니다,";
+    public static final String ERROR_NUMBER_OUF_OF_RANGE= "[ERROR] 금액이 입력조건을 충족하지 못했습니다.";
     public static final String ERROR_WRONG_FORMAT_PRODUCT = "[ERROR] 상품 입력 양식이 잘못되었습니다.";
-    public static final String ERROR_NOT_DIVIDED_BY_HUNDRED = "[ERROR] 100의 배수가 아닙니다.";
 }
 
--- src/main/java/UI/InputView.java
+++ src/main/java/UI/InputView.java
@@ -1,4 +1,76 @@
 package UI;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import Constants.CommonValues;
+import Constants.ErrorMessages;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class InputView {
+    OutputView outputView = new OutputView();
+
+    public int askVendingMachineMoney() {
+        outputView.enterVendingMachineMoney();
+        return askMoney(CommonValues.MINIMUM_COIN_VALUE);
+    }
+
+    public int askInputPrice() {
+        outputView.enterInputPrice();
+        return askMoney(CommonValues.MINIMUM_PRODUCT_VALUE);
+    }
+
+    private int askMoney(int dividend) {
+        while (true) {
+            try {
+                String input = readLine();
+                validateMoney(input,dividend);
+                return Integer.parseInt(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private void validateMoney(String input, int dividend) {
+        Pattern pattern = Pattern.compile(CommonValues.MONEY_REGEX);
+        Matcher matcher = pattern.matcher(input);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(ErrorMessages.ERROR_WRONG_FORMAT_PRODUCT);
+        }
+        int value = Integer.parseInt(input);
+        if (value <= 0 || value / dividend != 0) {
+            throw new IllegalArgumentException(ErrorMessages.ERROR_NUMBER_OUF_OF_RANGE);
+        }
+    }
+
+    public List<String> askProductsInfo() {
+        while (true) {
+            try {
+                outputView.enterProductInfo();
+                String input = readLine();
+                List<String> productInfo = validateProductsInfo(input);
+                return productInfo;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private List<String> validateProductsInfo(String input) {
+        Pattern pattern = Pattern.compile(CommonValues.PRODUCT_TOTAL_REGEX);
+        Matcher matcher = pattern.matcher(input);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(ErrorMessages.ERROR_WRONG_FORMAT_PRODUCT);
+        }
+        List<String> productInfo = new ArrayList<>();
+        while(matcher.find()){
+            productInfo.add(matcher.group(0));
+        }
+        return productInfo;
+    }
+
+
 }
