aa7e51799e06b4f950d6630b312f71bf26c23a69
BuildTools <unconfigured@null.spigotmc.org>
2022-12-10 03:50:46 +0900

    test: 추가 남은 금액이 상품의 최저 가격보다 적거나, 모든 상품이 소진된 경우 잔돈 반환 기능테스트

--- src/test/java/vendingmachine/CoinsTest.java
+++ src/test/java/vendingmachine/CoinsTest.java
@@ -2,11 +2,14 @@ package vendingmachine;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import vendingmachine.domain.coins.Coin;
 import vendingmachine.domain.coins.Coins;
 import vendingmachine.domain.Money;
 import vendingmachine.domain.coins.NumberGenerator;
 
+import java.util.EnumMap;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,6 +28,26 @@ public class CoinsTest {
         assertThat(coins.getCoins().get(Coin.COIN_100)).isEqualTo(1);
         assertThat(coins.getCoins().get(Coin.COIN_50)).isEqualTo(1);
         assertThat(coins.getCoins().get(Coin.COIN_10)).isEqualTo(2);
+    }
+
+    @DisplayName("잔돈을 성공적으로 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1100, 1, 2, 4, 20", "1000, 0, 2, 4, 20", "800, 0, 2, 4, 20", "500, 0, 1, 4, 20", "400, 0, 0, 4, 20",
+            "350, 0, 0, 3, 20", "200, 0, 0, 0, 20", "220, 0, 0, 0, 20", "130, 0, 0, 0, 13", "0, 0, 0, 0, 0"})
+    void makeLargestCoinsSuccess(int money, int coin500, int coin100, int coin50, int coin10) {
+        EnumMap<Coin, Integer> coins = new EnumMap<>(Coin.class);
+        coins.put(Coin.COIN_500, 1);
+        coins.put(Coin.COIN_100, 2);
+        coins.put(Coin.COIN_50, 4);
+        coins.put(Coin.COIN_10, 20);
+        Coins temp = new Coins(coins);
+        Coins changes = temp.makeLargestCoins(Money.from(money));
+
+        assertThat(changes.getCoins().get(Coin.COIN_500)).isEqualTo(coin500);
+        assertThat(changes.getCoins().get(Coin.COIN_100)).isEqualTo(coin100);
+        assertThat(changes.getCoins().get(Coin.COIN_50)).isEqualTo(coin50);
+        assertThat(changes.getCoins().get(Coin.COIN_10)).isEqualTo(coin10);
     }
 
     static class TestNumberGenerator implements NumberGenerator {
