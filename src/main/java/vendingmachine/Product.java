29f632e273ad8a6a45a1f9e6c2756a57df26920b
hdsung98 <hdsung98@gmail.com>
2022-12-10 00:28:36 +0900

    feat: Product 구현

--- src/main/java/vendingmachine/Product.java
+++ src/main/java/vendingmachine/Product.java
@@ -1,4 +1,32 @@
 package vendingmachine;
 
 public class Product {
+    private final String name;
+    private final int price;
+    private int amount;
+
+    public Product(String name, String price, String amount) {
+        this.name = name;
+        this.price = Integer.parseInt(price);
+        this.amount = Integer.parseInt(amount);
+    }
+
+    public void sellProduct() {
+        if (!isEmpty()) {
+            amount--;
+        }
+    }
+
+    public boolean isEmpty() {
+        return amount == 0;
+    }
+
+    public String askName() {
+        return new String(name);
+    }
+
+    public int askPrice() {
+        return price;
+    }
+
 }
