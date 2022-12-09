bcae353d4557a039965345783d848b50760143e9
hdsung98 <hdsung98@gmail.com>
2022-12-09 20:57:02 +0900

    feat: GuideMessages 구현

--- src/main/java/UI/OutputView.java
+++ src/main/java/UI/OutputView.java
@@ -1,4 +1,29 @@
 package UI;
 
+import static Constants.GuideMessages.*;
+
 public class OutputView {
+    public void enterVendingMachineMoney(){
+        System.out.println(ENTER_VENDING_MACHINE_MONEY);
+    }
+
+    public void printVendingMachineCoins(){
+        System.out.println(COIN_OWNED_BY_MACHINE);
+        /*for(){
+
+        }*/
+    }
+    public void enterProductInfo(){
+        System.out.println(ENTER_PRODUCT_INFO);
+    }
+    public void enterInputPrice(){
+        System.out.println(ENTER_INPUT_PRICE);
+    }
+
+    public void enterProductYouWillBuy(){
+        //System.out.println(REMAINING_PRICE+돈+원);
+        System.out.println(ENTER_PRODUCT_YOU_WILL_BUY);
+    }
+
+
 }
