d530275af2c022dc8c4aa82ed3f4f4bb0ef00b6d
SEONGJOON KIM <jobcho6320@naver.com>
2022-12-09 12:13:19 +0900

    feat: Product 객체 생성자 정의

--- src/main/java/vendingmachine/domain/Product.java
+++ src/main/java/vendingmachine/domain/Product.java
@@ -1,4 +1,13 @@
 package vendingmachine.domain;
 
 public class Product {
+    private String name;
+    private int price;
+    private int quantity;
+
+    public Product(String name, int price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
 }
