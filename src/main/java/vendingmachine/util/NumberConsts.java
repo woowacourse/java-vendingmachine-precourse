0e3ba711540c304104c94bba39050b38afec6bef
HaODay <ycis5013@naver.com>
2022-12-12 19:30:52 +0900

    refactor(NumberConsts): 상수 모음 클래스 생성

---
+++ src/main/java/vendingmachine/util/NumberConsts.java
@@ -0,0 +1,7 @@
+package vendingmachine.util;
+
+public class NumberConsts {
+    public static final int MIN_PRICE = 100;
+    public static final int MIN_UNIT = 10;
+    public static final int NUMBER_OF_ELEMENTS = 3;
+}
