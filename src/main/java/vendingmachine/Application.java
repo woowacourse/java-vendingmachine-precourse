fe37cf0a8964c3ba153547d2a85384e537c8ac1c
hdsung98 <hdsung98@gmail.com>
2022-12-12 15:30:38 +0900

    feat, refactor: VendingMachineCreator 구현
    VendingMachine 도메인 내부에서 다루고 있던 view 분리

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,10 +1,17 @@
 package vendingmachine;
 
+import Constants.Coin;
+import Controller.VendingMachineCreator;
+import UI.OutputView;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        VendingMachine vendingMachine = new VendingMachine();
+        VendingMachineCreator vendingMachineCreator = new VendingMachineCreator();
+        VendingMachine vendingMachine = vendingMachineCreator.createVendingMachine();
         Customer customer = new Customer();
         customer.purchaseProduct(vendingMachine);
-        vendingMachine.returnChanges(customer);
+        Map<Coin, Integer> returnedChanges = vendingMachine.returnChanges(customer);
+        OutputView.showReturningChanges(returnedChanges, customer.getInputMoney());
     }
 }
--- src/main/java/vendingmachine/VendingMachine.java
+++ src/main/java/vendingmachine/VendingMachine.java
@@ -1,65 +1,25 @@
 package vendingmachine;
 
-import static Constants.CommonValues.*;
+import static Constants.CommonValues.FALSE;
 
 import Constants.Coin;
-import UI.InputView;
-import UI.OutputView;
-import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import DTO.VendingMachineDto;
 
 public class VendingMachine {
     private int vendingMachineMoney;
     private Map<Coin, Integer> coinBox;
     private List<Product> productShelf;
 
-    public VendingMachine() {
-        vendingMachineMoney = InputView.askVendingMachineMoney();
-        coinBox = fillCoinsInBox(vendingMachineMoney);
-        productShelf = fillProducts(InputView.askProductsInfo());
-    }
-
-    private Map<Coin, Integer> fillCoinsInBox(int vendingMachineMoney) {
-        Map<Coin, Integer> coinBox = new HashMap<>();
-        splitMoneyIntoCoins(coinBox);
-        return coinBox;
-    }
-
-    private void splitMoneyIntoCoins(Map<Coin, Integer> coinBox) {
-        for (Coin coin : Coin.values()) {
-            int coinNumber = decideCoinNumber(coin);
-            coinBox.put(coin, coinNumber);
-            vendingMachineMoney -= coin.getAmount() * coinNumber;
-            if (vendingMachineMoney == 0) {
-                return;
-            }
-        }
-    }
-
-    private int decideCoinNumber(Coin coin) {
-        if (coin.getAmount() == MINIMUM_COIN_VALUE) {
-            return vendingMachineMoney / MINIMUM_COIN_VALUE;
-        }
-        return Randoms.pickNumberInRange(0, vendingMachineMoney / coin.getAmount());
-    }
-
-    private List<Product> fillProducts(List<String> productsInfo) {
-        List<Product> productShelf = new ArrayList<>();
-        for (String productInfo : productsInfo) {
-            String[] info = productInfo.replaceAll(REMOVE_REGEX, BLANK).split(COMMA);
-            Product product = new Product(info[0], info[1], info[2]);
-            productShelf.add(product);
-        }
-        return productShelf;
+    public VendingMachine(int vendingMachineMoney, Map<Coin, Integer> coinBox, List<Product> productShelf) {
+        this.vendingMachineMoney = vendingMachineMoney;
+        this.coinBox = coinBox;
+        this.productShelf = productShelf;
     }
 
     public int hasSuchProduct(String wishList) {
         for (Product product : productShelf) {
-            if (product.askName().equals(wishList)) {
+            if (product.askItsName().equals(wishList)) {
                 return productShelf.indexOf(product);
             }
         }
@@ -73,7 +33,7 @@ public class VendingMachine {
     public int getPrice(String wishList) {
         int targetIndex = hasSuchProduct(wishList);
         Product product = productShelf.get(targetIndex);
-        return product.askPrice();
+        return product.askItsPrice();
     }
 
     public void decreaseStock(String wishList) {
@@ -82,9 +42,12 @@ public class VendingMachine {
         product.sellProduct();
     }
 
-    public void returnChanges(Customer customer) {
+    public Map<Coin, Integer> returnChanges(Customer customer) {
         ChangeCalculator changeCalculator = new ChangeCalculator(vendingMachineMoney, coinBox, customer);
-        Map<Coin, Integer> result = changeCalculator.calculateResult();
-        OutputView.showReturningChanges(result, customer.getInputMoney());
+        return changeCalculator.calculateResult();
+    }
+
+    public Map<Coin, Integer> askItsCoinBox() {
+        return coinBox;
     }
 }
