d8d3fc9a51115eacbbdc520f47d33a5e472d96f4
HaODay <ycis5013@naver.com>
2022-12-12 19:32:04 +0900

    refactor(Validator): 생성한 열거 상수 적용

--- src/main/java/vendingmachine/model/Validator.java
+++ src/main/java/vendingmachine/model/Validator.java
@@ -1,28 +1,81 @@
 package vendingmachine.model;
 
-import camp.nextstep.edu.missionutils.Console;
-
 import java.util.Map;
+
+import static vendingmachine.util.NumberConsts.MIN_PRICE;
+import static vendingmachine.util.NumberConsts.MIN_UNIT;
+import static vendingmachine.util.NumberConsts.NUMBER_OF_ELEMENTS;
+import static vendingmachine.util.message.ErrorMessage.INVALID_FORMAT;
+import static vendingmachine.util.message.ErrorMessage.INVALID_PRICE;
+import static vendingmachine.util.message.ErrorMessage.INVALID_UNIT;
+import static vendingmachine.util.message.ErrorMessage.IS_NEGATIVE;
+import static vendingmachine.util.message.ErrorMessage.NOT_NUMBER;
+import static vendingmachine.util.message.ProductErrorMessage.INSUFFICIENT_BALANCE;
+import static vendingmachine.util.message.ProductErrorMessage.INVALID_NAME;
+import static vendingmachine.util.message.ProductErrorMessage.NOT_FOR_SALE;
+import static vendingmachine.util.message.ProductErrorMessage.OUT_OF_STOCK;
 
 public class Validator {
 
+    public static int validateBalance(String input) throws IllegalArgumentException {
+        int balance = validateNum(input);
+        validateNegative(balance);
+        validateInvalidUnit(balance);
+        return balance;
+    }
+
+    public static int validateAmountOfInput(String input) throws IllegalArgumentException {
+        int amountOfInput = validateNum(input);
+        validateNegative(amountOfInput);
+        return amountOfInput;
+    }
+
+    public static void validatePrice(int price) {
+        if (price < MIN_PRICE) {
+            throw new IllegalArgumentException(INVALID_PRICE.fullMessage());
+        }
+        validateInvalidUnit(price);
+    }
+
     public static void validateProduct(String[] productInfo, Map<String, Product> products) throws IllegalArgumentException {
-        if (productInfo.length != 3) {
-            throw new IllegalArgumentException("형식에 맞지 않습니다.");
+        if (productInfo.length != NUMBER_OF_ELEMENTS) {
+            throw new IllegalArgumentException(INVALID_FORMAT.fullMessage());
         }
         if (products.containsKey(productInfo[0])) {
-            throw new IllegalArgumentException("같은 이름의 상품은 입력할 수 없습니다.");
+            throw new IllegalArgumentException(INVALID_NAME.fullMessage());
         }
     }
-    public static void validateBuyingProduct(String buyingProduct, Map<String, Product> products, int amountOfInput) throws IllegalArgumentException {
+
+    public static void validateBuyingProduct(String buyingProduct, Map<String, Product> products,
+                                             int amountOfInput) throws IllegalArgumentException {
         if (!products.containsKey(buyingProduct)) {
-            throw new IllegalArgumentException("판매하지 않는 상품입니다.");
+            throw new IllegalArgumentException(NOT_FOR_SALE.fullMessage());
         }
-        if (products.get(buyingProduct).price.get() > amountOfInput) {
-            throw new IllegalArgumentException("잔액이 부족합니다.");
+        if (products.get(buyingProduct).getPrice() > amountOfInput) {
+            throw new IllegalArgumentException(INSUFFICIENT_BALANCE.fullMessage());
         }
-        if (products.get(buyingProduct).amount <= 0) {
-            throw new IllegalArgumentException("상품이 품절되었습니다.");
+        if (!products.get(buyingProduct).stockIsLeft()) {
+            throw new IllegalArgumentException(OUT_OF_STOCK.fullMessage());
+        }
+    }
+
+    public static int validateNum(String input) throws IllegalArgumentException {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_NUMBER.fullMessage());
+        }
+    }
+
+    public static void validateNegative(int num) throws IllegalArgumentException {
+        if (num < 0) {
+            throw new IllegalArgumentException(IS_NEGATIVE.fullMessage());
+        }
+    }
+
+    public static void validateInvalidUnit(int num) throws IllegalArgumentException {
+        if (num % MIN_UNIT != 0) {
+            throw new IllegalArgumentException(INVALID_UNIT.fullMessage());
         }
     }
 }
