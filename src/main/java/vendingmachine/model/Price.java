ec2a16396e2ab3d2237b3f06462a80a29123ff23
HaODay <ycis5013@naver.com>
2022-12-12 19:27:18 +0900

    refactor(Price): 검증 관련 코드 Validator로 이동

--- src/main/java/vendingmachine/model/Price.java
+++ src/main/java/vendingmachine/model/Price.java
@@ -1,27 +1,16 @@
 package vendingmachine.model;
 
-public class Price {
-    private int price;
+import static vendingmachine.model.Validator.validateNum;
+import static vendingmachine.model.Validator.validatePrice;
 
+public class Price {
+    private final int price;
     Price(String price) {
-        try {
-            this.price = Integer.parseInt(price);
-            validate(this.price);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("가격은 숫자여야 합니다.");
-        }
+        this.price = validateNum(price);
+        validatePrice(this.price);
     }
 
     public int get() {
         return price;
-    }
-
-    private void validate(int price) {
-        if (price < 100) {
-            throw new IllegalArgumentException("가격은 100원 이상이어야 합니다.");
-        }
-        if (price % 10 != 0) {
-            throw new IllegalArgumentException("가격은 10원으로 나누어떨어져야 합니다.");
-        }
     }
 }
