965ed3898ea0d6aa6ca6b9594df96557b58fc625
LHS-11 <ghktjq1119@naver.com>
2022-12-16 15:33:51 +0900

    [Docs] : 기능 구현 목록 작성

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,7 +1,13 @@
 package vendingmachine;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int i = Randoms.pickNumberInList(Arrays.asList(0, 4, 9, 45));
+        System.out.println(i);
     }
 }
