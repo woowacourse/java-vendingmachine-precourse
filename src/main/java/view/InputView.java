43a49f8f51a64a2dcab5cc997373de00aee58004
LHS-11 <ghktjq1119@naver.com>
2022-12-16 18:21:09 +0900

    [Feat] : 구매할 상품명 입력 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,6 +7,7 @@ public class InputView {
     private static final String INPUT_VENDING_MACHINE_MONEY_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
     private static final String INPUT_PRODUCT_MESSAGE = "상품명과 가격, 수량을 입력해 주세요.";
     private static final String INPUT_INSERT_MESSAGE = "투입 금액을 입력해 주세요.";
+    private static final String INPUT_BUY_PRODUCT_MESSAGE = "구매할 상품명을 입력해 주세요.";
     public String inputVendingMachineMoney(){
         System.out.println(INPUT_VENDING_MACHINE_MONEY_MESSAGE);
         return Console.readLine().trim();
@@ -19,6 +20,11 @@ public class InputView {
 
     public String inputInsertMoney(){
         System.out.println("\n"+INPUT_INSERT_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String inputBuyProduct(){
+        System.out.println(INPUT_BUY_PRODUCT_MESSAGE);
         return Console.readLine();
     }
 }
