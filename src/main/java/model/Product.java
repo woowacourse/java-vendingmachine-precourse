fe670ce0de5d6544316427ed090b47abbd262aff
LHS-11 <ghktjq1119@naver.com>
2022-12-16 16:03:59 +0900

    [Feat] : 상품 입력 기능 구현

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -6,5 +6,10 @@ public class Product {
     private int price;
     private int total;
 
+    public Product(String name, int price, int total) {
+        this.name = name;
+        this.price = price;
+        this.total = total;
+    }
 
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,10 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private static final String INPUT_VENDING_MACHINE_MONEY = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    private static final String INPUT_VENDING_MACHINE_MONEY_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    private static final String INPUT_PRODUCT_MESSAGE = "상품명과 가격, 수량을 입력해 주세요.";
 
     public String inputVendingMachineMoney(){
-        System.out.println(INPUT_VENDING_MACHINE_MONEY);
+        System.out.println(INPUT_VENDING_MACHINE_MONEY_MESSAGE);
+        return Console.readLine().trim();
+    }
+
+    public String inputProduct(){
+        System.out.println(INPUT_PRODUCT_MESSAGE);
         return Console.readLine().trim();
     }
 }
