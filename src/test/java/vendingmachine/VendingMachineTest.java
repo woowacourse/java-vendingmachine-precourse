d74b11aaeb0f2e5e433fae2a6caf64998bd00f69
jhon3242 <rldotnr2@gmail.com>
2023-11-20 19:53:35 +0900

    feat : 상품 구매에 대한 테스트 케이스 추가

--- src/test/java/vendingmachine/VendingMachineTest.java
+++ src/test/java/vendingmachine/VendingMachineTest.java
@@ -1,6 +1,7 @@
 package vendingmachine;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -16,4 +17,42 @@ public class VendingMachineTest {
         });
     }
 
+    @DisplayName("남은 금액이 부족한 경우 구매 시 예외 발생")
+    @ParameterizedTest
+    @ValueSource(ints = {1500, 2000, 2999})
+    void purchaseProductFailWithLackMoney(int money) {
+        VendingMachine vendingMachine = new VendingMachine();
+        vendingMachine.initMoney(0);
+
+        ProductRepository productRepository = new ProductRepository();
+        productRepository.initProductsByString("[콜라,1500,1]");
+        vendingMachine.initProducts(productRepository);
+
+        vendingMachine.initInputMoney(money);
+
+        vendingMachine.purchaseProduct("콜라");
+
+        Assertions.assertThatThrownBy(() -> {
+            vendingMachine.purchaseProduct("콜라");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품이 소진된 경우 구매 시 예외 발생")
+    @Test
+    void purchaseProductFailWithQuantity() {
+        VendingMachine vendingMachine = new VendingMachine();
+        vendingMachine.initMoney(0);
+
+        ProductRepository productRepository = new ProductRepository();
+        productRepository.initProductsByString("[콜라,1500,1]");
+        vendingMachine.initProducts(productRepository);
+
+        vendingMachine.initInputMoney(3000);
+
+        vendingMachine.purchaseProduct("콜라");
+
+        Assertions.assertThatThrownBy(() -> {
+            vendingMachine.purchaseProduct("콜라");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
 }
