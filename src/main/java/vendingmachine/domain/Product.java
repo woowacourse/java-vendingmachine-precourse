c7a2b21df33805b9a90ba340b26815bf196cf2e1
oyoungsun <oyoungsun1030@gmail.com>
2023-11-20 17:18:17 +0900

    feat(Product) : 상품 도메인 생성 기능 구현

---
+++ src/main/java/vendingmachine/domain/Product.java
@@ -0,0 +1,19 @@
+package vendingmachine.domain;
+
+import vendingmachine.validators.ProductValidator;
+
+public class Product {
+    private final String name;
+    private final int price;
+
+    private Product(final String name, final int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public static Product of (String name, int price){
+        ProductValidator.validate(name, price);
+        return new Product(name, price);
+    }
+
+}
