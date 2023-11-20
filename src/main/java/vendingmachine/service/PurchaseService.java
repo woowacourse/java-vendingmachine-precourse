917e9c33563b586bec4fb0a67fc87012c08a113f
oyoungsun <oyoungsun1030@gmail.com>
2023-11-20 21:26:33 +0900

    docs : 상품 구매 관련 상세 기능 추가

--- src/main/java/vendingmachine/service/PurchaseService.java
+++ src/main/java/vendingmachine/service/PurchaseService.java
@@ -11,8 +11,12 @@ public class PurchaseService {
         this.inputAmount = inputAmount;
     }
 
-
     public static PurchaseService of(final Products products, final Integer inputAmount) {
         return new PurchaseService(products, inputAmount);
+    }
+
+    public void purchase() {
+        //TODO : products에 존재하나?
+        //현재 금액으로 구매가 가능한가?
     }
 }
