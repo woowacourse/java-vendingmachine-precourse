b86b176d141e97ab607bbc7ba12a741bb49dd312
oyoungsun <oyoungsun1030@gmail.com>
2023-11-20 18:24:00 +0900

    test(Product) : 예외 반환 테스트 coverage 100% 작성

--- src/main/java/vendingmachine/validators/ProductPriceValidator.java
+++ src/main/java/vendingmachine/validators/ProductPriceValidator.java
@@ -5,7 +5,7 @@ import vendingmachine.domain.Coin;
 public class ProductPriceValidator {
     private static final int MINIMAL_PRODUCT_MONEY = 100;
     private static final String BOUNDARY_EXCEPTION = String.format("상품의 최소 금액은 %d원입니다", MINIMAL_PRODUCT_MONEY);
-    private static final String DIVIDED_BYCOIN_EXCEPTION = String.format("상품 금액은 10원 단위로 나누어 떨어집니다.",
+    private static final String DIVIDED_BYCOIN_EXCEPTION = String.format("상품 금액은 %d원 단위로 나누어 떨어집니다.",
             Coin.COIN_10.getAmount());
 
 
@@ -23,6 +23,7 @@ public class ProductPriceValidator {
 
     private static void isBoundary(final int price) {
         if (price < MINIMAL_PRODUCT_MONEY) {
+            System.out.println("Test");
             throw new IllegalArgumentException(BOUNDARY_EXCEPTION);
         }
     }
--- src/test/java/vendingmachine/validators/ProductPriceValidatorTest.java
+++ src/test/java/vendingmachine/validators/ProductPriceValidatorTest.java
@@ -1,10 +1,12 @@
 package vendingmachine.validators;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import vendingmachine.domain.Coin;
 
 class ProductPriceValidatorTest {
 
@@ -16,17 +18,19 @@ class ProductPriceValidatorTest {
 
     @ParameterizedTest
     @ValueSource(ints = {99, 0, -1})
-    void validate는_가격이_100원미만이면_예외반환(int lessThanMinimum) {
-        assertThatThrownBy(() -> ProductPriceValidator.validate(lessThanMinimum))
-                .isInstanceOf(IllegalArgumentException.class);
+    void validate는_가격이_100원미만이면_실패(int lessThanMinimum) {
+        final IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> ProductPriceValidator.validate(lessThanMinimum));
+        assertEquals(exception.getMessage(), String.format("상품의 최소 금액은 %d원입니다", 100));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {199, 123, 1000000001})
-    void validate는_가격이_10원으로_나누어떨어지지않으면_예외반환(int nonDiveded) {
-        String name = "사이다";
-        assertThatThrownBy(() -> ProductPriceValidator.validate(nonDiveded))
-                .isInstanceOf(IllegalArgumentException.class);
+    void validate는_가격이_10원으로_나누어떨어지지않으면_실패(int nonDiveded) {
+        final IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> ProductPriceValidator.validate(nonDiveded));
+        assertEquals(exception.getMessage(), String.format("상품 금액은 10원 단위로 나누어 떨어집니다.",
+                Coin.COIN_10.getAmount()));
     }
 
 }
