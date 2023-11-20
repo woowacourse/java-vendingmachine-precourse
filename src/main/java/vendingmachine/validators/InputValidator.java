1410922bb40be90e19b9a449a686e5705a74a78f
oyoungsun <oyoungsun1030@gmail.com>
2023-11-20 17:54:57 +0900

    feat(InputValidator) : 입력값 검증 기능 목록 구현

---
+++ src/main/java/vendingmachine/validators/InputValidator.java
@@ -0,0 +1,11 @@
+package vendingmachine.validators;
+
+public class InputValidator {
+    public static String validateInt(final String intInput) {
+        return null;
+    }
+
+    public static String validateStringint(final String stringInput) {
+        return null;
+    }
+}
--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,23 +1,36 @@
 package vendingmachine.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import vendingmachine.validators.InputValidator;
+
 public class InputView implements Input{
     @Override
     public String readMoney() {
-        return null;
+        return readInt();
     }
 
     @Override
     public String readProducts() {
-        return null;
+        return readString();
     }
 
     @Override
     public String readInputAmount() {
-        return null;
+        return readInt();
     }
 
     @Override
     public String readWanted() {
-        return null;
+        return readString();
+    }
+
+    private String readInt(){
+        String intInput = Console.readLine();
+        return InputValidator.validateInt(intInput);
+    }
+
+    private String readString(){
+        String stringInput = Console.readLine();
+        return InputValidator.validateStringint(stringInput);
     }
 }
