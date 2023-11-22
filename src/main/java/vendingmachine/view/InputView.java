b02888a453e1080696c8d0384b08f0023546104e
Hanjaemo <hjm2530@gmail.com>
2023-11-22 15:55:22 +0900

    feat(InputView): 구매할 상품명 입력 기능 구현

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -36,4 +36,9 @@ public class InputView {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
         }
     }
+
+    public static String readProductNameToPurchase() {
+        System.out.println("구매할 상품명을 입력해 주세요.");
+        return Console.readLine();
+    }
 }
