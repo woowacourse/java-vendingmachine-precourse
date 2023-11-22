ded3b4460cda5a36c0f57602356f2375215dfcfd
Hanjaemo <hjm2530@gmail.com>
2023-11-22 18:48:24 +0900

    feat(InputAmount, Controller): 투입 금액 검증 기능 구현

--- src/main/java/vendingmachine/controller/Controller.java
+++ src/main/java/vendingmachine/controller/Controller.java
@@ -3,7 +3,6 @@ package vendingmachine.controller;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
-
 import vendingmachine.domain.Coin;
 import vendingmachine.domain.InputAmount;
 import vendingmachine.domain.Product;
@@ -15,18 +14,36 @@ import vendingmachine.view.OutputView;
 public class Controller {
 
     public void run() {
-        VendingMachineAmount vendingMachineAmount = repeatReadForInvalid(this::createVendingMachineAmount);
         VendingMachine vendingMachine = new VendingMachine();
+        VendingMachineAmount vendingMachineAmount = repeatReadForInvalid(this::createVendingMachineAmount);
+        addCoinsToVendingMachine(vendingMachineAmount, vendingMachine);
+        addProductsToVendingMachine(vendingMachine);
+
+        InputAmount inputAmount = repeatReadForInvalid(this::createInputAmount);
+        repeatPurchaseProduct(vendingMachine, inputAmount);
+
+        Map<Coin, Integer> changes = vendingMachine.changes(inputAmount);
+        OutputView.printChanges(changes);
+    }
+
+    private void addCoinsToVendingMachine(VendingMachineAmount vendingMachineAmount, VendingMachine vendingMachine) {
         vendingMachine.addCoins(vendingMachineAmount.getVendingMachineAmount());
         OutputView.printCoinsOfVendingMachine(vendingMachine.getCoins());
+    }
 
+    private void addProductsToVendingMachine(VendingMachine vendingMachine) {
         List<String> inputProducts = InputView.readProducts();
         for (String inputProduct : inputProducts) {
             String[] split = inputProduct.split(",");
             vendingMachine.addProduct(repeatReadForInvalid(() -> this.createProduct(split)));
         }
+    }
 
-        InputAmount inputAmount = new InputAmount(InputView.readInputAmount());
+    private InputAmount createInputAmount() {
+        return new InputAmount(InputView.readInputAmount());
+    }
+
+    private void repeatPurchaseProduct(VendingMachine vendingMachine, InputAmount inputAmount) {
         while (true) {
             OutputView.printRemainingInputAmount(inputAmount.getAmount());
             if (vendingMachine.isAllPriceGreaterThan(inputAmount.getAmount()) || vendingMachine.isAllProductSoldOut()) {
@@ -35,9 +52,6 @@ public class Controller {
             String productNameToPurchase = InputView.readProductNameToPurchase();
             vendingMachine.purchase(productNameToPurchase, inputAmount);
         }
-
-        Map<Coin, Integer> changes = vendingMachine.changes(inputAmount);
-        OutputView.printChanges(changes);
     }
 
     private Product createProduct(String[] split) {
--- src/main/java/vendingmachine/domain/InputAmount.java
+++ src/main/java/vendingmachine/domain/InputAmount.java
@@ -5,7 +5,17 @@ public class InputAmount {
     private int amount;
 
     public InputAmount(int amount) {
+        validate(amount);
         this.amount = amount;
+    }
+
+    private void validate(int amount) {
+        if (amount < 10) {
+            throw new IllegalArgumentException("[ERROR] 투입 금액은 10원 이상이어야 합니다.");
+        }
+        if (amount % 10 != 0) {
+            throw new IllegalArgumentException("[ERROR] 투입 금액은 10원 단위만 가능합니다.");
+        }
     }
 
     public void decrease(int amount) {
