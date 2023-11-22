d73a5393ab277527f754d437b7822324e6790d22
Hanjaemo <hjm2530@gmail.com>
2023-11-22 18:56:25 +0900

    chore(RandomNumberGenerator, VendingMachine): RandomNumberGenerator 클래스 패키지 위치 변경

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
-import vendingmachine.RandomNumberGenerator;
+import vendingmachine.utils.RandomNumberGenerator;
 
 public class VendingMachine {
 
--- src/main/java/vendingmachine/utils/RandomNumberGenerator.java
+++ src/main/java/vendingmachine/utils/RandomNumberGenerator.java
@@ -1,4 +1,4 @@
-package vendingmachine;
+package vendingmachine.utils;
 
 import java.util.List;
 
