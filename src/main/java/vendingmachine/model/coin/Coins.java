5f88b75a8fb3f53d36820dff4530121df0df1036
HongYeseul <ghd8119@naver.com>
2023-11-22 17:12:17 +0900

    :art: style(coin): 동전 패키지 생성

--- src/main/java/vendingmachine/model/coin/Coins.java
+++ src/main/java/vendingmachine/model/coin/Coins.java
@@ -1,4 +1,4 @@
-package vendingmachine.model;
+package vendingmachine.model.coin;
 
 import java.util.ArrayList;
 import java.util.HashMap;
--- src/main/java/vendingmachine/model/coin/RandomCoins.java
+++ src/main/java/vendingmachine/model/coin/RandomCoins.java
@@ -1,4 +1,4 @@
-package vendingmachine.model;
+package vendingmachine.model.coin;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
