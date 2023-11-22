64bf97345af8e95c2900b24c9c8282eb0c6908c5
Hanjaemo <hjm2530@gmail.com>
2023-11-22 19:01:14 +0900

    refactor(Constants, InputAmount): 매직 리터럴 상수화 처리

--- src/main/java/vendingmachine/domain/InputAmount.java
+++ src/main/java/vendingmachine/domain/InputAmount.java
@@ -1,6 +1,11 @@
 package vendingmachine.domain;
 
+import vendingmachine.constants.Constants;
+
 public class InputAmount {
+
+    private static final int MIN_INPUT_AMOUNT = 10;
+    private static final int INPUT_AMOUNT_UNIT = 10;
 
     private int amount;
 
@@ -10,11 +15,13 @@ public class InputAmount {
     }
 
     private void validate(int amount) {
-        if (amount < 10) {
-            throw new IllegalArgumentException("[ERROR] 투입 금액은 10원 이상이어야 합니다.");
+        if (amount < MIN_INPUT_AMOUNT) {
+            throw new IllegalArgumentException(
+                    String.format("%s 투입 금액은 %d원 이상이어야 합니다.", Constants.ERROR_PREFIX, MIN_INPUT_AMOUNT));
         }
-        if (amount % 10 != 0) {
-            throw new IllegalArgumentException("[ERROR] 투입 금액은 10원 단위만 가능합니다.");
+        if (amount % INPUT_AMOUNT_UNIT != 0) {
+            throw new IllegalArgumentException(
+                    String.format("%s 투입 금액은 %d원 단위만 가능합니다.", Constants.ERROR_PREFIX, INPUT_AMOUNT_UNIT));
         }
     }
 
