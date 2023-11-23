a73e345e26cda96c5f2f7965d60f880139cc7b9e
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-11-24 02:04:37 +0900

    fix: 테스트에서 사용되는 randoms 메서드로 변경

--- src/main/java/vendingmachine/domain/CoinGenerator.java
+++ src/main/java/vendingmachine/domain/CoinGenerator.java
@@ -1,27 +1,29 @@
 package vendingmachine.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import vendingmachine.Coin;
 
 public class CoinGenerator {
-    private final Map<Coin,Integer> coins;
+    private final Map<Coin, Integer> coins;
 
     public CoinGenerator(int money) {
         this.coins = generate(money);
     }
 
-    private Map<Coin,Integer> generate(int money){
+    private Map<Coin, Integer> generate(int money) {
         Map<Coin, Integer> coins = new HashMap<>();
+        List<Integer> coinList = Arrays.asList(1, 2, 3, 4);
+        while (money > 0) {
+            Coin randomCoin = Coin.values()[Randoms.pickNumberInList(coinList) % 4];
 
-        while(money > 0){
-            Coin randomCoin = Coin.values()[Randoms.pickNumberInRange(0, Coin.values().length-1)];
-
-            if(randomCoin.getAmount() <= money){
-                coins.put(randomCoin, coins.getOrDefault(randomCoin,0) + 1);
+            if (randomCoin.getAmount() <= money) {
+                coins.put(randomCoin, coins.getOrDefault(randomCoin, 0) + 1);
                 money -= randomCoin.getAmount();
             }
         }
