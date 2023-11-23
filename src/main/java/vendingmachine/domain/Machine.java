eb55fe317e0b3ba66139edb0331d1a0296bca7c3
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-11-24 01:20:15 +0900

    refactor: 스트림 코드 간결화

--- src/main/java/vendingmachine/domain/Machine.java
+++ src/main/java/vendingmachine/domain/Machine.java
@@ -18,65 +18,43 @@ public class Machine {
 
 
     public int buy(String productName) {
-        for (int i = 0; i < products.size(); i++) {
-            if (productName.equals(products.get(i).getName())) {
-                products.get(i).soldProduct();
-                return products.get(i).getPrice();
+        for (Product product : products) {
+            if (productName.equals(product.getName())) {
+                product.soldProduct();
+                return product.getPrice();
             }
         }
         return 0;
     }
 
     public boolean canBuyWithMoney(int userAmount) {
-        boolean flag = false;
-        for (int i = 0; i < products.size(); i++) {
-            if (userAmount >= products.get(i).getPrice()) {
-                flag = true;
-                break;
-            }
-        }
-        return flag;
+        return products.stream().anyMatch(product -> userAmount >= product.getPrice());
     }
 
     public boolean canBuyWithQuantity() {
-        boolean flag = false;
-        for(int i = 0; i < products.size(); i++){
-            if(products.get(i).getQuantity() > 0) {
-                flag = true;
-                break;
-            }
-        }
-        return flag;
+        return products.stream().anyMatch(product -> product.getQuantity() > 0);
     }
 
-    public void isExistsQuantity(String productName){
-        boolean flag = false;
-        for(int i = 0; i < products.size(); i++){
-            Product product = products.get(i);
-            if(product.getName().equals(productName)) {
-                if(product.getQuantity() > 0)
-                flag = true;
-                break;
-            }
-        }
-        if(!flag){
+    public void isExistsQuantity(String productName) {
+        boolean productAvailable = products.stream()
+                .filter(product -> product.getName().equals(productName))
+                .anyMatch(product -> product.getQuantity() > 0);
+
+        if (!productAvailable) {
             throw new IllegalArgumentException("[ERROR] 주문하려는 상품이 소진되었습니다. 다른 상품을 주문해 주세요.");
         }
     }
 
     public void isExistsProduct(String productName) {
-        boolean flag = false;
-        for (int i = 0; i < products.size(); i++) {
-            if (productName.equals(products.get(i).getName())) {
-                flag = true;
-            }
-        }
-        if (!flag) {
+        boolean productExists = products.stream()
+                .anyMatch(product -> productName.equals(product.getName()));
+
+        if (!productExists) {
             throw new IllegalArgumentException("[ERROR] 존재하지 않는 상품입니다.");
         }
     }
 
-    public Map<Coin,Integer> calculateChange(int userAmount) {
+    public Map<Coin, Integer> calculateChange(int userAmount) {
         Map<Coin, Integer> sortedCoins = new TreeMap<>(Comparator.comparing(Coin::getAmount).reversed());
 
         for (Entry<Coin, Integer> entry : coins.entrySet()) {
