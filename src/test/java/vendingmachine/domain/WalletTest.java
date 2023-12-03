00b29f554835edbb7d2e95181a8da214227e745c
slowcloud <sju9417@gmail.com>
2023-12-03 15:11:17 +0900

    test: Wallet의 getRests에 대한 테스트 코드 추가

--- src/test/java/vendingmachine/domain/WalletTest.java
+++ src/test/java/vendingmachine/domain/WalletTest.java
@@ -4,8 +4,16 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+import static org.mockito.Mockito.*;
 
 class WalletTest {
     @Nested
@@ -31,6 +39,24 @@ class WalletTest {
                             "50원 - 0개",
                             "10원 - 1개"
                     );
+        }
+    }
+
+    @Nested
+    class getRests {
+        @DisplayName("잔돈을 잘 구하는지 확인한다.")
+        @Test
+        void testGetRests() {
+            Wallet wallet = new Wallet(510);
+            Money money = mock(Money.class);
+            given(money.getMoney()).willReturn(110);
+
+            List<CoinCount> coinCounts = wallet.getRests(money);
+            String message = coinCounts.stream()
+                    .map(CoinCount::getMessage)
+                    .collect(Collectors.joining("\n"));
+
+            assertEquals(message, "10원 - 1개");
         }
     }
 }
