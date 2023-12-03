fec3760fa76d984bf666c5e81445a80144036665
slowcloud <sju9417@gmail.com>
2023-12-03 12:23:57 +0900

    feat: ItemName 구현

--- src/main/java/vendingmachine/domain/VendingMachine/ItemName.java
+++ src/main/java/vendingmachine/domain/VendingMachine/ItemName.java
@@ -1,4 +1,13 @@
 package vendingmachine.domain.VendingMachine;
 
 public class ItemName {
+    private final String name;
+
+    public ItemName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
