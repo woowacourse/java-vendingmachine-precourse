18a766e73ae881eb08bc8678c1a9707f8fd66091
slowcloud <sju9417@gmail.com>
2023-12-03 12:31:45 +0900

    feat: Price 구현

--- src/main/java/vendingmachine/domain/VendingMachine/Price.java
+++ src/main/java/vendingmachine/domain/VendingMachine/Price.java
@@ -1,4 +1,29 @@
 package vendingmachine.domain.VendingMachine;
 
+import java.util.IllformedLocaleException;
+
 public class Price {
+    private final int price;
+
+    public Price(int price) {
+        validatePrice(price);
+        this.price = price;
+    }
+
+    private void validatePrice(int price) {
+        validateOverHundred(price);
+        validateDivisibleByTen(price);
+    }
+
+    private void validateOverHundred(int price) {
+        if (price < 100) {
+            throw new IllegalArgumentException("[ERROR] 가격이 100원 미만입니다!");
+        }
+    }
+
+    private void validateDivisibleByTen(int price) {
+        if(price % 10 != 0) {
+            throw new IllegalArgumentException("[ERROR] 10원으로 나누어지지 않습니다!");
+        }
+    }
 }
