d801d008bf091d44dc58e683240911c96e49cfdc
slowcloud <sju9417@gmail.com>
2023-12-03 12:25:46 +0900

    feat: Count 구현

--- src/main/java/vendingmachine/domain/VendingMachine/Count.java
+++ src/main/java/vendingmachine/domain/VendingMachine/Count.java
@@ -1,4 +1,24 @@
 package vendingmachine.domain.VendingMachine;
 
 public class Count {
+    private int count;
+
+    public Count(int count) {
+        this.count = count;
+    }
+
+    public boolean isZero() {
+        return count == 0;
+    }
+
+    public void reduce() {
+        validateZero(count);
+        count--;
+    }
+
+    private void validateZero(int count) {
+        if(isZero()) {
+            throw new IllegalArgumentException("[ERROR] 이미 개수가 0입니다!");
+        }
+    }
 }
